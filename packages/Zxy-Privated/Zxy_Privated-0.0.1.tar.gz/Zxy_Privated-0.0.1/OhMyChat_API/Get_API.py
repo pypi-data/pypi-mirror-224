3d8cf5805456a4b99f06ab7b5d4020273973f715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:07:40 +0000

    Add package Zxy-Privated/0.0.1/Zxy_Privated-0.0.1.tar.gz

---
+++ packages/Zxy-Privated/Zxy_Privated-0.0.1.tar.gz/Zxy_Privated-0.0.1/OhMyChat_API/Get_API.py
@@ -0,0 +1,111 @@
+# -*- encoding: utf-8 -*-
+'''
+@Project_name    :  OhMyChat_API
+@ProjectDescription: ..........
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/8/8 9:25     zxy       1.0         None
+'''
+import json
+import time
+import traceback
+
+import requests
+
+
+class GetAPI():
+    def __init__(self, api_key=""):
+        self.api_key = api_key
+
+    def timeStamp_to_datetime(self, timeStamp):
+        timeStamp = float(timeStamp)
+        timeArray = time.localtime(timeStamp)
+        otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+        return otherStyleTime
+
+    def get_chat_api(self, query="", rety_num=0):
+        url = "https://cfwus02.opapi.win/v1/chat/completions"
+
+        payload = json.dumps({
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": query,
+                }
+            ],
+            "stream": True
+        })
+        headers = {
+            'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+            "Authorization": "Bearer {api_key}".format(api_key=self.api_key),
+            'Content-Type': 'application/json'
+        }
+        try:
+            if query:
+                if not rety_num:
+                    raise
+                response = requests.post(url, headers=headers, data=payload, stream=True)
+                for index, line in enumerate(response.iter_lines()):
+                    yield bytes.decode(line), index
+        except:
+            if rety_num < 3:
+                for line_data, index in self.get_chat_api(query, rety_num + 1):
+                    yield line_data, index
+        else:
+            return None
+
+    def handler_api_result(self, line_data: str, index: int):
+        if not line_data or line_data.endswith("[DONE]"):
+            return
+        data = json.loads(str(line_data).split(":", 1)[-1])
+        if not index:
+            release_time = self.timeStamp_to_datetime(data["created"])
+            model = data["model"]
+        else:
+            release_time, model = "", ""
+        for choice in data["choices"]:
+            if not index:
+                username = choice["delta"]["role"]
+                start_str = f"{username}({model})({release_time}){'=' * 100}"
+                print(start_str)
+                self.header_lenth = len(start_str)
+            if choice["finish_reason"]:
+                equalSign_num = (self.header_lenth - 14) // 2
+                print(f"\n{'=' * equalSign_num} divisionLine {'=' * equalSign_num}")
+            else:
+                content = choice["delta"]["content"]
+                print(content, end="")
+
+    def run(self):
+        while True:
+            try:
+                query = input("please input your question:")
+                if not query:
+                    continue
+                elif query.lower() in ('q', 'quit', 'exit'):
+                    while True:
+                        query = input("Do you want to terminate (Y/N)?:")
+                        if query.lower() == "y":
+                            print("Welcome to use it next time")
+                            return
+                        elif query.lower() == 'n':
+                            break
+                        else:
+                            print("Your input is illegal!")
+                            continue
+                    continue
+                else:
+                    print("Please be patient and wait for a response . . .")
+
+                for line_data, index in self.get_chat_api(query):
+                    self.handler_api_result(line_data, index)
+            except:
+                traceback.print_exc()
+                print("There is a problem with your API interface !!!")
+                return
+
+
+if __name__ == '__main__':
+    X = GetAPI()
+    X.run()
