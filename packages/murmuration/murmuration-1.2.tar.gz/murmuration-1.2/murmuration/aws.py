e94eb2eadb556b6d114f7292b7f44f6f7ebed035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:19 +0000

    Add package murmuration/1.2/murmuration-1.2.tar.gz

---
+++ packages/murmuration/murmuration-1.2.tar.gz/murmuration-1.2/murmuration/aws.py
@@ -0,0 +1,17 @@
+from boto3.session import Session
+
+
+def kms_client(
+        region: str = None,
+        profile: str = None,
+        session: Session = None,
+        client=None,):
+    """
+    returns a kms session
+    """
+    if client:
+        return client
+    if session:
+        return session.client('kms')
+    session = Session(region_name=region, profile_name=profile)
+    return session.client('kms')
