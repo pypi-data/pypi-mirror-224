e737b2acbbcb6838dba7a0f66232978497c207ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:08:16 +0000

    Add package reactpy-django/3.3.1/reactpy_django-3.3.1-py2.py3-none-any.whl

---
+++ packages/reactpy-django/reactpy_django-3.3.1-py2.py3-none-any.whl/reactpy_django/checks.py
@@ -0,0 +1,224 @@
+import sys
+
+from django.contrib.staticfiles.finders import find
+from django.core.checks import Error, Tags, Warning, register
+from django.template import loader
+
+
+@register(Tags.compatibility)
+def reactpy_warnings(app_configs, **kwargs):
+    from django.conf import settings
+    from django.urls import reverse
+
+    from reactpy_django.config import REACTPY_FAILED_COMPONENTS
+
+    warnings = []
+
+    # REACTPY_DATABASE is not an in-memory database.
+    if (
+        getattr(settings, "DATABASES", {})
+        .get(getattr(settings, "REACTPY_DATABASE", "default"), {})
+        .get("NAME", None)
+        == ":memory:"
+    ):
+        warnings.append(
+            Warning(
+                "Using ReactPy with an in-memory database can cause unexpected "
+                "behaviors.",
+                hint="Configure settings.py:DATABASES[REACTPY_DATABASE], to use a "
+                "multiprocessing and thread safe database.",
+                id="reactpy_django.W001",
+            )
+        )
+
+    # ReactPy URLs exist
+    try:
+        reverse("reactpy:web_modules", kwargs={"file": "example"})
+        reverse("reactpy:view_to_component", kwargs={"view_path": "example"})
+    except Exception:
+        warnings.append(
+            Warning(
+                "ReactPy URLs have not been registered.",
+                hint="""Add 'path("reactpy/", include("reactpy_django.http.urls"))' """
+                "to your application's urlpatterns.",
+                id="reactpy_django.W002",
+            )
+        )
+
+    # Warn if REACTPY_BACKHAUL_THREAD is set to True on Linux with Daphne
+    if (
+        sys.argv
+        and sys.argv[0].endswith("daphne")
+        and getattr(settings, "REACTPY_BACKHAUL_THREAD", False)
+        and sys.platform == "linux"
+    ):
+        warnings.append(
+            Warning(
+                "REACTPY_BACKHAUL_THREAD is enabled but you running with Daphne on Linux. "
+                "This configuration is known to be unstable.",
+                hint="Set settings.py:REACTPY_BACKHAUL_THREAD to False or use a different webserver.",
+                id="reactpy_django.W003",
+            )
+        )
+
+    # Check if reactpy_django/client.js is available
+    if not find("reactpy_django/client.js"):
+        warnings.append(
+            Warning(
+                "ReactPy client.js could not be found within Django static files!",
+                hint="Check your Django static file configuration.",
+                id="reactpy_django.W004",
+            )
+        )
+
+    # Check if any components failed to be registered
+    if REACTPY_FAILED_COMPONENTS:
+        warnings.append(
+            Warning(
+                "ReactPy failed to register the following components:\n\t+ "
+                + "\n\t+ ".join(REACTPY_FAILED_COMPONENTS),
+                hint="Check if these paths are valid, or if an exception is being "
+                "raised during import.",
+                id="reactpy_django.W005",
+            )
+        )
+
+    # Check if the reactpy/component.html template exists
+    try:
+        loader.get_template("reactpy/component.html")
+    except Exception:
+        warnings.append(
+            Warning(
+                "ReactPy HTML templates could not be found!",
+                hint="Check your settings.py:TEMPLATES configuration and make sure "
+                "ReactPy-Django is installed properly.",
+                id="reactpy_django.W006",
+            )
+        )
+
+    # Check if REACTPY_WEBSOCKET_URL doesn't end with a slash
+    REACTPY_WEBSOCKET_URL = getattr(settings, "REACTPY_WEBSOCKET_URL", "")
+    if isinstance(REACTPY_WEBSOCKET_URL, str):
+        if not REACTPY_WEBSOCKET_URL or not REACTPY_WEBSOCKET_URL.endswith("/"):
+            warnings.append(
+                Warning(
+                    "REACTPY_WEBSOCKET_URL did not end with a forward slash.",
+                    hint="Change your URL to be written in the following format: 'example_url/'",
+                    id="reactpy_django.W007",
+                )
+            )
+
+        # Check if REACTPY_WEBSOCKET_URL doesn't start with an alphanumeric character
+        if not REACTPY_WEBSOCKET_URL or not REACTPY_WEBSOCKET_URL[0].isalnum():
+            warnings.append(
+                Warning(
+                    "REACTPY_WEBSOCKET_URL did not start with an alphanumeric character.",
+                    hint="Change your URL to be written in the following format: 'example_url/'",
+                    id="reactpy_django.W008",
+                )
+            )
+
+    return warnings
+
+
+@register(Tags.compatibility)
+def reactpy_errors(app_configs, **kwargs):
+    from django.conf import settings
+
+    errors = []
+
+    # Make sure ASGI is enabled
+    if not getattr(settings, "ASGI_APPLICATION", None):
+        errors.append(
+            Error(
+                "ASGI_APPLICATION is not defined, but ReactPy requires ASGI.",
+                hint="Add ASGI_APPLICATION to settings.py.",
+                id="reactpy_django.E001",
+            )
+        )
+
+    # DATABASE_ROUTERS is properly configured when REACTPY_DATABASE is defined
+    if getattr(
+        settings, "REACTPY_DATABASE", None
+    ) and "reactpy_django.database.Router" not in getattr(
+        settings, "DATABASE_ROUTERS", []
+    ):
+        errors.append(
+            Error(
+                "ReactPy database has been changed but the database router is "
+                "not configured.",
+                hint="Set settings.py:DATABASE_ROUTERS to "
+                "['reactpy_django.database.Router', ...]",
+                id="reactpy_django.E002",
+            )
+        )
+
+    # All settings in reactpy_django.conf are the correct data type
+    if not isinstance(getattr(settings, "REACTPY_WEBSOCKET_URL", ""), str):
+        errors.append(
+            Error(
+                "Invalid type for REACTPY_WEBSOCKET_URL.",
+                hint="REACTPY_WEBSOCKET_URL should be a string.",
+                obj=settings.REACTPY_WEBSOCKET_URL,
+                id="reactpy_django.E003",
+            )
+        )
+    if not isinstance(getattr(settings, "REACTPY_RECONNECT_MAX", 0), int):
+        errors.append(
+            Error(
+                "Invalid type for REACTPY_RECONNECT_MAX.",
+                hint="REACTPY_RECONNECT_MAX should be an integer.",
+                obj=settings.REACTPY_RECONNECT_MAX,
+                id="reactpy_django.E004",
+            )
+        )
+    if not isinstance(getattr(settings, "REACTPY_CACHE", ""), str):
+        errors.append(
+            Error(
+                "Invalid type for REACTPY_CACHE.",
+                hint="REACTPY_CACHE should be a string.",
+                obj=settings.REACTPY_CACHE,
+                id="reactpy_django.E005",
+            )
+        )
+    if not isinstance(getattr(settings, "REACTPY_DATABASE", ""), str):
+        errors.append(
+            Error(
+                "Invalid type for REACTPY_DATABASE.",
+                hint="REACTPY_DATABASE should be a string.",
+                obj=settings.REACTPY_DATABASE,
+                id="reactpy_django.E006",
+            )
+        )
+    if not isinstance(
+        getattr(settings, "REACTPY_DEFAULT_QUERY_POSTPROCESSOR", ""), str
+    ):
+        errors.append(
+            Error(
+                "Invalid type for REACTPY_DEFAULT_QUERY_POSTPROCESSOR.",
+                hint="REACTPY_DEFAULT_QUERY_POSTPROCESSOR should be a string.",
+                obj=settings.REACTPY_DEFAULT_QUERY_POSTPROCESSOR,
+                id="reactpy_django.E007",
+            )
+        )
+    if not isinstance(getattr(settings, "REACTPY_AUTH_BACKEND", ""), str):
+        errors.append(
+            Error(
+                "Invalid type for REACTPY_AUTH_BACKEND.",
+                hint="REACTPY_AUTH_BACKEND should be a string.",
+                obj=settings.REACTPY_AUTH_BACKEND,
+                id="reactpy_django.E008",
+            )
+        )
+
+    # Check for dependencies
+    if "channels" not in settings.INSTALLED_APPS:
+        errors.append(
+            Error(
+                "Django Channels is not installed.",
+                hint="Add 'channels' to settings.py:INSTALLED_APPS.",
+                id="reactpy_django.E009",
+            )
+        )
+
+    return errors
---
+++ packages/reactpy-django/reactpy_django-3.3.1-py2.py3-none-any.whl/reactpy_django/templatetags/reactpy.py
@@ -0,0 +1,103 @@
+from logging import getLogger
+from uuid import uuid4
+
+import dill as pickle
+from django import template
+from django.urls import reverse
+
+from reactpy_django import models
+from reactpy_django.config import (
+    REACTPY_DEBUG_MODE,
+    REACTPY_RECONNECT_MAX,
+    REACTPY_WEBSOCKET_URL,
+)
+from reactpy_django.exceptions import ComponentDoesNotExistError, ComponentParamError
+from reactpy_django.types import ComponentParamData
+from reactpy_django.utils import (
+    _register_component,
+    check_component_args,
+    func_has_args,
+)
+
+REACTPY_WEB_MODULES_URL = reverse("reactpy:web_modules", args=["x"])[:-1][1:]
+register = template.Library()
+_logger = getLogger(__name__)
+
+
+@register.inclusion_tag("reactpy/component.html")
+def component(dotted_path: str, *args, **kwargs):
+    """This tag is used to embed an existing ReactPy component into your HTML template.
+
+    Args:
+        dotted_path: The dotted path to the component to render.
+        *args: The positional arguments to provide to the component.
+
+    Keyword Args:
+        **kwargs: The keyword arguments to provide to the component.
+
+    Example ::
+
+        {% load reactpy %}
+        <!DOCTYPE html>
+        <html>
+        <body>
+            {% component "example_project.my_app.components.hello_world" recipient="World" %}
+        </body>
+        </html>
+    """
+
+    # Register the component if needed
+    try:
+        component = _register_component(dotted_path)
+        uuid = uuid4().hex
+        class_ = kwargs.pop("class", "")
+        kwargs.pop("key", "")  # `key` is effectively useless for the root node
+
+    except Exception as e:
+        if isinstance(e, ComponentDoesNotExistError):
+            _logger.error(str(e))
+        else:
+            _logger.exception(
+                "An unknown error has occurred while registering component '%s'.",
+                dotted_path,
+            )
+        return failure_context(dotted_path, e)
+
+    # Store the component's args/kwargs in the database if needed
+    # This will be fetched by the websocket consumer later
+    try:
+        check_component_args(component, *args, **kwargs)
+        if func_has_args(component):
+            params = ComponentParamData(args, kwargs)
+            model = models.ComponentSession(uuid=uuid, params=pickle.dumps(params))
+            model.full_clean()
+            model.save()
+
+    except Exception as e:
+        if isinstance(e, ComponentParamError):
+            _logger.error(str(e))
+        else:
+            _logger.exception(
+                "An unknown error has occurred while saving component params for '%s'.",
+                dotted_path,
+            )
+        return failure_context(dotted_path, e)
+
+    # Return the template rendering context
+    return {
+        "class": class_,
+        "reactpy_websocket_url": REACTPY_WEBSOCKET_URL,
+        "reactpy_web_modules_url": REACTPY_WEB_MODULES_URL,
+        "reactpy_reconnect_max": REACTPY_RECONNECT_MAX,
+        "reactpy_mount_uuid": uuid,
+        "reactpy_component_path": f"{dotted_path}/{uuid}/",
+    }
+
+
+def failure_context(dotted_path: str, error: Exception):
+    return {
+        "reactpy_failure": True,
+        "reactpy_debug_mode": REACTPY_DEBUG_MODE,
+        "reactpy_dotted_path": dotted_path,
+        "reactpy_error": type(error).__name__,
+    }
