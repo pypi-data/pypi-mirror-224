c41c6c9a70a746ee7d6e3291a93cf86b404439e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:06 +0000

    Add package marqo/1.2.0/marqo-1.2.0-py3-none-any.whl

---
+++ packages/marqo/marqo-1.2.0-py3-none-any.whl/marqo/enums.py
@@ -0,0 +1,16 @@
+"""enums to be used by consumers of the Marqo client"""
+from enum import Enum
+
+class SearchMethods(str, Enum):
+    LEXICAL = "LEXICAL"
+    TENSOR = "TENSOR"
+
+
+class Devices:
+    cpu = "cpu"
+    cuda = "cuda"
+
+
+class IndexStatus:
+    CREATED = "READY"
+    DELETED = "DELETED"
