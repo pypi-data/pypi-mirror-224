e9d4a05b583799f2346229e5dcd4feed70ef58e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:08:22 +0000

    Add package robson-package/0.0.28/robson_package-0.0.28-py3-none-any.whl

---
+++ packages/robson-package/robson_package-0.0.28-py3-none-any.whl/robson_package/robson_package.py
@@ -0,0 +1,16 @@
+class Conversor:
+    def __init__(self, celsius):
+        self.valor = celsius
+
+    def converter_farenheit(self):
+        return self.valor * 1.8 + 32
+    
+    def converter_kelvin(self):
+        return self.valor + 273.15
+    
+
+if __name__ == "__main__":
+    celsius = float(input("Digite a temperatura em Celsius: "))
+    conversor = Conversor(celsius)
+    print(f"{celsius}°C equivale a {conversor.converter_farenheit()}°F")
+    print(f"{celsius}°C equivale a {conversor.converter_kelvin()}K")
