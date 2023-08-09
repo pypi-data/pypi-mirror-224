c4d4c15c45ed692b92e495216198d69d68e5e569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:06:35 +0000

    Add package social-gpt/0.0.15/social_gpt-0.0.15.tar.gz

---
+++ packages/social-gpt/social_gpt-0.0.15.tar.gz/social_gpt-0.0.15/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = [line.split('==')[0] for line in fh.read().splitlines()]
+
+setuptools.setup(
+    name="social_gpt",
+    version="0.0.15",
+    author="dinesh1301",
+    author_email="dinesh@topmate.io",
+    description="Social gpt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/topmate-io/social_gpt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements
+)
