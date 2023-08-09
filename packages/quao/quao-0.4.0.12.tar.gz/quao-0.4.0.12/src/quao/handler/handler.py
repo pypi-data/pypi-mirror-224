7e5ac22e3b5b49268d820a0730a8077535b31f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:06:39 +0000

    Add package quao/0.4.0.12/quao-0.4.0.12.tar.gz

---
+++ packages/quao/quao-0.4.0.12.tar.gz/quao-0.4.0.12/src/quao/handler/handler.py
@@ -0,0 +1,8 @@
+"""
+    QuaO Project handler.py Copyright © CITYNOW Co. Ltd. All rights reserved.
+"""
+
+
+class Handler:
+    def __init__(self, request_data: dict):
+        self.request_data = request_data
