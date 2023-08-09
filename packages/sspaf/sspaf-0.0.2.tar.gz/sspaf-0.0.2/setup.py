c99762fcde71bd38ff663cbd7c9b11f06f9d619a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:08:08 +0000

    Add package sspaf/0.0.2/sspaf-0.0.2.tar.gz

---
+++ packages/sspaf/sspaf-0.0.2.tar.gz/sspaf-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="sspaf",
+    version="0.0.2",
+    description="A superglued single page application framework",
+    author="plusleft",
+    url="https://github.com/0xleft/sspaf",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'flask',
+        'waitress',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sspaf = sspaf.sspaf:main'
+        ]
+    }
+)
