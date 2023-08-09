707369c0c1639efb741170e4091d498dcecf9c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:06:56 +0000

    Add package Rot13Bruteforce/1.0.1/Rot13Bruteforce-1.0.1.tar.gz

---
+++ packages/Rot13Bruteforce/Rot13Bruteforce-1.0.1.tar.gz/Rot13Bruteforce-1.0.1/README.md
@@ -0,0 +1,25 @@
+# Rot13Bruteforce
+Python pip makes it easy to run rot13_bruteforce
+> PYPI : https://pypi.org/project/Rot13Bruteforce/
+# PiP install
+```
+pip install Rot13Bruteforce
+```
+# Key Features
+```python
+rot13_bruteforce(str)
+print_rot13_results(str)
+```
+# Example
+```python
+import Rot13Bruteforce
+
+result = Rot13Bruteforce.rot13_bruteforce('abcd')
+print_rot13_results(result)
+```
+# ETC
+> For improvements, please call Full Requests
+> 
+> Mail : gms8757@naver.com
+>
+> [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FGaeduck-0908%2Frot13-bruteforce&count_bg=%23000000&title_bg=%23000000&icon=&icon_color=%23000000&title=View&edge_flat=false)](https://hits.seeyoufarm.com)
---
+++ packages/Rot13Bruteforce/Rot13Bruteforce-1.0.1.tar.gz/Rot13Bruteforce-1.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name='Rot13Bruteforce',
+    version='1.0.1',
+    description='Python pip makes it easy to run rot13_bruteforce',
+    author='Gaeduck',
+    author_email='gms8757@naver.com',
+    url='https://github.com/Gaeduck-0908/Rot13Bruteforce',
+    packages= setuptools.find_packages(),
+    keywords=['Python','rot13','bruteforce'],
+    python_requires='>=3',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
