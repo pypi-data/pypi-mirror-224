acaa57ec7ac7ffd47eb7144b997708d7026a8251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:07:36 +0000

    Add package dwai/0.1.1/dwai-0.1.1.tar.gz

---
+++ packages/dwai/dwai-0.1.1.tar.gz/dwai-0.1.1/tests/test_dwai.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `dwai` package."""
+
+
+import unittest
+
+from dwai import dwai
+
+
+class TestDwai(unittest.TestCase):
+    """Tests for `dwai` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
