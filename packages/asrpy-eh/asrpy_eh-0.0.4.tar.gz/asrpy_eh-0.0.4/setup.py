c4788fd373b2de3d8415689fd74d5997733e784e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:47 +0000

    Add package asrpy-eh/0.0.4/asrpy_eh-0.0.4.tar.gz

---
+++ packages/asrpy-eh/asrpy_eh-0.0.4.tar.gz/asrpy_eh-0.0.4/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "asrpy_eh",
+    version = "0.0.4",
+
+    author = "Dirk Gütlin",
+    author_email = "dirk.guetlin@gmail.com",
+    description = ("Artifact Subspace Reconstruction in Python."),
+    license = "BSD-3",
+    keywords = "EEG MEG Signal Processing",
+    url = "https://github.com/DiGyt/asrpy",
+    package_dir = {"":"./"},
+    # packages = find_packages(where="asrpy"),
+    packages = ["asrpy"],
+    
+    include_package_data=True,
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "mne",
+    ],
+)
