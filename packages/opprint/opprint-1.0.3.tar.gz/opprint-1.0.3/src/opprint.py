1d02d1cbf4c9804db26d72b7a0ef156edb668d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:17 +0000

    Add package opprint/1.0.3/opprint-1.0.3.tar.gz

---
+++ packages/opprint/opprint-1.0.3.tar.gz/opprint-1.0.3/src/opprint.py
@@ -0,0 +1,29 @@
+import pprint
+import yaml
+
+
+def op(label_or_var, var=None, format=False, yml=False):
+    """
+    short-hand override print() formatter
+
+    @params
+    label_or_var
+    var=None
+    format=False
+    yml=False
+    """
+    try:
+        if var and format:
+            if yml:
+                return print(f'\n{label_or_var}\n', yaml.dump(var))
+            return print(f'\n{label_or_var}\n', pprint.pformat(var))
+        if not var and format:
+            if yml:
+                return print('\n', yaml.dump(label_or_var))
+            return print('\n', pprint.pformat(label_or_var))
+        if var:
+            return print(f'\n{label_or_var}\n', var, '\n')
+        return print('\n', label_or_var, '\n')
+    except Exception as error:
+        print('\nopprint internal error...')
+        print(error, '\n')
