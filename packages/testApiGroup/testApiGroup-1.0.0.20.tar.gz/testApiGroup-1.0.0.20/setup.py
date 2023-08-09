1171b0c0284f7ed48182dc8ecb421f73d3a8f249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:07:36 +0000

    Add package testApiGroup/1.0.0.20/testApiGroup-1.0.0.20.tar.gz

---
+++ packages/testApiGroup/testApiGroup-1.0.0.20.tar.gz/testApiGroup-1.0.0.20/setup.py
@@ -0,0 +1,12 @@
+# encoding: utf-8 
+from setuptools import setup, find_packages
+
+setup(
+    name='testApiGroup',
+    version='1.0.0.20',  # 替换为您的版本号
+    description='${Description}',
+    author='dyc',
+    author_email='your@email.com',
+    packages=['testApiGroup'],
+    install_requires=['requests']
+)
