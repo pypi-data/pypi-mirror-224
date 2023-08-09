a40e310cec4e033a7454cdd41551499969007f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:06:07 +0000

    Add package pyatp/1.2.1/pyatp-1.2.1.tar.gz

---
+++ packages/pyatp/pyatp-1.2.1.tar.gz/pyatp-1.2.1/src/ailab/examples/chinese_llama_vicuna/train.py
@@ -0,0 +1,17 @@
+import os
+from ailab.utils.other import install_requiremet
+
+def install_req():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    install_requiremet(dir_path)
+
+def chinese_vicuna_test():
+    import subprocess
+    # 启动 shell 脚本
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    shell_path = os.path.join(dir_path,"train.sh")
+    subprocess.run(["sh", shell_path])
+
+if __name__ == '__main__':
+    install_req()
+    chinese_vicuna_test()
