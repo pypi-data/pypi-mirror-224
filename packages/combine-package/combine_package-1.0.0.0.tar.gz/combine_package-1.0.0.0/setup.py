fcb658a69bb7cc066be65e7363497bd41d91e0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:51 +0000

    Add package combine-package/1.0.0.0/combine_package-1.0.0.0.tar.gz

---
+++ packages/combine-package/combine_package-1.0.0.0.tar.gz/combine_package-1.0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='combine_package',
+    version='1.0.0.0',  # 替换为您的版本号
+    description='Description of your package',
+    author='dyc',
+    author_email='your@email.com',
+    packages=['yuan_cheng_package', 'dyc_package'],
+    install_requires=[
+        # 列出您的包依赖的其他Python包
+        # 例如：'requests', 'numpy', 等等
+    ]
+)
