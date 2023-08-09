5546ecc6a5edcabedcdad16877b3c01968b9bd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:08:04 +0000

    Add package EasyDeL/0.0.23/EasyDeL-0.0.23-py3-none-any.whl

---
+++ packages/EasyDeL/EasyDeL-0.0.23-py3-none-any.whl/EasyDel/__init__.py
@@ -0,0 +1,23 @@
+from .utils import make_shard_and_gather_fns
+from .modules import FlaxLlamaModel, FlaxLlamaForCausalLM, LlamaConfig, \
+    FlaxLTModelModule, FlaxLTConfig, FlaxLTForCausalLM, FlaxLTModel, GPTJConfig, FlaxGPTJModule, \
+    FlaxGPTJForCausalLMModule, FlaxGPTJModel, FlaxGPTJForCausalLM, FlaxMptForCausalLM, MptConfig, FlaxMptModel, \
+    FlaxFalconForCausalLM, FlaxFalconModel, FalconConfig, FlaxGPTNeoXForCausalLM, GPTNeoXConfig, FlaxGPTNeoXModel, \
+    FlaxT5ForConditionalGeneration, FlaxT5Model, FlaxPalmForCausalLM, PalmModel, PalmConfig, T5Config, \
+    FlaxOPTForCausalLM, FlaxOPTModel, OPTConfig
+from .trainer import TrainArguments, fsdp_train_step, get_training_modules, CausalLMTrainer
+from .serve import JAXServer
+
+__version__ = '0.0.23'
+
+__all__ = "TrainArguments", "fsdp_train_step", 'get_training_modules', \
+    'FlaxLlamaForCausalLM', \
+    'FlaxLlamaModel', 'FlaxGPTJModule', 'FlaxGPTJForCausalLMModule', \
+    'FlaxGPTJModel', 'FlaxGPTJForCausalLM', 'GPTJConfig', \
+    'FlaxLTModel', 'FlaxLTConfig', 'FlaxLTModelModule', 'FlaxLTForCausalLM', \
+    "FlaxMptForCausalLM", "MptConfig", "FlaxMptModel", \
+    "FlaxFalconForCausalLM", "FlaxFalconModel", "FalconConfig", \
+    "FlaxGPTNeoXForCausalLM", "GPTNeoXConfig", "FlaxGPTNeoXModel", \
+    "FlaxT5ForConditionalGeneration", "FlaxT5Model", \
+    "FlaxPalmForCausalLM", "PalmModel", "PalmConfig", 'T5Config', \
+    "FlaxOPTForCausalLM", "FlaxOPTModel", "OPTConfig", 'CausalLMTrainer', 'LlamaConfig', "__version__", 'JAXServer'
