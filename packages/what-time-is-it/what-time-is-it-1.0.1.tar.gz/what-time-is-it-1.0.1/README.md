18b248cf2452258f3eeacfe9069be73a6808054e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:03 +0000

    Add package what-time-is-it/1.0.1/what-time-is-it-1.0.1.tar.gz

---
+++ packages/what-time-is-it/what-time-is-it-1.0.1.tar.gz/what-time-is-it-1.0.1/README.md
@@ -0,0 +1,14 @@
+# what-time-is-it
+Sample python package to show current time
+
+# Install locally
+
+```sh
+pip install -e .
+```
+
+# Build
+
+```sh
+twine upload dist/what-time-is-it-1.0.0.tar.gz
+```
---
+++ packages/what-time-is-it/what-time-is-it-1.0.1.tar.gz/what-time-is-it-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from what_time_is_it import __version__
+
+setup(
+    name='what-time-is-it',
+    version=__version__,
+
+    url='https://github.com/blacksmithop/what-time-is-it',
+    author='Abhinav KM',
+    author_email='angstycoder101@gmail.com',
+
+    py_modules=['what_time_is_it'],
+    install_requires=[
+        'pytest>=4',
+        'pytest-cov>=2'
+    ],
+    extras_require={
+    'extra': [
+        'tabulate',
+    ],
+    },
+    entry_points={
+    'console_scripts': [
+        'add=what_time_is_it.math:cmd_add',
+    ],
+},
+)
