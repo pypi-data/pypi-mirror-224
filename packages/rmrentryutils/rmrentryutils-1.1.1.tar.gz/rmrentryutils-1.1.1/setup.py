24a387756754fa24d5b3a72d3e9be86f4e8a8d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:06 +0000

    Add package rmrentryutils/1.1.1/rmrentryutils-1.1.1.tar.gz

---
+++ packages/rmrentryutils/rmrentryutils-1.1.1.tar.gz/rmrentryutils-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmrentryutils',
+    version='1.1.1',
+    description='rmrentryutils',
+    author='jesus212406',
+    author_email='jesus212406@0xckh4.gpa.lu',
+    packages=find_packages(),
+)
