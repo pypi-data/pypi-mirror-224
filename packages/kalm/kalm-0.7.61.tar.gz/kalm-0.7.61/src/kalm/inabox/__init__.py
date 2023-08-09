e09fb45af509dc0dc84becfa66b2ca59e74113b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:07:36 +0000

    Add package kalm/0.7.61/kalm-0.7.61.tar.gz

---
+++ packages/kalm/kalm-0.7.61.tar.gz/kalm-0.7.61/src/kalm/inabox/__init__.py
@@ -0,0 +1,44 @@
+from . import inabox
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Keep kalm and automate", usage="kalm <action> \n\n \
+               \
+               version : 0.1.2 BETA \n                                              \
+               actions:\n                                                      \
+               list        list inabox \n  \
+               ansible_automation_platform  in a box\n  \
+               rancher     activate rancher in a box\n  \
+               \
+               2023 Knowit Miracle\
+               ")
+    parser.add_argument('action', metavar='<action>', type=str, nargs='+', help='setup netbox')
+    args = parser.parse_args()
+    ready = False
+    print("check if we are ready to go")
+
+    if args.action[0] == "list":
+        print("list inabox")
+        inabox.list_inabox()
+        
+    if args.action[0] == "rancher":
+        print("Start rancher in a boc")
+        inabox.rancher_inabox()
+
+    if args.action[0] == "ansible_role":
+        print("Create ansible role directory structure")
+        inabox.ansible_role_inabox()
+
+    if args.action[0] == "ansible_automation_platform":
+        print("Create ansible automation platform directory structure")
+        inabox.ansible_automation_platform()
+
+        
+
+
+    
+
+
+
+
+
