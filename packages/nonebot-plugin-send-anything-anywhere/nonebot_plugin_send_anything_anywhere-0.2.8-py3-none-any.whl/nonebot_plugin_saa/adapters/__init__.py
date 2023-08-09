280d935a05447b2e89d7af32211baac613de951a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:06:23 +0000

    Add package nonebot-plugin-send-anything-anywhere/0.2.8/nonebot_plugin_send_anything_anywhere-0.2.8-py3-none-any.whl

---
+++ packages/nonebot-plugin-send-anything-anywhere/nonebot_plugin_send_anything_anywhere-0.2.8-py3-none-any.whl/nonebot_plugin_saa/adapters/__init__.py
@@ -0,0 +1,6 @@
+from . import feishu as feishu
+from . import qqguild as qqguild
+from . import kaiheila as kaiheila
+from . import telegram as telegram
+from . import onebot_v11 as onebot_v11
+from . import onebot_v12 as onebot_v12
