04a9f9b512a69295122015a20470e20b2b77534b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:07:31 +0000

    Add package eliasliu/0.1.16/eliasliu-0.1.16.tar.gz

---
+++ packages/eliasliu/eliasliu-0.1.16.tar.gz/eliasliu-0.1.16/elias/missions/pytest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 14:23:23 2023
+
+@author: nlsm
+"""
+
+
+from elias import wechat as w
+w.run_warning('mission_test','test', user='刘益廷',key = w.robots(name='nl_bi_test'))
