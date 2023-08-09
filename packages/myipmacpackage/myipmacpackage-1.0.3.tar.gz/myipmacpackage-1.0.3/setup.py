b0176b1bac4e8ea4ed1c294207c30a290e351a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:07:11 +0000

    Add package myipmacpackage/1.0.3/myipmacpackage-1.0.3.tar.gz

---
+++ packages/myipmacpackage/myipmacpackage-1.0.3.tar.gz/myipmacpackage-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        subprocess.run(["python3", "post_install.py"])
+
+setup(
+    name="myipmacpackage",
+    version="1.0.3",
+    description="Fetch and store IP and MAC addresses in a database",
+    author="Muneer",
+    author_email="Muneer.moosa@electrifex.com",
+    packages=find_packages(),
+    install_requires=[
+        "mysql-connector-python"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={
+        "install": PostInstallCommand,
+    }
+)
