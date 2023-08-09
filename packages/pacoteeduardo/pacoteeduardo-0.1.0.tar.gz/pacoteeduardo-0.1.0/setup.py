f94cd705d42eb32b4d4b7ad877ead6d094a800eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 14:06:27 +0000

    Add package pacoteeduardo/0.1.0/pacoteeduardo-0.1.0.tar.gz

---
+++ packages/pacoteeduardo/pacoteeduardo-0.1.0.tar.gz/pacoteeduardo-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Meu primeiro pacote'
+LONG_DESCRIPTION = 'descricao detalhada.'
+
+# Setting up
+setup(
+    name="pacoteeduardo",
+    version=VERSION,
+    author="Luis Eduardo",
+    author_email="le1307114@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='python pacote example',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
