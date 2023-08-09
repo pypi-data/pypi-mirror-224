83b58d7b6d3bc8c1cd362f3a08a9b8316e33000c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:08:00 +0000

    Add package ionmob/0.1.1/ionmob-0.1.1.tar.gz

---
+++ packages/ionmob/ionmob-0.1.1.tar.gz/ionmob-0.1.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    ld = fh.read()
+
+setup(
+    name='ionmob',
+    version='0.1.1',
+    description='predict peptide collision-cross sections / ion-mobilities',
+    packages=[
+        'ionmob.models',
+        'ionmob.preprocess',
+        'ionmob.utilities',
+        'tests'
+    ],
+    package_data={
+        'example_data': ['*.parquet'],
+        'pretrained_models': ['*'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent"
+    ],
+    long_description=ld,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "tensorflow >=2.7",
+        "pyopenms >= 1.0",
+        "pandas >=1.1",
+        "pyarrow >= 0.5",
+        "scipy >=1.5",
+        "scikit-learn >=1.0.0",
+        "h5py >=3.0.0",
+        "biopython >=1.5",
+        "matplotlib >=3.0"
+    ],
+)
