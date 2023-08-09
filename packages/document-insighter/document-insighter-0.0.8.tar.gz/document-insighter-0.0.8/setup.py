b47616062d8cb0cc57076f60be8618ba4ed7a2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:07:59 +0000

    Add package document-insighter/0.0.8/document-insighter-0.0.8.tar.gz

---
+++ packages/document-insighter/document-insighter-0.0.8.tar.gz/document-insighter-0.0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="document-insighter",
+    version="0.0.8",
+    author="Wang Kai",
+    author_email="wangkai@godeepsite.com",
+    description="Document Insighter Python Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepsite/document-insighter-python",
+    project_urls={
+        "Bug Tracker": "https://github.com/deepsite/document-insighter-python/issues"
+    },
+    license="MIT",
+    packages=find_packages(exclude=("tests*",)),
+    install_requires=["requests==2.31.0", "requests-oauthlib==1.3.1", "dataclasses-json==0.5.7", "polling2==0.5.0"],
+)
---
+++ packages/document-insighter/document-insighter-0.0.8.tar.gz/document-insighter-0.0.8/tests/test_okta_application_client.py
@@ -0,0 +1,21 @@
+from document_insighter.api_client import OktaApplicationClient
+from document_insighter.model import Env, Extraction
+from datetime import datetime
+
+
+def test_fetch_token_with_token_json():
+    document_insighter = OktaApplicationClient(Env.STAGING)
+    document_insighter.fetch_token()
+
+
+def test_query_extractions_pages():
+    document_insighter = OktaApplicationClient(Env.STAGING)
+    document_insighter.fetch_token()
+    pages_generator = document_insighter.query_extractions_pages(
+        "NB_COA", datetime(2022, 3, 1), datetime(2022, 5, 17), page_size=50
+    )
+
+    extraction_dicts = [x for page in pages_generator for x in page]
+    extractions = [Extraction.from_dict(x) for x in extraction_dicts]
+    if extractions:
+        print(extractions[0])
