fb8ff2288a75185c13dd795d1e2bb7216666e5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 15:07:42 +0000

    Add package v-quantum-cloud/0.0.3/v_quantum_cloud-0.0.3.tar.gz

---
+++ packages/v-quantum-cloud/v_quantum_cloud-0.0.3.tar.gz/v_quantum_cloud-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Package metadata
+name = 'v_quantum_cloud'
+version = '0.0.3'
+description = 'V-Quantum cloud library for everyone, product of V-Quantum Technology'
+  
+# Package dependencies
+dependencies = [    
+]
+  
+# Package setup
+setup(
+    name=name,
+    version=version,
+    description=description,    
+    install_requires=dependencies,
+    author="The v_quantum_cloud Developers",
+    author_email="info@v-quantum-technology.com",
+    long_description_content_type="text/markdown",
+    url="https://github.com/C2iteam/v-quantum-cloud.git",
+    license="Apache 2",
+)
+#setup()
