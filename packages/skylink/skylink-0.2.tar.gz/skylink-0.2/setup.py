dd70957abf066f91dcafcc8c7a48d3b969361056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 05:06:40 +0000

    Add package skylink/0.2/skylink-0.2.tar.gz

---
+++ packages/skylink/skylink-0.2.tar.gz/skylink-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='skylink' , 
+      description='Chatbot link v2',
+      version=0.2,
+      author='Caique santos carvalho',
+      packages=['skylink'],
+      zip_safe=False)
---
+++ packages/skylink/skylink-0.2.tar.gz/skylink-0.2/skylink/skylink.py
@@ -0,0 +1,6 @@
+from webbrowser import * 
+def display():
+    def start():
+        webbrowser.open("https://mediafiles.botpress.cloud/edb8bf7d-0c50-4db4-867e-fc1cb3eab7c3/webchat/bot.html")
+        print("Brazil-Salvador-Bahia | Liberdade-Lapinha-Travessa Marcelina")
+        print("author: Caique Santos Carvalho ")
