9e3b0cc014827bd050fd4d970bc96a67520a60e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:39 +0000

    Add package openeo/0.22.0/openeo-0.22.0-py3-none-any.whl

---
+++ packages/openeo/openeo-0.22.0-py3-none-any.whl/openeo/__init__.py
@@ -0,0 +1,26 @@
+"""
+
+
+"""
+
+__title__ = 'openeo'
+__author__ = 'Jeroen Dries'
+
+
+class BaseOpenEoException(Exception):
+    pass
+
+
+from openeo._version import __version__
+
+from openeo.rest.datacube import DataCube, UDF
+from openeo.rest.connection import connect, session, Connection
+from openeo.rest.job import BatchJob, RESTJob
+
+
+def client_version() -> str:
+    try:
+        import importlib.metadata
+        return importlib.metadata.version("openeo")
+    except Exception:
+        return __version__
