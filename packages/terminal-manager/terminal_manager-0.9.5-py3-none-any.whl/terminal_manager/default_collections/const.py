d0ce868d90ca8a99af34083bfe221cdcb14dd7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:34:24 +0000

    Add package terminal-manager/0.9.5/terminal_manager-0.9.5-py3-none-any.whl

---
+++ packages/terminal-manager/terminal_manager-0.9.5-py3-none-any.whl/terminal_manager/default_collections/const.py
@@ -0,0 +1,52 @@
+class ActionKey:
+    TURN_OFF = "turn_off"
+    RESTART = "restart"
+
+
+class ActionName:
+    TURN_OFF = "Turn off"
+    RESTART = "Restart"
+
+
+class SensorKey:
+    NETWORK_INTERFACE = "network_interface"
+    MAC_ADDRESS = "mac_address"
+    WAKE_ON_LAN = "wake_on_lan"
+    MACHINE_TYPE = "machine_type"
+    HOSTNAME = "hostname"
+    OS_NAME = "os_name"
+    OS_VERSION = "os_version"
+    OS_ARCHITECTURE = "os_architecture"
+    CPU_COUNT = "cpu_count"
+    CPU_NAME = "cpu_name"
+    HARDWARE = "hardware"
+    MODEL = "model"
+    SERIAL_NUMBER = "serial_number"
+    TOTAL_MEMORY = "total_memory"
+    FREE_MEMORY = "free_memory"
+    CPU_LOAD = "cpu_load"
+    FREE_DISK_SPACE = "free_disk_space"
+    TEMPERATURE = "temperature"
+    PROCESSES = "processes"
+
+
+class SensorName:
+    NETWORK_INTERFACE = "Network interface"
+    MAC_ADDRESS = "MAC address"
+    WAKE_ON_LAN = "Wake on LAN"
+    MACHINE_TYPE = "Machine type"
+    HOSTNAME = "Hostname"
+    OS_NAME = "OS name"
+    OS_VERSION = "OS version"
+    OS_ARCHITECTURE = "OS architecture"
+    CPU_COUNT = "CPU count"
+    CPU_NAME = "CPU name"
+    HARDWARE = "Hardware"
+    MODEL = "Model"
+    SERIAL_NUMBER = "Serial number"
+    TOTAL_MEMORY = "Total memory"
+    FREE_MEMORY = "Free memory"
+    CPU_LOAD = "CPU load"
+    FREE_DISK_SPACE = "Free disk space"
+    TEMPERATURE = "Temperature"
+    PROCESSES = "Processes"
