a2e92347609adf0fe58ec2e8acf8533985ef4a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:33:41 +0000

    Add package cap-from-youtube/0.0.10/cap_from_youtube-0.0.10.tar.gz

---
+++ packages/cap-from-youtube/cap_from_youtube-0.0.10.tar.gz/cap_from_youtube-0.0.10/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='cap_from_youtube',
+    version='0.0.10',
+    license='MIT',
+    description='Get an OpenCV video capture from an YouTube video URL',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ibai Gorordo',
+    url='https://github.com/ibaiGorordo/cap_from_youtube',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'yt_dlp',
+    ],
+)
