4f76590d13ce340f4d96ca6f4c7335b6294c0a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:34:26 +0000

    Add package BeyotekTools/0.0.28/BeyotekTools-0.0.28.tar.gz

---
+++ packages/BeyotekTools/BeyotekTools-0.0.28.tar.gz/BeyotekTools-0.0.28/src/BeyotekTools/mongodbproxyutil.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import pymongo
+import pytz
+
+class mongodb:
+    def __init__(self, host, DB, collection):
+
+        # ----------------Start Setup Data Base -------------------------------------------------------------------------------
+        self.myclient = pymongo.MongoClient(f"mongodb://{host}/")
+        self.dblist = self.myclient.list_database_names()
+
+        if DB in self.dblist:
+            print("The Database Exists.")
+            self.mydb = self.myclient[DB]
+            self.DB = self.mydb[collection]
+        else:
+            raise Exception(f"{DB} Database or collection {collection} Does Not Exist")
+
+        self.mydb = self.myclient["UPCDB"]
+        self.DB = self.mydb["UPC"]
+
+
+
+        # ----------------End Setup Data Base ----------------------------------------------------------------------------------
+
+
+    def GetTimeStamp(self):
+        format = "%Y-%m-%d %H:%M:%S %Z%z"
+        timezone = pytz.timezone("America/New_York")
+        now = datetime.now()
+        time = now.astimezone(timezone)
+        return time.strftime(format)
