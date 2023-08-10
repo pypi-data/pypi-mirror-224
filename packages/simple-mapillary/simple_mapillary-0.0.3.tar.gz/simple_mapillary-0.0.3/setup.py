d2667426d4f063372d4190191cbdb37cf4bcbb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:33:50 +0000

    Add package simple-mapillary/0.0.3/simple_mapillary-0.0.3.tar.gz

---
+++ packages/simple-mapillary/simple_mapillary-0.0.3.tar.gz/simple_mapillary-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='simple_mapillary',
+    version='0.0.3',
+    license='MIT',
+    description='Simplified Python library for accessing the Mapillary API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ibai Gorordo',
+    url='https://github.com/ibaiGorordo/simple-mapillary-python-sdk',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'imread-from-url',
+        'requests',
+    ],
+)
