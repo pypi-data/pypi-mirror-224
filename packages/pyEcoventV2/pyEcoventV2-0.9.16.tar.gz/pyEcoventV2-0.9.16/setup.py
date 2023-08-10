2d43edc822bf04c4376448a5437de7363435b6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:35:08 +0000

    Add package pyEcoventV2/0.9.16/pyEcoventV2-0.9.16.tar.gz

---
+++ packages/pyEcoventV2/pyEcoventV2-0.9.16.tar.gz/pyEcoventV2-0.9.16/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+long_description = None
+
+with open("README.md", 'r') as fp:
+    long_description = fp.read()
+
+setup(
+    name = 'pyEcoventV2',
+    packages = ['ecovent','ecoventv2'],
+    version='0.9.16',
+    description='Python3 library for single-room energy recovery ventilators from Vents / Blauberg / Flexit',
+    long_description=long_description,
+    python_requires='>=3.6.7',
+    author='Aleksander Lehmann',
+    author_email='aleksander@flovik.no',
+    maintainer='Matjaž Godec',
+    maintainer_email = 'matjaz.godec@gmail.com',
+    url='https://github.com/gody0/pyEcoventV2',
+    license="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
