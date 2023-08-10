b237a5292dfc440a469e2d4d202b559c0163e639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:08:06 +0000

    Add package databricks-rocket/1.3.0/databricks-rocket-1.3.0.tar.gz

---
+++ packages/databricks-rocket/databricks-rocket-1.3.0.tar.gz/databricks-rocket-1.3.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="databricks-rocket",
+    version="1.3.0",
+    author="GetYourGuide",
+    author_email="engineering.data-products@getyourguide.com",
+    description="Keep your local python scripts installed and in sync with a databricks notebook. Shortens the feedback loop to develop projects using a hybrid enviroment",
+    url="https://github.com/getyourguide/databricks-rocket",
+    packages=setuptools.find_packages(),
+    install_requires=["fire", "watchdog~=2.1.9", "build", "databricks_cli"],
+    entry_points={
+        "console_scripts": ["rocket=rocket.rocket:main", "dbrocket=rocket.rocket:main"]
+    },
+)
