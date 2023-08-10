000ef5be6da1aa7cc9cf89536cd8cac12a3fddcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:16 +0000

    Add package fast-tradier-client/0.0.3/fast_tradier_client-0.0.3-py3-none-any.whl

---
+++ packages/fast-tradier-client/fast_tradier_client-0.0.3-py3-none-any.whl/fast_tradier/utils/OptionUtils.py
@@ -0,0 +1,5 @@
+
+OptionChain_Headers = ['symbol', 'strike', 'lastPrice', 'openInterest', 'ask', 'bid', 'expiration_date', 'bid_date', 'volume', 'underlying_price', 'updated_at', 'gamma', 'delta', 'vega']
+
+class OptionUtils:
+    pass
---
+++ packages/fast-tradier-client/fast_tradier_client-0.0.3-py3-none-any.whl/fast_tradier/utils/TimeUtils.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import pandas as pd
+import pytz
+
+US_Eastern_TZ = pytz.timezone('US/Eastern')
+YMD_Format = "%Y-%m-%d"
+YYYYMDHHMM_Format = "YYYY-M-D HH:mm"
+YMDHMS_Format = "%Y-%m-%d %H:%M:%S"
+
+class TimeUtils:
+
+    @staticmethod
+    def convert_unix_ts(ts: int, tz: pytz.timezone = US_Eastern_TZ) -> datetime:
+        '''convert unix timestamp to datetime with the given timezone, US East by default'''
+        if ts is None:
+            return None
+
+        if isinstance(ts, pd.Timestamp):
+            ts = ts.to_pydatetime().timestamp()
+
+        while ts > 1e10:
+            ts = ts/1000
+        return datetime.fromtimestamp(ts, tz)
+
+    @staticmethod
+    def us_east_now() -> datetime:
+        return datetime.now().astimezone(US_Eastern_TZ)
