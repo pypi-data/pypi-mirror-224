1525477051a3926d9de399ed7482211640714f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:16 +0000

    Add package tclab-sdk/1.4.0/tclab-sdk-1.4.0.tar.gz

---
+++ packages/tclab-sdk/tclab-sdk-1.4.0.tar.gz/tclab-sdk-1.4.0/README.md
@@ -0,0 +1,31 @@
+# TC Laboratory Central_Management_System Client Module
+
+## 简介
+
+天创实验室中央管理程序Python端SDK
+
+## CMS设计
+
+### 简介
+
+程序总共分为三个模块:
+
++ 文件访问
+  
+  + webdav协议
+  
+  + 下载/上传
+
++ 设备管理
+  
+  + 外部设备
+  
+  + 服务管理(基于Docker)
+    
+    + CMS插件区
+    
+    + 外部服务区
+
++ 权限系统
+
+tclab-sdk设计与之一一对应.
---
+++ packages/tclab-sdk/tclab-sdk-1.4.0.tar.gz/tclab-sdk-1.4.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="tclab-sdk",  # 模块名称
+    version="1.4.0",  # 当前版本
+    author="Haotian Yang",  # 作者
+    author_email="yht1592754117@126.com",  # 作者邮箱
+    description="TC Laboratory Central_Management_System Client Module",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=[
+        'requests',
+        'pycryptodome'
+    ],
+    python_requires='>=3',
+)
