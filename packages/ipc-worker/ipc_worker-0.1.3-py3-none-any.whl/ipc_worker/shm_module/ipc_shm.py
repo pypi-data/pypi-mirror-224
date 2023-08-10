5cce5ebf9b3c417d748dd778d4363ce938feeddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:08:16 +0000

    Add package ipc-worker/0.1.3/ipc_worker-0.1.3-py3-none-any.whl

---
+++ packages/ipc-worker/ipc_worker-0.1.3-py3-none-any.whl/ipc_worker/shm_module/ipc_shm.py
@@ -0,0 +1,163 @@
+#coding: utf-8
+import math
+import multiprocessing
+import time
+import threading
+from .ipc_shm_utils import SHM_manager,SHM_woker
+import pickle
+from ..utils import logger
+
+
+class SHM_process_worker(SHM_woker):
+    def __init__(self,*args,**kwargs):
+        super(SHM_process_worker,self).__init__(*args,**kwargs)
+
+    #Process begin trigger this func
+    def run_begin(self):
+        raise NotImplementedError
+
+    # Process end trigger this func
+    def run_end(self):
+        raise NotImplementedError
+
+    #any data put will trigger this func
+    def run_once(self,request_data):
+        raise NotImplementedError
+
+
+
+class IPC_shm:
+    def __init__(self,
+                 CLS_worker,
+                 worker_args: tuple,
+                 worker_num: int,
+                 manager_num: int,
+                 group_name,
+                 evt_quit=multiprocessing.Manager().Event(),
+                 shm_size=1 * 1024 * 1024,
+                 queue_size=20,
+                 is_log_time=False,
+                 daemon=False
+                 ):
+        self.__manager_lst = []
+        self.__woker_lst = []
+        self.__signal_list = []
+        self.__shm_name_list = []
+
+        self.__input_queue = None
+        self.__output_queue = None
+
+
+        self.request_id = 0
+        self.pending_request = {}
+        self.pending_response = {}
+
+        self.locker = threading.Lock()
+
+        assert isinstance(worker_args, tuple)
+        self.__input_queue = multiprocessing.Manager().Queue(queue_size)
+        self.__output_queue = multiprocessing.Manager().Queue(queue_size)
+
+        semaphore = multiprocessing.Manager().Semaphore(worker_num)
+
+        for i in range(worker_num):
+            shm_name = '{}_jid_{}'.format(group_name, i)
+            worker = CLS_worker(
+                *worker_args,
+                evt_quit,
+                semaphore,
+                shm_name,
+                shm_size,
+                is_log_time=is_log_time,
+                idx=i,
+                group_name=group_name,
+                daemon=daemon)
+            self.__signal_list.append(worker.get_signal())
+            self.__shm_name_list.append(shm_name)
+            self.__woker_lst.append(worker)
+
+        semaphore = multiprocessing.Manager().Semaphore(manager_num)
+        for i in range(manager_num):
+            manager = SHM_manager(evt_quit,
+                                  self.__signal_list,
+                                  semaphore,
+                                  self.__shm_name_list,
+                                  self.__input_queue,
+                                  self.__output_queue,
+                                  is_log_time=is_log_time,
+                                  idx=i)
+            self.__manager_lst.append(manager)
+        self.__last_t = time.time()
+    def start(self):
+        for w in self.__woker_lst:
+            w.start()
+        for w in self.__manager_lst:
+            w.start()
+
+    def put(self,data):
+        self.locker.acquire()
+        self.request_id += 1
+        request_id = self.request_id
+        self.pending_request[request_id] = time.time()
+        self.__input_queue.put((request_id,pickle.dumps(data)))
+        self.locker.release()
+        return request_id
+
+    def __clean__private__(self):
+        c_t = time.time()
+        if math.floor((c_t - self.__last_t) / 600) > 0:
+            self.__last_t = c_t
+            invalid = set({rid for rid, t in self.pending_request.items() if math.floor((c_t - t) / 3600) > 0})
+            logger.debug('remove {}'.format(str(list(invalid))))
+            for rid in invalid:
+                self.pending_request.pop(rid)
+
+    def get(self,request_id):
+        response = None
+        while True:
+            is_end = False
+            self.locker.acquire(blocking=False)
+            if self.locker.locked():
+                self.__clean__private__()
+
+            if request_id in self.pending_request:
+                self.pending_request[request_id] = time.time()
+                if request_id in self.pending_response:
+                    response = self.pending_response.pop(request_id)
+                    is_end = True
+                else:
+                    r_id,_,seq_id, response = self.__output_queue.get()
+                    if r_id != request_id:
+                        self.pending_response[r_id] = response
+                    else:
+                        is_end = True
+            else:
+                print('bad request_id {}'.format(request_id))
+                is_end = True
+            if self.locker.locked():
+                self.locker.release()
+            if is_end:
+                break
+        return response
+
+    def join(self):
+        for p in self.__manager_lst:
+            p.join()
+        for p in self.__woker_lst:
+            p.join()
+
+    def terminate(self):
+        for p in self.__woker_lst + self.__manager_lst:
+            try:
+                p.release()
+            except Exception as e:
+                pass
+            p.terminate()
+
+    @property
+    def manager_process_list(self):
+        return self.__manager_lst
+
+    @property
+    def woker_process_list(self):
+        return self.__woker_lst
---
+++ packages/ipc-worker/ipc_worker-0.1.3-py3-none-any.whl/ipc_worker/zmq_module/ipc_zmq.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/29 13:45
+# @Author  : tk
+import math
+import multiprocessing
+import os
+import random
+import threading
+import time
+from .ipc_zmq_utils import ZMQ_manager,ZMQ_sink,ZMQ_worker
+import pickle
+from ..utils import logger
+
+
+
+class ZMQ_process_worker(ZMQ_worker):
+    def __init__(self,*args,**kwargs):
+        super(ZMQ_process_worker,self).__init__(*args,**kwargs)
+
+    #Process begin trigger this func
+    def run_begin(self):
+        raise NotImplementedError
+
+    # Process end trigger this func
+    def run_end(self):
+        raise NotImplementedError
+
+    #any data put will trigger this func
+    def run_once(self,request_data):
+        raise NotImplementedError
+
+
+
+class IPC_zmq:
+    def __init__(self,
+                 CLS_worker,
+                 worker_args: tuple,
+                 worker_num: int,
+                 group_name,
+                 evt_quit=multiprocessing.Manager().Event(),
+                 queue_size=20,
+                 is_log_time=False,
+                 daemon=False
+                 ):
+        self.__manager_lst = []
+        self.__woker_lst = []
+        self.__group_idenity = []
+
+        assert isinstance(worker_args, tuple)
+        manager = ZMQ_manager(0, queue_size, group_name,evt_quit)
+        self.__manager_lst.append(manager)
+
+        sink = ZMQ_sink(queue_size,group_name,evt_quit)
+        self.__manager_lst.append(sink)
+
+        for i in range(worker_num):
+            identity = bytes('{}_{}'.format(group_name,i),encoding='utf-8')
+            worker = CLS_worker(
+                *worker_args,
+                identity=identity,
+                group_name=group_name,
+                evt_quit=evt_quit,
+                is_log_time=is_log_time,
+                idx=i,
+                daemon=daemon
+            )
+            self.__group_idenity.append(identity)
+            self.__woker_lst.append(worker)
+        self.__last_worker_id = len(self.__group_idenity) - 1
+        self.pending_request = {}
+        self.pending_response = {}
+        self.locker = threading.Lock()
+        self.__last_t = time.time()
+    def start(self):
+        for w in self.__manager_lst:
+            w.start()
+
+        for w in self.__manager_lst:
+            w.wait_init()
+
+
+        for w in self.__woker_lst:
+            w._set_addr(self.__manager_lst[1].addr,self.__manager_lst[0].addr)
+            w.start()
+
+        for w in self.__woker_lst:
+            while not w.signal.is_set():
+                pass
+            del w.signal
+
+
+    def put(self,data):
+        self.__last_worker_id = (self.__last_worker_id + 1 ) % len(self.__group_idenity)
+        idenity = self.__group_idenity[self.__last_worker_id]
+        request_id = self.__manager_lst[0].put(idenity,pickle.dumps(data))
+        self.locker.acquire()
+        self.pending_request[request_id] = time.time()
+        self.locker.release()
+        return request_id
+
+    def get(self,request_id):
+        d = self.__get_private__(request_id)
+        return d if d is None else pickle.loads(d)
+
+
+    def __clean__unsafe__(self):
+        c_t = time.time()
+        if math.floor((c_t - self.__last_t) / 600) > 0:
+            self.__last_t = c_t
+            invalid = set({rid for rid, t in self.pending_request.items() if math.floor((c_t - t) / 3600) > 0})
+            logger.debug('remove {}'.format(str(list(invalid))))
+            for rid in invalid:
+                self.pending_request.pop(rid)
+
+    def __get_private__(self, request_id):
+        sink = self.__manager_lst[1]
+        response = None
+        is_end = False
+        timeout = 0.005
+        while not is_end:
+            sink.get_signal().wait(timeout)
+            self.locker.acquire(timeout=timeout)
+            if not self.locker.locked():
+                continue
+            if request_id in self.pending_request:
+                self.pending_request[request_id] = time.time()
+                if request_id in self.pending_response:
+                    response = self.pending_response.pop(request_id)
+                    is_end = True
+                else:
+                    r_id, w_id, seq_id, response = sink.get_queue().get()
+                    if r_id != request_id:
+                        self.pending_response[r_id] = response
+                    else:
+                        is_end = True
+            else:
+                logger.error('bad request_id {}'.format(request_id))
+                is_end = True
+            if self.locker.locked():
+                self.locker.release()
+            if is_end:
+                break
+
+        self.locker.acquire()
+        self.__clean__unsafe__()
+        self.locker.release()
+        return response
+
+
+
+
+    def join(self):
+        for p in self.__manager_lst:
+            p.join()
+        time.sleep(1)
+        for p in self.__woker_lst:
+            p.join()
+
+    @property
+    def manager_process_list(self):
+        return self.__manager_lst
+
+    @property
+    def woker_process_list(self):
+        return self.__woker_lst
+
+    def terminate(self):
+        for p in self.__woker_lst + self.__manager_lst:
+            try:
+                p.release()
+            except Exception as e:
+                pass
+            p.terminate()
