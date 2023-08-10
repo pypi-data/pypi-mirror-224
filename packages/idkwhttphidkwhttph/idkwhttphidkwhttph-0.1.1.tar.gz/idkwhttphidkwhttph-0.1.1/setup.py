22d9198db9f4b8021c8e8954ce5caab49e5b5347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:36:01 +0000

    Add package idkwhttphidkwhttph/0.1.1/idkwhttphidkwhttph-0.1.1.tar.gz

---
+++ packages/idkwhttphidkwhttph/idkwhttphidkwhttph-0.1.1.tar.gz/idkwhttphidkwhttph-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['idkwhttphidkwhttph']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'idkwhttphidkwhttph',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
