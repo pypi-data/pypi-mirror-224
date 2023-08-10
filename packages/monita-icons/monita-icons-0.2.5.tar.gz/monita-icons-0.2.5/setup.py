1206e01df26c9e5f4de9c9b47d6820bd4cc110e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:02 +0000

    Add package monita-icons/0.2.5/monita-icons-0.2.5.tar.gz

---
+++ packages/monita-icons/monita-icons-0.2.5.tar.gz/monita-icons-0.2.5/setup.py
@@ -0,0 +1,6 @@
+import setuptools
+
+setuptools.setup(
+    name="monita-icons",                     # This is the name of the package
+    version="0.2.5",                        # The initial release version
+)
