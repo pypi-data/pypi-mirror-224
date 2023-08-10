de6c4e503a160100bb68f060bdd09b724a8e62bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:11:44 +0000

    Add package pyecotrend-ista/2.1.0/pyecotrend_ista-2.1.0-py3-none-any.whl

---
+++ packages/pyecotrend-ista/pyecotrend_ista-2.1.0-py3-none-any.whl/pyecotrend_ista/const.py
@@ -0,0 +1,10 @@
+VERSION = "2.1.0"
+LOGIN_HEADER = {"Content-Type": "application/json"}
+BASE_URL = "https://api.prod.eed.ista.com/"
+ACCOUNT_URL = BASE_URL + "account"
+CONSUMPTIONS_URL = BASE_URL + "consumptions?consumptionUnitUuid="
+LOGIN_URL = BASE_URL + "login"
+REFRESH_TOKEN_URL = BASE_URL + "account/refresh-token"
+
+MAX_RETRIES = 3
+RETRY_DELAY = 2
