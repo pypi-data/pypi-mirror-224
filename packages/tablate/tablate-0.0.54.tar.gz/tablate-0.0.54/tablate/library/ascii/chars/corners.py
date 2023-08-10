bed9eec09dfadbdcf778f9aac3e51a46b6723686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:10:04 +0000

    Add package tablate/0.0.54/tablate-0.0.54.tar.gz

---
+++ packages/tablate/tablate-0.0.54.tar.gz/tablate-0.0.54/tablate/library/ascii/chars/corners.py
@@ -0,0 +1,27 @@
+top_left = {
+    "blank": " ",
+    "thin": "┌",
+    "thick": "┏",
+    "double": "╔"
+}
+
+top_right = {
+    "blank": " ",
+    "thin": "┐",
+    "thick": "┓",
+    "double": "╗"
+}
+
+bottom_left = {
+    "blank": " ",
+    "thin": "└",
+    "thick": "┗",
+    "double": "╚"
+}
+
+bottom_right = {
+    "blank": " ",
+    "thin": "┘",
+    "thick": "┛",
+    "double": "╝"
+}
