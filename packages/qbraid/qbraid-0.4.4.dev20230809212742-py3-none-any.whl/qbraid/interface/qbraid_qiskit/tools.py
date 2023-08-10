260088eec142eada7b77499fb23b21eb4f77cb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:10 +0000

    Add package qbraid/0.4.4.dev20230809212742/qbraid-0.4.4.dev20230809212742-py3-none-any.whl

---
+++ packages/qbraid/qbraid-0.4.4.dev20230809212742-py3-none-any.whl/qbraid/interface/qbraid_qiskit/tools.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2023 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module containing Qiskit tools
+
+"""
+from collections import OrderedDict
+
+import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.converters import circuit_to_dag, dag_to_circuit
+from qiskit.qasm3 import dumps, loads
+from qiskit.quantum_info import Operator
+
+QASMType = str
+
+
+def _unitary_from_qiskit(circuit: QuantumCircuit) -> np.ndarray:
+    """Return the unitary of a Qiskit quantum circuit."""
+    return Operator(circuit).data
+
+
+def _unitary_from_qasm3(qasmstr: QASMType) -> np.ndarray:
+    """Return the unitary of the QASM 3 string"""
+    circuit = loads(qasmstr)
+    return _unitary_from_qiskit(circuit)
+
+
+def _convert_to_contiguous_qiskit(circuit: QuantumCircuit) -> QuantumCircuit:
+    """Delete qubit(s) with no gate, if any exist."""
+    dag = circuit_to_dag(circuit)
+
+    idle_wires = list(dag.idle_wires())
+    for w in idle_wires:
+        dag._remove_idle_wire(w)
+        dag.qubits.remove(w)
+
+    dag.qregs = OrderedDict()
+
+    return dag_to_circuit(dag)
+
+
+def _convert_to_contiguous_qasm3(qasmstr: QASMType) -> QASMType:
+    """Delete qubit(s) with no gate, if any exist."""
+    circuit = loads(qasmstr)
+    circuit_contig = _convert_to_contiguous_qiskit(circuit)
+    return dumps(circuit_contig)
