9b7e7d1d789bf7da8b7b2c1fd8c4d56989439f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:11:42 +0000

    Add package gisting-test/0.2.5/gisting_test-0.2.5.tar.gz

---
+++ packages/gisting-test/gisting_test-0.2.5.tar.gz/gisting_test-0.2.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="gisting_test",
+    version="0.2.5",
+    author="Owais Zahid",
+    email="owais.zahid@mail.utoronto.ca",
+    packages=['gisting_test','gisting_test/src','gisting_test/src/data'],
+    description="Test version for gisting, original repo authored by Jesse Mu.",
+    url='https://github.com/jayelm/gisting',
+
+    classifiers=['Natural Language :: English',
+        'Programming Language :: Python :: 3']
+)
