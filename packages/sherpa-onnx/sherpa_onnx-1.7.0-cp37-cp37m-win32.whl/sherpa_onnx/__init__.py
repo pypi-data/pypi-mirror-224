ba8211e9918bacf98f8ed9563e732db4c788653a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:07:23 +0000

    Add package sherpa-onnx/1.7.0/sherpa_onnx-1.7.0-cp37-cp37m-win32.whl

---
+++ packages/sherpa-onnx/sherpa_onnx-1.7.0-cp37-cp37m-win32.whl/sherpa_onnx/__init__.py
@@ -0,0 +1,13 @@
+from typing import Dict, List, Optional
+
+from _sherpa_onnx import Display
+
+from .online_recognizer import OnlineRecognizer
+from .online_recognizer import OnlineStream
+from .offline_recognizer import OfflineRecognizer
+
+from .utils import encode_contexts
+
+
+
+__version__ = '1.7.0'
