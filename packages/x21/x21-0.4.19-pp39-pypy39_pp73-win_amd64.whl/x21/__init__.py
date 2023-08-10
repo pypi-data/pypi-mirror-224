04377376f2cc8be93d15d6f2e7bd8d47b55338ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:11:21 +0000

    Add package x21/0.4.19/x21-0.4.19-pp39-pypy39_pp73-win_amd64.whl

---
+++ packages/x21/x21-0.4.19-pp39-pypy39_pp73-win_amd64.whl/x21/__init__.py
@@ -0,0 +1,17 @@
+from _x21 import dex_23c
+
+# backwards compat:
+from ._main import (
+    __dex_22b__,
+    __dex_22c__,
+    __dex_23a__,
+    __dex_23b__,
+)
+
+__all__ = [
+    "__dex_22b__",
+    "__dex_22c__",
+    "__dex_23a__",
+    "__dex_23b__",
+    "dex_23c",
+]
