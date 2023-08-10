5d093d6e8fe1cb8f69781dada736707f924d5887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 15:08:00 +0000

    Add package xethhung12-minio-download-file/0.0.4/xethhung12_minio_download_file-0.0.4-py3-none-any.whl

---
+++ packages/xethhung12-minio-download-file/xethhung12_minio_download_file-0.0.4-py3-none-any.whl/xethhung12_minio_download_file/download_file.py
@@ -0,0 +1,23 @@
+from minio import Minio
+
+def download(url, access_key, secret_key, bucket, remote, local, proxy=None):
+    client = Minio(
+        url,
+        access_key=access_key,
+        secret_key=secret_key,
+    ) if proxy != None else Minio(
+        url,
+        access_key=access_key,
+        secret_key=secret_key,
+        http_client=urllib3.ProxyManager(proxy)
+    )
+
+    rs = client.fget_object(
+        bucket,
+        object_name=remote,
+        file_path=local
+    )
+
+    return rs
+
+
