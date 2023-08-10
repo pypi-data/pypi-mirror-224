6325c1474f7005ab1247f351363f47ceabcf2917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:04 +0000

    Add package probable-fiesta/0.2.1/probable_fiesta-0.2.1-py3-none-any.whl

---
+++ packages/probable-fiesta/probable_fiesta-0.2.1-py3-none-any.whl/probable_fiesta/config/builder/config_builder_factory.py
@@ -0,0 +1,31 @@
+from .config_builder import ConfigBuilder
+from ...logger.builder.logger_machine import LoggerMachine as loggerMachine
+from ..variables import PackageDef as pd
+from ..variables import LoggerDef as ld
+from ..variables import VariablesDef as vd
+from ..variables import DotEnvDef as ded
+
+
+class ConfigFactory:
+    @staticmethod
+    def new_config_builder():
+        return ConfigBuilder()
+
+    @staticmethod
+    def new_config():
+        return ConfigBuilder().build()
+
+    @staticmethod
+    def new_default_config_builder(log_type="default", log_name="default"):
+        lM = loggerMachine()
+        logger = lM.make_logger(log_type, log_name)
+        cB = ConfigBuilder()
+        config = (
+            cB.package.set_package_name(pd.NAME)
+            .logger.set_logger(logger)
+            .variables.set_variable("VERSION", vd.VERSION)
+            .dotenv.load_dotenv()
+            .set_vars(ded().__dict__)  # Only public variables are loaded
+            .build()
+        )
+        return config
