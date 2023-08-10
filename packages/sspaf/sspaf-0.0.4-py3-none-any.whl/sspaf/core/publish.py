59e15e5bfa7c2952decb8888c35a6bb511d82c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:33:53 +0000

    Add package sspaf/0.0.4/sspaf-0.0.4-py3-none-any.whl

---
+++ packages/sspaf/sspaf-0.0.4-py3-none-any.whl/sspaf/core/publish.py
@@ -0,0 +1,15 @@
+import os
+import time
+from core import render
+
+def publish(path: str) -> None:
+    if path == ".":
+        path = os.getcwd()
+
+    print(f'Publishing project {path}')
+    start = time.time()
+
+    render.render(path, dev=False)
+
+    print(f'Published project {path} in {time.time() - start} seconds')
+    print(f'Project published to {os.path.join(path, "output")}')
