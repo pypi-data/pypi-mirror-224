06227370e983a2f48bd1f72eb4928e7d9cf83e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:16 +0000

    Add package fast-tradier-client/0.0.3/fast_tradier_client-0.0.3.tar.gz

---
+++ packages/fast-tradier-client/fast_tradier_client-0.0.3.tar.gz/fast_tradier_client-0.0.3/fast_tradier/models/DataClassModelBase.py
@@ -0,0 +1,13 @@
+from typing import Dict
+from dataclasses import dataclass
+from fast_tradier.models.ModelBase import ModelBase
+
+@dataclass
+class DataClassModelBase(ModelBase):
+    def __init__(self, api_resp_dict: Dict):
+        self.__resp_dict = api_resp_dict
+        for k, v in api_resp_dict.items():
+            setattr(self, k, v)
+    
+    def __iter__(self):
+        yield from self.__resp_dict.items()
