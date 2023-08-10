1ed636ef64a072076c6857869c65b29748c9bf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:40 +0000

    Add package fastmessage/0.1.0/fastmessage-0.1.0-py3-none-any.whl

---
+++ packages/fastmessage/fastmessage-0.1.0-py3-none-any.whl/fastmessage/__init__.py
@@ -0,0 +1,15 @@
+from .exceptions import (
+    FastMessageException,
+    SpecialDefaultValueException,
+    MissingCallbackException,
+    DuplicateCallbackException,
+    UnnamedCallableException,
+    NotAllowedParamKindException,
+
+)
+from .fastmessage_handler import (
+    FastMessage,
+    FastMessageOutput,
+    InputDeviceName,
+    MultipleReturnValues,
+)
