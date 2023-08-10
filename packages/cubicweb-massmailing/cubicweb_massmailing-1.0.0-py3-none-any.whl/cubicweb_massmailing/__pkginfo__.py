994e29772c99d5f5ef94a7f0ff77712ccd7961f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:08:10 +0000

    Add package cubicweb-massmailing/1.0.0/cubicweb_massmailing-1.0.0-py3-none-any.whl

---
+++ packages/cubicweb-massmailing/cubicweb_massmailing-1.0.0-py3-none-any.whl/cubicweb_massmailing/__pkginfo__.py
@@ -0,0 +1,28 @@
+# pylint: disable=W0622
+"""cubicweb-massmailing application packaging information"""
+
+
+modname = 'cubicweb_massmailing'
+distname = 'cubicweb-massmailing'
+
+numversion = (1, 0, 0)
+version = '.'.join(str(num) for num in numversion)
+
+license = 'LGPL'
+author = 'LOGILAB S.A. (Paris, FRANCE)'
+author_email = 'contact@logilab.fr'
+description = 'Mass mailing handling: send mail to entities adaptable to IEmailable'
+web = 'http://www.cubicweb.org/project/%s' % distname
+
+__depends__ = {
+    'cubicweb': '>=4.0.0,<5.0.0',
+    'cubicweb_web': '>=1.0.0,<2.0.0',
+}
+__recommends__ = {}
+
+classifiers = [
+    'Environment :: Web Environment',
+    'Framework :: CubicWeb',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: JavaScript',
+]
