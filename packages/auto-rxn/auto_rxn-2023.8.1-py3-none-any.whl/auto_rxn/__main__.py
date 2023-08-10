fc3f8da4853dc96bec69f5dfe9c8df49ddbd48f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:08:25 +0000

    Add package auto-rxn/2023.8.1/auto_rxn-2023.8.1-py3-none-any.whl

---
+++ packages/auto-rxn/auto_rxn-2023.8.1-py3-none-any.whl/auto_rxn/__main__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import click
+
+from ._device import happi_client
+from .__version__ import __version__
+from ._recipe import Recipe
+from ._run import run
+
+
+@click.group()
+@click.version_option(__version__)
+def main():
+    pass
+
+
+@main.command(name="list_devices")
+def _list_devices():
+    for item, result in happi_client.items():
+        print(item)
+        print(f"    device_class: {result.metadata['device_class']}")
+        try:
+            print(f"    lower_safety_limit: {result.metadata['auto_rxn_lower_safety_limit']}")
+        except KeyError:
+            print(f"    lower_safety_limit: None")
+        try:
+            print(f"    upper_safety_limit: {result.metadata['auto_rxn_upper_safety_limit']}")
+        except KeyError:
+            print(f"    upper_safety_limit: None")
+        try:
+            print(f"    fallback_positon: {result.metadata['auto_rxn_fallback_position']}")
+        except KeyError:
+            print(f"    fallback_positon: None")
+
+
+@main.command(name="run")
+@click.argument("recipe", type=click.Path())
+def _run(recipe):
+    r = Recipe(recipe)
+    run(r)
