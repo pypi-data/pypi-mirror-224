83e10bc0fa3ce6858ff474c2829098b869872020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:49 +0000

    Add package django-simulate-user/0.1/django-simulate-user-0.1.tar.gz

---
+++ packages/django-simulate-user/django-simulate-user-0.1.tar.gz/django-simulate-user-0.1/simulate_user/apps.py
@@ -0,0 +1,26 @@
+from django.apps import AppConfig
+
+
+class SimulateUserConfig(AppConfig):
+    """
+    Configuration class for the SimulateUser application.
+
+    Attributes:
+    - default_auto_field: Specifies the type of auto-created primary key.
+    - name: The full Python path of the application.
+    - verbose_name: Human-readable name for the application.
+
+    Methods:
+    - ready: This method is called when the application is ready. It's used here to import signals.
+    """
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'simulate_user'
+    verbose_name = "Simulate Users"
+
+    # noinspection PyUnresolvedReferences
+    def ready(self):
+        """
+        Import signals when the application is ready.
+        This ensures that the signal handlers are connected when the app is loaded.
+        """
+        from . import signals
