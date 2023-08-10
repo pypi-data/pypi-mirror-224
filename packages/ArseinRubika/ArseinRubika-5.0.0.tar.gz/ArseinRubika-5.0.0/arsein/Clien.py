d7edea913b2ad16205dd491fe1d9b5df84b8777c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 15:08:22 +0000

    Add package ArseinRubika/5.0.0/ArseinRubika-5.0.0.tar.gz

---
+++ packages/ArseinRubika/ArseinRubika-5.0.0.tar.gz/ArseinRubika-5.0.0/arsein/Clien.py
@@ -0,0 +1,17 @@
+class clien:
+    web = {
+        "app_name": "Main",
+        "app_version": "4.3.5",
+        "platform": "Web",
+        "package": "web.rubika.ir",
+        "lang_code": "fa"
+    }
+
+    android = {
+        "app_name": "Main",
+        "app_version": "4.3.5",
+        "platform": "Web",
+        "package": "web.rubika.ir",
+        "lang_code": "fa"
+    }
+		
