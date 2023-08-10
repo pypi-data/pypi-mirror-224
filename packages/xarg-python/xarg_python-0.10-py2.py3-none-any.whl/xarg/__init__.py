ca5b3b30ebeb4c135e79369ad89780bf3b6eb05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:35:49 +0000

    Add package xarg-python/0.10/xarg_python-0.10-py2.py3-none-any.whl

---
+++ packages/xarg-python/xarg_python-0.10-py2.py3-none-any.whl/xarg/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+__version__ = "0.10"
+
+from .decorator import commands
+from .decorator import add_command
+from .decorator import run_command
+
+from .logger import FATAL
+from .logger import ERROR
+from .logger import WARN
+from .logger import INFO
+from .logger import DEBUG
+from .logger import TRACE
+
+from .parser import argp
