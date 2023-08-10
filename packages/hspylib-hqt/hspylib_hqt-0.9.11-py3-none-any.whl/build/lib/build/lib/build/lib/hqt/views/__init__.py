382599cda66895a23283b8c94b1871773506e7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:08:12 +0000

    Add package hspylib-hqt/0.9.11/hspylib_hqt-0.9.11-py3-none-any.whl

---
+++ packages/hspylib-hqt/hspylib_hqt-0.9.11-py3-none-any.whl/build/lib/build/lib/build/lib/hqt/views/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hqt v0.9.11
+#
+# Package: main.build.lib.build.lib.build.lib.build.lib.hqt.views
+"""Package initialization."""
+
+__all__ = [
+    'main_view', 
+    'qt_view'
+]
+__version__ = '0.9.11'
