b4f7c53deb6773f2bb01409fd16488c05debdf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:11:38 +0000

    Add package satdigitalinvoice/4.0.14/satdigitalinvoice-4.0.14-py3-none-any.whl

---
+++ packages/satdigitalinvoice/satdigitalinvoice-4.0.14-py3-none-any.whl/satdigitalinvoice/log_tools.py
@@ -0,0 +1,19 @@
+import yaml
+
+
+class NoAliasDumper(yaml.SafeDumper):
+    def ignore_aliases(self, data):
+        return True
+
+
+def print_yaml(data):
+    print(
+        yaml.dump(data, Dumper=NoAliasDumper, allow_unicode=True, width=1280, sort_keys=False)
+    )
+
+
+def header_line(text):
+    ln = (150 - len(text)) // 2
+    return ("=" * ln) + " " + text + " " + ("=" * ln) + "\n"
+
+
