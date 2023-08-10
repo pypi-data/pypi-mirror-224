ee2719389d7666927f99a1b365c4725bf0cb0588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:05 +0000

    Add package thorpy/2.0.7/thorpy-2.0.7.tar.gz

---
+++ packages/thorpy/thorpy-2.0.7.tar.gz/thorpy-2.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+# import thorpy
+# print("VERSION", thorpy.__version__)
+
+setup(name='thorpy',
+      version='2.0.7',
+      description='GUI library for pygame',
+      long_description='ThorPy is a non-intrusive, straightforward GUI kit for Pygame.',
+      author='Yann Thorimbert',
+      author_email='yann.thorimbert@gmail.com',
+      url='http://www.thorpy.org/',
+      keywords=['pygame', 'gui', 'menus', 'buttons', 'widgets', 'user interface', 'toolkit'],
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={
+          'thorpy': ['data/*'],
+      },
+      license='MIT')
