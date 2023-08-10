b68d7ffbc9e2843e7061b7471fc994c5cbd0e8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:07:59 +0000

    Add package verysimplemodulevis/0.0.51/verysimplemodulevis-0.0.51.tar.gz

---
+++ packages/verysimplemodulevis/verysimplemodulevis-0.0.51.tar.gz/verysimplemodulevis-0.0.51/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+VERSION = '0.0.51'
+DESCRIPTION = 'Meu primeiro projeto em Python'
+LONG_DESCRIPTION = 'Esta é a descrição longa do meu projeto.'
+
+# Configurando o setup
+setup(
+    name="verysimplemodulevis",
+    version=VERSION,
+    author="Victor Macedo",
+    author_email="victormacedocarvalho09@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=['verysimplemodulevis'],  # Adicione "verysimplemodule" à lista de pacotes
+    install_requires=[],
+)
