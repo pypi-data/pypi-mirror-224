c0c87b741682601f6f61220f0cb05a81dc1c1540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:02 +0000

    Add package minchin.pelican.plugins.nojekyll/1.2.0/minchin.pelican.plugins.nojekyll-1.2.0-py3-none-any.whl

---
+++ packages/minchin.pelican.plugins.nojekyll/minchin.pelican.plugins.nojekyll-1.2.0-py3-none-any.whl/minchin/pelican/plugins/nojekyll.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*- #
+"""
+NoJekyll
+========
+
+This plugin for Pelican add a *.nojekyll* file to the output root.
+"""
+
+import logging
+import os
+
+from pelican import signals
+
+__version__ = "1.2.0"
+
+logger = logging.getLogger(__name__)
+
+
+def add_nojekyll(p):
+    """
+    :param p: pelican instance
+    :return: None
+    """
+
+    nojekyll_path = os.path.join(p.output_path, ".nojekyll")
+    content = " "
+    with open(nojekyll_path, "w") as f:
+        f.write(content)
+    logger.info("[noJekyll] written to %s" % nojekyll_path)
+
+
+def register():
+    signals.finalized.connect(add_nojekyll)
