f599cc75ed3b562c9878faafb4cc6430fd0a3c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:34:42 +0000

    Add package flgo/0.0.23/flgo-0.0.23-py3-none-any.whl

---
+++ packages/flgo/flgo-0.0.23-py3-none-any.whl/flgo/__init__.py
@@ -0,0 +1,15 @@
+from .utils.fflow import init, gen_task, gen_task_from_para, gen_benchmark_from_file, gen_decentralized_benchmark, gen_hierarchical_benchmark, convert_model,tune, run_in_parallel, module2fmodule, multi_init_and_run
+
+communicator = None
+
+class VirtualCommunicator:
+    """
+    Communicator that simulates the communication phase between any two objects
+    """
+    def __init__(self, objects):
+        self.objects_map = {obj.id:obj for obj in objects}
+        self.objects = objects
+
+    def request(self, source, target, package):
+        # send package to the target object with `package` and `mtype`, and then listen from it
+        return self.objects_map[target].message_handler(package)
