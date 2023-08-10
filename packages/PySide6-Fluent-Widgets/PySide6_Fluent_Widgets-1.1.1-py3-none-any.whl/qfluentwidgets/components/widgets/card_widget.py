c8b3b15fec013bd494555b6e2b09680be343f5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:08:08 +0000

    Add package PySide6-Fluent-Widgets/1.1.1/PySide6_Fluent_Widgets-1.1.1-py3-none-any.whl

---
+++ packages/PySide6-Fluent-Widgets/PySide6_Fluent_Widgets-1.1.1-py3-none-any.whl/qfluentwidgets/components/widgets/card_widget.py
@@ -0,0 +1,99 @@
+# coding:utf-8
+from PySide6.QtCore import Qt, Signal, QRectF, Property
+from PySide6.QtGui import QPixmap, QPainter, QColor, QPainterPath
+from PySide6.QtWidgets import QWidget, QFrame
+
+from ...common.style_sheet import isDarkTheme
+from ...common.animation import BackgroundAnimationWidget
+
+
+class CardWidget(BackgroundAnimationWidget, QFrame):
+    """ Card widget """
+
+    clicked = Signal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self._isClickEnabled = False
+        self._borderRadius = 5
+
+    def mouseReleaseEvent(self, e):
+        super().mouseReleaseEvent(e)
+        self.clicked.emit()
+
+    def setClickEnabled(self, isEnabled: bool):
+        self._isClickEnabled = isEnabled
+        self.update()
+
+    def isClickEnabled(self):
+        return self._isClickEnabled
+
+    def _normalBackgroundColor(self):
+        return QColor(255, 255, 255, 13 if isDarkTheme() else 170)
+
+    def _hoverBackgroundColor(self):
+        return QColor(255, 255, 255, 21 if isDarkTheme() else 64)
+
+    def _pressedBackgroundColor(self):
+        return QColor(255, 255, 255, 8 if isDarkTheme() else 64)
+
+    def getBorderRadius(self):
+        return self._borderRadius
+
+    def setBorderRadius(self, radius: int):
+        self._borderRadius = radius
+        self.update()
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.Antialiasing)
+
+        w, h = self.width(), self.height()
+        r = 5
+        d = 2 * r
+
+        isDark = isDarkTheme()
+
+        # draw top border
+        path = QPainterPath()
+        # path.moveTo(1, h - r)
+        path.arcMoveTo(1, h - d - 1, d, d, 225)
+        path.arcTo(1, h - d - 1, d, d, 225, -45)
+        path.lineTo(1, r)
+        path.arcTo(1, 1, d, d, -180, -90)
+        path.lineTo(w - r, 1)
+        path.arcTo(w - d - 1, 1, d, d, 90, -90)
+        path.lineTo(w - 1, h - r)
+        path.arcTo(w - d - 1, h - d - 1, d, d, 0, -45)
+
+        topBorderColor = QColor(0, 0, 0, 20)
+        if isDark:
+            if self.isPressed:
+                topBorderColor = QColor(255, 255, 255, 18)
+            elif self.isHover:
+                topBorderColor = QColor(255, 255, 255, 13)
+        else:
+            topBorderColor = QColor(0, 0, 0, 15)
+
+        painter.strokePath(path, topBorderColor)
+
+        # draw bottom border
+        path = QPainterPath()
+        path.arcMoveTo(1, h - d - 1, d, d, 225)
+        path.arcTo(1, h - d - 1, d, d, 225, 45)
+        path.lineTo(w - r - 1, h - 1)
+        path.arcTo(w - d - 1, h - d - 1, d, d, 270, 45)
+
+        bottomBorderColor = topBorderColor
+        if not isDark and self.isHover and not self.isPressed:
+            bottomBorderColor = QColor(0, 0, 0, 27)
+
+        painter.strokePath(path, bottomBorderColor)
+
+        # draw background
+        painter.setPen(Qt.NoPen)
+        rect = self.rect().adjusted(1, 1, -1, -1)
+        painter.setBrush(self.backgroundColor)
+        painter.drawRoundedRect(rect, r, r)
+
+    borderRadius = Property(int, getBorderRadius, setBorderRadius)
