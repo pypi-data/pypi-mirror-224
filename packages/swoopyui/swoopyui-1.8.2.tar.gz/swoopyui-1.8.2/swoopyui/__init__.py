7a0b1d43eff9fd047925594c96efd195eb211dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:34:37 +0000

    Add package swoopyui/1.8.2/swoopyui-1.8.2.tar.gz

---
+++ packages/swoopyui/swoopyui-1.8.2.tar.gz/swoopyui-1.8.2/swoopyui/__init__.py
@@ -0,0 +1,25 @@
+from .tools.check_platform import is_device_a_ios, is_device_a_mac
+if is_device_a_mac() or is_device_a_ios():
+    pass
+else:
+    raise Exception ("Your device is not a mac. So cannot use this library.")
+
+from .swoopyui import app
+
+from .view import View
+
+from .views.text import Text
+from .views.button import Button
+from .views.navigationview import NavigationView
+from .views.navigationstack import NavigationStack
+from .views.navigationlink import NavigationLink
+from .views.textfield import TextField
+from .views.stack import Stack, VSTACK, HSTACK, ZSTACK
+from .views.scrollview import ScrollView
+from .views.spacer import Spacer
+from .views.list import List
+from .views.image import Image
+from .views.webview import WebView
+from .views.animatedview import AnimatedView
+
+from .views.shapes.circle import Circle
