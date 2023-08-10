a39975ec1789be0735cb8173bfe8cc95e785c3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 15:08:05 +0000

    Add package visiongraph/0.1.50.2/visiongraph-0.1.50.2-py3-none-any.whl

---
+++ packages/visiongraph/visiongraph-0.1.50.2-py3-none-any.whl/visiongraph/data/__init__.py
@@ -0,0 +1,14 @@
+import glob
+import os
+
+import visiongraph.cache
+
+
+def reset_data_cache():
+    data_path = os.path.abspath(os.path.dirname(visiongraph.cache.__file__))
+    for file in glob.glob(os.path.join(data_path, "*")):
+        if file.endswith(".py"):
+            continue
+
+        if os.path.isfile(file):
+            os.remove(file)
