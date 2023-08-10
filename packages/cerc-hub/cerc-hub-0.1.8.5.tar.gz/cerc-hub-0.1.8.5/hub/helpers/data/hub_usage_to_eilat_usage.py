404249d03bde90a549780f3fa7277042f1469066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:45 +0000

    Add package cerc-hub/0.1.8.5/cerc-hub-0.1.8.5.tar.gz

---
+++ packages/cerc-hub/cerc-hub-0.1.8.5.tar.gz/cerc-hub-0.1.8.5/hub/helpers/data/hub_usage_to_eilat_usage.py
@@ -0,0 +1,29 @@
+"""
+Dictionaries module for hub usage to Eilat usage
+SPDX - License - Identifier: LGPL - 3.0 - or -later
+Copyright © 2023 Concordia CERC group
+Project Coder Pilar Monsalvete Alvarez de Uribarri pilar.monsalvete@concordia.ca
+"""
+
+import hub.helpers.constants as cte
+
+
+class HubUsageToEilatUsage:
+  """
+  Hub usage to Eilat usage class
+  """
+
+  def __init__(self):
+    self._dictionary = {
+      cte.RESIDENTIAL: 'Residential',
+      cte.HOTEL: 'Hotel employees',
+      cte.DORMITORY: 'Dormitory'
+    }
+
+  @property
+  def dictionary(self) -> dict:
+    """
+    Get the dictionary
+    :return: {}
+    """
+    return self._dictionary
