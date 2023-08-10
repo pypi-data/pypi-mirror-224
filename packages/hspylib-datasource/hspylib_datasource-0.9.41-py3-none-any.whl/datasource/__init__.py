ce83f54852bf9dcc04192976a70069bf76b8794a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:13:11 +0000

    Add package hspylib-datasource/0.9.41/hspylib_datasource-0.9.41-py3-none-any.whl

---
+++ packages/hspylib-datasource/hspylib_datasource-0.9.41-py3-none-any.whl/datasource/__init__.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-datasource v0.9.41
+#
+# Package: main.build.lib.datasource
+"""Package initialization."""
+
+__all__ = [
+    'crud_entity', 
+    'crud_repository', 
+    'crud_service', 
+    'db_configuration', 
+    'db_repository', 
+    'exception', 
+    'firebase', 
+    'identity', 
+    'mysql', 
+    'redis', 
+    'sqlite'
+]
+__version__ = '0.9.41'
