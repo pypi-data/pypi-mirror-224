9f80a7bae21ad603ec4e74a2c14c88df76a65a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 15:07:55 +0000

    Add package pytorch-benchmark/0.3.6/pytorch-benchmark-0.3.6.tar.gz

---
+++ packages/pytorch-benchmark/pytorch-benchmark-0.3.6.tar.gz/pytorch-benchmark-0.3.6/README.md
@@ -0,0 +1,316 @@
+# ⏱ pytorch-benchmark
+__Easily benchmark model inference FLOPs, latency, throughput, max allocated memory and energy consumption__
+<div align="left">
+  <a href="https://pypi.org/project/pytorch-benchmark/">
+    <img src="https://img.shields.io/pypi/pyversions/pytorch-benchmark" height="20" >
+  </a>
+  <a href="https://badge.fury.io/py/pytorch-benchmark">
+    <img src="https://badge.fury.io/py/pytorch-benchmark.svg" height="20" >
+  </a>
+  <a href="https://pepy.tech/project/pytorch-benchmark">
+    <img src="https://pepy.tech/badge/pytorch-benchmark" height="20">
+  </a>
+  <a href="https://www.codefactor.io/repository/github/lukashedegaard/pytorch-benchmark/overview/main">
+    <img src="https://www.codefactor.io/repository/github/lukashedegaard/pytorch-benchmark/badge/main" alt="CodeFactor" />
+  </a>
+  <a href="https://opensource.org/licenses/Apache-2.0">
+    <img src="https://img.shields.io/badge/License-Apache%202.0-blue.svg" height="20">
+  </a>
+  <a href="https://github.com/psf/black">
+    <img src="https://img.shields.io/badge/code%20style-black-000000.svg" height="20">
+  </a>
+  <a href="https://codecov.io/gh/LukasHedegaard/pytorch-benchmark">
+    <img src="https://codecov.io/gh/LukasHedegaard/pytorch-benchmark/branch/main/graph/badge.svg?token=B91XGSKSFJ"/>
+  </a>
+   <sup>*</sup>
+</div>
+
+###### \*Actual coverage is higher as GPU-related code is skipped by Codecov
+
+## Install 
+```bash
+pip install pytorch-benchmark
+```
+
+## Usage 
+```python
+import torch
+from torchvision.models import efficientnet_b0
+from pytorch_benchmark import benchmark
+
+
+model = efficientnet_b0().to("cpu")  # Model device sets benchmarking device
+sample = torch.randn(8, 3, 224, 224)  # (B, C, H, W)
+results = benchmark(model, sample, num_runs=100)
+```
+
+### Sample results 💻
+<details>
+  <summary>Macbook Pro (16-inch, 2019), 2.6 GHz 6-Core Intel Core i7</summary>
+  
+  ```
+  device: cpu
+  flops: 401669732
+  machine_info:
+    cpu:
+      architecture: x86_64
+      cores:
+        physical: 6
+        total: 12
+      frequency: 2.60 GHz
+      model: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+    gpus: null
+    memory:
+      available: 5.86 GB
+      total: 16.00 GB
+      used: 7.29 GB
+    system:
+      node: d40049
+      release: 21.2.0
+      system: Darwin
+  params: 5288548
+  timing:
+    batch_size_1:
+      on_device_inference:
+        human_readable:
+          batch_latency: 74.439 ms +/- 6.459 ms [64.604 ms, 96.681 ms]
+          batches_per_second: 13.53 +/- 1.09 [10.34, 15.48]
+        metrics:
+          batches_per_second_max: 15.478907181264278
+          batches_per_second_mean: 13.528026359855625
+          batches_per_second_min: 10.343281300091244
+          batches_per_second_std: 1.0922382209314958
+          seconds_per_batch_max: 0.09668111801147461
+          seconds_per_batch_mean: 0.07443853378295899
+          seconds_per_batch_min: 0.06460404396057129
+          seconds_per_batch_std: 0.006458734193132054
+    batch_size_8:
+      on_device_inference:
+        human_readable:
+          batch_latency: 509.410 ms +/- 30.031 ms [405.296 ms, 621.773 ms]
+          batches_per_second: 1.97 +/- 0.11 [1.61, 2.47]
+        metrics:
+          batches_per_second_max: 2.4673319862230025
+          batches_per_second_mean: 1.9696935126370148
+          batches_per_second_min: 1.6083039834656554
+          batches_per_second_std: 0.11341204895590185
+          seconds_per_batch_max: 0.6217730045318604
+          seconds_per_batch_mean: 0.509410228729248
+          seconds_per_batch_min: 0.40529608726501465
+          seconds_per_batch_std: 0.030031445467788704
+  ```
+</details>
+
+<details>
+  <summary>Server with NVIDIA GeForce RTX 2080 and Intel Xeon 2.10GHz CPU</summary>
+  
+  ```
+  device: cuda
+  flops: 401669732
+  machine_info:
+    cpu:
+      architecture: x86_64
+      cores:
+        physical: 16
+        total: 32
+      frequency: 3.00 GHz
+      model: Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz
+    gpus:
+    - memory: 8192.0 MB
+      name: NVIDIA GeForce RTX 2080
+    - memory: 8192.0 MB
+      name: NVIDIA GeForce RTX 2080
+    - memory: 8192.0 MB
+      name: NVIDIA GeForce RTX 2080
+    - memory: 8192.0 MB
+      name: NVIDIA GeForce RTX 2080
+    memory:
+      available: 119.98 GB
+      total: 125.78 GB
+      used: 4.78 GB
+    system:
+      node: monster
+      release: 4.15.0-167-generic
+      system: Linux
+  max_inference_memory: 736250368
+  params: 5288548
+  post_inference_memory: 21402112
+  pre_inference_memory: 21402112
+  timing:
+    batch_size_1:
+      cpu_to_gpu:
+        human_readable:
+          batch_latency: "144.815 \xB5s +/- 16.103 \xB5s [136.614 \xB5s, 272.751 \xB5\
+            s]"
+          batches_per_second: 6.96 K +/- 535.06 [3.67 K, 7.32 K]
+        metrics:
+          batches_per_second_max: 7319.902268760908
+          batches_per_second_mean: 6962.865857677197
+          batches_per_second_min: 3666.3496503496503
+          batches_per_second_std: 535.0581873859935
+          seconds_per_batch_max: 0.0002727508544921875
+          seconds_per_batch_mean: 0.00014481544494628906
+          seconds_per_batch_min: 0.0001366138458251953
+          seconds_per_batch_std: 1.6102982159292097e-05
+      gpu_to_cpu:
+        human_readable:
+          batch_latency: "106.168 \xB5s +/- 17.829 \xB5s [53.167 \xB5s, 248.909 \xB5\
+            s]"
+          batches_per_second: 9.64 K +/- 1.60 K [4.02 K, 18.81 K]
+        metrics:
+          batches_per_second_max: 18808.538116591928
+          batches_per_second_mean: 9639.942102368092
+          batches_per_second_min: 4017.532567049808
+          batches_per_second_std: 1595.7983033708472
+          seconds_per_batch_max: 0.00024890899658203125
+          seconds_per_batch_mean: 0.00010616779327392578
+          seconds_per_batch_min: 5.316734313964844e-05
+          seconds_per_batch_std: 1.7829135190772566e-05
+      on_device_inference:
+        human_readable:
+          batch_latency: "15.567 ms +/- 546.154 \xB5s [15.311 ms, 19.261 ms]"
+          batches_per_second: 64.31 +/- 1.96 [51.92, 65.31]
+        metrics:
+          batches_per_second_max: 65.31149174711928
+          batches_per_second_mean: 64.30692850265713
+          batches_per_second_min: 51.918698784442846
+          batches_per_second_std: 1.9599322351815833
+          seconds_per_batch_max: 0.019260883331298828
+          seconds_per_batch_mean: 0.015567030906677246
+          seconds_per_batch_min: 0.015311241149902344
+          seconds_per_batch_std: 0.0005461537255227954
+      total:
+        human_readable:
+          batch_latency: "15.818 ms +/- 549.873 \xB5s [15.561 ms, 19.461 ms]"
+          batches_per_second: 63.29 +/- 1.92 [51.38, 64.26]
+        metrics:
+          batches_per_second_max: 64.26476266356143
+          batches_per_second_mean: 63.28565696640637
+          batches_per_second_min: 51.38378232692614
+          batches_per_second_std: 1.9198343850767468
+          seconds_per_batch_max: 0.019461393356323242
+          seconds_per_batch_mean: 0.01581801414489746
+          seconds_per_batch_min: 0.015560626983642578
+          seconds_per_batch_std: 0.0005498731526138171
+    batch_size_8:
+      cpu_to_gpu:
+        human_readable:
+          batch_latency: "805.674 \xB5s +/- 157.254 \xB5s [773.191 \xB5s, 2.303 ms]"
+          batches_per_second: 1.26 K +/- 97.51 [434.24, 1.29 K]
+        metrics:
+          batches_per_second_max: 1293.3407338883749
+          batches_per_second_mean: 1259.5653105357776
+          batches_per_second_min: 434.23791282741485
+          batches_per_second_std: 97.51424036939879
+          seconds_per_batch_max: 0.002302885055541992
+          seconds_per_batch_mean: 0.000805673599243164
+          seconds_per_batch_min: 0.0007731914520263672
+          seconds_per_batch_std: 0.0001572538140613121
+      gpu_to_cpu:
+        human_readable:
+          batch_latency: "104.215 \xB5s +/- 12.658 \xB5s [59.605 \xB5s, 128.031 \xB5\
+            s]"
+          batches_per_second: 9.81 K +/- 1.76 K [7.81 K, 16.78 K]
+        metrics:
+          batches_per_second_max: 16777.216
+          batches_per_second_mean: 9806.840626578907
+          batches_per_second_min: 7810.621973929236
+          batches_per_second_std: 1761.6008872740726
+          seconds_per_batch_max: 0.00012803077697753906
+          seconds_per_batch_mean: 0.00010421514511108399
+          seconds_per_batch_min: 5.9604644775390625e-05
+          seconds_per_batch_std: 1.2658293070174213e-05
+      on_device_inference:
+        human_readable:
+          batch_latency: "16.623 ms +/- 759.017 \xB5s [16.301 ms, 22.584 ms]"
+          batches_per_second: 60.26 +/- 2.22 [44.28, 61.35]
+        metrics:
+          batches_per_second_max: 61.346243290283894
+          batches_per_second_mean: 60.25881046175457
+          batches_per_second_min: 44.27827629162004
+          batches_per_second_std: 2.2193085956672296
+          seconds_per_batch_max: 0.02258443832397461
+          seconds_per_batch_mean: 0.01662288188934326
+          seconds_per_batch_min: 0.01630091667175293
+          seconds_per_batch_std: 0.0007590167680596548
+      total:
+        human_readable:
+          batch_latency: "17.533 ms +/- 836.015 \xB5s [17.193 ms, 23.896 ms]"
+          batches_per_second: 57.14 +/- 2.20 [41.85, 58.16]
+        metrics:
+          batches_per_second_max: 58.16374528511205
+          batches_per_second_mean: 57.140338855126565
+          batches_per_second_min: 41.84762740950632
+          batches_per_second_std: 2.1985066663972677
+          seconds_per_batch_max: 0.023896217346191406
+          seconds_per_batch_mean: 0.01753277063369751
+          seconds_per_batch_min: 0.017192840576171875
+          seconds_per_batch_std: 0.0008360147274630088
+  ```
+</details>
+
+... Your turn
+
+## How we benchmark
+The overall flow can be summarized with the diagram shown below (best viewed on GitHub):
+```mermaid
+flowchart TB;
+    A([Start]) --> B
+    B(prepare_samples)
+    B --> C[get_machine_info]
+    C --> D[measure_params]
+    D --> E[warm_up, batch_size=1]
+    E --> F[measure_flops]
+    
+    subgraph SG[Repeat for batch_size 1 and x]
+        direction TB
+        G[measure_allocated_memory]
+        G --> H[warm_up, given batch_size]
+        H --> I[measure_detailed_inference_timing]
+        I --> J[measure_repeated_inference_timing]
+        J --> K[measure_energy]
+    end
+
+    F --> SG
+    SG --> END([End])
+```
+
+Usually, the sample and model don't reside on the same device initially (e.g., a GPU holds the model while the sample is on CPU after being loaded from disk or collected as live data). Accordingly, we measure timing in three parts: `cpu_to_gpu`, `on_device_inference`, and `gpu_to_cpu`, as well as a sum of the three, `total`. Note that the `model.device()` determines the execution device. The inference flow is shown below:
+
+```mermaid
+flowchart LR;
+    A([sample])
+    A --> B[cpu -> gpu]
+    B --> C[model __call__]
+    C --> D[gpu -> cpu]
+    D --> E([result])
+```
+
+## Advanced use
+Trying to benchmark a custom class, which is not a `torch.nn.Module`?
+You can pass custom functions to `benchmark` as seen in [this example](tests/test_custom_class.py).
+
+
+## Limitations
+- Allocated memory measurements are only available on CUDA devices.
+- Energy consumption can only be measured on NVIDIA Jetson platforms at the moment.
+- FLOPs and parameter count is not support for custom classes.
+
+
+## Acknowledgement
+This work has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement No 871449 (OpenDR).
+It was developed for benchmarking tools in [OpenDR](https://github.com/opendr-eu/opendr), a non-proprietary toolkit for deep learning based functionalities for robotics and vision.
+
+
+## Citation
+If you like the tool and use it in research, please consider citing it:
+```bibtex
+@software{hedegaard2022pytorchbenchmark,
+  author = {Hedegaard, Lukas},
+  doi = {10.5281/zenodo.7223585},
+  month = {10},
+  title = {{PyTorch-Benchmark}},
+  version = {0.3.5},
+  year = {2022}
+}
+```
---
+++ packages/pytorch-benchmark/pytorch-benchmark-0.3.6.tar.gz/pytorch-benchmark-0.3.6/pytorch_benchmark/benchmark.py
@@ -0,0 +1,448 @@
+import inspect
+from logging import getLogger
+from time import time
+from typing import Any, Callable
+
+import numpy as np
+import torch
+import yaml
+from ptflops import get_model_complexity_info
+from tqdm import tqdm
+
+from .format import format_num, format_time
+from .machine_info import get_machine_info
+
+logger = getLogger("torch-benchmark")
+_INVALID = float("nan")
+
+
+def _is_valid(val):
+    return val == val
+
+
+def get_call_arg_names(module_or_fn):
+    if isinstance(module_or_fn, torch.nn.Module):
+        return inspect.getfullargspec(module_or_fn.forward)[0][1:]
+    return inspect.getfullargspec(module_or_fn)[0]
+
+
+def measure_flops(model, sample, print_details=False):
+    flops = _INVALID
+
+    def input_constructor(*args, **kwrags):
+        nonlocal sample
+        return {get_call_arg_names(model)[0]: sample}
+
+    try:
+        flops, _ = get_model_complexity_info(
+            model,
+            (1,),  # dummy
+            print_per_layer_stat=print_details,
+            as_strings=False,
+            input_constructor=input_constructor,
+            verbose=print_details,
+        )
+        flops = int(flops)
+    except Exception as e:  # pragma: no cover
+        logger.error(f"Unable to measure model FLOPs due to error: {e}")
+
+    return flops
+
+
+def get_device(model):
+    return next(model.parameters()).device
+
+
+def measure_params(model):
+    num_params = _INVALID
+
+    try:
+        num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    except AttributeError as e:
+        logger.error(f"Unable to measure model params due to error: {e}")
+
+    return num_params
+
+
+def measure_allocated_memory(
+    model,
+    sample,
+    model_device,
+    transfer_to_device_fn=torch.Tensor.to,
+    print_details=False,
+):
+    assert model_device.type == "cuda"
+
+    torch.cuda.reset_peak_memory_stats(device=model_device)
+    pre_mem = torch.cuda.memory_allocated(device=model_device)
+
+    transfer_to_device_fn(
+        model(transfer_to_device_fn(sample, model_device)),
+        "cpu",
+    )
+
+    if print_details:
+        logger.info(torch.cuda.memory_summary(device=model_device, abbreviated=True))
+
+    post_mem = torch.cuda.memory_allocated(device=model_device)
+    max_mem = torch.cuda.max_memory_allocated(device=model_device)
+
+    return pre_mem, post_mem, max_mem
+
+
+def warm_up(
+    model,
+    sample,
+    model_device,
+    transfer_to_device_fn=torch.Tensor.to,
+    num_runs=10,
+    batch_size: int = None,
+):
+    for _ in tqdm(range(num_runs), desc=f"Warming up with batch_size={batch_size}"):
+        transfer_to_device_fn(
+            model(transfer_to_device_fn(sample, model_device)),
+            "cpu",
+        )
+
+
+def measure_detailed_inference_timing(
+    model, sample, model_device, transfer_to_device_fn=torch.Tensor.to
+):
+    try:
+        with torch.autograd.profiler.profile(
+            use_cuda=(model_device.type == "cuda"), profile_memory=True
+        ) as prof:
+            transfer_to_device_fn(
+                model(transfer_to_device_fn(sample, model_device)),
+                "cpu",
+            )
+
+        detailed_timing = prof.key_averages().table(sort_by="self_cpu_time_total")
+        logger.info(detailed_timing)
+
+    except Exception as e:
+        logger.error(
+            f"Caught exception while attempting to measure detailed model inference: {e}"
+        )
+
+
+def measure_repeated_inference_timing(
+    model,
+    sample,
+    model_device,
+    transfer_to_device_fn=torch.Tensor.to,
+    num_runs=100,
+    batch_size: int = None,
+):
+    t_c2d = []
+    t_inf = []
+    t_d2c = []
+    t_tot = []
+
+    for _ in tqdm(
+        range(num_runs), desc=f"Measuring inference for batch_size={batch_size}"
+    ):
+        start_on_cpu = time()
+        device_sample = transfer_to_device_fn(sample, model_device)
+
+        if model_device.type == "cuda":
+            start_event = torch.cuda.Event(enable_timing=True)
+            stop_event = torch.cuda.Event(enable_timing=True)
+            start_event.record()  # For GPU timing
+        start_on_device = time()  # For CPU timing
+
+        device_result = model(device_sample)
+
+        if model_device.type == "cuda":
+            stop_event.record()
+            torch.cuda.synchronize()
+            elapsed_on_device = stop_event.elapsed_time(start_event)
+            stop_on_device = time()
+        else:
+            stop_on_device = time()
+            elapsed_on_device = stop_on_device - start_on_device
+
+        transfer_to_device_fn(device_result, "cpu")
+        stop_on_cpu = time()
+
+        t_c2d.append(start_on_device - start_on_cpu)
+        t_inf.append(elapsed_on_device)
+        t_d2c.append(stop_on_cpu - stop_on_device)
+        t_tot.append(stop_on_cpu - start_on_cpu)
+
+    results_dict = {}
+
+    times_and_titles = [(t_inf, "on_device_inference")]
+    if model_device.type == "cuda":
+        times_and_titles.extend(
+            [
+                (t_c2d, "cpu_to_gpu"),
+                (t_d2c, "gpu_to_cpu"),
+                (t_tot, "total"),
+            ]
+        )
+
+    for s_per_batch, title in times_and_titles:
+        s_per_batch = np.array(s_per_batch)
+        batches_per_s = 1 / s_per_batch
+
+        metrics = {
+            "batches_per_second_mean": float(batches_per_s.mean()),
+            "batches_per_second_std": float(batches_per_s.std()),
+            "batches_per_second_min": float(batches_per_s.min()),
+            "batches_per_second_max": float(batches_per_s.max()),
+            "seconds_per_batch_mean": float(s_per_batch.mean()),
+            "seconds_per_batch_std": float(s_per_batch.std()),
+            "seconds_per_batch_min": float(s_per_batch.min()),
+            "seconds_per_batch_max": float(s_per_batch.max()),
+        }
+
+        human_readable = {
+            "batches_per_second": f"{format_num(batches_per_s.mean())} +/- {format_num(batches_per_s.std())} [{format_num(batches_per_s.min())}, {format_num(batches_per_s.max())}]",
+            "batch_latency": f"{format_time(s_per_batch.mean())} +/- {format_time(s_per_batch.std())} [{format_time(s_per_batch.min())}, {format_time(s_per_batch.max())}]",
+        }
+
+        results_dict[title] = {"metrics": metrics, "human_readable": human_readable}
+
+    return results_dict
+
+
+def measure_energy(
+    model,
+    sample,
+    model_device,
+    transfer_to_device_fn=torch.Tensor.to,
+    num_runs=100,
+    batch_size: int = None,
+    include_transfer_costs=True,
+    print_fn=logger.info,
+):
+    inference_joules = _INVALID
+
+    def test_with_transfer():
+        nonlocal model, sample
+        transfer_to_device_fn(
+            model(transfer_to_device_fn(sample, model_device)),
+            "cpu",
+        )
+
+    def test_without_transfer():
+        nonlocal model, sample
+        model(sample)
+
+    if include_transfer_costs:
+        test_fn = test_with_transfer
+    else:
+        test_fn = test_without_transfer
+        sample = sample.to(model_device)
+
+    # # Try carbon-tracker: The library is still too young
+    # try:
+    #     from carbontracker import tracker
+
+    #     # Check if components are available (TODO: find a less brittle implementation for this)
+    #     pids = tracker.CarbonTracker._get_pids(None)
+    #     components = tracker.component.create_components(
+    #         components="all", pids=pids, devices_by_pid=False
+    #     )
+    #     if not any([cmp for cmp in components if cmp.available()]):
+    #         raise Exception("Valid CarbonTracker device not available")
+
+    #     tracker = tracker.CarbonTracker(epochs=1, verbose=print_details)
+    #     tracker.epoch_start()
+    #     test_fn()
+    #     tracker.epoch_end()
+
+    #     # Grab results from logger
+
+    # except Exception:
+    #     pass
+
+    # Try jetson power
+    try:
+        from .jetson_power import PowerEstimator
+
+        p_est = PowerEstimator(print_fn=print_fn)
+        # index 0 is total energy, index 1 is energy over idle consumption:
+        meas = []
+        for _ in tqdm(
+            range(num_runs), desc=f"Measuring energy for batch_size={batch_size}"
+        ):
+            meas.append(p_est.estimate_fn_power(test_fn)[0] / 1000)
+        inference_joules = float(np.array(meas).mean())
+    except Exception:
+        pass
+
+    if not _is_valid(inference_joules):
+        logger.error(
+            "Unable to measure energy consumption. Device must be a NVIDIA Jetson."
+        )
+
+    return inference_joules
+
+
+def fmt(d: dict):
+    return yaml.dump(d)
+
+
+def benchmark(
+    model: torch.nn.Module,
+    sample: torch.Tensor,
+    num_runs: int = 100,
+    print_details=False,
+    get_device_fn: Callable[[Any], torch.device] = get_device,
+    transfer_to_device_fn=torch.Tensor.to,
+    sample_with_batch_size1: Any = None,
+    batch_size: int = None,
+    print_fn=logger.info,
+    warm_up_fn=warm_up,
+):
+    results = {}
+    batch_size = batch_size or sample.shape[0]
+
+    sample = transfer_to_device_fn(sample, "cpu")
+
+    # Prepare sample with batch size 1
+    if sample_with_batch_size1:
+        sample1 = sample_with_batch_size1
+    else:
+        sample1_shape = (1, *sample.shape[1:])
+        sample1 = torch.randn(sample1_shape)
+
+    prevously_training = getattr(model, "training", False)
+    if hasattr(model, "eval"):
+        model.eval()
+
+    # Get machine info
+    machine_info = get_machine_info()
+    results["machine_info"] = machine_info
+    print_fn(fmt({"Machine info": machine_info}))
+
+    model_device = get_device_fn(model)
+    assert isinstance(
+        model_device, torch.device
+    ), "model_device should be a `torch.device`"
+    results["device"] = model_device.type
+    print_fn(f"Model device: {model_device}")
+
+    # Measure params
+    params = measure_params(model)
+    if _is_valid(params):
+        results["params"] = params
+        print_fn(f"Model parameters: {params} ({format_num(params)})")
+
+    # Measure FLOPs
+    warm_up_fn(
+        model,
+        sample1,
+        model_device,
+        transfer_to_device_fn,
+        num_runs=1,
+        batch_size=1,
+    )
+
+    with torch.no_grad():
+        flops = measure_flops(
+            model, transfer_to_device_fn(sample1, model_device), print_details
+        )
+
+    if _is_valid(flops):
+        results["flops"] = flops
+        print_fn(f"Model FLOPs: {flops} ({format_num(flops)})")
+
+    # Measure inference timing
+    memory = {}
+    timing = {}
+    energy = {}
+    with torch.no_grad():
+        for bs in sorted(set([1, batch_size])):
+            s = sample1 if bs == 1 else sample
+
+            # Measure Allocated Memory
+            if model_device.type == "cuda":
+                pre_mem, post_mem, max_mem = measure_allocated_memory(
+                    model, s, model_device, transfer_to_device_fn, print_details
+                )
+                memory[f"batch_size_{bs}"] = {
+                    "pre_inference_bytes": pre_mem,
+                    "max_inference_bytes": max_mem,
+                    "post_inference_bytes": post_mem,
+                    "pre_inference": format_num(pre_mem, bytes=True),
+                    "max_inference": format_num(max_mem, bytes=True),
+                    "post_inference": format_num(post_mem, bytes=True),
+                }
+                print_fn(
+                    fmt(
+                        {
+                            f"Memory results (batch_size={bs})": memory[
+                                f"batch_size_{bs}"
+                            ]
+                        }
+                    )
+                )
+            else:
+                logger.warning(
+                    "Measurement of allocated memory is only available on CUDA devices"
+                )
+
+            # Inference timing
+            warm_up_fn(
+                model,
+                s,
+                model_device,
+                transfer_to_device_fn,
+                num_runs=max(1, num_runs // 10),
+                batch_size=bs,
+            )
+            if print_details:
+                measure_detailed_inference_timing(model, s, model_device)
+
+            timing[f"batch_size_{bs}"] = measure_repeated_inference_timing(
+                model,
+                s,
+                model_device,
+                transfer_to_device_fn,
+                num_runs,
+                bs,
+            )
+            print_fn(
+                fmt({f"Timing results (batch_size={bs})": timing[f"batch_size_{bs}"]})
+            )
+
+            # Energy measurement
+            energy_joules = measure_energy(
+                model,
+                s,
+                model_device,
+                transfer_to_device_fn,
+                num_runs=max(1, num_runs // 10),
+                batch_size=bs,
+                include_transfer_costs=True,
+                print_fn=print_fn,
+            )
+            if _is_valid(energy_joules):
+                energy_kwh = energy_joules / 3.6e6
+                energy[f"batch_size_{bs}"] = {
+                    "joules": energy_joules,
+                    "kWh": energy_kwh,
+                }
+                print_fn(
+                    fmt(
+                        {
+                            f"Energy results (batch_size={bs})": energy[
+                                f"batch_size_{bs}"
+                            ]
+                        }
+                    )
+                )
+
+    results["timing"] = timing
+    if memory:
+        results["memory"] = memory
+    if energy:
+        results["energy"] = energy
+
+    if prevously_training:
+        model.train()
+
+    return results
