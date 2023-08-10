86560f97799368c19a068bd477fa3b3bbc2a41f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:34:18 +0000

    Add package myhlcb/0.1.0/myhlcb-0.1.0.tar.gz

---
+++ packages/myhlcb/myhlcb-0.1.0.tar.gz/myhlcb-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myhlcb",
+    version="0.1.0",
+    description="An example package",
+    author="myhlcb",
+    author_email="osimple23@email.com",
+    packages=find_packages(),
+)
