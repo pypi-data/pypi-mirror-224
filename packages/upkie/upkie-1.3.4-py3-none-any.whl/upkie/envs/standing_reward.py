5baf050bff80bdd26f32a84cf0e53d6186344b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:34:11 +0000

    Add package upkie/1.3.4/upkie-1.3.4-py3-none-any.whl

---
+++ packages/upkie/upkie-1.3.4-py3-none-any.whl/upkie/envs/standing_reward.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Inria
+
+from typing import Tuple
+
+import numpy as np
+
+
+class StandingReward:
+
+    """!
+    Reward function for balancing in place.
+
+    This class has the following attributes:
+
+    - ``lookahead_duration``: Length of the receding horizon, used to compute
+      an internal divergent component of motion.
+    - ``max_pitch``: Maximum pitch angle we expect to observe, in [rad].
+    - ``max_position``: Maximum ground position we expect to observe, in [m].
+    - ``pitch_weight``: Weight of the pitch objective in the reward.
+    - ``position_weight``: Weight of the position objective in the reward.
+    """
+
+    lookahead_duration: float
+    max_pitch: float
+    max_position: float
+    pitch_weight: float
+    position_weight: float
+
+    @staticmethod
+    def get_range() -> Tuple[float, float]:
+        """!
+        Get range of the reward.
+
+        This is part of the Gym API.
+        """
+        return (-float("inf"), 1.0)
+
+    def __init__(
+        self,
+        lookahead_duration: float = 0.2,
+        max_pitch: float = 1.5707963267948966,
+        max_position: float = 0.5,
+        pitch_weight: float = 1.0,
+        position_weight: float = 1.0,
+    ):
+        """!
+        Initialize reward.
+
+        @param lookahead_duration Length of the receding horizon, used to
+            compute an internal divergent component of motion.
+        @param max_pitch Maximum pitch angle for standardization, in [rad].
+        @param max_position Large ground position for standardization, in [m].
+        @param pitch_weight Weight of the pitch objective in the reward.
+        @param position_weight Weight of the position objective in the reward.
+        """
+        self.lookahead_duration = lookahead_duration
+        self.max_pitch = max_pitch
+        self.max_position = max_position
+        self.pitch_weight = pitch_weight
+        self.position_weight = position_weight
+
+    def get(self, observation: np.ndarray) -> float:
+        """!
+        Get reward corresponding to an observation.
+
+        @param observation Observation to compute reward from.
+        @returns Reward.
+        """
+        pitch = observation[0]
+        ground_position = observation[1]
+        ground_velocity = observation[2]
+        angular_velocity = observation[3]
+
+        T = self.lookahead_duration
+        lookahead_pitch = pitch + T * angular_velocity
+        lookahead_position = ground_position + T * ground_velocity
+        normalized_lookahead_pitch = lookahead_pitch / self.max_pitch
+        normalized_lookahead_position = lookahead_position / self.max_position
+        return (
+            1.0
+            - self.pitch_weight * abs(normalized_lookahead_pitch)
+            - self.position_weight * abs(normalized_lookahead_position)
+        )
---
+++ packages/upkie/upkie-1.3.4-py3-none-any.whl/upkie/envs/tests/upkie_servos_env_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Inria
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for UpkieServosEnv."""
+
+import unittest
+
+import posix_ipc
+
+from envs import UpkieServosEnv
+
+from envs.tests.mock_spine import MockSpine
+
+
+class TestUpkieServosEnv(unittest.TestCase):
+    def setUp(self):
+        shm_name = "/vroum"
+        shared_memory = posix_ipc.SharedMemory(
+            shm_name, posix_ipc.O_RDWR | posix_ipc.O_CREAT, size=42
+        )
+        self.env = UpkieServosEnv(
+            fall_pitch=1.0,
+            frequency=100.0,
+            shm_name=shm_name,
+        )
+        shared_memory.close_fd()
+        self.env._spine = MockSpine()
+
+    def test_reset(self):
+        observation, info = self.env.reset()
+        observation_dict = info["observation"]
+        self.assertAlmostEqual(
+            observation[1], observation_dict["wheel_odometry"]["position"]
+        )
+        self.assertGreaterEqual(observation_dict["number"], 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
