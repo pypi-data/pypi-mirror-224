2ecdcc4f843ae095c13cdc5e3852c9dacb6c55fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:07:59 +0000

    Add package targer-api/1.1.1/targer_api-1.1.1-py3-none-any.whl

---
+++ packages/targer-api/targer_api-1.1.1-py3-none-any.whl/targer_api/model.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Dict
+
+
+class ArgumentLabel(Enum):
+    C_B = "C-B"
+    C_I = "C-I"
+    MC_B = "MC-B"
+    MC_I = "MC-I"
+    P_B = "P-B"
+    P_I = "P-I"
+    MP_B = "MP-B"
+    MP_I = "MP-I"
+    O = "O"  # noqa: E741
+    B_B = "B-B"
+    B_I = "B-I"
+    X = "-X-"
+
+
+@dataclass
+class ArgumentTag:
+    label: ArgumentLabel
+    probability: float
+    token: str
+
+
+ArgumentSentence = List[ArgumentTag]
+
+ArgumentSentences = List[ArgumentSentence]
+
+ArgumentModelSentences = Dict[str, ArgumentSentences]
