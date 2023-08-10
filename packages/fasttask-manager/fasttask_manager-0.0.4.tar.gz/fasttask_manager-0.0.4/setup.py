82cdf88d29fd4b6f3de3bce41b8e6f8265637f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:59 +0000

    Add package fasttask-manager/0.0.4/fasttask_manager-0.0.4.tar.gz

---
+++ packages/fasttask-manager/fasttask_manager-0.0.4.tar.gz/fasttask_manager-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fasttask_manager",
+    version="0.0.4",
+    author="Irid",
+    author_email="irid.zzy@gmail.com",
+    description="fasttask's manager ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iridesc/fasttask_manager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "retry",
+        "requests"
+    ],
+)
