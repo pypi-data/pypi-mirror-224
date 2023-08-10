2e7776c3bb50ebe52af903addf5e0361cb9ae5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:37 +0000

    Add package langkit/0.0.14/langkit-0.0.14-py3-none-any.whl

---
+++ packages/langkit/langkit-0.0.14-py3-none-any.whl/langkit/__init__.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass, field
+
+import importlib.resources as resources
+
+
+def _resource_filename(file_name):
+    with resources.path(__package__, file_name) as path:
+        return str(path)
+
+
+@dataclass
+class LangKitConfig:
+    pattern_file_path: str = field(
+        default_factory=lambda: _resource_filename("pattern_groups.json")
+    )
+    theme_file_path: str = field(
+        default_factory=lambda: _resource_filename("themes.json")
+    )
+    transformer_name: str = "sentence-transformers/all-MiniLM-L6-v2"
+    prompt_column: str = "prompt"
+    response_column: str = "response"
+    topics: list = field(
+        default_factory=lambda: [
+            "law",
+            "finance",
+            "medical",
+            "education",
+            "politics",
+            "support",
+        ]
+    )
+    nlp_scores: list = field(
+        default_factory=lambda: [
+            "bleu",
+            "rouge",
+            "meteor",
+        ]
+    )
+    reference_corpus: str = ""
+
+
+def package_version(package: str = __package__) -> str:
+    """Calculate version number based on pyproject.toml"""
+    try:
+        from importlib import metadata
+
+        version = metadata.version(package)
+    except metadata.PackageNotFoundError:
+        version = f"{package} is not installed."
+
+    return version
+
+
+__version__ = package_version()
+
+__ALL__ = [__version__, LangKitConfig]
---
+++ packages/langkit/langkit-0.0.14-py3-none-any.whl/langkit/regexes.py
@@ -0,0 +1,100 @@
+import json
+import re
+from logging import getLogger
+
+from whylogs.experimental.core.udf_schema import register_dataset_udf
+from . import LangKitConfig
+from whylogs.core.metrics.metrics import FrequentItemsMetric
+from whylogs.core.resolvers import MetricSpec
+from whylogs.core.stubs import pd
+from typing import Optional
+
+diagnostic_logger = getLogger(__name__)
+
+
+class PatternLoader:
+    def __init__(self):
+        self.config: LangKitConfig = LangKitConfig()
+        self.regex_groups = self.load_patterns()
+
+    def load_patterns(self):
+        json_path = self.config.pattern_file_path
+        try:
+            skip = False
+            with open(json_path, "r") as myfile:
+                _REGEX_GROUPS = json.load(myfile)
+            regex_groups = []
+            for group in _REGEX_GROUPS:
+                compiled_expressions = []
+                for expression in group["expressions"]:
+                    compiled_expressions.append(re.compile(expression))
+
+                regex_groups.append(
+                    {"name": group["name"], "expressions": compiled_expressions}
+                )
+                diagnostic_logger.info(f"Loaded regex pattern for {group['name']}")
+        except FileNotFoundError:
+            skip = True
+            diagnostic_logger.warning(f"Could not find {json_path}")
+        except json.decoder.JSONDecodeError as json_error:
+            skip = True
+            diagnostic_logger.warning(f"Could not parse {json_path}: {json_error}")
+        if not skip:
+            return regex_groups
+        return None
+
+    def set_config(self, config: LangKitConfig):
+        self.config = config
+
+    def update_patterns(self):
+        self.regex_groups = self.load_patterns()
+
+    def get_regex_groups(self):
+        return self.regex_groups
+
+
+pattern_loader = PatternLoader()
+
+
+def has_patterns(text):
+    regex_groups = pattern_loader.get_regex_groups()
+    result = []
+    if regex_groups:
+        index = (
+            text.columns[0] if isinstance(text, pd.DataFrame) else list(text.keys())[0]
+        )
+        for input in text[index]:
+            matched = None
+            for group in regex_groups:
+                for expression in group["expressions"]:
+                    if expression.search(input):
+                        matched = matched or group["name"]
+                        break
+                if matched is not None:
+                    break
+
+            result.append(matched)
+
+    return result
+
+
+def init(
+    pattern_file_path: Optional[str] = None, lang_config: Optional[LangKitConfig] = None
+):
+    if lang_config is None:
+        lang_config = LangKitConfig()
+    if pattern_file_path:
+        lang_config.pattern_file_path = pattern_file_path
+        pattern_loader.set_config(lang_config)
+        pattern_loader.update_patterns()
+    else:
+        pattern_loader.set_config(lang_config)
+        pattern_loader.update_patterns()
+
+    if pattern_loader.get_regex_groups() is not None:
+        for column in [lang_config.prompt_column, lang_config.response_column]:
+            register_dataset_udf(
+                [column],
+                udf_name=f"{column}.has_patterns",
+                metrics=[MetricSpec(FrequentItemsMetric)],
+            )(has_patterns)
