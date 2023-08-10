3669a4cbe4657f339932cdaf1bdabc1b59f16efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:11:33 +0000

    Add package open-interpreter/0.0.287/open_interpreter-0.0.287.tar.gz

---
+++ packages/open-interpreter/open_interpreter-0.0.287.tar.gz/open_interpreter-0.0.287/interpreter/cli.py
@@ -0,0 +1,32 @@
+import argparse
+
+def cli(interpreter):
+  """
+  Takes an instance of interpreter.
+  Modifies it according to command line flags, then runs chat.
+  """
+
+  parser = argparse.ArgumentParser(description='Chat with Open Interpreter.')
+  parser.add_argument('-y',
+                      '--yes',
+                      action='store_true',
+                      help='execute code without user confirmation')
+  parser.add_argument('-f',
+                      '--fast',
+                      action='store_true',
+                      help='use gpt-3.5-turbo instead of gpt-4')
+  parser.add_argument('-l',
+                      '--local',
+                      action='store_true',
+                      help='run fully local with llama-2')
+  args = parser.parse_args()
+
+  if args.yes:
+    interpreter.auto_run = True
+  if args.local:
+    interpreter.local = True
+  if args.fast:
+    interpreter.model = "gpt-3.5-turbo-0613"
+
+  # Now run the chat method
+  interpreter.chat()
