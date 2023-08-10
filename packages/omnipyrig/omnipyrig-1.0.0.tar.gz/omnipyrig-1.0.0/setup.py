2fad12d3b369d5835f666df1434b899b045f6c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:08:47 +0000

    Add package omnipyrig/1.0.0/omnipyrig-1.0.0.tar.gz

---
+++ packages/omnipyrig/omnipyrig-1.0.0.tar.gz/omnipyrig-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A python wrapper for Omni-Rig'
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="omnipyrig",
+    version=VERSION,
+    author="4Z1KD (Gil)",
+    author_email="<4z1kd@iarc.org>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pywin32'],
+    keywords=['python', 'amateur radio', 'ham', 'omni-rig', 'omnirig', 'radio'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
