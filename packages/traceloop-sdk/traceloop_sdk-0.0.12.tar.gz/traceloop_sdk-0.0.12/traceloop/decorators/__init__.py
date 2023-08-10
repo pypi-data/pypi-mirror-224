82fa6850e7e334a5ebf5106ea0d18ba011fd3a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:09:05 +0000

    Add package traceloop-sdk/0.0.12/traceloop_sdk-0.0.12.tar.gz

---
+++ packages/traceloop-sdk/traceloop_sdk-0.0.12.tar.gz/traceloop_sdk-0.0.12/traceloop/decorators/__init__.py
@@ -0,0 +1,46 @@
+from functools import wraps
+from typing import Optional
+
+from traceloop.semconv import SpanAttributes, TraceloopSpanKindValues
+from traceloop.tracing.tracer import Tracer
+
+
+def task(name: Optional[str] = None, tlp_span_kind: Optional[TraceloopSpanKindValues] = TraceloopSpanKindValues.TASK):
+    def decorate(fn):
+        @wraps(fn)
+        def wrap(*args, **kwargs):
+            span_name = f"{name}.{tlp_span_kind.value}" if name else f"{fn.__name__}.{tlp_span_kind.value}"
+            with Tracer.instance().start_as_current_span(span_name) as span:
+                span.set_attribute(SpanAttributes.TRACELOOP_SPAN_KIND, tlp_span_kind.value)
+                span.set_attribute(SpanAttributes.TRACELOOP_ENTITY_NAME, name)
+                return fn(*args, **kwargs)
+
+        return wrap
+
+    return decorate
+
+
+def workflow(name: Optional[str] = None, correlation_id: Optional[str] = None):
+    def decorate(fn):
+        @wraps(fn)
+        def wrap(*args, **kwargs):
+            span_name = f"{name}.workflow" if name else f"{fn.__name__}.workflow"
+            with Tracer.instance().start_as_current_span(span_name) as span:
+                span.set_attribute(SpanAttributes.TRACELOOP_SPAN_KIND, TraceloopSpanKindValues.WORKFLOW.value)
+                span.set_attribute(SpanAttributes.TRACELOOP_ENTITY_NAME, name)
+
+                if correlation_id:
+                    span.set_attribute(SpanAttributes.TRACELOOP_CORRELATION_ID, correlation_id)
+                return fn(*args, **kwargs)
+
+        return wrap
+
+    return decorate
+
+
+def agent(name: Optional[str] = None):
+    return task(name=name, tlp_span_kind=TraceloopSpanKindValues.AGENT)
+
+
+def tool(name: Optional[str] = None):
+    return task(name=name, tlp_span_kind=TraceloopSpanKindValues.TOOL)
