1b3a81268149e505ea5d927b11c24e7e4023cb43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:28 +0000

    Add package Exegol/4.2.5/Exegol-4.2.5.tar.gz

---
+++ packages/Exegol/Exegol-4.2.5.tar.gz/Exegol-4.2.5/exegol-docker-build/osint.dockerfile
@@ -0,0 +1,34 @@
+# Author: The Exegol Project
+
+FROM debian:11-slim
+
+ARG TAG="local"
+ARG VERSION="local"
+ARG BUILD_DATE="n/a"
+
+LABEL org.exegol.tag="${TAG}"
+LABEL org.exegol.version="${VERSION}"
+LABEL org.exegol.build_date="${BUILD_DATE}"
+LABEL org.exegol.app="Exegol"
+LABEL org.exegol.src_repository="https://github.com/ThePorgs/Exegol-images"
+
+COPY sources /root/sources/
+
+WORKDIR /root/sources/install
+
+# WARNING: package_most_used can't be used with other functions other than: package_base, post_install
+# ./entrypoint.sh package_most_used
+
+RUN echo "${TAG}-${VERSION}" > /opt/.exegol_version && \
+    chmod +x entrypoint.sh && \
+    ./entrypoint.sh package_base && \
+    ./entrypoint.sh package_misc && \
+    ./entrypoint.sh package_misc_configure && \
+    ./entrypoint.sh package_osint && \
+    ./entrypoint.sh package_osint_configure && \
+    ./entrypoint.sh post_install && \
+    rm -rf /root/sources /var/lib/apt/lists/*
+
+WORKDIR /workspace
+
+ENTRYPOINT ["/.exegol/entrypoint.sh"]
---
+++ packages/Exegol/Exegol-4.2.5.tar.gz/Exegol-4.2.5/exegol/model/ExegolContainerTemplate.py
@@ -0,0 +1,46 @@
+import os
+from typing import Optional
+
+from rich.prompt import Prompt
+
+from exegol.config.EnvInfo import EnvInfo
+from exegol.model.ContainerConfig import ContainerConfig
+from exegol.model.ExegolImage import ExegolImage
+
+
+class ExegolContainerTemplate:
+    """Exegol template class used to create a new container"""
+
+    def __init__(self, name: Optional[str], config: ContainerConfig, image: ExegolImage, hostname: Optional[str] = None):
+        if name is None:
+            name = Prompt.ask("[bold blue][?][/bold blue] Enter the name of your new exegol container", default="default")
+        assert name is not None
+        if (EnvInfo.isWindowsHost() or EnvInfo.isMacHost()) and not name.startswith("exegol-"):
+            # Force container as lowercase because the filesystem of windows / mac are case-insensitive => https://github.com/ThePorgs/Exegol/issues/167
+            name = name.lower()
+        self.container_name: str = name if name.startswith("exegol-") else f'exegol-{name}'
+        self.name: str = name.replace('exegol-', '')
+        if hostname:
+            self.hostname: str = hostname
+        else:
+            self.hostname = self.container_name
+        self.image: ExegolImage = image
+        self.config: ContainerConfig = config
+
+    def __str__(self):
+        """Default object text formatter, debug only"""
+        return f"{self.name} - {self.image.getName()}{os.linesep}{self.config}"
+
+    def prepare(self):
+        """Prepare the model before creating the docker container"""
+        self.config.prepareShare(self.name)
+
+    def rollback(self):
+        """Rollback change in case of container creation fail."""
+        self.config.rollback_preparation(self.name)
+
+    def getDisplayName(self) -> str:
+        """Getter of the container's name for TUI purpose"""
+        if self.container_name != self.hostname:
+            return f"{self.name} [bright_black]({self.hostname})[/bright_black]"
+        return self.name
