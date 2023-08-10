c1d4a6aa5bf139b4d43a43537e8bd5df41c59aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:57 +0000

    Add package promptogen/0.0.2/promptogen-0.0.2.tar.gz

---
+++ packages/promptogen/promptogen-0.0.2.tar.gz/promptogen-0.0.2/promptogen/prompt_collection/prompts/__init__.py
@@ -0,0 +1,17 @@
+from .prompt_creator import PromptCreatorPrompt
+from .prompt_example_creator import ExampleCreatorPrompt
+from .prompt_optimizer import PromptOptimizerPrompt
+from .python_code_generator import PythonCodeGeneratorPrompt
+from .text_categorizer import TextCategorizerPrompt
+from .text_condenser import TextCondenserPrompt
+from .text_summarizer import TextSummarizerPrompt
+
+__all__ = [
+    "PromptCreatorPrompt",
+    "ExampleCreatorPrompt",
+    "PromptOptimizerPrompt",
+    "PythonCodeGeneratorPrompt",
+    "TextCategorizerPrompt",
+    "TextSummarizerPrompt",
+    "TextCondenserPrompt",
+]
