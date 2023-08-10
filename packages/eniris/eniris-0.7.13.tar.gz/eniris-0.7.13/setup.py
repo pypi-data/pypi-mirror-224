d9d199c163f6b194b1cb0a2088a8fbeb24b40508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:35:50 +0000

    Add package eniris/0.7.13/eniris-0.7.13.tar.gz

---
+++ packages/eniris/eniris-0.7.13.tar.gz/eniris-0.7.13/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+    
+setup(
+  name = 'eniris',
+  packages = find_packages(),
+  package_data={
+      'eniris': ['py.typed'],
+  },
+  version = '0.7.13',
+  description = 'Eniris API driver for Python',
+  license='MIT',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Enris BV',
+  author_email = 'info@eniris.be',
+  url = 'https://github.com/eniris-international/eniris-api-python-driver',
+  download_url = 'https://github.com/eniris-international/eniris-api-python-driver/archive/refs/tags/v0.7.13.tar.gz',
+  keywords = ['eniris', 'api', 'rest'],   # Keywords that define your package best
+  install_requires=[
+    'requests'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
