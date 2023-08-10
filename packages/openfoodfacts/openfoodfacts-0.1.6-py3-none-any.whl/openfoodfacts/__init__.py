8de8637fe58ebaa5e94fc20d896519af701d0cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:35:56 +0000

    Add package openfoodfacts/0.1.6/openfoodfacts-0.1.6-py3-none-any.whl

---
+++ packages/openfoodfacts/openfoodfacts-0.1.6-py3-none-any.whl/openfoodfacts/__init__.py
@@ -0,0 +1,19 @@
+from .api import API
+from .dataset import ProductDataset, get_dataset
+from .ocr import OCRResult
+from .types import APIConfig, APIVersion, Country, DatasetType, Environment, Flavor
+
+__all__ = [
+    "API",
+    "APIConfig",
+    "APIVersion",
+    "Country",
+    "DatasetType",
+    "Flavor",
+    "Environment",
+    "OCRResult",
+    "ProductDataset",
+    "get_dataset",
+]
+
+__version__ = "0.1.6"
