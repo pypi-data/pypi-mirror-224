1f8252b80337f1870dc25cb8832844451b45450a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:45 +0000

    Add package python-urbandict/0.3.1/python_urbandict-0.3.1.tar.gz

---
+++ packages/python-urbandict/python_urbandict-0.3.1.tar.gz/python_urbandict-0.3.1/pyurbandict/parse.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import requests
+
+
+@dataclass
+class Definition:
+    word: str
+    definition: str
+    example: str
+    author: str
+    thumbs_up: int
+    thumbs_down: int
+    sound_urls: list[str]
+    written_on: str
+    permalink: str
+    defid: int
+    current_vote: str
+
+
+class UrbanDict:
+    def __init__(self, word: Optional[str] = None) -> None:
+        self.url = f"https://api.urbandictionary.com/v0/define?term={word}"
+        if not word:
+            self.url = "https://api.urbandictionary.com/v0/random"
+
+        self.searched = False
+        self.definitions = None
+
+    def search(self) -> dict:
+        """Returns a dictionary with JSON data from urbandictionary."""
+
+        data = requests.get(self.url).json()
+        self.searched = True
+
+        # Parse each listing and pass it
+        # to the Definition dataclass.
+        definitions = [Definition(**item) for item in data["list"]]
+        self.definitions = definitions
+        return definitions
