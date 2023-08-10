9a022320276b83729fe0068107d06672a6316f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:35:53 +0000

    Add package verysimplemodulevis/0.0.3/verysimplemodulevis-0.0.3.tar.gz

---
+++ packages/verysimplemodulevis/verysimplemodulevis-0.0.3.tar.gz/verysimplemodulevis-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Meu primeiro projeto em Python'
+LONG_DESCRIPTION = 'Esta é a descrição longa do meu projeto.'
+
+# Configurando o setup
+setup(
+    name="verysimplemodulevis",
+    version=VERSION,
+    author="Victor Macedo",
+    author_email="victormacedocarvalho09@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=['verysimplemodulevis'],  # Adicione "verysimplemodule" à lista de pacotes
+    install_requires=[],
+)
