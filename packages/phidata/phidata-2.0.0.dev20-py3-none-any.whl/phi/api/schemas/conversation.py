b73608294f8ffe29d1bf352bac0401c0c9f387f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:26 +0000

    Add package phidata/2.0.0.dev20/phidata-2.0.0.dev20-py3-none-any.whl

---
+++ packages/phidata/phidata-2.0.0.dev20-py3-none-any.whl/phi/api/schemas/conversation.py
@@ -0,0 +1,23 @@
+from typing import Optional, Dict, Any
+
+from pydantic import BaseModel
+
+
+class ConversationWorkspace(BaseModel):
+    id_workspace: int
+    ws_hash: Optional[str] = None
+
+
+class ConversationEventCreate(BaseModel):
+    """Data sent to API to create a new conversation event"""
+
+    id_user: Optional[int] = None
+    conversation_key: str
+    conversation_data: Optional[Dict[str, Any]] = None
+    event_type: str
+    event_data: Optional[Dict[str, Any]] = None
+
+
+class ConversationEventCreateResopnse(BaseModel):
+    id_event: Optional[int] = None
+    id_conversation: Optional[int] = None
