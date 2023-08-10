981c0020e1e51c7ab3f1a722636039eab1b6e95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:07:24 +0000

    Add package simpar-cli/0.0.12/simpar_cli-0.0.12-py3-none-any.whl

---
+++ packages/simpar-cli/simpar_cli-0.0.12-py3-none-any.whl/simpar_cli/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import argparse
+from . import main
+
+
+	
+parser = argparse.ArgumentParser(description='This is CLI for simple paragraph recoginitive based on morphological operator')
+		
+parser.add_argument('--image', type=str, help="""Entrer l\'image de que tu souhaite reconnaitre""")
+
+parser.add_argument('--image_reco', type=str, help="""enter le nom de l\'image une fois reconnu""")
+		
+args = parser.parse_args()
+
+if __name__ == '__main__':
+
+	# parser = argparse.ArgumentParser(description='This is CLI for simple paragraph recoginitive based on morphological operator')
+		
+	# parser.add_argument('--image', type=str, help="""Entrer l\'image de que tu souhaite reconnaitre""")
+
+	# parser.add_argument('--image_reco', type=str, help="""enter le nom de l\'image une fois reconnu""")
+		
+	# args = parser.parse_args()
+	
+	main(args.image, args.image_reco)
