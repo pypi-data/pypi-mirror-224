cdd9eb1efc36f5eae37966697bdeea68830c0bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:11:21 +0000

    Add package bliss-toolkit/0.3/bliss_toolkit-0.3.tar.gz

---
+++ packages/bliss-toolkit/bliss_toolkit-0.3.tar.gz/bliss_toolkit-0.3/bliss/simulator/decoder.py
@@ -0,0 +1,222 @@
+import copy
+from typing import Tuple
+
+import galsim
+import numpy as np
+import torch
+from astropy.wcs import WCS
+from torch import nn
+
+from bliss.catalog import SourceType, TileCatalog
+
+
+class ImageDecoder(nn.Module):
+    def __init__(
+        self,
+        psf,
+        bands: Tuple[int, ...],
+        flux_calibration_dict: dict,
+        pixel_shift: int,
+        ref_band: int,
+    ) -> None:
+        """Construct a decoder for a set of images.
+
+        Args:
+            psf: PSF object
+            bands: bands to use for constructing the decoder, passed from Survey
+            flux_calibration_dict: dictionary specifying elec count conversions by imageid
+            ref_band: reference band for pixel alignment
+            pixel_shift: int indicating parameters for a Unif() to model pixel shifting
+        """
+
+        super().__init__()
+
+        self.n_bands = len(bands)
+        self.psf_galsim = psf.psf_galsim  # Dictionary indexed by image_id
+        self.psf_params = psf.psf_params  # Dictionary indexed by image_id
+        self.psf_draw_method = getattr(psf, "psf_draw_method", "auto")
+        self.pixel_scale = psf.pixel_scale
+        self.ref_band = ref_band
+        self.shift = pixel_shift
+        self.flux_calibration_dict = flux_calibration_dict
+
+    def render_star(self, psf, band, source_params):
+        """Render a star with given params and PSF.
+
+        Args:
+            source_params (Tensor): Tensor containing the parameters for a particular source
+                (see prior.py for details about these parameters)
+            psf (List): a list of PSFs for each band
+            band (int): band
+
+        Returns:
+            GSObject: a galsim representation of the rendered star convolved with the PSF
+        """
+        return psf[band].withFlux(source_params["star_fluxes"][band].item())
+
+    def render_galaxy(self, psf, band, source_params):
+        """Render a galaxy with given params and PSF.
+
+        Args:
+            psf (List): a list of PSFs for each band
+            band (int): band
+            source_params (Tensor): Tensor containing the parameters for a particular source
+                (see prior.py for details about these parameters)
+
+        Returns:
+            GSObject: a galsim representation of the rendered galaxy convolved with the PSF
+        """
+        galaxy_fluxes = source_params["galaxy_fluxes"]
+        galaxy_params = source_params["galaxy_params"]
+
+        total_flux = galaxy_fluxes[band]
+        disk_frac, beta_radians, disk_q, a_d, bulge_q, a_b = galaxy_params
+
+        disk_flux = total_flux * disk_frac
+        bulge_frac = 1 - disk_frac
+        bulge_flux = total_flux * bulge_frac
+
+        components = []
+        if disk_flux > 0:
+            b_d = a_d * disk_q
+            disk_hlr_arcsecs = np.sqrt(a_d * b_d)
+            disk = galsim.Exponential(flux=disk_flux, half_light_radius=disk_hlr_arcsecs)
+            sheared_disk = disk.shear(q=disk_q, beta=beta_radians * galsim.radians)
+            components.append(sheared_disk)
+        if bulge_flux > 0:
+            b_b = bulge_q * a_b
+            bulge_hlr_arcsecs = np.sqrt(a_b * b_b)
+            bulge = galsim.DeVaucouleurs(flux=bulge_flux, half_light_radius=bulge_hlr_arcsecs)
+            sheared_bulge = bulge.shear(q=bulge_q, beta=beta_radians * galsim.radians)
+            components.append(sheared_bulge)
+        galaxy = galsim.Add(components)
+        return galsim.Convolution(galaxy, psf[band])
+
+    @property
+    def source_renderers(self):
+        return {
+            SourceType.STAR: self.render_star,
+            SourceType.GALAXY: self.render_galaxy,
+        }
+
+    def pixel_shifts(self, coadd_depth: int, n_bands: int, ref_band: int, ref_depth: int = 0):
+        """Generate random pixel shifts and corresponding WCS list.
+        This function generates `n_shifts` random pixel shifts `shifts` and corresponding WCS list
+        `wcs` to undo these shifts, relative to `wcs[ref_band]`.
+
+        Args:
+            coadd_depth (int): number of images per band that will be co-added
+            ref_depth (int): depth of the reference band
+            n_bands (int): number of bands
+            ref_band (int): index of the reference band
+
+        Returns:
+            shifts (np.ndarray): array of pixel shifts
+            wcs (List[WCS]): list of WCS objects
+        """
+        shifts = np.random.uniform(-self.shift, self.shift, (coadd_depth, n_bands, 2))  # 2 for x, y
+        shifts[ref_depth, ref_band] = np.array([0.0, 0.0])
+        wcs_base = WCS()
+        base = np.array([5.0, 5.0])
+        wcs_base.wcs.crpix = base
+        wcs_list = [[] for _ in range(coadd_depth)]
+        for d in range(coadd_depth):
+            for b in range(n_bands):
+                if d == ref_depth and b == ref_band:
+                    wcs_list[d].append(wcs_base.low_level_wcs)
+                    continue
+                bnd_wcs = WCS()
+                bnd_wcs.wcs.crpix = base + shifts[d, b]
+                wcs_list[d].append(bnd_wcs.low_level_wcs)
+        return shifts, wcs_list
+
+    def draw_sources_on_band_image(
+        self, band_img, n_sources, full_cat, batch, psf, band, image_dims, shift
+    ):
+        slen_h, slen_w = image_dims
+        for s in range(n_sources):
+            source_params = full_cat.one_source(batch, s)
+            source_type = source_params["source_type"].item()
+            renderer = self.source_renderers[source_type]
+            galsim_obj = renderer(psf, band, source_params)
+            plocs0, plocs1 = source_params["plocs"]
+            offset = np.array([plocs1 - (slen_w / 2), plocs0 - (slen_h / 2)])
+            offset += shift
+
+            # essentially all the runtime of the simulator is incurred by this call
+            # to drawImage
+            galsim_obj.drawImage(
+                offset=offset, method=self.psf_draw_method, add_to_image=True, image=band_img
+            )
+
+    def render_images(self, tile_cat: TileCatalog, image_ids, coadd_depth=1):
+        """Render images from a tile catalog.
+
+        Args:
+            tile_cat (TileCatalog): the tile catalog to create images from
+            image_ids (ndarray): array containing the image_ids of PSFs to use
+            coadd_depth (int): number of images (in each band) that will be co-added
+
+        Raises:
+            AssertionError: image_ids must contain `batch_size` values
+
+        Returns:
+            Tuple[Tensor, List, Tensor]: tensor of images, list of PSFs, tensor of PSF params
+        """
+        tile_cat = copy.deepcopy(tile_cat)  # make a copy to avoid modifying input
+        batch_size, n_tiles_h, n_tiles_w = tile_cat.n_sources.shape
+        assert (
+            len(image_ids) == batch_size
+        ), "image_ids array should contain `batch_size` identical values"
+
+        slen_h = tile_cat.tile_slen * n_tiles_h
+        slen_w = tile_cat.tile_slen * n_tiles_w
+        image_shape = (self.n_bands, slen_h, slen_w)
+        images = np.zeros((batch_size, coadd_depth, *image_shape), dtype=np.float32)
+
+        # use the PSF from specified image_id
+        psfs = [self.psf_galsim[image_ids[b]] for b in range(batch_size)]
+        param_list = [self.psf_params[image_ids[b]] for b in range(batch_size)]
+        psf_params = torch.stack(param_list, dim=0)
+
+        # use the specified flux_calibration ratios indexed by image_id
+        flux_calibration_rats = [
+            self.flux_calibration_dict[image_ids[b]] for b in range(batch_size)
+        ]
+
+        for b in range(batch_size):
+            # Convert to electron counts
+            tile_cat["star_fluxes"][b] *= flux_calibration_rats[b]
+            tile_cat["galaxy_fluxes"][b] *= flux_calibration_rats[b]
+
+        full_cat = tile_cat.to_full_params()
+
+        # generate random WCS shifts as manual image dithering via unaligning WCS
+        wcs_batch = []
+
+        for b in range(batch_size):
+            n_sources = int(full_cat.n_sources[b].item())
+            psf = psfs[b]
+            for d in range(coadd_depth):
+                depth_band_shifts, depth_band_wcs_list = self.pixel_shifts(
+                    coadd_depth, self.n_bands, self.ref_band
+                )
+                wcs_batch.append(depth_band_wcs_list)
+                for band in range(self.n_bands):
+                    band_img = galsim.Image(array=images[b, d, band], scale=self.pixel_scale)
+                    self.draw_sources_on_band_image(
+                        band_img,
+                        n_sources,
+                        full_cat,
+                        b,
+                        psf,
+                        band,
+                        image_dims=(slen_h, slen_w),
+                        shift=depth_band_shifts[d, band],
+                    )
+
+        # clamping here helps with an strange issue caused by galsim rendering
+        images = torch.from_numpy(images).clamp(1e-8)
+        images = torch.squeeze(images, dim=1)  # remove coadd depth dimension if 1
+
+        return images, psfs, psf_params, wcs_batch
---
+++ packages/bliss-toolkit/bliss_toolkit-0.3.tar.gz/bliss_toolkit-0.3/bliss/unconstrained_dists.py
@@ -0,0 +1,118 @@
+import torch
+from torch.distributions import (
+    AffineTransform,
+    Categorical,
+    Distribution,
+    Independent,
+    LogNormal,
+    Normal,
+    SigmoidTransform,
+    TransformedDistribution,
+)
+
+
+class UnconstrainedBernoulli:
+    def __init__(self):
+        self.dim = 1
+
+    def get_dist(self, params):
+        yes_prob = params.sigmoid().clamp(1e-4, 1 - 1e-4)
+        no_yes_prob = torch.cat([1 - yes_prob, yes_prob], dim=3)
+        return Categorical(no_yes_prob)
+
+
+class UnconstrainedNormal:
+    def __init__(self, low_clamp=-20, high_clamp=20):
+        self.dim = 2
+        self.low_clamp = low_clamp
+        self.high_clamp = high_clamp
+
+    def get_dist(self, params):
+        mean = params[:, :, :, 0]
+        sd = params[:, :, :, 1].clamp(self.low_clamp, self.high_clamp).exp().sqrt()
+        return Normal(mean, sd)
+
+
+class TruncatedDiagonalMVN(Distribution):
+    """A truncated diagonal multivariate normal distribution."""
+
+    def __init__(self, mu, sigma):
+        super().__init__(validate_args=False)
+
+        multiple_normals = Normal(mu, sigma)
+        prob_in_unit_box_hw = multiple_normals.cdf(torch.ones_like(mu))
+        prob_in_unit_box_hw -= multiple_normals.cdf(torch.zeros_like(mu))
+        self.log_prob_in_unit_box = prob_in_unit_box_hw.log().sum(dim=-1)
+
+        self.base_dist = Independent(multiple_normals, 1)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.base_dist.base_dist})"
+
+    def sample(self, **args):
+        # some ideas for how to sample it here, if we need to:
+        # https://cran.r-project.org/web/packages/truncnorm/
+        raise NotImplementedError("sampling a truncated normal isn't straightforward")
+
+    @property
+    def mean(self):
+        return self.base_dist.mean
+
+    @property
+    def stddev(self):
+        return self.base_dist.stddev
+
+    @property
+    def mode(self):
+        # a mode still exists if this assertion is false, but I haven't implemented code
+        # to compute it because I don't think we need it
+        assert (self.mean >= 0).all() and (self.mean <= 1).all()
+        return self.base_dist.mode
+
+    def log_prob(self, value):
+        assert (value >= 0).all() and (value <= 1).all()
+        # subtracting log probability that the base RV is in the unit box
+        # is equivalent in log space to dividing the normal pdf by the normalizing constant
+        return self.base_dist.log_prob(value) - self.log_prob_in_unit_box
+
+    def cdf(self, value):
+        cdf_at_val = self.base_dist.base_dist.cdf(value)
+        cdf_at_lb = self.base_dist.base_dist.cdf(torch.zeros_like(self.mean))
+        log_cdf = (cdf_at_val - cdf_at_lb + 1e-9).log().sum(dim=-1) - self.log_prob_in_unit_box
+        return log_cdf.exp()
+
+
+class UnconstrainedTDBN:
+    """Produces truncated bivariate normal distributions from unconstrained parameters."""
+
+    def __init__(self):
+        self.dim = 4
+
+    def get_dist(self, params):
+        mu = params[:, :, :, :2].sigmoid()
+        sigma = params[:, :, :, 2:].clamp(-6, 3).exp().sqrt()
+        return TruncatedDiagonalMVN(mu, sigma)
+
+
+class UnconstrainedLogNormal:
+    def __init__(self):
+        self.dim = 2
+
+    def get_dist(self, params):
+        mu = params[:, :, :, 0]
+        sigma = params[:, :, :, 1].clamp(-6, 10).exp().sqrt()
+        return LogNormal(mu, sigma, validate_args=False)  # we may evaluate at 0 for masked tiles
+
+
+class UnconstrainedLogitNormal:
+    def __init__(self, low=0, high=1):
+        self.dim = 2
+        self.low = low
+        self.high = high
+
+    def get_dist(self, params):
+        mu = params[:, :, :, 0]
+        sigma = params[:, :, :, 1].clamp(-10, 10).exp().sqrt()
+        base_dist = Normal(mu, sigma)
+        transforms = [SigmoidTransform(), AffineTransform(loc=self.low, scale=self.high)]
+        return TransformedDistribution(base_dist, transforms)
