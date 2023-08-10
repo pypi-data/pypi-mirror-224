e3639dfa387db40a96b8dc3355ec163bcda1b51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:13:04 +0000

    Add package CVHSSmoothing/1.1/CVHSSmoothing-1.1.tar.gz

---
+++ packages/CVHSSmoothing/CVHSSmoothing-1.1.tar.gz/CVHSSmoothing-1.1/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from CVHSSmoothing.version import __version__ as myVersion
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf8")
+
+setup(
+ name='CVHSSmoothing',
+ url = 'https://github.com/danhamill/CVHSSmoothing',
+ download_url = 'https://github.com/danhamill/CVHSSmoothing',
+ author = 'Daniel Hamill',
+ author_email = 'daniel.hamill@hey.com',
+ description = 'Cubic spline interpolation of hydrographs',
+ long_description=long_description,
+ long_description_content_type="text/markdown",
+ packages = ['CVHSSmoothing'],
+ license='MIT',
+ version=myVersion,
+ install_requires = ['numpy','pandas','scipy'],
+ classifiers=[
+    "Development Status :: 4 - Beta",
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    "License :: OSI Approved :: MIT License",
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    "Topic :: Utilities",
+    'Topic :: Scientific/Engineering :: GIS',
+]
+)
