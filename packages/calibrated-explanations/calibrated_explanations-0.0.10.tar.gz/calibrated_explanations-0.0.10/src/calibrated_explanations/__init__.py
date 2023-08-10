7a999ec9905b4e616ed8afd9f6fc7a89ca7850eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:08:25 +0000

    Add package calibrated-explanations/0.0.10/calibrated_explanations-0.0.10.tar.gz

---
+++ packages/calibrated-explanations/calibrated_explanations-0.0.10.tar.gz/calibrated_explanations-0.0.10/src/calibrated_explanations/__init__.py
@@ -0,0 +1,13 @@
+"""
+Calibrated Explanations (calibrated_explanations)
+
+is a Python package for explaining black-box models.
+
+It is based on the paper "Calibrated Explanations: with Uncertainty Information and Counterfactuals" 
+by Helena Löfström et al.
+"""
+from .core import CalibratedExplainer, __version__
+from .wrappers import CalibratedAsLimeTabularExplainer, CalibratedAsShapExplainer
+from ._discretizers import BinaryDiscretizer, BinaryEntropyDiscretizer
+from ._explanations import CalibratedExplanation
+from .VennAbers import VennAbers
