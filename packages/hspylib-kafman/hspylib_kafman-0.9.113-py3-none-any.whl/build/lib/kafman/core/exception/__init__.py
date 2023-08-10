901e88196916621a907269d1c06b65c71c623dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:13:11 +0000

    Add package hspylib-kafman/0.9.113/hspylib_kafman-0.9.113-py3-none-any.whl

---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.113-py3-none-any.whl/build/lib/kafman/core/exception/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-kafman v0.9.113
+#
+# Package: main.build.lib.kafman.core.exception
+"""Package initialization."""
+
+__all__ = [
+    'exceptions'
+]
+__version__ = '0.9.113'
---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.113-py3-none-any.whl/build/lib/kafman/core/schema/json/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-kafman v0.9.113
+#
+# Package: main.build.lib.kafman.core.schema.json
+"""Package initialization."""
+
+__all__ = [
+    'json_parser', 
+    'json_schema', 
+    'json_type', 
+    'property'
+]
+__version__ = '0.9.113'
