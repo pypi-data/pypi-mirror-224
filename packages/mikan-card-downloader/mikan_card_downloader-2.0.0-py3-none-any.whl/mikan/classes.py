cb66f3471c2e85c64575fc20a11f30ed866ecf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 03:07:28 +0000

    Add package mikan-card-downloader/2.0.0/mikan_card_downloader-2.0.0-py3-none-any.whl

---
+++ packages/mikan-card-downloader/mikan_card_downloader-2.0.0-py3-none-any.whl/mikan/classes.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2022-2023 DemonicSavage
+# This file is part of Mikan.
+
+# Mikan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+
+# Mikan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+
+from dataclasses import dataclass
+from typing import ClassVar, TypeAlias
+
+
+@dataclass
+class Card:
+    results_dir: ClassVar = "SIFAS_Cards"
+    list_url_template: ClassVar = "https://idol.st/ajax/allstars/cards/?page="
+    url_template: ClassVar = "https://idol.st/ajax/allstars/card/"
+
+
+@dataclass
+class Still:
+    results_dir: ClassVar = "SIFAS_Stills"
+    list_url_template: ClassVar = "https://idol.st/ajax/allstars/stills/?page="
+    url_template: ClassVar = "https://idol.st/ajax/allstars/still/"
+
+
+@dataclass
+class SIFCard:
+    results_dir: ClassVar = "SIF_Cards"
+    list_url_template: ClassVar = "https://schoolido.lu/api/cardids/"
+    url_template: ClassVar = "https://schoolido.lu/api/cards/"
+
+
+@dataclass
+class SIF2Card:
+    results_dir: ClassVar = "SIF2_Cards"
+    list_url_template: ClassVar = "https://idol.st/ajax/SIF2/cards/?page="
+    url_template: ClassVar = "https://idol.st/ajax/SIF2/card/"
+
+
+Item: TypeAlias = Card | Still | SIFCard | SIF2Card
