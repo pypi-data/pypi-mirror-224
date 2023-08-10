f418c91d25a3b7a2931df62114a76842a7215bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:35:49 +0000

    Add package aleph-alpha-client/3.2.3/aleph-alpha-client-3.2.3.tar.gz

---
+++ packages/aleph-alpha-client/aleph-alpha-client-3.2.3.tar.gz/aleph-alpha-client-3.2.3/aleph_alpha_client/__init__.py
@@ -0,0 +1,101 @@
+from .prompt import (
+    ControlTokenOverlap,
+    Image,
+    ImageControl,
+    Prompt,
+    Text,
+    TextControl,
+    TokenControl,
+    Tokens,
+)
+from .aleph_alpha_client import (
+    POOLING_OPTIONS,
+    AsyncClient,
+    Client,
+    QuotaError,
+)
+from .completion import CompletionRequest, CompletionResponse
+from .detokenization import DetokenizationRequest, DetokenizationResponse
+from .document import Document
+from .embedding import (
+    EmbeddingRequest,
+    EmbeddingResponse,
+    BatchSemanticEmbeddingRequest,
+    BatchSemanticEmbeddingResponse,
+    SemanticEmbeddingRequest,
+    SemanticEmbeddingResponse,
+    SemanticRepresentation,
+)
+from .evaluation import EvaluationRequest, EvaluationResponse
+from .explanation import (
+    CustomGranularity,
+    Explanation,
+    ExplanationPostprocessing,
+    ExplanationRequest,
+    ExplanationResponse,
+    ImagePromptItemExplanation,
+    ImageScore,
+    PromptGranularity,
+    TargetGranularity,
+    TargetPromptItemExplanation,
+    TargetScore,
+    TextPromptItemExplanation,
+    TextScore,
+    TokenPromptItemExplanation,
+    TokenScore,
+)
+from .qa import QaRequest, QaResponse
+from .summarization import SummarizationRequest, SummarizationResponse
+from .tokenization import TokenizationRequest, TokenizationResponse
+from .utils import load_base64_from_file, load_base64_from_url
+from .version import __version__
+
+__all__ = [
+    "AsyncClient",
+    "BatchSemanticEmbeddingRequest",
+    "BatchSemanticEmbeddingResponse",
+    "Client",
+    "CompletionRequest",
+    "CompletionResponse",
+    "ControlTokenOverlap",
+    "CustomGranularity",
+    "DetokenizationRequest",
+    "DetokenizationResponse",
+    "Document",
+    "EmbeddingRequest",
+    "EmbeddingResponse",
+    "EvaluationRequest",
+    "EvaluationResponse",
+    "Explanation",
+    "ExplanationPostprocessing",
+    "ExplanationRequest",
+    "ExplanationResponse",
+    "Image",
+    "ImageControl",
+    "ImagePromptItemExplanation",
+    "ImageScore",
+    "POOLING_OPTIONS",
+    "Prompt",
+    "PromptGranularity",
+    "QaRequest",
+    "QaResponse",
+    "QuotaError",
+    "SemanticEmbeddingRequest",
+    "SemanticEmbeddingResponse",
+    "SemanticRepresentation",
+    "SummarizationRequest",
+    "SummarizationResponse",
+    "TargetGranularity",
+    "TargetPromptItemExplanation",
+    "TargetScore",
+    "Text",
+    "TextControl",
+    "TextPromptItemExplanation",
+    "TextScore",
+    "TokenControl",
+    "TokenizationRequest",
+    "TokenizationResponse",
+    "TokenPromptItemExplanation",
+    "Tokens",
+    "TokenScore",
+]
