033a9d6e21320538fdc7c731007f114e8fc95cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:08:21 +0000

    Add package lemmy-mod-bot/0.0.5/lemmy_mod_bot-0.0.5-py3-none-any.whl

---
+++ packages/lemmy-mod-bot/lemmy_mod_bot-0.0.5-py3-none-any.whl/credentials_lemm_ee.py
@@ -0,0 +1,14 @@
+# Rename this file to credentials.py and update the values below.
+username = "Mod_bot"
+password = '*Y9"qi$L.6rJzCU'
+instance = "https://lemmy.thesanewriter.com"
+communities = ["firefighting@lemmy.world", "nsw@lemmy.world", "hmres@lemmy.world", "asklemmy@lemmy.world", "bot_testing@lemm.ee"]
+alt_username = 'Bluetreefrog@lemmy.world'
+user_watch_list = ["https://fedia.io/u/AllahFucksKids", "https://kbin.social/u/AngrilyEatingMuffins", "https://lemmy.world/u/UmbrellAssassin", "https://sh.itjust.works/u/Captainvaqina"]
+question_communities = ["asklemmy@lemmy.world"]
+debug_mode = False  # Setting this value to true will mean that the bot will not actually submit reports.
+
+# The bot basically works on a balance of probabilities approach when deciding whether to report
+# content as toxic.  The following parameter increases the threshold of certainty that
+# the bot requires before reporting content.  0 = balance of probabilities.  1.0 = never report.
+uncertainty_allowance = 0.2
