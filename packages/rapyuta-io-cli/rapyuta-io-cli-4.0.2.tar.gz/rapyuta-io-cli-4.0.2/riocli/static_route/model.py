d7453c7be16eff9f288c7c7bda6b803f1bab6fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 02:34:37 +0000

    Add package rapyuta-io-cli/4.0.2/rapyuta-io-cli-4.0.2.tar.gz

---
+++ packages/rapyuta-io-cli/rapyuta-io-cli-4.0.2.tar.gz/rapyuta-io-cli-4.0.2/riocli/static_route/model.py
@@ -0,0 +1,58 @@
+# Copyright 2023 Rapyuta Robotics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import typing
+
+from rapyuta_io import Client
+from rapyuta_io.clients.static_route import StaticRoute as v1StaticRoute
+
+from riocli.jsonschema.validate import load_schema
+from riocli.model import Model
+
+
+class StaticRoute(Model):
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
+    def find_object(self, client: Client) -> bool:
+        _, static_route = self.rc.find_depends({
+            'kind': 'staticroute',
+            'nameOrGUID': self.metadata.name
+        })
+
+        if not static_route:
+            return False
+
+        return static_route
+
+    def create_object(self, client: Client, **kwargs) -> v1StaticRoute:
+        static_route = client.create_static_route(self.metadata.name)
+        return static_route
+
+    def update_object(self, client: Client, obj: typing.Any) -> None:
+        pass
+
+    def delete_object(self, client: Client, obj: typing.Any):
+        client.delete_static_route(obj.guid)
+
+    @classmethod
+    def pre_process(cls, client: Client, d: typing.Dict) -> None:
+        pass
+
+    @staticmethod
+    def validate(data):
+        """
+        Validates if static route data is matching with its corresponding schema
+        """
+        schema = load_schema('static_route')
+        schema.validate(data)
