d3e770dd223d3b5f4db8c6e8d60ae1af4dd733a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 15:08:16 +0000

    Add package sapphire.py/0.1.13/sapphire.py-0.1.13-py3-none-any.whl

---
+++ packages/sapphire.py/sapphire.py-0.1.13-py3-none-any.whl/sapphirepy/wrapper.py
@@ -0,0 +1,66 @@
+import os
+import ctypes
+import platform
+import sys
+
+lib = None
+bin_dir = os.path.join(sys.prefix, "sapphirepy_bin")
+
+if platform.system() == "Darwin":
+    lib_path = os.path.join(bin_dir, "sapphirewrapper.dylib")
+    lib = ctypes.CDLL(lib_path)
+
+if platform.system() == "Windows":
+    raise Exception("Windows is not supported")
+
+if platform.system() == "Linux":
+    lib_path = os.path.join(bin_dir, "sapphirewrapper.so")
+    lib = ctypes.CDLL(lib_path)
+
+
+# Define argument types
+lib.SendETHTransaction.argtypes = [
+    ctypes.c_char_p,
+    ctypes.c_char_p,
+    ctypes.c_char_p,
+    ctypes.c_char_p,
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.c_char_p,
+]
+
+
+class SendETHTransactionResult(ctypes.Structure):
+    _fields_ = [("resultCode", ctypes.c_int), ("resultStr", ctypes.c_char_p)]
+
+
+# Define return type
+lib.SendETHTransaction.restype = SendETHTransactionResult
+
+
+def send_encrypted_sapphire_tx(
+    pk: str,
+    sender: str,
+    recipient: str,
+    rpc_url: str,
+    eth_amount: int,
+    gas_limit: int,
+    data: str,
+    gas_cost_gwei: int,
+    nonce: int,
+) -> tuple:
+    result = lib.SendETHTransaction(
+        pk.encode("utf-8"),
+        sender.encode("utf-8"),
+        recipient.encode("utf-8"),
+        rpc_url.encode("utf-8"),
+        eth_amount,
+        gas_limit,
+        data.encode("utf-8"),
+        gas_cost_gwei,
+        nonce,
+    )
+    return (
+        result.resultCode,
+        result.resultStr.decode("utf-8") if result.resultStr is not None else None,
+    )
