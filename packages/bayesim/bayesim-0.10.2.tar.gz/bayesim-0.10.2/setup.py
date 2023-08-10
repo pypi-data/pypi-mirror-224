9d80bcb1d317e021fd3641d169ebb0d5a46ff226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:11:24 +0000

    Add package bayesim/0.10.2/bayesim-0.10.2.tar.gz

---
+++ packages/bayesim/bayesim-0.10.2.tar.gz/bayesim-0.10.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import os, sys
+
+setup(name='bayesim',
+      version='0.10.2',
+      description='Fast model fitting via Bayesian inference',
+      author='Rachel Kurchin, Giuseppe Romano',
+      author_email='rkurchin@alum.mit.edu, romanog@mit.edu',
+      classifiers=['Programming Language :: Python :: 3.7'],
+      long_description=open('README.rst').read(),
+      install_requires=['pandas', 'scipy', 'matplotlib', 'joblib', 'tables'],
+      license='GPLv2',
+      packages = ['bayesim'],
+      entry_points = {
+     'console_scripts': [
+      'bayesim=bayesim.__main__:main'],
+      },
+      zip_safe=False)
