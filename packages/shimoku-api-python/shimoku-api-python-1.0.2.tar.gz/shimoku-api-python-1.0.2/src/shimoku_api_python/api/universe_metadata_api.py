053eae07084677caa79f3c2d56cbfa9f860506cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 15:08:08 +0000

    Add package shimoku-api-python/1.0.2/shimoku-api-python-1.0.2.tar.gz

---
+++ packages/shimoku-api-python/shimoku-api-python-1.0.2.tar.gz/shimoku-api-python-1.0.2/src/shimoku_api_python/api/universe_metadata_api.py
@@ -0,0 +1,23 @@
+from ..async_execution_pool import async_auto_call_manager, ExecutionPoolContext
+from ..resources.universe import Universe
+
+import logging
+from ..execution_logger import logging_before_and_after
+logger = logging.getLogger(__name__)
+
+
+class UniverseMetadataApi:
+    """
+    This class is used to interact with the Universe metadata API.
+    """
+    @logging_before_and_after(logging_level=logger.debug)
+    def __init__(self, api_client, execution_pool_context: ExecutionPoolContext):
+        self.epc = execution_pool_context
+        self._api_client = api_client
+
+    @async_auto_call_manager(execute=True)
+    @logging_before_and_after(logging_level=logger.info)
+    def get_universe_workspaces(self, uuid: str):
+        universe = Universe(api_client=self._api_client, uuid=uuid)
+        return universe.get_businesses()
+
