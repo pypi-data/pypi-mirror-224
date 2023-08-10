9b5ddca74d1b25d8073b503e411a16ad220b1b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 14:09:32 +0000

    Add package PacoteCalculoMat/1.1.3/PacoteCalculoMat-1.1.3.tar.gz

---
+++ packages/PacoteCalculoMat/PacoteCalculoMat-1.1.3.tar.gz/PacoteCalculoMat-1.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.3'
+DESCRIPTION = 'Meu primeiro pacote'
+LONG_DESCRIPTION = 'Meu primeiro pacote completo longo'
+
+#setting up
+setup(
+        #'name' deve corresponder ao nome da pasta 'PacoteTeste'
+        name = "PacoteCalculoMat",
+        version=VERSION,
+        author="Milene Fialho",
+        author_email="milefialho16@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        license='MIT',
+        packages=find_packages(),
+        install_requires=[],# adicione outros pacotes que precisem ser instalados com o seu pacote. EX: 'caer'
+        
+) 
+    
