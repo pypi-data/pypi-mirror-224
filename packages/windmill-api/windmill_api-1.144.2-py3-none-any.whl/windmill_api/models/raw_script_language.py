3d5c076a51e91f00439fc7e067c67e823e44603d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-10 04:12:59 +0000

    Add package windmill-api/1.144.2/windmill_api-1.144.2-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.144.2-py3-none-any.whl/windmill_api/models/raw_script_language.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class RawScriptLanguage(str, Enum):
+    DENO = "deno"
+    PYTHON3 = "python3"
+    GO = "go"
+    BASH = "bash"
+    POSTGRESQL = "postgresql"
+    MYSQL = "mysql"
+    BIGQUERY = "bigquery"
+    SNOWFLAKE = "snowflake"
+    GRAPHQL = "graphql"
+    NATIVETS = "nativets"
+
+    def __str__(self) -> str:
+        return str(self.value)
