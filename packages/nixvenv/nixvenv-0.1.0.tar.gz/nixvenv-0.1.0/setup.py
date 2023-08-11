3e7551965f88212b539d74ab4c09ee70be3eab50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:08:52 +0000

    Add package nixvenv/0.1.0/nixvenv-0.1.0.tar.gz

---
+++ packages/nixvenv/nixvenv-0.1.0.tar.gz/nixvenv-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nixvenv',
+    version='0.1.0',
+    license='CC BY-NC-SA 4.0',
+    author="TheCookingSenpai",
+    author_email='administration@blockdrops.org',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/thecookingsenpai/nixvenv',
+    keywords='unix linux macos mac venv environment virtual',
+    install_requires=[],
+
+)
