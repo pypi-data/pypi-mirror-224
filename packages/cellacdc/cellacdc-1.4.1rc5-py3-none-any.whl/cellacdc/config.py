fe40047b2b4c426489a42413dc103220c9e427e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:09:29 +0000

    Add package cellacdc/1.4.1rc5/cellacdc-1.4.1rc5-py3-none-any.whl

---
+++ packages/cellacdc/cellacdc-1.4.1rc5-py3-none-any.whl/cellacdc/config.py
@@ -0,0 +1,55 @@
+import argparse
+import configparser
+import pprint
+
+class ConfigParser(configparser.ConfigParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.optionxform = str
+    
+    def __repr__(self) -> str:
+        string = pprint.pformat(
+            {section: dict(self[section]) for section in self.sections()}
+        )
+        return string
+
+from . import GUI_INSTALLED
+
+if GUI_INSTALLED:
+    from qtpy.QtCore import QObject, Signal, qInstallMessageHandler
+
+    class QtWarningHandler(QObject):
+        sigGeometryWarning = Signal(object)
+
+        def _resizeWarningHandler(self, msg_type, msg_log_context, msg_string):
+            if msg_string.find('Unable to set geometry') != -1:
+                try:
+                    self.sigGeometryWarning.emit(msg_string)
+                except Exception as e:
+                    pass
+            elif msg_string:
+                print(msg_string)
+
+    warningHandler = QtWarningHandler()
+    qInstallMessageHandler(warningHandler._resizeWarningHandler)
+
+try:
+    ap = argparse.ArgumentParser(description='Cell-ACDC parser')
+    ap.add_argument(
+        '-d', '--debug', action='store_true',
+        help=(
+            'Used for debugging. Test code with'
+            '"from cellacdc.config import parser_args, debug = parser_args["debug"]", '
+            'if debug: <debug code here>'
+        )
+    )
+
+    # Add dummy argument for stupid Jupyter
+    # ap.add_argument('-f')
+
+    parser_args, unknown = ap.parse_known_args()
+    parser_args = vars(parser_args)
+except:
+    print('Importing from notebook, ignoring Cell-ACDC argument parser...')
+    parser_args = {}
+    parser_args['debug'] = False
