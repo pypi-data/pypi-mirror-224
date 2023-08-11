7f6498af21bea64838f7a049b22e1b7abcc80df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:09:35 +0000

    Add package near-calculator/2.6.1/near_calculator-2.6.1-py3-none-any.whl

---
+++ packages/near-calculator/near_calculator-2.6.1-py3-none-any.whl/near_calculator/near_calculator.py
@@ -0,0 +1,10 @@
+class Calc(object):
+    def __init__(self):
+        def sumx(num,intx):
+            return num + intx
+        def subtract(num,intx):
+            return num - intx
+        def multiply(num,intx):
+            return num * intx 
+        def divide(num,intx):
+            return num / intx
