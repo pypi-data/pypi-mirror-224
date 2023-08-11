0ffbabaf82dc2741b76ef5d6b67be1c99dc6e49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:08:11 +0000

    Add package leetquery/0.2.5/leetquery-0.2.5-py3-none-any.whl

---
+++ packages/leetquery/leetquery-0.2.5-py3-none-any.whl/leetquery/__init__.py
@@ -0,0 +1,10 @@
+from .__version__ import __version__
+
+from . import user
+from .user import get_submissions
+
+from . import problem
+from .problem import (
+    get_discription,
+    get_stats
+)
