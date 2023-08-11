f03d8e0a7e5e6899eef528998303a4a9b9d10335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:09:40 +0000

    Add package near-calculator/1.9/near_calculator-1.9.tar.gz

---
+++ packages/near-calculator/near_calculator-1.9.tar.gz/near_calculator-1.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="near_calculator",
+    version='1.9',
+    author="Caique Santos Carvalho",
+    author_email="subdr2nk@gmail.com",
+    packages=['near_calculator'], # ```README.type >= .md``
+    license='LICENSE.txt',
+    python_requires='>=3.4.0'
+)
