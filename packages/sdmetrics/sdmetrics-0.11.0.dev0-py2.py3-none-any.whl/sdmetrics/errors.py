b91638d7c1efff7bb2ea5659038eced2db806fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:09:32 +0000

    Add package sdmetrics/0.11.0.dev0/sdmetrics-0.11.0.dev0-py2.py3-none-any.whl

---
+++ packages/sdmetrics/sdmetrics-0.11.0.dev0-py2.py3-none-any.whl/sdmetrics/errors.py
@@ -0,0 +1,9 @@
+"""Custom errors for SDMetrics."""
+
+
+class IncomputableMetricError(Exception):
+    """Raised when a metric cannot be computed."""
+
+
+class ConstantInputError(Exception):
+    """Thrown when the input data has all the same values."""
