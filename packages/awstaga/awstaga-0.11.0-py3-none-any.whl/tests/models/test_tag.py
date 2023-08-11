c90fb6936defd65e9207b98af21c95bab074db11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:09:16 +0000

    Add package awstaga/0.11.0/awstaga-0.11.0-py3-none-any.whl

---
+++ packages/awstaga/awstaga-0.11.0-py3-none-any.whl/tests/models/test_tag.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code,too-many-locals
+import unittest
+from awstaga.models.tag import Tag
+
+class TestTag(unittest.TestCase):
+
+    def test_getters(self):
+        tag = Tag('somekey', 'somevalue')
+        self.assertEqual(tag.get_key(), 'somekey')
+        self.assertEqual(tag.get_value(), 'somevalue')
+        self.assertEqual(str(tag), 'somekey=somevalue')
+        self.assertEqual(repr(tag), 'somekey=somevalue')
