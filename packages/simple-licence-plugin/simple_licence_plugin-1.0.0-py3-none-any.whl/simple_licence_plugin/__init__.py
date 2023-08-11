1f25fe761894fe2aa9fbc18a588e1f980ade6cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:07:34 +0000

    Add package simple-licence-plugin/1.0.0/simple_licence_plugin-1.0.0-py3-none-any.whl

---
+++ packages/simple-licence-plugin/simple_licence_plugin-1.0.0-py3-none-any.whl/simple_licence_plugin/__init__.py
@@ -0,0 +1,8 @@
+""" simple-licence-plugin: plugin for pyarmor for checking and auditing licenses """
+
+from .models import AuditEventBase
+from .utils import check_licence, get_licence_data
+
+__author__ = "Alex Hunt"
+__email__ = "alex.hunt@csiro.au"
+__version__ = "1.0.0"
