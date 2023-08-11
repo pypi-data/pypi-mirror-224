a5de15dc9d2cccc7fe0f727f4c8d1a26fcf7cf75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 06:13:18 +0000

    Add package metricsoperator/0.0.15/metricsoperator-0.0.15.tar.gz

---
+++ packages/metricsoperator/metricsoperator-0.0.15.tar.gz/metricsoperator-0.0.15/metricsoperator/utils.py
@@ -0,0 +1,41 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+
+import json
+
+import yaml
+
+
+def read_file(filename):
+    """
+    Read file into text blob
+    """
+    with open(filename, "r") as fd:
+        text = fd.read()
+    return text
+
+
+def read_yaml(filename):
+    """
+    Read yaml into dict
+    """
+    with open(filename, "r") as file:
+        configuration = yaml.safe_load(file)
+    return configuration
+
+
+def write_json(obj, filename):
+    """
+    Write dict to json
+    """
+    with open(filename, "w") as fd:
+        fd.write(json.dumps(obj, indent=4))
+
+
+def read_json(filename):
+    """
+    Read json into dict
+    """
+    with open(filename, "r") as fd:
+        content = json.loads(fd.read())
+    return content
