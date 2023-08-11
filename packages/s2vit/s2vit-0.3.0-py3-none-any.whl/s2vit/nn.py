6e8105ec2797ba3106e0e90b2cfc825b40a9cd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:08:34 +0000

    Add package s2vit/0.3.0/s2vit-0.3.0-py3-none-any.whl

---
+++ packages/s2vit/s2vit-0.3.0-py3-none-any.whl/s2vit/nn.py
@@ -0,0 +1,151 @@
+import math
+from collections.abc import Callable, Sequence
+
+import torch
+from einops import pack, rearrange, unpack
+from timm.layers import LayerNorm2d
+from torch import nn
+from torch.nn import functional as F
+
+from .utils import to_pair
+
+
+class StarReLU(nn.Module):
+    def __init__(
+        self,
+        scale_value: float = 0.8944,
+        bias_value: float = -0.4472,
+        scale_learnable: bool = False,
+        bias_learnable: bool = False,
+        inplace: bool = False,
+    ):
+        super().__init__()
+
+        self.inplace = inplace
+        self.relu = nn.ReLU(inplace=inplace)
+        self.scale = nn.Parameter(scale_value * torch.ones(1), requires_grad=scale_learnable)
+        self.bias = nn.Parameter(bias_value * torch.ones(1), requires_grad=bias_learnable)
+
+    def forward(self, x):
+        return self.scale * self.relu(x) ** 2 + self.bias
+
+
+class LayerNormNoBias(nn.LayerNorm):
+    def __init__(self, normalized_shape: int | Sequence[int], eps: float = 1e-6, elementwise_affine: bool = True):
+        super().__init__(normalized_shape, eps, elementwise_affine)  # type: ignore
+        self.bias = None
+
+
+class LayerNormNoBias2d(LayerNorm2d):
+    def __init__(self, num_channels: int, eps: float = 1e-6, affine: bool = True):
+        super().__init__(num_channels, eps=eps, affine=affine)
+
+        self.bias = None
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        dim_out: int,
+        patch_size: int | tuple[int, int],
+        norm_layer: Callable[[int], nn.Module] = LayerNormNoBias2d,
+        bias: bool = False,
+    ):
+        super().__init__()
+
+        self.dim = dim
+        self.dim_out = dim_out
+        self.patch_size = to_pair(patch_size)
+
+        dim_in = math.prod(self.patch_size) * dim
+        self.proj = nn.Sequential(
+            norm_layer(dim_in),
+            nn.Conv2d(dim_in, dim_out, kernel_size=1, bias=bias),
+            norm_layer(dim_out),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        p1, p2 = self.patch_size
+        x = rearrange(x, " b c (h p1) (w p2) -> b (c p1 p2) h w", p1=p1, p2=p2)
+        return self.proj(x)
+
+
+class PEG(nn.Module):
+    def __init__(self, dim: int, kernel_size: int = 3):
+        super().__init__()
+
+        self.proj = nn.Conv2d(dim, dim, kernel_size=kernel_size, stride=1, padding=kernel_size // 2, groups=dim)
+
+    def forward(self, x):
+        return x + self.proj(x)
+
+
+class WindowMHSA(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        heads: int = 8,
+        dim_head: int = 64,
+        window_size: int = 8,
+        shared_kv: bool = False,
+        drop_rate: float = 0.0,
+        bias: bool = False,
+    ):
+        super().__init__()
+
+        self.dim = dim
+        self.heads = heads
+        self.dim_head = dim_head
+        self.window_size = window_size
+        self.shared_kv = shared_kv
+        self.drop_rate = drop_rate
+
+        # Credit to @lucidrains for this idea.
+        if shared_kv:
+            self.to_qkv = nn.Linear(dim, dim * 2, bias=bias)
+        else:
+            self.to_qkv = nn.Linear(dim, dim * 3, bias=bias)
+        self.to_out = nn.Conv2d(dim, dim, kernel_size=1, bias=bias)
+        # Avoid if/else in forward pass
+        self._to_qkv = self._get_q_kv if shared_kv else self._get_qkv
+
+    def _get_qkv(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, "b n (h d) -> b h n d", h=self.heads), qkv)
+        return q, k, v
+
+    def _get_q_kv(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        qkv = self.to_qkv(x).chunk(2, dim=-1)
+        q, kv = map(lambda t: rearrange(t, "b n (h d) -> b h n d", h=self.heads), qkv)
+        return q, kv, kv
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = rearrange(x, "b c (h p1) (w p2) -> b h w (p1 p2) c", p1=self.window_size, p2=self.window_size)
+        x, ps = pack([x], "* n d")
+
+        q, k, v = self._to_qkv(x)
+        x = F.scaled_dot_product_attention(q, k, v, dropout_p=self.drop_rate if self.training else 0.0)
+        x = rearrange(x, "b h n d -> b n (h d)")
+        (x,) = unpack(x, ps, "* n d")
+        x = rearrange(x, "b h w (p1 p2) c -> b c (h p1) (w p2)", p1=self.window_size, p2=self.window_size)
+        return self.to_out(x)
+
+
+class Shift2d(nn.Module):
+    def __init__(self, amount: int = 1):
+        super().__init__()
+
+        self.amount = amount
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        c = x.shape[1]
+        x_shifted = x.clone()
+        x_shifted[:, : c // 4, self.amount :, :] = x[:, : c // 4, : -self.amount, :]
+        x_shifted[:, c // 4 : c // 2, : -self.amount, :] = x[:, c // 4 : c // 2, self.amount :, :]
+        x_shifted[:, c // 2 : (3 * c) // 4, :, self.amount :] = x[:, c // 2 : (3 * c) // 4, :, : -self.amount]
+        x_shifted[:, (3 * c) // 4 :, :, : -self.amount] = x[:, (3 * c) // 4 :, :, self.amount :]
+        return x_shifted
+
+
+__all__ = ["WindowMHSA", "LayerNormNoBias", "LayerNormNoBias2d", "PatchEmbedding", "PEG", "Shift2d", "StarReLU"]
---
+++ packages/s2vit/s2vit-0.3.0-py3-none-any.whl/s2vit/transformer.py
@@ -0,0 +1,195 @@
+import warnings
+from collections import OrderedDict
+from collections.abc import Callable, Sequence
+from typing import Any
+
+import torch
+from einops.layers.torch import Reduce
+from torch import nn
+from torchvision.ops import DropBlock2d
+
+from .nn import PEG, LayerNormNoBias, LayerNormNoBias2d, PatchEmbedding, Shift2d, StarReLU, WindowMHSA
+
+
+class S2ViTBlock(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        dim_head: int = 32,
+        window_size: int = 8,
+        dim_ff: int | None = None,
+        shared_kv: bool = False,
+        norm_layer: Callable[[int], nn.Module] = LayerNormNoBias2d,
+        attn_drop_rate: float = 0.0,
+        ff_drop_rate: float = 0.0,
+        drop_block_rate: float = 0.0,
+        drop_block_size: int = 7,
+        bias: bool = False,
+    ):
+        super().__init__()
+
+        if dim_ff is None:
+            dim_ff = dim * 4
+        heads = dim // dim_head
+
+        self.attn = nn.Sequential(
+            norm_layer(dim),
+            Shift2d(),
+            WindowMHSA(
+                dim,
+                heads=heads,
+                dim_head=dim_head,
+                window_size=window_size,
+                shared_kv=shared_kv,
+                drop_rate=attn_drop_rate,
+                bias=bias,
+            ),
+            DropBlock2d(drop_block_rate, drop_block_size),
+        )
+        self.ff = nn.Sequential(
+            norm_layer(dim),
+            nn.Conv2d(dim, dim_ff, kernel_size=1, bias=bias),
+            StarReLU(),
+            nn.Dropout(ff_drop_rate),
+            nn.Conv2d(dim_ff, dim, kernel_size=1, bias=bias),
+            DropBlock2d(drop_block_rate, drop_block_size),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x + self.attn(x)
+        x = x + self.ff(x)
+        return x
+
+
+class S2ViTStage(nn.Module):
+    def __init__(
+        self,
+        dim_in: int,
+        dim_out: int,
+        depth: int,
+        use_peg: bool = True,
+        patch_size: int | tuple[int, int] | None = None,
+        dim_head: int = 32,
+        window_size: int = 8,
+        dim_ff: int | None = None,
+        shared_kv: bool = False,
+        norm_layer: Callable[[int], nn.Module] = LayerNormNoBias2d,
+        input_norm: Callable[[int], nn.Module] = LayerNormNoBias2d,
+        drop_rate: float = 0.0,
+        attn_drop_rate: float = 0.0,
+        drop_block_rate: float = 0.0,
+        drop_block_size: int = 7,
+        bias: bool = False,
+    ):
+        super().__init__()
+
+        if patch_size is not None:
+            patch_embedding = PatchEmbedding(dim_in, dim_out, patch_size=patch_size, norm_layer=input_norm, bias=bias)
+        else:
+            patch_embedding = nn.Identity()
+        blocks: list[S2ViTBlock | PEG] = []
+        for i in range(depth):
+            if i == 1 and use_peg and patch_size is not None:
+                blocks.append(PEG(dim_out))
+            block = S2ViTBlock(
+                dim_out,
+                dim_head=dim_head,
+                window_size=window_size,
+                dim_ff=dim_ff,
+                shared_kv=shared_kv,
+                norm_layer=norm_layer,
+                attn_drop_rate=attn_drop_rate,
+                ff_drop_rate=drop_rate,
+                drop_block_rate=drop_block_rate,
+                drop_block_size=drop_block_size,
+                bias=bias,
+            )
+            blocks.append(block)
+        self.patch_embedding = patch_embedding
+        self.blocks = nn.Sequential(*blocks)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.patch_embedding(x)
+        x = self.blocks(x)
+        return x
+
+
+class S2ViT(nn.Module):
+    def __init__(
+        self,
+        depths: Sequence[int] = (2, 2, 6, 2),
+        dims: Sequence[int] = (64, 128, 160, 320),
+        patch_sizes: Sequence[int | tuple[int, int]] = (4, 2, 2, 2),
+        in_channels: int = 3,
+        global_pool: bool = False,
+        num_classes: int | None = None,
+        use_peg: bool = True,
+        dim_head: int = 32,
+        window_sizes: Sequence[int] = (8, 8, 8, 8),
+        ff_expansions: Sequence[int] = (4, 4, 4, 4),
+        shared_kv: bool = False,
+        norm_layer: Callable[[int], nn.Module] = LayerNormNoBias2d,
+        input_norm: Callable[[int], nn.Module] = LayerNormNoBias2d,
+        output_norm: Callable[[int], nn.Module] = LayerNormNoBias,
+        drop_rate: float = 0.0,
+        attn_drop_rate: float = 0.0,
+        drop_block_rate: float = 0.0,
+        drop_block_size: int = 7,
+        bias: bool = False,
+        **kwargs: Any,
+    ):
+        super().__init__()
+
+        if "block_size" in kwargs:
+            warnings.warn(
+                "block_size is deprecated, use window_sizes instead", category=DeprecationWarning, stacklevel=1
+            )
+            window_sizes = [kwargs.pop("block_size")] * len(depths)
+        if "block_sizes" in kwargs:
+            warnings.warn(
+                "block_sizes is deprecated, use window_sizes instead", category=DeprecationWarning, stacklevel=1
+            )
+            window_sizes = kwargs.pop("block_sizes")
+
+        stages: list[S2ViTStage] = []
+        for dim_in, dim_out, depth, patch_size, window_size, ff_expansion in zip(
+            (in_channels, *dims[:-1]), dims, depths, patch_sizes, window_sizes, ff_expansions, strict=True
+        ):
+            stage = S2ViTStage(
+                dim_in,
+                dim_out,
+                depth,
+                use_peg=use_peg,
+                patch_size=patch_size,
+                dim_head=dim_head,
+                window_size=window_size,
+                dim_ff=dim_out * ff_expansion,
+                shared_kv=shared_kv,
+                norm_layer=norm_layer,
+                input_norm=input_norm,
+                drop_rate=drop_rate,
+                attn_drop_rate=attn_drop_rate,
+                drop_block_rate=drop_block_rate,
+                drop_block_size=drop_block_size,
+                bias=bias,
+            )
+            stages.append(stage)
+        self.encoder = nn.Sequential(*stages)
+        self.head = nn.Sequential(
+            OrderedDict(
+                [
+                    ("global_pool", Reduce("b c h w -> b c", "mean") if global_pool else nn.Identity()),
+                    ("norm", output_norm(dims[-1])),
+                    ("drop", nn.Dropout(drop_rate)),
+                    ("fc", nn.Linear(dims[-1], num_classes, bias=bias) if num_classes else nn.Identity()),
+                ]
+            )
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.encoder(x)
+        x = self.head(x)
+        return x
+
+
+__all__ = ["S2ViT", "S2ViTBlock", "S2ViTStage"]
