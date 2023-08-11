97a991cf21218577be14cb9b2648b664efac1eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:09:57 +0000

    Add package alissonSomav4/0.1/alissonSomav4-0.1.tar.gz

---
+++ packages/alissonSomav4/alissonSomav4-0.1.tar.gz/alissonSomav4-0.1/alissonSomav4/__init__.py
@@ -0,0 +1,1 @@
+from .somav2 import alissonSomav3
---
+++ packages/alissonSomav4/alissonSomav4-0.1.tar.gz/alissonSomav4-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alissonSomav4',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Pacote simples de soma',
+    install_requires=[],
+    url='https://github.com/Alisson-Rodrigo',
+    author='Alisson')
