04d2b2c0818bda5aac1975fc13533fc6f14a54b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 15:08:48 +0000

    Add package d8analysis/0.1.1/d8analysis-0.1.1-py3-none-any.whl

---
+++ packages/d8analysis/d8analysis-0.1.1-py3-none-any.whl/d8analysis/container.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Enter Project Name in Workspace Settings                                            #
+# Version    : 0.1.19                                                                              #
+# Python     : 3.10.10                                                                             #
+# Filename   : /d8analysis/container.py                                                            #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : Enter URL in Workspace Settings                                                     #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Monday March 27th 2023 07:02:56 pm                                                  #
+# Modified   : Thursday August 10th 2023 10:32:21 pm                                               #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
+"""Framework Dependency Container"""
+import logging.config  # pragma: no cover
+
+from dependency_injector import containers, providers
+
+
+# ------------------------------------------------------------------------------------------------ #
+#                                        LOGGING                                                   #
+# ------------------------------------------------------------------------------------------------ #
+class LoggingContainer(containers.DeclarativeContainer):
+    config = providers.Configuration()
+
+    logging = providers.Resource(
+        logging.config.dictConfig,
+        config=config,
+    )
+
+
+# ------------------------------------------------------------------------------------------------ #
+#                                          FRAMEWORK                                               #
+# ------------------------------------------------------------------------------------------------ #
+class d8analysisContainer(containers.DeclarativeContainer):
+    log_config = providers.Configuration(yaml_files=["config/logging.yml"])
+
+    logs = providers.Container(LoggingContainer, config=log_config.logging)
