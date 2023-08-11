6393c08d3a669a098fa0db2a28e165c5af0ff65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:07:57 +0000

    Add package cdp-socket/1.0.2/cdp-socket-1.0.2.tar.gz

---
+++ packages/cdp-socket/cdp-socket-1.0.2.tar.gz/cdp-socket-1.0.2/src/cdp_socket/scripts/abstract.py
@@ -0,0 +1,23 @@
+from cdp_socket.socket import SingleCDPSocket
+import uuid
+import asyncio
+
+
+class CDPEventIter(object):
+    def __init__(self, method, socket: SingleCDPSocket):
+        self._method = method
+        self._socket = socket
+        self._id = uuid.uuid4().hex
+        self._fut = None
+
+    def __aiter__(self):
+        return self
+
+    def _new_fut(self, result=None):
+        self._fut = asyncio.Future()
+        # noinspection PyProtectedMember
+        self._socket._iter_callbacks[self._method][self._id] = self._fut.set_result
+
+    async def __anext__(self) -> asyncio.Future:
+        self._new_fut()
+        return await self._fut
