b3659a8d8a5f57b34654d8da664381ea9412f750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:11:29 +0000

    Add package owntrack2gpx/0.1.6/owntrack2gpx-0.1.6.tar.gz

---
+++ packages/owntrack2gpx/owntrack2gpx-0.1.6.tar.gz/owntrack2gpx-0.1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="owntrack2gpx",
+    version="0.1.6",
+    packages=find_packages(),
+    install_requires=[
+        "gpx==0.2.1",
+        "gpxpy==1.5.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "owntrack2gpx=src:main",
+        ],
+    },
+    long_description="OwnTrack have its own tool ocat but with it you loose"
+    "some data like battery and speed data. For this reason you should export data to"
+    "json and then use this tool to convert to gpx.",
+)
