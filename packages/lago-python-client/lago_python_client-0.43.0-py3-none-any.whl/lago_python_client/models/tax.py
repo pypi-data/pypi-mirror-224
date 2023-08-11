48ec4ae5195e78ef74e3d1befb5bef51874c615b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:13:29 +0000

    Add package lago-python-client/0.43.0/lago_python_client-0.43.0-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-0.43.0-py3-none-any.whl/lago_python_client/models/tax.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from ..base_model import BaseResponseModel
+
+
+class Tax(BaseModel):
+    name: Optional[str]
+    code: Optional[str]
+    rate: Optional[float]
+    description: Optional[str]
+    applied_to_organization: Optional[bool]
+
+
+class Taxes(BaseModel):
+    __root__: List[Tax]
+
+
+class TaxResponse(BaseResponseModel):
+    lago_id: str
+    name: str
+    code: str
+    rate: float
+    description: Optional[str]
+    customers_count: int
+    plans_count: int
+    charges_count: int
+    applied_to_organization: bool
+    created_at: str
+
+
+class TaxesResponse(BaseResponseModel):
+    __root__: List[TaxResponse]
