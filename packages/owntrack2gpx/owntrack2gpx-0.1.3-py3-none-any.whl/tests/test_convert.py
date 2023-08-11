556c1adb815f6075a6b911c5746b177a851eaab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:11:15 +0000

    Add package owntrack2gpx/0.1.3/owntrack2gpx-0.1.3-py3-none-any.whl

---
+++ packages/owntrack2gpx/owntrack2gpx-0.1.3-py3-none-any.whl/tests/test_convert.py
@@ -0,0 +1,21 @@
+import json
+import unittest
+
+import freezegun
+
+from convert import owntrack2gpx
+
+
+class OwnTrack2GPX(unittest.TestCase):
+    @freezegun.freeze_time("2023-07-26T13:20:25Z")
+    def test_owntrack2gpx_integration(self):
+        with open('./tests/data.json', 'r') as data:
+            with open('./tests/expected.gpx', 'r') as expected:
+                j = json.load(data)
+                o = owntrack2gpx(j).to_xml().splitlines()
+                er = expected.read().splitlines()
+                self.assertEqual(o[2:], er[2:])
+
+
+if __name__ == '__main__':
+    unittest.main()
