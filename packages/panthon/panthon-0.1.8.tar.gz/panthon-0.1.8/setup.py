dba9a8c2aaf632f9f4cad5278adb5e1296d2187c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:09:20 +0000

    Add package panthon/0.1.8/panthon-0.1.8.tar.gz

---
+++ packages/panthon/panthon-0.1.8.tar.gz/panthon-0.1.8/setup.py
@@ -0,0 +1,33 @@
+"""Setup file for the Panthon package.
+
+Specifies package details and dependencies.
+"""
+from setuptools import setup, find_packages
+
+# # Read in requirements.txt with specified encoding
+# with open("~/panthon/requirements.txt", "r", encoding="utf-8") as f:
+#     requirements = f.read().splitlines()
+
+requirements = ["torch", "scapy", "netifaces", "bs4", "tld", "fuzzywuzzy", "requests"]
+
+setup(
+    name="panthon",
+    version="0.1.8",
+    url="https://github.com/nripeshn/panthon",
+    author="Nripesh Niketan",
+    author_email="nripesh14@gmail.com",
+    description="A Machine Learning-powered Cybersecurity Attack Simulation Library",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
---
+++ packages/panthon/panthon-0.1.8.tar.gz/panthon-0.1.8/src/__init__.py
@@ -0,0 +1,7 @@
+from src.dos import DoSAttack
+from .ddos_attack import DDoSAttack
+from .sql_injection import SQLInjectionAttack
+from .mitm_attack import MITMAttack
+from .dns_spoofer import DNSSpoofer
+from .xss_attack import XSSAttack
+from .random_string_generator import RandomStringGenerator
