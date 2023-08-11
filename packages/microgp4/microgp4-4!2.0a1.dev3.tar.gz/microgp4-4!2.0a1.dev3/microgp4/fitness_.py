512218ab14f018eaa56260263d6a2925ae1bc85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:12:33 +0000

    Add package microgp4/4!2.0a1.dev3/microgp4-4!2.0a1.dev3.tar.gz

---
+++ packages/microgp4/microgp4-4!2.0a1.dev3.tar.gz/microgp4-4!2.0a1.dev3/microgp4/fitness_.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#           __________                                                      #
+#    __  __/ ____/ __ \__ __   This file is part of MicroGP v4!2.0          #
+#   / / / / / __/ /_/ / // /   A versatile evolutionary optimizer & fuzzer  #
+#  / /_/ / /_/ / ____/ // /_   https://github.com/microgp/microgp4          #
+#  \__  /\____/_/   /__  __/                                                #
+#    /_/ --MicroGP4-- /_/      You don't need a big goal, be μ-ambitious!   #
+#                                                                           #
+#############################################################################
+
+# Copyright 2022-23 Giovanni Squillero and Alberto Tonda
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# =[ HISTORY ]===============================================================
+# v1 / May 2023 / Squillero (GX)
+
+from microgp4.classes.fitness import FitnessABC
+from microgp4.fitness import *
