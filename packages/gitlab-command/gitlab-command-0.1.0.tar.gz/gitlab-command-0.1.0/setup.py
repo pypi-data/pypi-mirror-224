e08fe7a749835a8ac4c72a0cba5389523f084af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:09:18 +0000

    Add package gitlab-command/0.1.0/gitlab-command-0.1.0.tar.gz

---
+++ packages/gitlab-command/gitlab-command-0.1.0.tar.gz/gitlab-command-0.1.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['gl']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bump-pydantic>=0.6.1,<0.7.0',
+ 'pydantic-settings>=2.0.2,<3.0.0',
+ 'pydantic>=2.1.1,<3.0.0',
+ 'python-gitlab>=3.15.0,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['gl = gl.main:main']}
+
+setup_kwargs = {
+    'name': 'gitlab-command',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jason Viloria',
+    'author_email': 'jason.viloria@optiscangroup.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
