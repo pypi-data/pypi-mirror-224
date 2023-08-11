7d286b006ae39aff4f004e6f95c135dc7fafbe99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:08:52 +0000

    Add package ipylab/1.0.0/ipylab-1.0.0.tar.gz

---
+++ packages/ipylab/ipylab-1.0.0.tar.gz/ipylab-1.0.0/src/plugin.ts
@@ -0,0 +1,55 @@
+// Copyright (c) ipylab contributors
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  JupyterFrontEndPlugin,
+  JupyterFrontEnd,
+  ILabShell
+} from '@jupyterlab/application';
+
+import { ICommandPalette } from '@jupyterlab/apputils';
+
+import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
+
+import { MODULE_NAME, MODULE_VERSION } from './version';
+
+const EXTENSION_ID = 'ipylab:plugin';
+
+/**
+ * The default plugin.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: EXTENSION_ID,
+  autoStart: true,
+  requires: [IJupyterWidgetRegistry],
+  optional: [ICommandPalette, ILabShell],
+  activate: (
+    app: JupyterFrontEnd,
+    registry: IJupyterWidgetRegistry,
+    palette: ICommandPalette,
+    labShell: ILabShell | null
+  ): void => {
+    registry.registerWidget({
+      name: MODULE_NAME,
+      version: MODULE_VERSION,
+      exports: async () => {
+        const widgetExports = await import('./widget');
+
+        // add globals
+        widgetExports.JupyterFrontEndModel.app = app;
+        widgetExports.ShellModel.shell = app.shell;
+        widgetExports.ShellModel.labShell = labShell;
+        widgetExports.CommandRegistryModel.commands = app.commands;
+        widgetExports.CommandPaletteModel.palette = palette;
+        widgetExports.SessionManagerModel.sessions =
+          app.serviceManager.sessions;
+        widgetExports.SessionManagerModel.shell = app.shell;
+        widgetExports.SessionManagerModel.labShell = labShell;
+
+        return widgetExports;
+      }
+    });
+  }
+};
+
+export default extension;
