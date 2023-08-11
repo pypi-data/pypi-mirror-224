2491b8db93334327584ee87dcb530db6c04d61fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:10:03 +0000

    Add package specmatic/0.15.0/specmatic-0.15.0.tar.gz

---
+++ packages/specmatic/specmatic-0.15.0.tar.gz/specmatic-0.15.0/specmatic/coverage/coverage_route.py
@@ -0,0 +1,4 @@
+class CoverageRoute:
+    def __init__(self, url, methods):
+        self.url = url
+        self.methods = methods
---
+++ packages/specmatic/specmatic-0.15.0.tar.gz/specmatic-0.15.0/specmatic/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.15.0'
+__specmatic_version__ = '0.74.0'
