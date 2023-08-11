1834303a67ab366039a56bd6a1a57c598b13bedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:09:24 +0000

    Add package hpcflow-new2/0.2.0a76/hpcflow_new2-0.2.0a76-py3-none-any.whl

---
+++ packages/hpcflow-new2/hpcflow_new2-0.2.0a76-py3-none-any.whl/hpcflow/tests/unit/test_submission.py
@@ -0,0 +1,169 @@
+from datetime import timedelta
+from hpcflow.sdk.submission.jobscript import group_resource_map_into_jobscripts
+from hpcflow.sdk.submission.submission import timedelta_format, timedelta_parse
+
+
+def test_group_resource_map_into_jobscripts():
+    # x-axis corresponds to elements; y-axis corresponds to actions:
+    examples = (
+        {
+            "resources": [
+                [1, 1, 1, 2, -1, 2, 4, -1, 1],
+                [1, 3, 1, 2, 2, 2, 4, 4, 1],
+                [1, 1, 3, 2, 2, 2, 4, -1, 1],
+            ],
+            "expected": [
+                {
+                    "resources": 1,
+                    "elements": {0: [0, 1, 2], 1: [0], 2: [0, 1], 8: [0, 1, 2]},
+                },
+                {"resources": 2, "elements": {3: [0, 1, 2], 4: [1, 2], 5: [0, 1, 2]}},
+                {"resources": 4, "elements": {6: [0, 1, 2], 7: [1]}},
+                {"resources": 3, "elements": {1: [1]}},
+                {"resources": 1, "elements": {1: [2]}},
+                {"resources": 3, "elements": {2: [2]}},
+            ],
+        },
+        {
+            "resources": [
+                [2, 2, -1],
+                [8, 8, 1],
+                [4, 4, 1],
+            ],
+            "expected": [
+                {"resources": 2, "elements": {0: [0], 1: [0]}},
+                {"resources": 1, "elements": {2: [1, 2]}},
+                {"resources": 8, "elements": {0: [1], 1: [1]}},
+                {"resources": 4, "elements": {0: [2], 1: [2]}},
+            ],
+        },
+        {
+            "resources": [
+                [2, 2, -1],
+                [2, 2, 1],
+                [4, 4, 1],
+            ],
+            "expected": [
+                {"resources": 2, "elements": {0: [0, 1], 1: [0, 1]}},
+                {"resources": 1, "elements": {2: [1, 2]}},
+                {"resources": 4, "elements": {0: [2], 1: [2]}},
+            ],
+        },
+        {
+            "resources": [
+                [2, 1, 2],
+                [1, 1, 1],
+                [1, 1, 1],
+            ],
+            "expected": [
+                {"resources": 1, "elements": {1: [0, 1, 2]}},
+                {"resources": 2, "elements": {0: [0], 2: [0]}},
+                {"resources": 1, "elements": {0: [1, 2], 2: [1, 2]}},
+            ],
+        },
+        {
+            "resources": [
+                [2, -1, 2],
+                [1, 1, 1],
+                [1, 1, 1],
+            ],
+            "expected": [
+                {"resources": 2, "elements": {0: [0], 2: [0]}},
+                {"resources": 1, "elements": {0: [1, 2], 1: [1, 2], 2: [1, 2]}},
+            ],
+        },
+        {
+            "resources": [
+                [1, 1],
+                [1, 1],
+                [1, 1],
+            ],
+            "expected": [{"resources": 1, "elements": {0: [0, 1, 2], 1: [0, 1, 2]}}],
+        },
+        {
+            "resources": [
+                [1, 1, 1],
+                [1, 1, -1],
+                [1, 1, 1],
+            ],
+            "expected": [
+                {"resources": 1, "elements": {0: [0, 1, 2], 1: [0, 1, 2], 2: [0, 2]}}
+            ],
+        },
+        {
+            "resources": [
+                [1, 1, -1],
+                [1, 1, 1],
+                [1, 1, 1],
+            ],
+            "expected": [
+                {"resources": 1, "elements": {0: [0, 1, 2], 1: [0, 1, 2], 2: [1, 2]}}
+            ],
+        },
+        {
+            "resources": [
+                [2, 2, -1],
+                [4, 4, 1],
+                [4, 4, -1],
+                [2, 2, 1],
+            ],
+            "expected": [
+                {"resources": 2, "elements": {0: [0], 1: [0]}},
+                {"resources": 1, "elements": {2: [1, 3]}},
+                {"resources": 4, "elements": {0: [1, 2], 1: [1, 2]}},
+                {"resources": 2, "elements": {0: [3], 1: [3]}},
+            ],
+        },
+        {
+            "resources": [
+                [2, 2, -1],
+                [4, 4, 1],
+                [4, 4, -1],
+                [1, 1, 1],
+            ],
+            "expected": [
+                {"resources": 2, "elements": {0: [0], 1: [0]}},
+                {"resources": 1, "elements": {2: [1, 3]}},
+                {"resources": 4, "elements": {0: [1, 2], 1: [1, 2]}},
+                {"resources": 1, "elements": {0: [3], 1: [3]}},
+            ],
+        },
+        {
+            "resources": [
+                [2, 2, -1],
+                [4, 4, 1],
+                [4, 8, -1],
+                [1, 1, 1],
+            ],
+            "expected": [
+                {"resources": 2, "elements": {0: [0], 1: [0]}},
+                {"resources": 1, "elements": {2: [1, 3]}},
+                {"resources": 4, "elements": {0: [1, 2], 1: [1]}},
+                {"resources": 8, "elements": {1: [2]}},
+                {"resources": 1, "elements": {0: [3], 1: [3]}},
+            ],
+        },
+        {
+            "resources": [
+                [2, 2, -1],
+                [4, 4, 1],
+                [4, -1, -1],
+                [1, 1, 1],
+            ],
+            "expected": [
+                {"resources": 2, "elements": {0: [0], 1: [0]}},
+                {"resources": 1, "elements": {2: [1, 3]}},
+                {"resources": 4, "elements": {0: [1, 2], 1: [1]}},
+                {"resources": 1, "elements": {0: [3], 1: [3]}},
+            ],
+        },
+    )
+    for i in examples:
+        jobscripts_i, _ = group_resource_map_into_jobscripts(i["resources"])
+        assert jobscripts_i == i["expected"]
+
+
+def test_timedelta_parse_format_round_trip():
+    td = timedelta(days=2, hours=25, minutes=92, seconds=77)
+    td_str = timedelta_format(td)
+    assert td_str == timedelta_format(timedelta_parse(td_str))
---
+++ packages/hpcflow-new2/hpcflow_new2-0.2.0a76-py3-none-any.whl/hpcflow/tests/workflows/test_workflows.py
@@ -0,0 +1,22 @@
+from importlib import resources
+import sys
+import pytest
+from hpcflow.app import app as hf
+
+
+@pytest.fixture
+def null_config(tmp_path):
+    if not hf.is_config_loaded:
+        hf.load_config(config_dir=tmp_path)
+
+
+@pytest.mark.skipif(
+    sys.platform != "win32",
+    reason="fails for non-windows - need to implement conditional actions based on OS name.",
+)
+def test_workflow_1(tmp_path, null_config):
+    package = "hpcflow.sdk.demo.data"
+    with resources.path(package=package, resource="workflow_1.yaml") as path:
+        wk = hf.Workflow.from_YAML_file(YAML_path=path, path=tmp_path)
+    wk.submit(wait=True)
+    assert wk.tasks[0].elements[0].outputs.p2.value == "201"
