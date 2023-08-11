66687d8c7b9eb9c3803ee7bcf82c75dc1bf205b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 15:08:59 +0000

    Add package naist-codehash/0.1.0/naist-codehash-0.1.0.tar.gz

---
+++ packages/naist-codehash/naist-codehash-0.1.0.tar.gz/naist-codehash-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# Author: Kazuki F. <doradora.prog@gmail.com>
+# Copyright (c) 2023 Kazuki F.
+# LICENSE: MIT
+
+from setuptools import setup
+setup(
+    name="naist-codehash",
+    version="0.1.0",
+    description="Support the use of CodeHash, a tool for measuring similarity between source code, from Python.",
+    author="Kazuki F.",
+    author_email="doradora.prog@gmail.com",
+    url="https://github.com/pgDora56/pycodehash",
+    packages=["codehash"],
+)
