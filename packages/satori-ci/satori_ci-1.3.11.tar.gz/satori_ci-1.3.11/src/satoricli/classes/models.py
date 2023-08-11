93e6c0c4168ca645fdce3ea03781ea4f8e8b19b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 06:12:45 +0000

    Add package satori-ci/1.3.11/satori_ci-1.3.11.tar.gz

---
+++ packages/satori-ci/satori_ci-1.3.11.tar.gz/satori_ci-1.3.11/src/satoricli/classes/models.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Any, Union
+from argparse import Namespace
+
+
+@dataclass
+class args:
+    id: str
+    action: str
+    profile: str
+    debug: bool
+    json: bool
+    path: str
+    data: Any
+    sync: bool
+    timeout: int
+    playbook: str
+    page: int
+    public: bool
+    report: bool
+    output: bool
+    config_name: str
+    config_value: str
+    email: str
+    clean: bool
+    deleted: bool
+    files: bool
+
+
+arguments = Union[Namespace, args]
