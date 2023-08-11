f1fd75e7dc838c6475237be43b6ceec2a6dcde33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:10:01 +0000

    Add package basic-type-operations/0.0.6/basic_type_operations-0.0.6-py3-none-any.whl

---
+++ packages/basic-type-operations/basic_type_operations-0.0.6-py3-none-any.whl/basic_type_operations/dict_operation.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+"""
+@Time : 2023/3/29
+@Author : skyoceanchen
+@TEL: 18916403796
+@File : dict_operation.py 
+@PRODUCT_NAME : PyCharm 
+"""
+from collections import Counter
+from munch import Munch  ## munch-2.5.0
+
+
+# <editor-fold desc="字典类工具">
+class DictOperation(object):
+    @staticmethod
+    def twodictadd(dict1, dict2):
+        dict_new = dict(Counter(dict1) + Counter(dict2))
+        return dict_new
+
+    # <editor-fold desc="取出列表内所有字典的value值">
+    @staticmethod
+    def list_dict_value(lis):
+        """
+        :param lis:
+        :return:
+        """
+        res = [item[key] for item in lis for key in item]
+        return res
+
+    # </editor-fold>
+    # <editor-fold desc="取出列表内所有字典的keys值">
+    @staticmethod
+    def list_dict_keys(lis):
+        """
+        :param lis:
+        :return:
+        """
+        res = [key for item in lis for key in item]
+        return res
+
+    # </editor-fold>
+    # <editor-fold desc="字典组成的数组怎么进行去重">
+    @staticmethod
+    def dic_duplicate_remove(data):  # 适用一般情况
+        # data = reduce(lambda x, y: x + [y] if y not in x else x, [[], ] + data)#
+        data = [dict(t) for t in set([tuple(d.items()) for d in data])]
+        return data
+
+    @staticmethod
+    def delete_duplicate_str(data):  # 适用这种情况如： data2 = [{"a": {"b": "c"}}, {"a": {"b": "c"}}]
+        immutable_dict = set([str(item) for item in data])
+        data = [eval(i) for i in immutable_dict]
+        return data
+
+    # </editor-fold>
+    # <editor-fold desc="列表中的字典按某个字段排序">
+    @staticmethod
+    def list_order(data: list, fields=None):
+        """
+        :param data:
+        :param value:list 传入 索引  字典传入keys
+        :return: list
+        """
+        if fields:
+            data: list = sorted(data, key=lambda k: k[fields])
+        else:
+            data.sort()
+        return data
+    # </editor-fold>
+
+
+# </editor-fold>
+# Munch的使用
+"""
+from munch import Munch
+
+profile = Munch()
+print(isinstance(profile, dict))
+# 并实现了点式赋值与访问，profile.name 与 profile['name'] 是等价的
+profile.name = "iswbm"
+print(profile)
+profile.name = "iswbm1111"
+print(profile)
+print(Munch({'name': 'iswbm', 'age': 18, 'gender': 'male'}))
+# 删除元素
+profile.setdefault('gender')
+print(profile)
+# 转换成 JSON
+munch_obj = Munch(foo=Munch(lol=True), bar=100, msg='hello')
+#
+"""
