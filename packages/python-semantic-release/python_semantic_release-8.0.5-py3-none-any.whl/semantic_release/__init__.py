36ca660d2341afc3e138686aeb7b24779775f079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:09:35 +0000

    Add package python-semantic-release/8.0.5/python_semantic_release-8.0.5-py3-none-any.whl

---
+++ packages/python-semantic-release/python_semantic_release-8.0.5-py3-none-any.whl/semantic_release/__init__.py
@@ -0,0 +1,41 @@
+"""Python Semantic Release
+"""
+from __future__ import annotations
+
+from semantic_release.commit_parser import (
+    CommitParser as CommitParser,
+    ParsedCommit as ParsedCommit,
+    ParseError as ParseError,
+    ParseResult as ParseResult,
+    ParseResultType as ParseResultType,
+    ParserOptions as ParserOptions,
+)
+from semantic_release.enums import LevelBump as LevelBump
+from semantic_release.errors import (
+    CommitParseError as CommitParseError,
+    InvalidConfiguration as InvalidConfiguration,
+    InvalidVersion as InvalidVersion,
+    SemanticReleaseBaseError as SemanticReleaseBaseError,
+)
+from semantic_release.version import (
+    Version as Version,
+    VersionTranslator as VersionTranslator,
+    next_version as next_version,
+    tags_and_versions as tags_and_versions,
+)
+
+__version__ = "8.0.5"
+
+
+def setup_hook(argv: list[str]) -> None:
+    """
+    A hook to be used in setup.py to enable `python setup.py publish`.
+
+    :param argv: sys.argv
+    """
+    if len(argv) > 1 and any(
+        cmd in argv for cmd in ["version", "publish", "changelog"]
+    ):
+        from .cli import main
+
+        main()
