9abc1d2dca9ba3866555c831a837c60e4645a8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:11:13 +0000

    Add package zephyr-library/1.0.7/zephyr_library-1.0.7.tar.gz

---
+++ packages/zephyr-library/zephyr_library-1.0.7.tar.gz/zephyr_library-1.0.7/setup.py
@@ -0,0 +1,9 @@
+"""
+Building and Distributing Packages with SetupTools:
+Check the documentation here: https://setuptools.pypa.io/en/latest/userguide/index.html
+The code below is the launcher!
+"""
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup()
