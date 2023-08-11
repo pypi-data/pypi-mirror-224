1d0a1f92b5c67ca78266117dd24ff65a98df3ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:08:47 +0000

    Add package common-utils-pkg/0.1.6/common_utils_pkg-0.1.6.tar.gz

---
+++ packages/common-utils-pkg/common_utils_pkg-0.1.6.tar.gz/common_utils_pkg-0.1.6/common_utils_pkg/api_server.py
@@ -0,0 +1,28 @@
+from flask import Flask, Response, request
+
+
+class EndpointAction(object):
+    def __init__(self, action):
+        self.action = action
+        self.response = Response(status=200, headers={})
+
+    def __call__(self, *args):
+        result = self.action()
+        if result != None:
+            response = Response(response=result, status=200, headers={})
+            return response
+        else:
+            return self.response
+
+
+class FlaskAppWrapper(object):
+    app = None
+
+    def __init__(self, name):
+        self.app = Flask(name)
+
+    def run(self, host="0.0.0.0", port="80"):
+        self.app.run(host=host, port=port)
+
+    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, methods=None):
+        self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler), methods=methods)
