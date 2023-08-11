c5f8ce0400aae3d8e4520705c793018162d4e1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 06:13:24 +0000

    Add package chia-base/0.1.2/chia_base-0.1.2.tar.gz

---
+++ packages/chia-base/chia_base-0.1.2.tar.gz/chia_base-0.1.2/chia_base/atoms/__init__.py
@@ -0,0 +1,17 @@
+from .hexbytes import hexbytes
+from .ints import int8, uint8, int16, uint16, int32, uint32, int64, uint64
+from .sized_bytes import bytes32
+
+
+__all__ = [
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "hexbytes",
+    "bytes32",
+]
---
+++ packages/chia-base/chia_base-0.1.2.tar.gz/chia_base-0.1.2/chia_base/meta/streamable.py
@@ -0,0 +1,49 @@
+from typing import Callable, Type, BinaryIO, TypeVar
+
+import io
+
+from .make_parser import make_parser
+from .make_streamer import make_streamer
+
+
+_T = TypeVar("_T")
+_U = TypeVar("_U")
+
+
+class Streamable:
+    def __init_subclass__(subclass):
+        super().__init_subclass__()
+        Streamable.__build_stream_and_parse(subclass)
+
+    @classmethod
+    def __build_stream_and_parse(cls: Type["Streamable"], subclass: Type["Streamable"]):
+        """
+        Augment the subclass with two dynamically generated methods:
+        _class_stream: Callable[[Type[_T], _T, BinaryIO], None]
+        _parse: Callable[Type[_T], BinaryIO], _T]
+        """
+        subclass._class_stream = make_streamer(subclass)
+        subclass._parse = make_parser(subclass)
+
+    _class_stream: Callable[[BinaryIO], None]
+    _parse: Callable[[Type[_T], BinaryIO], _T]
+
+    @classmethod
+    def from_bytes(cls: Type["Streamable"], blob: bytes) -> "Streamable":
+        return cls.parse(io.BytesIO(blob))
+
+    @classmethod
+    def fromhex(cls: Type["Streamable"], s: str) -> "Streamable":
+        return cls.from_bytes(bytes.fromhex(s))
+
+    @classmethod
+    def parse(cls: Type["Streamable"], f: BinaryIO) -> "Streamable":
+        return cls._parse(cls, f)
+
+    def stream(self, f: BinaryIO) -> None:
+        return self._class_stream(f)
+
+    def __bytes__(self):
+        f = io.BytesIO()
+        self.stream(f)
+        return f.getvalue()
