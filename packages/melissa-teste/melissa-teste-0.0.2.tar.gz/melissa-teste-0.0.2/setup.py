e398c2c1f996f34300d095a74efec8f59f82a3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:10:35 +0000

    Add package melissa-teste/0.0.2/melissa-teste-0.0.2.tar.gz

---
+++ packages/melissa-teste/melissa-teste-0.0.2.tar.gz/melissa-teste-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Meu primeiro pacote'
+LONG_DESCRIPTION = 'Meu primeiro projeto em Python com uma descrição um pouco mais longa'
+
+setup(
+        name = "melissa-teste",
+        version = VERSION,
+        author = "Melissa",
+        author_email = "msnmelissaoa15@hotmail.com",
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        packages = find_packages(),
+        install_requires = [],
+
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent"
+        ]
+)
