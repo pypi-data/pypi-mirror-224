9eb389aa974046de72fc5112cbe9574430f1db89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:11:01 +0000

    Add package iam-ssh-cli/1.0.1/iam_ssh_cli-1.0.1-py3-none-any.whl

---
+++ packages/iam-ssh-cli/iam_ssh_cli-1.0.1-py3-none-any.whl/iam-ssh-cli/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Copyright 2023 Chathuranga Abeyrathna. All Rights Reserved.
+# iam-ssh-cli to sync IAM ssh keys to Linux boxes
+# 
+
+"""
+iam-ssh-cli
+----
+sync IAM ssh keys to Linux boxes.
+"""
+
