d0a81b58d43abb053f6bb56c3fe85bfe8a7086f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:08:37 +0000

    Add package metricsoperator/0.0.16/metricsoperator-0.0.16.tar.gz

---
+++ packages/metricsoperator/metricsoperator-0.0.16.tar.gz/metricsoperator-0.0.16/metricsoperator/metrics/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+
+import metricsoperator.metrics.application as apps
+import metricsoperator.metrics.network as network
+import metricsoperator.metrics.perf as perf
+import metricsoperator.metrics.storage as storage
+
+metrics = {
+    "io-sysstat": storage.io_sysstat,
+    "network-osu-benchmark": network.network_osu_benchmark,
+    "network-netmark": network.network_netmark,
+    "perf-sysstat": perf.perf_sysstat,
+    "io-fio": storage.io_fio,
+    "app-lammps": apps.app_lammps,
+}
+
+
+def get_metric(name):
+    """
+    Get a named metric parser.
+    """
+    metric = metrics.get(name)
+    if not metric:
+        raise ValueError(f"Metric {name} does not have a known parser")
+    return metric
