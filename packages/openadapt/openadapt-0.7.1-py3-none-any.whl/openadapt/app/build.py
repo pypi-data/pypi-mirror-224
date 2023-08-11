d15ea1b3b4a0ca98e0554fc78c2460d0bed9c3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:08:47 +0000

    Add package openadapt/0.7.1/openadapt-0.7.1-py3-none-any.whl

---
+++ packages/openadapt/openadapt-0.7.1-py3-none-any.whl/openadapt/app/build.py
@@ -0,0 +1,39 @@
+"""openadapt.app.build module.
+
+This module provides functionality for building the OpenAdapt application.
+
+Example usage:
+    python build.py
+"""
+
+from pathlib import Path
+import os
+import subprocess
+
+import nicegui
+
+spec = [
+    "pyi-makespec",
+    f"{Path(__file__).parent}/main.py",
+    f"--icon={Path(__file__).parent}/assets/logo.ico",
+    "--name",
+    "OpenAdapt",  # name
+    # "--onefile", # trade startup speed for smaller file size
+    "--onedir",
+    "--windowed",  # prevent console appearing, only use with ui.run(native=True, ...)
+    "--add-data",
+    f"{Path(nicegui.__file__).parent}{os.pathsep}nicegui",
+]
+
+subprocess.call(spec)
+
+# add import sys ; sys.setrecursionlimit(sys.getrecursionlimit() * 5)
+#  to line 2 of OpenAdapt.spec
+with open("OpenAdapt.spec", "r+") as f:
+    lines = f.readlines()
+    lines[1] = "import sys ; sys.setrecursionlimit(sys.getrecursionlimit() * 5)\n"
+    f.seek(0)
+    f.truncate()
+    f.writelines(lines)
+
+subprocess.call(["pyinstaller", "OpenAdapt.spec"])
