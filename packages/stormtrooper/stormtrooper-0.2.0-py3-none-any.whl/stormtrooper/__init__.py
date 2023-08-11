c5dfb535a6d9857d7e4f316b5bd1dae0236038b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 15:08:52 +0000

    Add package stormtrooper/0.2.0/stormtrooper-0.2.0-py3-none-any.whl

---
+++ packages/stormtrooper/stormtrooper-0.2.0-py3-none-any.whl/stormtrooper/__init__.py
@@ -0,0 +1,9 @@
+from stormtrooper.generative import (
+    GenerativeFewShotClassifier,
+    GenerativeZeroShotClassifier,
+)
+from stormtrooper.text2text import (
+    Text2TextFewShotClassifier,
+    Text2TextZeroShotClassifier,
+)
+from stormtrooper.zero_shot import ZeroShotClassifier
