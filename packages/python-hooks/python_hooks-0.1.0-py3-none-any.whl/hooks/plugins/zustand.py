f113faf27ef2be5854d2aa947f858d57f5ea11f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:08:47 +0000

    Add package python-hooks/0.1.0/python_hooks-0.1.0-py3-none-any.whl

---
+++ packages/python-hooks/python_hooks-0.1.0-py3-none-any.whl/hooks/plugins/zustand.py
@@ -0,0 +1,29 @@
+# mypy: ignore-errors
+
+from typing import Any, Callable
+
+from box import Box
+
+from hooks import create_context, set_context_value, use_context
+
+StateSelector = Callable[[Any], Any]
+SetTyping = Callable[[StateSelector], None]
+GetTyping = Callable[[], Box]
+
+
+class ZustandStore:
+    def __init__(self, store_config: Callable[[SetTyping, GetTyping], Any]):
+        self.context = create_context({})
+        self.setter = lambda state_selector: set_context_value(
+            self.context, state_selector(use_context(self.context))
+        )
+        self.getter = lambda: Box(use_context(self.context))
+        set_context_value(self.context, store_config(self.setter, self.getter))
+
+    def __call__(self, selector: StateSelector) -> Any:
+        return selector(self.getter())
+
+
+def create(store_config: Callable[[SetTyping, GetTyping], Any]) -> ZustandStore:
+    """ """
+    return ZustandStore(store_config)
