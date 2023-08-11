48f272186af671270b9eef0a4a81140f8c318c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:08:52 +0000

    Add package zntrack/0.6.3a2/zntrack-0.6.3a2-py3-none-any.whl

---
+++ packages/zntrack/zntrack-0.6.3a2-py3-none-any.whl/zntrack/__init__.py
@@ -0,0 +1,34 @@
+"""ZnTrack - Create, visualize, run & benchmark DVC pipelines in Python.
+
+GitHub: https://github.com/zincware/ZnTrack
+"""
+import importlib.metadata
+
+from zntrack import exceptions, tools
+from zntrack.core.load import from_rev
+from zntrack.core.node import Node
+from zntrack.core.nodify import NodeConfig, nodify
+from zntrack.fields import Field, FieldGroup, LazyField, dvc, meta, zn
+from zntrack.project import Project
+from zntrack.utils import config
+from zntrack.utils.node_wd import nwd
+
+__version__ = importlib.metadata.version("zntrack")
+
+__all__ = [
+    "Node",
+    "dvc",
+    "zn",
+    "Project",
+    "nwd",
+    "meta",
+    "config",
+    "Field",
+    "LazyField",
+    "FieldGroup",
+    "nodify",
+    "NodeConfig",
+    "tools",
+    "exceptions",
+    "from_rev",
+]
