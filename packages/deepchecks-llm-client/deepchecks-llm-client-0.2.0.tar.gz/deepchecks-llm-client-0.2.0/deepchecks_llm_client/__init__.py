a65a7c40feddc9be78439e27f0ba2b67049ddcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:08:12 +0000

    Add package deepchecks-llm-client/0.2.0/deepchecks-llm-client-0.2.0.tar.gz

---
+++ packages/deepchecks-llm-client/deepchecks-llm-client-0.2.0.tar.gz/deepchecks-llm-client-0.2.0/deepchecks_llm_client/__init__.py
@@ -0,0 +1,3 @@
+from importlib import metadata
+
+__version__ = metadata.version('deepchecks_llm_client')
---
+++ packages/deepchecks-llm-client/deepchecks-llm-client-0.2.0.tar.gz/deepchecks-llm-client-0.2.0/setup.py
@@ -0,0 +1,82 @@
+import pathlib
+import re
+import typing as t
+
+import setuptools
+
+SETUP_MODULE = pathlib.Path(__file__).absolute()
+ROOTDIR = SETUP_MODULE.parent
+VERSION_FILE = ROOTDIR / ".." / "VERSION"
+
+
+SEMANTIC_VERSIONING_RE = re.compile(
+   r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)"
+   r"(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)"
+   r"(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))"
+   r"?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+)
+
+
+PYTHON_VERSIONING_RE = re.compile(
+    r"^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*"
+    r"((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?"
+    r"(\.dev(0|[1-9][0-9]*))?$"
+)
+
+
+def is_correct_version_string(value: str) -> bool:
+    match = PYTHON_VERSIONING_RE.match(value)
+    return value == "dev" or match is not None
+
+
+def get_version_string() -> str:
+    if not (VERSION_FILE.exists() and VERSION_FILE.is_file()):
+        raise RuntimeError(
+            "Version file does not exist! "
+            f"(filepath: {str(VERSION_FILE)})")
+    else:
+        version = VERSION_FILE.open("r").readline()
+        if not is_correct_version_string(version):
+            raise RuntimeError(
+                "Incorrect version string! "
+                f"(filepath: {str(VERSION_FILE)})"
+            )
+        return version
+
+
+def read_requirements_file(path: pathlib.Path) -> t.Tuple[t.List[str], t.List[str]]:
+    if not (path.exists() and path.is_file()):
+        raise RuntimeError(f'Did not find requirements file - {path.name}')
+    dependencies = []
+    dependencies_links = []
+    for line in path.open("r").readlines():
+        if "-f" in line or "--find-links" in line:
+            dependencies_links.append(
+                line
+                .replace("-f", "")
+                .replace("--find-links", "")
+                .strip()
+            )
+        else:
+            dependencies.append(line)
+    return dependencies, dependencies_links
+
+
+# ===============================================================
+
+version = get_version_string()
+install_requires, dependency_links = read_requirements_file(ROOTDIR / 'requirements.txt')
+
+
+setuptools.setup(
+    name="deepchecks-llm-client",
+    version=version,
+    author="deepchecks",
+    author_email="info@deepchecks.com",
+    description="The SDK client for communicating with Deepchecks LLM service",
+    packages=setuptools.find_packages(where=".", include=["deepchecks_llm_client", "deepchecks_llm_client.*"]),
+    python_requires='>=3.8',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    include_package_data=True,
+)
