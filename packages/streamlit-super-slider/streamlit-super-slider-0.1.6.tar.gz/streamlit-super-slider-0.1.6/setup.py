5c1c14f026e82505031b74cff3713d8b95d1a81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:13:20 +0000

    Add package streamlit-super-slider/0.1.6/streamlit-super-slider-0.1.6.tar.gz

---
+++ packages/streamlit-super-slider/streamlit-super-slider-0.1.6.tar.gz/streamlit-super-slider-0.1.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='streamlit-super-slider',
+    version='0.1.6',
+    author='Dvir Itzkovits',
+    author_email='dvir.itzko@gmail.com',
+    description='A slider with a lot more features in it than the built in one.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/fgdvir/streamlit-super-slider',
+    # packages=['streamlit-super-slider'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",],
+    keywords=['Python', 'Streamlit', 'React', 'JavaScript'],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
