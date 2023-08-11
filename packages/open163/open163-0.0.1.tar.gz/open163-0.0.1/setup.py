96f2e89b46d4baa3b6ce856404d18143d027b02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:09:18 +0000

    Add package open163/0.0.1/open163-0.0.1.tar.gz

---
+++ packages/open163/open163-0.0.1.tar.gz/open163-0.0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(name='open163',
+      version='0.0.1',
+      description='网易公开课下载器，支持多线程',
+      url='https://github.com/JamesHoi/Open163-Downloader',
+      author='JamesHoi',
+      author_email='jameshoi@foxmail.com',
+      license='MIT',
+      packages=['open163'],
+      zip_safe=False)
