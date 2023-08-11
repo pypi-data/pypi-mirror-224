2e783cf0bd0c3c1a979791ac5c2e9ef9bf01b08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:12:42 +0000

    Add package flowmatic/0.1.0.9/flowmatic-0.1.0.9-py3-none-any.whl

---
+++ packages/flowmatic/flowmatic-0.1.0.9-py3-none-any.whl/flowmatic/forms/fields/text_field.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from typing import Any, Self
+
+import customtkinter as ctk
+
+
+class TextField:
+    master: tk.Frame
+    frame: tk.Frame
+    label: str
+    __value: tk.StringVar
+    placeholder_text: str | None
+
+    @property
+    def value(self) -> str:
+        return self.__value.get()
+
+    @value.setter
+    def value(self, value: str) -> None:
+        self.__value.set(value)
+
+    def __init__(
+        self,
+        label: str,
+        *,
+        value: str | None = None,
+        placeholder_text: str | None = None
+    ) -> None:
+        self.label = label
+        self.__value = tk.StringVar(value=value)
+        self.placeholder_text = placeholder_text
+
+    def __call__(self, master: tk.Frame) -> Self:
+        self.master = master
+        self.frame = ctk.CTkFrame(
+            self.master,
+            fg_color=ctk.ThemeManager.theme["CTkFrame"]["fg_color"],
+        )
+        return self
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, TextField):
+            return False
+        return self.value == __value.value and self.label == __value.label
+
+    def build(self, **kwargs: dict[str, Any]) -> Self:
+        self.frame.pack()
+        ctk.CTkLabel(master=self.frame, text=self.label).pack(side=tk.LEFT, **kwargs)
+        entry = ctk.CTkEntry(
+            self.frame,
+            textvariable=self.__value,
+            placeholder_text=self.placeholder_text,
+        )
+        entry.pack(side=tk.RIGHT, **kwargs)
+        return self
+
+    def pack(self, element: tk.Widget, **kwargs) -> None:
+        element.pack(**kwargs)
---
+++ packages/flowmatic/flowmatic-0.1.0.9-py3-none-any.whl/flowmatic/gui/screens/form_screen.py
@@ -0,0 +1,156 @@
+import tkinter as tk
+from typing import Callable, TypeVar, overload
+
+import customtkinter as ctk
+
+import flowmatic
+from flowmatic import gui
+from flowmatic.flows.flow import Flow
+from flowmatic.gui.screens.screen import FlowScreen, Screen
+from ...forms import FormValidation
+from ...forms.fields.field import Field
+
+T = TypeVar("T")
+
+
+class FormScreen(Screen):
+    field_list: list[Field] | None
+    fields: dict[str, Field]
+    validations: list[FormValidation] | None = None
+    __message: tk.StringVar
+
+    @property
+    def message(self) -> str:
+        return self.__message.get()
+
+    @message.setter
+    def message(self, value: str) -> None:
+        """Set message.
+
+        Args:
+            value (str): Message."""
+        self.__message.set(value)
+
+    def __init__(
+        self,
+    ) -> None:
+        self.fields = {}
+        self.__message = tk.StringVar(value="")
+
+    @property
+    def values(self) -> dict[str, str | int | list[str]]:
+        """Get values from fields.
+
+        Returns:
+            dict[str, str]: Values from fields."""
+        return {
+            label: field.value for label, field in self.fields.items() if field.value
+        }
+
+    def validate(self) -> None:
+        if not self.validations:
+            return
+
+        for validation in self.validations:
+            validation.validate(self.values)
+
+    @overload
+    def get(self, __key: str, /) -> str | int | list[str]:
+        ...
+
+    @overload
+    def get(self, __key: str, __default: T) -> str | int | list[str] | T:
+        ...
+
+    @overload
+    def get(self, __key: str, __default: T, __return: type[T]) -> T:
+        ...
+
+    def get(
+        self, __key: str, __default: str | T = "", __return: type[T] | None = None
+    ) -> str | int | list[str] | T:
+        """Get value from field.
+
+        Args:
+            label (str): Label of field.
+
+        Returns:
+            str: Value of field."""
+        value = self.values.get(__key, __default)
+        if __return is not None:
+            if not isinstance(value, __return):
+                raise TypeError(f"Expected {__return} got {type(value)}")
+        return value
+
+    def build(  # pylint: disable=arguments-differ
+        self,
+        *,
+        title: str,
+        validations: list[FormValidation] | None = None,
+        on_submit: Callable[[], None],
+        submit_text: str = "Submit",
+        back_command: Callable[[], None] | None = None,
+        fields: list[Field] | None = None,
+        field_factory: Callable[..., list[Field]] | None = None,
+    ) -> None:
+        """Build screen.
+
+        Args:
+
+            title (str): Title of screen.
+            validate_command (Callable[[], None]): Command to run when validate button is pressed.
+            validate_text (str, optional): Text of validate button. Defaults to "Submit".
+            back_command (Callable[[], None], optional): Command to run when back button is pressed.
+                 Defaults to None.
+        """
+        # Variables
+        self.field_list = (fields or []) + (field_factory() if field_factory else [])
+        self.validations = validations
+
+        def validate_command() -> None:
+            try:
+                self.validate()
+            except AssertionError as error:
+                self.message = str(error)
+                flowmatic.update_gui()
+            else:
+                on_submit()
+
+        # Title
+        ctk.CTkLabel(self.master, text=title).pack(**gui.pack_defaults)
+
+        # Fields
+        if self.field_list:
+            fields_frame = (
+                ctk.CTkScrollableFrame(
+                    self.master, height=gui.HEIGHT - 150, width=gui.WIDTH
+                )
+                if not isinstance(self.master, ctk.CTkScrollableFrame)
+                else ctk.CTkFrame(self.master)
+            )
+            fields_frame.pack(**gui.pack_defaults)
+            for field in self.field_list:
+                self.fields[field.label] = field(fields_frame).build(
+                    **gui.pack_defaults
+                )
+
+        # Message
+        ctk.CTkLabel(self.master, textvariable=self.__message).pack(**gui.pack_defaults)
+
+        # Buttons
+        pack_args = gui.pack_defaults  # | {"pady": (10, 20)}
+        button_frame = ctk.CTkFrame(self.master)
+        button_frame.pack(**pack_args)
+        ctk.CTkButton(button_frame, text=submit_text, command=validate_command).pack(
+            **gui.pack_defaults, side=tk.RIGHT
+        )
+        if back_command:
+            ctk.CTkButton(button_frame, text="Back", command=back_command).pack(
+                **gui.pack_defaults, side=tk.LEFT
+            )
+
+
+class FlowFormScreen(FormScreen, FlowScreen):
+    def __init__(self, flow: Flow) -> None:
+        self.flow = flow
+        super().__init__()
