58e518d596bd9cf3c8eebc695e169f809fb1da64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:08:11 +0000

    Add package brainpy/2.4.3.post5/brainpy-2.4.3.post5-py3-none-any.whl

---
+++ packages/brainpy/brainpy-2.4.3.post5-py3-none-any.whl/brainpy/dyn/synapses.py
@@ -0,0 +1,22 @@
+
+from brainpy._src.dyn.synapses.abstract_models import (
+  Delta,
+  Expon,
+  DualExpon,
+  DualExponV2,
+  NMDA,
+  STD,
+  STP,
+)
+from brainpy._src.dyn.synapses.bio_models import (
+  AMPA,
+  GABAa,
+  BioNMDA,
+)
+from brainpy._src.dyn.synapses.delay_couplings import (
+  DiffusiveCoupling,
+  AdditiveCoupling,
+)
+
+
+
