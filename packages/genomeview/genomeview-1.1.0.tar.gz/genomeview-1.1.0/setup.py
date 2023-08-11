313ea29df9f4ee3d1c9491ef0a5f387ef712e255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:08:22 +0000

    Add package genomeview/1.1.0/genomeview-1.1.0.tar.gz

---
+++ packages/genomeview/genomeview-1.1.0.tar.gz/genomeview-1.1.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages, Extension
+
+def get_version(string):
+    """ Parse the version number variable __version__ from a script. """
+    import re
+    version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    version_str = re.search(version_re, string, re.M).group(1)
+    return version_str
+
+
+def get_includes():
+    class Includes:
+        def __iter__(self):
+            import pysam
+            import numpy
+            return iter(pysam.get_include()+[numpy.get_include()])
+        def __getitem__(self, i):
+            return list(self)[i]
+    return Includes()
+
+def get_defines():
+    class Defines:
+        def __iter__(self):
+            import pysam
+            return iter(pysam.get_defines())
+        def __getitem__(self, i):
+            return list(self)[i]
+    return Defines()
+
+
+setup(name="genomeview",
+      version=get_version(open('genomeview/__init__.py').read()),
+      description="genomeview",
+      author="Noah Spies",
+      packages=find_packages(),
+
+      install_requires=["pysam", "numpy", "pandas", "pyBigWig"], 
+      python_requires=">=3.3"
+     )
