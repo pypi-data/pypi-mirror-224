72e6d7b84e85c5eea63bc245a44b4874375fefa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 15:08:53 +0000

    Add package easyofd/0.0.9/easyofd-0.0.9.tar.gz

---
+++ packages/easyofd/easyofd-0.0.9.tar.gz/easyofd-0.0.9/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools 
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="easyofd", 
+    version="0.0.9",    
+    author="renoyuan",    
+    author_email="renoyuan@foxmail.com",    
+    description="easy parser OFD",
+    long_description=long_description,   
+    long_description_content_type="text/markdown",
+    url="https://github.com/renoyuan/easyofd",    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',   
+)
