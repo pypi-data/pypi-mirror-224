0f772ccc05e7082386d378a3844cebc07fcdbf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:12:41 +0000

    Add package EnsembleParticleSwarmOptimization/0.1.9/EnsembleParticleSwarmOptimization-0.1.9.tar.gz

---
+++ packages/EnsembleParticleSwarmOptimization/EnsembleParticleSwarmOptimization-0.1.9.tar.gz/EnsembleParticleSwarmOptimization-0.1.9/PySO/__init__.py
@@ -0,0 +1,14 @@
+from .Model import Model
+from .MWE_Swarm import Swarm
+from .SwarmHandler import SwarmHandler
+from .HierarchicalSwarmHandler import HierarchicalSwarmHandler
+
+__version__ = '0.1.9'
+
+name = "PySO"
+
+__all__ = ['Model',
+           'Swarm',
+	   'SwarmHandler',
+	   'HierarchicalSwarmHandler']
+
