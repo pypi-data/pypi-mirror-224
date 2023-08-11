b126733d5199333ebd7e791234e79ac8f23650d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:10:37 +0000

    Add package flowmatic/0.1.0.10/flowmatic-0.1.0.10.tar.gz

---
+++ packages/flowmatic/flowmatic-0.1.0.10.tar.gz/flowmatic-0.1.0.10/flowmatic/gui/gui.py
@@ -0,0 +1,42 @@
+from typing import Protocol
+from .screens.screen import Screen
+from ..flows.flow import Flow
+
+
+class GUI(Protocol):
+    title: str
+    start_screen: Screen
+    current_screen: Screen
+    current_flow: Flow
+
+    def __init__(self, title: str, start_screen: Screen):
+        ...
+
+    def start(self):
+        ...
+
+    def setup(self):
+        ...
+
+    def switch_screen(self, screen: Screen):
+        ...
+
+    def start_flow(self, flow: Flow):
+        ...
+
+    def build_screen(self, screen: Screen):
+        ...
+
+    def clear_screen(self):
+        ...
+
+    def file_dialog(
+        self, title: str, max_files: int, filetypes: list[tuple[str, str]]
+    ) -> str | tuple[str, ...]:
+        ...
+
+    def update(self):
+        ...
+
+    def quit(self):
+        ...
