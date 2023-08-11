88c4ef877acb2b588a07c7ed7cbf1f0b76d3e7d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 05:08:49 +0000

    Add package intecomm-edc/0.1.56/intecomm-edc-0.1.56.tar.gz

---
+++ packages/intecomm-edc/intecomm-edc-0.1.56.tar.gz/intecomm-edc-0.1.56/intecomm_visit_schedule/constants.py
@@ -0,0 +1,4 @@
+COMM_SCHEDULE = "comm_schedule"
+INTE_SCHEDULE = "inte_schedule"
+SCHEDULE = "schedule"
+VISIT_SCHEDULE = "visit_schedule"
---
+++ packages/intecomm-edc/intecomm-edc-0.1.56.tar.gz/intecomm-edc-0.1.56/intecomm_visit_schedule/visit_schedules/visit_schedule.py
@@ -0,0 +1,17 @@
+from edc_locator.utils import get_locator_model
+from edc_visit_schedule import VisitSchedule
+
+from ..constants import VISIT_SCHEDULE
+from .comm_schedule import comm_schedule
+from .inte_schedule import inte_schedule
+
+visit_schedule = VisitSchedule(
+    name=VISIT_SCHEDULE,
+    verbose_name="INTECOMM",
+    offstudy_model="edc_offstudy.subjectoffstudy",
+    death_report_model="intecomm_ae.deathreport",
+    locator_model=get_locator_model(),
+    previous_visit_schedule=None,
+)
+visit_schedule.add_schedule(comm_schedule)
+visit_schedule.add_schedule(inte_schedule)
