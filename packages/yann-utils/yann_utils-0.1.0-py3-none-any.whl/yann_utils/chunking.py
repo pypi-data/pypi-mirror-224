6408429c72531babe03fe9b56200abd3cfb1c43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:11:13 +0000

    Add package yann-utils/0.1.0/yann_utils-0.1.0-py3-none-any.whl

---
+++ packages/yann-utils/yann_utils-0.1.0-py3-none-any.whl/yann_utils/chunking.py
@@ -0,0 +1,50 @@
+import os
+import pickle
+import re
+from pathlib import Path
+from typing import Any, Iterable, Union
+import tempfile
+
+
+def chunk_obj(obj: Any, chunk_size_mb: int) -> Iterable[bytes]:
+  """Chunk an object into bytes of a given size."""
+  ser = pickle.dumps(obj)
+  chunk_size_b = chunk_size_mb * 1024**2
+  return (ser[i : i + chunk_size_b] for i in range(0, len(ser), chunk_size_b))
+
+
+def reconstruct_obj(chunks: Iterable[bytes]) -> Any:
+  """Reconstruct an object from its chunks of bytes."""
+  acc = []
+  for chunk in chunks:
+    with open(chunk, "rb") as file:
+      acc.append(file.read())
+
+  return b"".join(acc)
+
+
+def persist_chunks(
+  chunks: Iterable[bytes], dir: Union[str, Path], obj_name: str
+) -> None:
+  """Persist chunks to disk and return directory."""
+  os.makedirs(dir, exist_ok=True)
+  for i, chunk in enumerate(chunks):
+    with open(f"{dir}/{obj_name}.pkl.part{i}", "wb") as file:
+      file.write(chunk)
+
+
+def get_numeric_suffix(file_name: str) -> None:
+  """Get the numeric suffix of a file name."""
+  match = re.search(r"\d+$", file_name)
+  if not match:
+    raise ValueError(f"File name {file_name} does not contain a numeric suffix")
+
+  return int(match.group())
+
+
+def get_chunks(dir: Union[str, Path]) -> Iterable[str]:
+  """Get the chunks of an object from a directory."""
+  chunks = Path(dir).glob("*.pkl.part*")
+  chunks = [str(path) for path in chunks]
+  chunks = sorted(chunks, key=get_numeric_suffix)
+  return chunks
