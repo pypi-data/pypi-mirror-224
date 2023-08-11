0feedebf42192feb26f7471e10fb9b46cacdf055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:12:52 +0000

    Add package arbor/0.9.0/arbor-0.9.0-cp39-cp39-macosx_10_15_universal2.whl

---
+++ packages/arbor/arbor-0.9.0-cp39-cp39-macosx_10_15_universal2.whl/arbor/include/arbor/lif_cell.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <arbor/serdes.hpp>
+#include <arbor/common_types.hpp>
+#include <arbor/export.hpp>
+
+namespace arb {
+
+// Model parameters of leaky integrate and fire neuron model.
+struct ARB_SYMBOL_VISIBLE lif_cell {
+    cell_tag_type source; // Label of source.
+    cell_tag_type target; // Label of target.
+
+    // Neuronal parameters.
+    double tau_m = 10;    // Membrane potential decaying constant [ms].
+    double V_th = 10;     // Firing threshold [mV].
+    double C_m = 20;      // Membrane capacitance [pF].
+    double E_L = 0;       // Resting potential [mV].
+    double E_R = E_L;     // Reset potential [mV].
+    double V_m = E_L;     // Initial value of the Membrane potential [mV].
+    double t_ref = 2;     // Refractory period [ms].
+
+    lif_cell() = default;
+    lif_cell(cell_tag_type source, cell_tag_type target): source(std::move(source)), target(std::move(target)) {}
+
+    ARB_SERDES_ENABLE(lif_cell, source, target, tau_m, V_th, C_m, E_L, E_R, V_m, t_ref);
+};
+
+// LIF probe metadata, to be passed to sampler callbacks. Intentionally left blank.
+struct ARB_SYMBOL_VISIBLE lif_probe_metadata {};
+
+// Voltage estimate [mV].
+// Sample value type: `double`
+struct ARB_SYMBOL_VISIBLE lif_probe_voltage {};
+
+} // namespace arb
