fc263d8816925759d8ad26555da198dd4a8897a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:11:05 +0000

    Add package nemusicapi/0.1.1/nemusicapi-0.1.1-py3-none-any.whl

---
+++ packages/nemusicapi/nemusicapi-0.1.1-py3-none-any.whl/NEmusicApi/__init__.py
@@ -0,0 +1,16 @@
+from .api import Api
+from .type import QualityLevel, EncodeType
+from .baseapi import BaseApi
+from .exception import NoDownloadDir
+
+
+__all__ = [
+    'BaseApi',
+    'Api',
+    'NoDownloadDir',
+    'QualityLevel',
+    'EncodeType',
+]
+
+# 大版本版本号，大版本更新时修改
+__version__ = (0, 1, 1)
