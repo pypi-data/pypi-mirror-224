3e426be800ef1a0d2ba4a0e33ac3cf823bc9139b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:09:43 +0000

    Add package cerberuspy/1.0.5/cerberuspy-1.0.5.tar.gz

---
+++ packages/cerberuspy/cerberuspy-1.0.5.tar.gz/cerberuspy-1.0.5/cerberuspy/client.py
@@ -0,0 +1,30 @@
+import requests
+
+from typing import Literal, Final
+
+
+class Client():
+	"""
+	Клиент для использования API ИС "Цербер".
+	"""
+	def __init__(self, cerber_id: str, proxies: dict = None):
+		self.api: str = "https://api.iscerberus.ru"
+		self.cerber_id: str = cerber_id
+
+	def get_info(self):
+		return requests.post(
+			f"{self.api}/get_info",
+			json={
+				"cerber_id": self.cerber_id
+			}
+		).json()
+
+	def search(self, target: str, target_type: str):
+		return requests.post(
+			f"{self.api}/search", 
+			json={
+				"cerber_id": self.cerber_id,
+				"target": target,
+				"target_type": target_type
+			}
+		).json()
