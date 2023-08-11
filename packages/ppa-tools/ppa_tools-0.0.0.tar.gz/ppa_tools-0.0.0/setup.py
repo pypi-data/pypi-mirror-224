ee7b26a3810db27d9f04c5c50d0a3cbabc52830b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:08:44 +0000

    Add package ppa-tools/0.0.0/ppa_tools-0.0.0.tar.gz

---
+++ packages/ppa-tools/ppa_tools-0.0.0.tar.gz/ppa_tools-0.0.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='ppa_tools',
+    version='0.0.0',
+    description='This is a placeholder package, do not install',
+    install_requires=['PPA_TOOLS_SHOULD_NOT_BE_INSTALLED_FROM_PYPI']
+)
+
