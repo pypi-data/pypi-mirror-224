6e626bc4ce5e184f33ca1ced51abb24d051a366d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 06:13:18 +0000

    Add package trulens-eval/0.9.0a0/trulens_eval-0.9.0a0-py3-none-any.whl

---
+++ packages/trulens-eval/trulens_eval-0.9.0a0-py3-none-any.whl/trulens_eval/__init__.py
@@ -0,0 +1,68 @@
+"""
+# Trulens-eval LLM Evaluation Library
+
+This top-level import should include everything to get started.
+
+## Module organization/dependency
+
+Modules on lower lines should not import modules on same or above lines as
+otherwise you might get circular import errors.
+
+    - `__init__.py`
+
+    - all UI/dashboard components
+
+    - `tru_chain.py` 
+    
+    - `tru_llama.py` (note: llama_index uses langchain internally for some things)
+
+    - `tru.py`
+
+    - `feedback.py`
+
+    - `app.py`
+
+    - `db.py`
+
+    - `instruments.py`
+
+    - `provider_apis.py` `feedback_prompts.py`
+
+    - `schema.py`
+
+    - `util.py` 
+    
+    - `keys.py`
+
+    - `utils/python.py` `utils/text.py`
+"""
+
+__version__ = "0.9.0a"
+
+from trulens_eval.feedback import Feedback
+from trulens_eval.feedback import Huggingface
+from trulens_eval.feedback import OpenAI
+from trulens_eval.feedback import Provider
+from trulens_eval.schema import FeedbackMode
+from trulens_eval.schema import Query
+from trulens_eval.schema import Select
+from trulens_eval.tru import Tru
+from trulens_eval.tru_basic_app import TruBasicApp
+from trulens_eval.tru_chain import TruChain
+from trulens_eval.tru_llama import TruLlama
+from trulens_eval.util import TP
+
+__all__ = [
+    'Tru',
+    'TruBasicApp',
+    'TruChain',
+    'TruLlama',
+    'Feedback',
+    'OpenAI',
+    'Huggingface',
+    'FeedbackMode',
+    'Provider',
+    'Query',  # to deprecate in 0.3.0
+    'Select',
+    'TP'
+]
