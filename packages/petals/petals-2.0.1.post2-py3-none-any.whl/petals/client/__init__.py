11d349e2a1f36944d9282029b298e1c4be225d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:08:45 +0000

    Add package petals/2.0.1.post2/petals-2.0.1.post2-py3-none-any.whl

---
+++ packages/petals/petals-2.0.1.post2-py3-none-any.whl/petals/client/__init__.py
@@ -0,0 +1,4 @@
+from petals.client.inference_session import InferenceSession
+from petals.client.remote_sequential import RemoteSequential
+from petals.client.routing.sequence_manager import RemoteSequenceManager
+from petals.client.routing.spending_policy import NoSpendingPolicy, SpendingPolicyBase
