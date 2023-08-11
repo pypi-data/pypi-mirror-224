889fa1d965b9f666090ce5ae1c45f166b3f6e2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:13:14 +0000

    Add package ieegprep/1.4.1/ieegprep-1.4.1.tar.gz

---
+++ packages/ieegprep/ieegprep-1.4.1.tar.gz/ieegprep-1.4.1/ieegprep/bids/__init__.py
@@ -0,0 +1,7 @@
+# define directory as package
+
+# flatten access
+from ieegprep.bids.data_structure import list_bids_datasets
+from ieegprep.bids.sidecars import load_channel_info, load_event_info, load_stim_event_info, load_ieeg_sidecar
+#from data_epoch import load_data_epochs, load_data_epochs_averages
+
