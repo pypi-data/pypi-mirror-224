d22958c8a8b0739ce7a072987f05ba4dda931755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 14:08:49 +0000

    Add package metaboatrace.scrapers/1.0.0/metaboatrace_scrapers-1.0.0.tar.gz

---
+++ packages/metaboatrace.scrapers/metaboatrace_scrapers-1.0.0.tar.gz/metaboatrace_scrapers-1.0.0/metaboatrace/scrapers/official/website/factories.py
@@ -0,0 +1,103 @@
+from boatrace.models import Prefecture
+
+
+# TODO: 別にボートと独立してエンティティなのでこのリポジトリからは切り離す
+class PrefectureFactory:
+    @staticmethod
+    def create(name: str):
+        if "北海道" in name:
+            return Prefecture.HOKKAIDO
+        elif "青森" in name:
+            return Prefecture.AOMORI
+        elif "岩手" in name:
+            return Prefecture.IWATE
+        elif "宮城" in name:
+            return Prefecture.MIYAGI
+        elif "秋田" in name:
+            return Prefecture.AKITA
+        elif "山形" in name:
+            return Prefecture.YAMAGATA
+        elif "福島" in name:
+            return Prefecture.FUKUSHIMA
+        elif "茨城" in name:
+            return Prefecture.IBARAKI
+        elif "栃木" in name:
+            return Prefecture.TOCHIGI
+        elif "群馬" in name:
+            return Prefecture.GUNMA
+        elif "埼玉" in name:
+            return Prefecture.SAITAMA
+        elif "千葉" in name:
+            return Prefecture.CHIBA
+        elif "東京" in name:
+            return Prefecture.TOKYO
+        elif "神奈川" in name:
+            return Prefecture.KANAGAWA
+        elif "新潟" in name:
+            return Prefecture.NIIGATA
+        elif "富山" in name:
+            return Prefecture.TOYAMA
+        elif "石川" in name:
+            return Prefecture.ISHIKAWA
+        elif "福井" in name:
+            return Prefecture.FUKUI
+        elif "山梨" in name:
+            return Prefecture.YAMANASHI
+        elif "長野" in name:
+            return Prefecture.NAGANO
+        elif "岐阜" in name:
+            return Prefecture.GIFU
+        elif "静岡" in name:
+            return Prefecture.SHIZUOKA
+        elif "愛知" in name:
+            return Prefecture.AICHI
+        elif "三重" in name:
+            return Prefecture.MIE
+        elif "滋賀" in name:
+            return Prefecture.SHIGA
+        elif "京都" in name:
+            return Prefecture.KYOTO
+        elif "大阪" in name:
+            return Prefecture.OSAKA
+        elif "兵庫" in name:
+            return Prefecture.HYOGO
+        elif "奈良" in name:
+            return Prefecture.NARA
+        elif "和歌山" in name:
+            return Prefecture.WAKAYAMA
+        elif "鳥取" in name:
+            return Prefecture.TOTTORI
+        elif "島根" in name:
+            return Prefecture.SHIMANE
+        elif "岡山" in name:
+            return Prefecture.OKAYAMA
+        elif "広島" in name:
+            return Prefecture.HIROSHIMA
+        elif "山口" in name:
+            return Prefecture.YAMAGUCHI
+        elif "徳島" in name:
+            return Prefecture.TOKUSHIMA
+        elif "香川" in name:
+            return Prefecture.KAGAWA
+        elif "愛媛" in name:
+            return Prefecture.EHIME
+        elif "高知" in name:
+            return Prefecture.KOCHI
+        elif "福岡" in name:
+            return Prefecture.FUKUOKA
+        elif "佐賀" in name:
+            return Prefecture.SAGA
+        elif "長崎" in name:
+            return Prefecture.NAGASAKI
+        elif "熊本" in name:
+            return Prefecture.KUMAMOTO
+        elif "大分" in name:
+            return Prefecture.OITA
+        elif "宮崎" in name:
+            return Prefecture.MIYAGI
+        elif "鹿児島" in name:
+            return Prefecture.KAGOSHIMA
+        elif "沖縄" in name:
+            return Prefecture.OKINAWA
+        else:
+            raise ValueError
