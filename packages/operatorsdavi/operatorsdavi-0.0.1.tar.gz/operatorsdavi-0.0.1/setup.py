3434450fbe695ba36c76ff93a719368471be642a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 03:11:05 +0000

    Add package operatorsdavi/0.0.1/operatorsdavi-0.0.1.tar.gz

---
+++ packages/operatorsdavi/operatorsdavi-0.0.1.tar.gz/operatorsdavi-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'MEU PRIMEIRO PACOTE EM PYTHON'
+LONG_DESCRIPTION = 'MEU PRIMEIRO PROJETO EM PYTHON COM UMA DESCRIÇÃO UM POUCO MAIS LONGA'
+
+setup(
+            nome = 'operatorsdavi',
+            version = VERSION,
+            author = 'davizetinha',
+            author_email = 'davi512018@gmail.com',
+            description = DESCRIPTION,
+            long_description = LONG_DESCRIPTION,
+            packages = ['operatorsdavi'],
+
+
+            classifiers = [
+                'Development Status :: 3 - Alpha',
+                'Intended Audience :: Education',
+                'Programming Language :: Python :: 3',
+                'Operating System :: OS Independent'
+            ]
+)
