337773b64fb003024783a3c468eccb1c9bf4d532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 06:13:16 +0000

    Add package django-glue/0.4.2.5/django_glue-0.4.2.5-py3-none-any.whl

---
+++ packages/django-glue/django_glue-0.4.2.5-py3-none-any.whl/django_glue/glue.py
@@ -0,0 +1,41 @@
+from typing import Union
+
+from django.db.models import Model
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+
+from django_glue.sessions import GlueSession, GlueKeepLiveSession
+
+
+def add_glue(
+        request: HttpRequest,
+        unique_name: str,
+        target: Union[Model, QuerySet, str],
+        access: str = 'view',
+        fields: Union[list, tuple] = ('__all__',),
+        exclude: Union[list, tuple] = ('__none__',),
+        methods: Union[list, tuple] = ('__none__',),
+) -> None:
+    if isinstance(fields, (list, tuple)) and isinstance(exclude, (list, tuple)):
+        glue_session = GlueSession(request)
+
+        if isinstance(target, Model):
+            glue_session.add_model_object(unique_name, target, access, fields, exclude, methods)
+
+        elif isinstance(target, QuerySet):
+            glue_session.add_query_set(unique_name, target, access, fields, exclude, methods)
+
+        elif isinstance(target, str):
+            glue_session.add_template(unique_name, target)
+
+        else:
+            raise f'target is not a valid type must be a django.db.models.Model or django.db.models.query.QuerySet'
+
+        glue_keep_live_session = GlueKeepLiveSession(request)
+
+        glue_keep_live_session.set_unique_name(unique_name)
+
+        glue_session.set_modified()
+
+    else:
+        raise f'fields or exclude is not a valid type must be a list or tuple'
