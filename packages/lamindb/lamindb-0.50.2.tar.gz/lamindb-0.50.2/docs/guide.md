d8f6f24a0d61ea6d3b093d1388e8cb6d5aaaa69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-11 04:11:13 +0000

    Add package lamindb/0.50.2/lamindb-0.50.2.tar.gz

---
+++ packages/lamindb/lamindb-0.50.2.tar.gz/lamindb-0.50.2/docs/guide.md
@@ -0,0 +1,46 @@
+```{include} ../README.md
+:start-line: 0
+:end-line: 3
+```
+
+# Guide
+
+```{include} ../README.md
+:start-line: 6
+:end-line: -4
+```
+
+```{toctree}
+:hidden:
+:caption: Tutorial
+
+tutorial1
+```
+
+```{toctree}
+:hidden:
+:caption: "How to: data"
+
+guide/data-lineage
+guide/select
+guide/stream
+guide/schemas
+guide/setup
+```
+
+```{toctree}
+:hidden:
+:caption: "How to: biology"
+
+biology/registries
+biology/lnschema-bionty
+biology/analyses
+```
+
+```{toctree}
+:hidden:
+:caption: Other topics
+
+faq
+storage
+```
---
+++ packages/lamindb/lamindb-0.50.2.tar.gz/lamindb-0.50.2/tests/test_feature_set.py
@@ -0,0 +1,86 @@
+from inspect import signature
+
+import lnschema_bionty as lb
+import pandas as pd
+import pytest
+
+import lamindb as ln
+from lamindb import _feature_set
+
+df = pd.DataFrame(
+    {
+        "feat1": [1, 2, 3],
+        "feat2": [3.1, 4.2, 5.3],
+        "feat3": ["cond1", "cond2", "cond2"],
+        "feat4": ["id1", "id2", "id3"],
+    }
+)
+
+
+def test_signatures():
+    # this seems currently the easiest and most transparent
+    # way to test violations of the signature equality
+    # the MockORM class is needed to get inspect.signature
+    # to work
+    class Mock:
+        pass
+
+    # class methods
+    class_methods = ["from_values", "from_df"]
+    for name in class_methods:
+        setattr(Mock, name, getattr(_feature_set, name))
+        assert signature(getattr(Mock, name)) == _feature_set.SIGS.pop(name)
+    # methods
+    for name, sig in _feature_set.SIGS.items():
+        assert signature(getattr(_feature_set, name)) == sig
+
+
+def test_feature_set_from_values():
+    gene_symbols = ["TCF7", "MYC"]
+    lb.settings.species = "human"
+    feature_set = ln.FeatureSet.from_values(gene_symbols, lb.Gene.symbol)
+    id = feature_set.id
+    assert feature_set._state.adding
+    assert feature_set.type == "float"
+    assert feature_set.registry == "bionty.Gene"
+    feature_set.save()
+    # test that the feature_set is retrieved from the database
+    # in case it already exists
+    feature_set = ln.FeatureSet.from_values(gene_symbols, lb.Gene.symbol)
+    assert not feature_set._state.adding
+    assert id == feature_set.id
+    feature_set.delete()
+
+    # edge cases
+    with pytest.raises(ValueError):
+        feature_set = ln.FeatureSet.from_values([])
+
+
+def test_feature_set_from_records():
+    features = ln.Feature.from_df(df)
+    feature_set = ln.FeatureSet(features)
+    id = feature_set.id
+    assert feature_set._state.adding
+    assert feature_set.type is None
+    assert feature_set.registry == "core.Feature"
+    feature_set.save()
+    # test that the feature_set is retrieved from the database
+    # in case it already exists
+    feature_set = ln.FeatureSet(features)
+    assert not feature_set._state.adding
+    assert id == feature_set.id
+    feature_set.delete()
+
+    # edge case
+    with pytest.raises(ValueError):
+        positional_arg = 1
+        ln.FeatureSet(features, positional_arg)
+
+
+def test_feature_set_from_df():
+    feature_set = ln.FeatureSet.from_df(df)
+    feature_set.save()
+    assert feature_set.type is None
+    for feature in feature_set.features.all():
+        feature.delete()
+    feature_set.delete()
