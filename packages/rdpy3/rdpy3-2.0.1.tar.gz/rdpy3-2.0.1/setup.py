eef31d0094d637c2e008c903acddd3977ea279d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:08:05 +0000

    Add package rdpy3/2.0.1/rdpy3-2.0.1.tar.gz

---
+++ packages/rdpy3/rdpy3-2.0.1.tar.gz/rdpy3-2.0.1/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup, Extension
+
+version = "2.0.1"
+packageName = "rdpy3"
+
+# import os
+# dist = os.listdir("dist")
+# vernos = []
+# for fname in dist:
+#     if fname.endswith(suffix := ".tar.gz"):
+#         verno = fname.strip(packageName+"-").strip(suffix)
+#         vernos.append(verno)
+# if version in vernos:
+#     raise Exception(
+#         f"{version} is not latest version!\nPrevious versions: {', '.join(vernos)}")
+
+setup(
+    name=packageName,
+    version=version,
+    description="Remote Desktop Protocol in Python3",
+    long_description_content_type="text/markdown",
+    python_requires=">=3",
+    long_description=open("README.md").read(),
+    # long_description="""
+    # RDPY is a pure Python implementation of the Microsoft RDP (Remote Desktop Protocol) protocol (Client and Server side). RDPY is built over the event driven network engine Twisted.
+    # RDPY provide RDP and VNC binaries : RDP Man In The Middle proxy which record session, RDP Honeypot, RDP screenshoter, RDP client, VNC client, VNC screenshoter, RSS Player
+    # """,
+    author="Sylvain Peyrefitte",
+    author_email="citronneur@gmail.com",
+    url="https://github.com/james4ever0/rdpy3",
+    packages=[
+        "rdpy3",
+        "rdpy3.model",
+        "rdpy3.security",
+        "rdpy3.core.pdu",
+        "rdpy3.core.nla",
+        "rdpy3.core.t125",
+        "rdpy3.ui",
+    ],
+    ext_modules=[Extension("rle", ["ext/rle.c"])],
+    scripts=[
+        "bin/rdpy3-rdpclient.py",
+        "bin/rdpy3-rdphoneypot.py",
+        "bin/rdpy3-rdpmitm.py",
+        "bin/rdpy3-rdpscreenshot.py",
+        "bin/rdpy3-rssplayer.py",
+        "bin/rdpy3-vncclient.py",
+        "bin/rdpy3-vncscreenshot.py",
+    ],
+    install_requires=[
+        #   'twisted',
+        "pyopenssl>=23.2.0",
+        #   'service_identity',
+        #   'qt5reactor',
+        #   'rsa',
+        #   'pyasn1'
+        "twisted",
+        "PyQt5",
+        "PyQt5-sip",
+        "service_identity",
+        "rsa",
+        "qt5reactor",
+        "pyasn1",
+    ],
+)
