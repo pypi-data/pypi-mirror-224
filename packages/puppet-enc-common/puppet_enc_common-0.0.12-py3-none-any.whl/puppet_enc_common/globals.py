02a3927537eb13eb620815d506e8a7135e058675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:10:09 +0000

    Add package puppet-enc-common/0.0.12/puppet_enc_common-0.0.12-py3-none-any.whl

---
+++ packages/puppet-enc-common/puppet_enc_common-0.0.12-py3-none-any.whl/puppet_enc_common/globals.py
@@ -0,0 +1,25 @@
+# Global Variables
+
+def init():
+
+    global SA_CONTAINER_NAME
+    global SA_CONNECTION_STRING
+    global COMMON_ROLE_NAME
+    global DATABASE_TYPE
+    global DB_NAME
+    global DB_USER
+    global DB_PASS
+    global DB_HOST
+    global DB_SSL_CA
+    global ADMIN_PASSWORD
+
+    SA_CONTAINER_NAME = ""
+    SA_CONNECTION_STRING = ""
+    COMMON_ROLE_NAME = ""
+    DATABASE_TYPE = ""
+    DB_NAME = ""
+    DB_USER = ""
+    DB_PASS = ""
+    DB_HOST = ""
+    DB_SSL_CA = ""
+    ADMIN_PASSWORD = ""
