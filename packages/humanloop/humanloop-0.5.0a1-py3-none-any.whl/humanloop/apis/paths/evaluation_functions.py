70d1bba9b3a9e520db25dd515bf7243e2bef93d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:19 +0000

    Add package humanloop/0.5.0a1/humanloop-0.5.0a1-py3-none-any.whl

---
+++ packages/humanloop/humanloop-0.5.0a1-py3-none-any.whl/humanloop/apis/paths/evaluation_functions.py
@@ -0,0 +1,9 @@
+from humanloop.paths.evaluation_functions.get import ApiForget
+from humanloop.paths.evaluation_functions.post import ApiForpost
+
+
+class EvaluationFunctions(
+    ApiForget,
+    ApiForpost,
+):
+    pass
