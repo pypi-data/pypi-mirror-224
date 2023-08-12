a06d20bee365d58ff6d27c2110fc8cc2e07434d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:08:27 +0000

    Add package aioclaude-api/1.0.19/aioclaude-api-1.0.19.tar.gz

---
+++ packages/aioclaude-api/aioclaude-api-1.0.19.tar.gz/aioclaude-api-1.0.19/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+base_path = Path(__file__).parent
+long_description = (base_path / "README.md").read_text()
+
+
+setup(
+    name="aioclaude-api",
+    version="1.0.19",
+    author="Hikamoru",
+    license="MIT",
+    author_email="me.thefarkhodov@gmail.com",
+    description="An unofficial API for Claude AI, allowing users to access and interact with Claude AII",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AmoreForever/Claude-API/",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_dir={"": "aioclaude-api"},
+    py_modules=["aioclaude_api", "claude_api"],
+    keywords=[
+        "claude",
+        "ai",
+        "claude-ai",
+        "API",
+        "requests",
+        "chatbot",
+        "async",
+        "aiohttp",
+    ],
+    install_requires=[
+        "aiohttp",
+        "requests",
+    ],
+    python_requires=">=3.8",
+)
