d879424331b689cd08b99c225978d374f71e1c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:08:23 +0000

    Add package django-mimicry/1.8/django-mimicry-1.8.tar.gz

---
+++ packages/django-mimicry/django-mimicry-1.8.tar.gz/django-mimicry-1.8/mimicry/__init__.py
@@ -0,0 +1,13 @@
+"""
+Mimicry
+------------
+
+The Mimicry app provides functionality for simulating user sessions
+and actions, allowing site administrators to view the website as if
+they are a specific user, aiding in debugging and user experience enhancements.
+
+This module sets the default application configuration class.
+"""
+
+# Point to the default AppConfig class for the 'mimicry' app.
+default_app_config = 'mimicry.apps.MimicryConfig'
