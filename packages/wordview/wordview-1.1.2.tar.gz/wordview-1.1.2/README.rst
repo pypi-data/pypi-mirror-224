cb2303d8e2af36b4e1e174d5c8a734d7f458bd0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:11:11 +0000

    Add package wordview/1.1.2/wordview-1.1.2.tar.gz

---
+++ packages/wordview/wordview-1.1.2.tar.gz/wordview-1.1.2/README.rst
@@ -0,0 +1,108 @@
+
+.. image:: https://img.shields.io/pypi/v/wordview
+   :alt: PyPI - Version
+.. image:: https://img.shields.io/pypi/dm/wordview
+   :alt: PyPI - Downloads
+.. image:: https://img.shields.io/librariesio/release/pypi/wordview
+   :alt: Dependencies
+.. image:: https://img.shields.io/pypi/l/wordview
+   :alt: License
+
+Wordview
+########
+Wordview is a Python package for Exploratory Data Analysis of text and provides many statistics about your data in the form of plots, tables, and descriptions allowing you to have both a high-level and detailed overview of your data.
+It has functions to analyze explicit text elements such as words, n-grams, POS tags, and multi-word expressions, as well as implicit elements such as clusters, anomalies, and biases.
+Wordview's Python API is open-source and available under the `MIT license <https://en.wikipedia.org/wiki/MIT_License>`__.
+
+|cover|
+
+Usage
+*****
+
+Install the package via ``pip``:
+
+``pip install wordview``
+
+To explore various features and functionalities, consult the documentation pages. The following sections
+present a high-level description of Wordview's features and functionalities. For details, tutorials and worked examples, corresponding 
+documentation pages are linked in each section.
+
+
+Text Analysis
+*************
+Using this feature, you can gain a comprehensive overview of your text data in terms of various statistics, plots, and distributions.
+It enables a rapid understanding of the underlying patterns present in your dataset.
+By visually representing the data's nuances, this feature can aid in making informed decisions for downstream applications.
+It's a step forward in ensuring that you have a grasp on the intricacies of your data before delving deeper into more complex tasks.
+See `text analysis documentation pages <./docs/source/textstats.rst>`__  for usage and examples.
+
+.. image:: docs/figs/textanalysiscover.png
+   :alt: Text Analysis Cover
+   :width: 100%
+   :align: center
+
+Analysis of Labels
+******************
+In the realm of Natural Language Processing (NLP), the proper analysis and understanding of labels within datasets can provide valuable insights, ensuring that models are trained on balanced and representative data.
+Recognizing this, Wordview is engineered to compute an array of statistics tailored for labeled datasets.
+These statistics cater to both document and sequence levels, providing a holistic view of the dataset's structure.
+By diving deep into the intricacies of the labels, Wordview offers an enriched perspective, helping researchers and practitioners identify
+potential biases, discrepancies, or areas of interest,
+which are essential for creating robust and effective models.
+See `label analysis documentation pages <./docs/source/labels.rst>`__ for usage and examples.
+
+.. image:: docs/figs/labels_peach.png
+   :alt: Text Analysis Cover
+   :width: 100%
+   :align: center
+
+
+
+Extraction & Analysis of Multiword Expressions
+**********************************************
+Multiword Expressions (MWEs) are phrases that can be treated as a single
+semantic unit. E.g. *swimming pool* and *climate change*. MWEs have
+application in different areas including: parsing, language models,
+language generation, terminology extraction, and topic models. Wordview can extract different types of MWEs from text.
+See `MWEs documentation page <./docs/source/mwes.rst>`__ for usage and examples.
+
+
+Bias Analysis
+**************
+In the rapidly evolving realm of Natural Language Processing (NLP), downstream models are as unbiased and fair as the data on which they are trained.
+Wordview Bias Analysis module is designed to assist in the rigorous task of ensuring that underlying training datasets are devoid of explicit negative biases related to categories such as gender, race, and religion.
+By identifying and rectifying these biases, Wordview attempts to pave the way for the creation of more inclusive, fair, and unbiased NLP applications, leading to better user experiences and more equitable technology.
+See the `bias analysis documentation page <./docs/source/bias.rst>`__ for usage and examples.
+
+
+Analysis of Anomalies and Outliers
+**********************************
+Anomalies and outliers have wide applications in Machine Learning. While in
+some cases, you can capture them and remove them from the data to improve the
+performance of a downstream ML model, in other cases, they become the data points
+of interest where we endeavor to find them in order to shed light into our data.
+
+Wordview offers several anomaly and outlier detection functions.
+See `anomalies documentation page <./docs/source/anomalies.rst>`__ for usage and examples.
+
+
+Cluster Analysis
+****************
+Clustering can be used to identify different groups of documents with similar information, in an unsupervised fashion.
+Despite it's ability to provide valuable insights into your data, you do not need labeled data for clustering. See
+`wordview`'s `clustering documentation page <./docs/source/clustering.rst>`__ for usage and examples.
+
+
+Utilities
+#########
+
+Wordview offers a number of utility functions that you can use for common pre and post processing tasks in NLP. 
+See `utilities documentation page <./docs/source/utilities.rst>`__ for usage and examples.
+
+Contributing
+############
+
+Thank you for contributing to wordview! We and the users of this repo
+appreciate your efforts! You can visit the `contributing page <CONTRIBUTING.rst>`__ for detailed instructions about how you can contribute to Wordview.
+
+.. |cover| image:: docs/figs/cover.png
---
+++ packages/wordview/wordview-1.1.2.tar.gz/wordview-1.1.2/wordview/text_analysis/wrapper.py
@@ -0,0 +1,254 @@
+from typing import Any, Dict, List, Set, Tuple
+
+import pandas
+import plotly.figure_factory as ff
+import plotly.graph_objs as go
+from tabulate import tabulate  # type: ignore
+
+from wordview.text_analysis.core import (
+    do_txt_analysis,
+    generate_label_plots,
+    plotly_wordcloud,
+)
+
+
+class TextStatsPlots:
+    """
+    Represents Text Statistics and Plots.
+    """
+
+    def __init__(
+        self,
+        df: pandas.DataFrame,
+        text_column: str,
+        distributions: Set = {"doc_len", "word_frequency_zipf"},
+        pos_tags: Set = {"NN", "VB", "JJ"},
+    ) -> None:
+        """Initialize a new TextStatsPlots object with the given arguments.
+
+        Args:
+            df (pandas.DataFrame): DataFrame with a text_column that contains the corpus.
+            text_column (str): Specifies the column of DataFrame where text data resides.
+            distributions (Set): Set of distribution types to generate and plot. Available distributions are:
+                doc_len: document lengths, word_frequency_zipf: Zipfian word frequency distribution.
+                Default = {"doc_len", "word_frequency_zipf"}.
+            pos_tags (Set): = Set of POS tags for which world cloud is shown. Default = {"NN", "VB", "JJ"}.
+
+        Returns:
+            None
+        """
+        self.df = df
+        self.analysis = do_txt_analysis(df=self.df, text_col=text_column)
+        self.distributions = distributions
+        self.pos_tags = pos_tags
+        self.languages = self.analysis.languages
+        self.type_count = self.analysis.type_count
+        self.token_count = self.analysis.token_count
+        self.num_docs = self.analysis.doc_count
+        self.median_doc_len = self.analysis.median_doc_len
+        self.num_nns = len(self.analysis.nns)
+        self.num_jjs = len(self.analysis.jjs)
+        self.num_vbs = len(self.analysis.vs)
+
+    def show_distplot(
+        self,
+        distribution: str,
+        layout_settings: Dict[str, str] = {},
+        plot_settings: Dict[str, str] = {},
+    ) -> None:
+        """Shows distribution plots for `dist`.
+
+        Args:
+            dist (str): The distribution for which the plot is to be shown.
+                        Can be either of: doc_len" or "word_frequency_zipf.
+            **kwargs: Additional arguments to be passed to self._create_dist_plots and then plotly figure factory.
+                      For available settings see: https://plotly.com/python/reference/layout/
+
+        Returns:
+            None
+        """
+        if distribution not in self.distributions:
+            raise ValueError(
+                f"Invalid distribution. Available distributions are: {self.distributions}"
+            )
+        if distribution == "doc_len":
+            self._create_doc_len_plot(layout_settings, plot_settings).show()
+        elif distribution == "word_frequency_zipf":
+            self._create_word_freq_zipf_plot(layout_settings, plot_settings).show()
+
+    def _create_doc_len_plot(
+        self, layout_settings: Dict[str, Any] = {}, plot_settings: Dict[str, str] = {}
+    ) -> go.Figure:
+        res = ff.create_distplot(
+            [self.analysis.doc_lengths],
+            group_labels=["distplot"],
+            colors=[plot_settings.get("color", "blue")],
+        )
+        tmp_layout_settings = layout_settings
+        tmp_layout_settings.update({"showlegend": False})
+        res.update_layout(tmp_layout_settings)
+        return res
+
+    def _create_word_freq_zipf_plot(
+        self, layout_settings: Dict[str, Any] = {}, plot_settings: Dict[str, str] = {}
+    ) -> go.Figure:
+        res = go.Figure()
+        res.add_trace(
+            go.Scattergl(
+                x=self.analysis.zipf_x,
+                y=self.analysis.zipf_y_emp,
+                mode=plot_settings.get("mode", "markers"),
+                marker=dict(
+                    color=self.analysis.zipf_x,
+                    colorscale=plot_settings.get(
+                        "emperical_zipf_colorscale", "Tealgrn"
+                    ),
+                ),
+            )
+        )
+        res.add_trace(
+            go.Scattergl(
+                x=self.analysis.zipf_x,
+                y=self.analysis.zipf_y_theory,
+                mode=plot_settings.get("mode", "markers"),
+                marker=dict(
+                    color=self.analysis.zipf_x,
+                    colorscale=plot_settings.get("theoritical_zipf_colorscale", "Reds"),
+                ),
+            )
+        )
+        tmp_layout_settings = layout_settings
+        tmp_layout_settings.update({"showlegend": False})
+        res.update_layout(tmp_layout_settings)
+        return res
+
+    def _create_pos_plots(
+        self,
+        pos: str,
+        layout_settings: Dict[str, Any] = {},
+        plot_settings: Dict[str, Any] = {},
+    ) -> go.Figure:
+        """Create plots for the POS tags specified in items in `self.pos_tags`.
+
+        Args:
+            pos (str): The POS tag for which the plot is to be shown.
+
+        Returns:
+            Dictionary of POS tags to plotly go.Figure objects.
+
+        """
+        word_cloud_layout_fixed_settings = {
+            "showlegend": False,
+            "xaxis_showgrid": False,
+            "yaxis_showgrid": False,
+            "xaxis_zeroline": False,
+            "yaxis_zeroline": False,
+            "yaxis_visible": False,
+            "yaxis_showticklabels": False,
+            "xaxis_visible": False,
+            "xaxis_showticklabels": False,
+        }
+        layout_settings = layout_settings
+        layout_settings.update(word_cloud_layout_fixed_settings)
+
+        if pos == "NN" and "NN" in self.pos_tags:
+            return go.Figure(
+                plotly_wordcloud(self.analysis.nns, plot_settings)
+            ).update_layout(layout_settings)
+        elif pos == "JJ" and "JJ" in self.pos_tags:
+            return go.Figure(
+                plotly_wordcloud(self.analysis.jjs, plot_settings)
+            ).update_layout(layout_settings)
+        elif pos == "VB" and "VB" in self.pos_tags:
+            return go.Figure(
+                plotly_wordcloud(self.analysis.vs, plot_settings)
+            ).update_layout(layout_settings)
+        else:
+            raise ValueError(
+                f"Invalid value for pos: {pos}. Valid values are: {self.pos_tags}"
+            )
+
+    def show_word_clouds(
+        self,
+        pos: str,
+        layout_settings: Dict[str, Any] = {},
+        plot_settings: Dict[str, str] = {},
+    ) -> None:
+        """Shows POS word clouds.
+
+        Args:
+            pos (str): Type of POS. Can be any of: [NN, JJ, VB].
+            **kwargs: Keyword arguments to be passed to self._create_pos_plots() and wordview.text_analysis.core.plotly_wordcloud().
+            This includes:
+            - plot_settings: Dictionary of form: for self._create_pos_plots(). For available settings see: https://plotly.com/python/reference/layout/.
+            - wc_settings: Dictionary of form: {"color": "<color>", "max_words": int} for core.plotly_wordcloud(). Accepted values are color strings as usable by PIL/Pillow.
+
+        Returns:
+            None
+        """
+        self._create_pos_plots(pos, layout_settings, plot_settings).show()
+
+    def show_stats(self) -> None:
+        """Print dataset statistics, including:
+        Language/s
+        Number of unique words
+        Number of all words
+        Number of documents
+        Median document length
+        Number of nouns
+        Number of adjectives
+        Number of verbs.
+        """
+        table = tabulate(
+            [
+                ["Language/s", ", ".join(self.languages)],
+                ["Unique Words", f"{self.type_count:,d}"],
+                ["All Words", f"{self.token_count:,d}"],
+                ["Documents", f"{self.num_docs:,d}"],
+                ["Median Doc Length", self.median_doc_len],
+                ["Nouns", f"{self.num_nns:,d}"],
+                ["Adjectives", f"{self.num_jjs:,d}"],
+                ["Verbs", f"{self.num_vbs:,d}"],
+            ],
+            tablefmt="simple_grid",
+        )
+        print(table)
+
+    def show_insights(self):
+        "Show topics, MWEs, clusters,"
+        raise NotImplementedError
+
+
+class LabelStatsPlots:
+    """
+    Represents Label Statistics and Plots.
+    """
+
+    def __init__(
+        self,
+        df: pandas.DataFrame,
+        label_columns: List[Tuple],
+    ) -> None:
+        """Initialize a new LabelStatsPlots object with the given arguments.
+
+        Args:
+            df (pandas.DataFrame): DataFrame with one or more label column/s.
+            label_columns (List): List of tuples (column_name, label_type) that specify a label column and its type (categorical or numerical).
+
+        Returns:
+            None
+        """
+        self.df = df
+        self.label_columns = label_columns
+
+    def show_label_plots(self, layout_settings: Dict[str, Any] = {}) -> None:
+        """Renders label plots for columns specified in `self.label_columns`.
+
+        Args:
+            **kwargs: Additional arguments to be passed to generate_label_plots() to be used by plotly.Figure.update_layout(). For more details
+                  see https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.update_layout
+
+        Returns:
+            None
+        """
+        generate_label_plots(self.df, self.label_columns, layout_settings).show()
