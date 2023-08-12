cf5fa61abf41d59cb1a53ca65b4bdce8dda0aebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:11:11 +0000

    Add package flexible-dict/1.1.5/flexible_dict-1.1.5.tar.gz

---
+++ packages/flexible-dict/flexible_dict-1.1.5.tar.gz/flexible_dict-1.1.5/flexible_dict/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .json_object import json_object, Field, MISSING, JsonObject
+from .about import __version__
+
+__all__ = [
+    'json_object', 'Field', 'MISSING', 'JsonObject',
+    '__version__',
+]
