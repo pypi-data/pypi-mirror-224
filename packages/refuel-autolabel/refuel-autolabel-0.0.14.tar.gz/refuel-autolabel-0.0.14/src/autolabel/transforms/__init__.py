2703bcc03f88a6e61a04638fed889349bae01580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:48 +0000

    Add package refuel-autolabel/0.0.14/refuel-autolabel-0.0.14.tar.gz

---
+++ packages/refuel-autolabel/refuel-autolabel-0.0.14.tar.gz/refuel-autolabel-0.0.14/src/autolabel/transforms/__init__.py
@@ -0,0 +1,46 @@
+import logging
+
+from .base import BaseTransform
+from .pdf import PDFTransform
+from .webpage_transform import WebpageTransform
+from typing import Dict, List
+from autolabel.schema import TransformType
+from autolabel.cache import BaseCache
+
+logger = logging.getLogger(__name__)
+
+TRANSFORM_REGISTRY = {
+    TransformType.PDF: PDFTransform,
+    TransformType.WEBPAGE_TRANSFORM: WebpageTransform,
+}
+
+
+class TransformFactory:
+    """The ModelFactory class is used to create a BaseModel object from the given AutoLabelConfig configuration."""
+
+    @staticmethod
+    def from_dict(dict: Dict, cache: BaseCache = None) -> BaseTransform:
+        """
+        Returns a Transform object based on the given name and parameters
+        """
+        assert "name" in dict, f"Transform name must be specified in transform {dict}"
+        assert (
+            "output_columns" in dict
+        ), f"Output columns must be specified in transform {dict}"
+
+        name = dict["name"]
+        params = dict.get("params", {})
+        output_columns = dict["output_columns"]
+
+        if name not in TRANSFORM_REGISTRY:
+            raise ValueError(f"Unknown transform type {name}")
+        model_cls = TRANSFORM_REGISTRY[name]
+        return model_cls(
+            output_columns=output_columns,
+            cache=cache,
+            **params,
+        )
+
+
+def register_transform(name, transform_cls):
+    TRANSFORM_REGISTRY[name] = transform_cls
