1c4d6b65d50ce92b74df3c44ed3396fe95686eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:11:19 +0000

    Add package xerparser/0.10.3/xerparser-0.10.3-py3-none-any.whl

---
+++ packages/xerparser/xerparser-0.10.3-py3-none-any.whl/xerparser/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.10.3"
+
+from xerparser.src.errors import find_xer_errors, CorruptXerFile
+from xerparser.src.parser import parser, file_reader
+from xerparser.src.xer import Xer
+from xerparser.schemas.actvcode import ACTVCODE
+from xerparser.schemas.actvtype import ACTVTYPE
+from xerparser.schemas.calendars import CALENDAR
+from xerparser.schemas.ermhdr import ERMHDR
+from xerparser.schemas.findates import FINDATES
+from xerparser.schemas.pcattype import PCATTYPE
+from xerparser.schemas.pcatval import PCATVAL
+from xerparser.schemas.project import PROJECT
+from xerparser.schemas.projwbs import PROJWBS
+from xerparser.schemas.rsrc import RSRC
+from xerparser.schemas.task import TASK
+from xerparser.schemas.taskfin import TASKFIN
+from xerparser.schemas.taskmemo import TASKMEMO
+from xerparser.schemas.taskpred import TASKPRED
+from xerparser.schemas.taskrsrc import TASKRSRC
+from xerparser.schemas.trsrcfin import TRSRCFIN
+from xerparser.schemas.udftype import UDFTYPE
