c02c327facd0f82a99bbb3af99ceb91e300003d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:11:15 +0000

    Add package pyding/1.8.3/pyding-1.8.3-py3-none-any.whl

---
+++ packages/pyding/pyding-1.8.3-py3-none-any.whl/pyding/__init__.py
@@ -0,0 +1,12 @@
+from .structures import EventCall, EventHandler, EventSupport, Contains
+from .exceptions import UncancellableEvent
+from .event_space import EventSpace, global_event_space
+
+__name__ = "pyding"
+__version__ = "1.8.3"
+
+def __getattr__(attr):
+    return global_event_space.__getattribute__(attr)
+
+def __dir__():
+    return global_event_space.__dir__()
