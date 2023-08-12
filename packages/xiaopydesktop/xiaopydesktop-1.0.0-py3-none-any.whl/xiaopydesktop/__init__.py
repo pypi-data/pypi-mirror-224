4c830cff00fcd1d81429cef46052a8faa667783e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:11:16 +0000

    Add package xiaopydesktop/1.0.0/xiaopydesktop-1.0.0-py3-none-any.whl

---
+++ packages/xiaopydesktop/xiaopydesktop-1.0.0-py3-none-any.whl/xiaopydesktop/__init__.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : __init__.py.py
+# Time       ：2023.8.11 22:37
+# Author     ：Lex
+# HomePage   : xiaopy.cn
+# Email      : 2983997560@qq.com
+# Description：
+"""
+name = "xiaopydesktop"
