373aa32d420227fbec43ac4eb645df3d74572075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:08:59 +0000

    Add package lamindb/0.50.3/lamindb-0.50.3-py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.50.3-py3-none-any.whl/lamindb/setup/dev/__init__.py
@@ -0,0 +1,13 @@
+"""Setup developer API.
+
+Settings:
+
+.. autosummary::
+   :toctree:
+
+   UserSettings
+   InstanceSettings
+   StorageSettings
+
+"""
+from lamindb_setup.dev import InstanceSettings, StorageSettings, UserSettings  # noqa
