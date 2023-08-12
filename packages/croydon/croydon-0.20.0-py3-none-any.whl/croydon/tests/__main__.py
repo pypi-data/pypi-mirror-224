d01cb4407f888e78886d965872f0659e6cc1112e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:10:30 +0000

    Add package croydon/0.20.0/croydon-0.20.0-py3-none-any.whl

---
+++ packages/croydon/croydon-0.20.0-py3-none-any.whl/croydon/tests/__main__.py
@@ -0,0 +1,10 @@
+import unittest
+from .test_base_model import TestBaseModel
+from .test_storable_model import TestStorableModel
+from .test_submodel import TestSubmodel
+from .test_validators import TestValidators
+from .test_reference import TestReference
+
+
+if __name__ == "__main__":
+    unittest.main()
