6875ac306eeef9f4ac504161ab4528322f0be838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:08:06 +0000

    Add package pyGSTi/0.9.11.2/pyGSTi-0.9.11.2-cp310-cp310-win_amd64.whl

---
+++ packages/pyGSTi/pyGSTi-0.9.11.2-cp310-cp310-win_amd64.whl/pygsti/extras/devices/__init__.py
@@ -0,0 +1,31 @@
+""" Exteneral Device Specifications Sub-package """
+#***************************************************************************************************
+# Copyright 2015, 2019 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+# Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights
+# in this software.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.  You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0 or in the LICENSE file in the root pyGSTi directory.
+#***************************************************************************************************
+
+from .devcore import *
+# from . import ibmq_athens
+# from . import ibmq_belem
+# from . import ibmq_burlington
+# from . import ibmq_cambrige
+# from . import ibmq_essex
+# from . import ibmq_gaudalupe
+# from . import ibmq_london
+# from . import ibmq_manhattan
+# from . import ibmq_melbourne
+# from . import ibmq_ourense
+# from . import ibmq_rueschlikon
+# from . import ibmq_sydney
+# from . import ibmq_tenerife
+# from . import ibmq_toronto
+# from . import ibmq_vigo
+# from . import ibmq_yorktown
+# from . import rigetti_agave
+# from . import rigetti_aspen4
+# from . import rigetti_aspen6
+# from . import rigetti_aspen7
