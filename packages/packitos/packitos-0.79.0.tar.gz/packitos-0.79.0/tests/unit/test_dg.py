442ba05b49728280ceebb8bf88ed31bcc9113374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:11:17 +0000

    Add package packitos/0.79.0/packitos-0.79.0.tar.gz

---
+++ packages/packitos/packitos-0.79.0.tar.gz/packitos-0.79.0/tests/unit/test_dg.py
@@ -0,0 +1,114 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+import pytest
+from flexmock import flexmock
+
+from packit.config import CommonPackageConfig, Config, PackageConfig
+from packit.distgit import DistGit
+from packit.local_project import LocalProjectBuilder
+
+
+@pytest.mark.parametrize(
+    "title, description, branch, source_branch, prs, exists",
+    [
+        (
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+            "f31",
+            "f31-update",
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    source_branch="f31-update",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+                    author="packit",
+                )
+            ],
+            True,
+        ),
+        (
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+            "f31",
+            "f31-update",
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    source_branch="f31-update",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 8957453b",
+                    author="packit",
+                )
+            ],
+            False,
+        ),
+        (
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+            "f32",
+            "f31-update",
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    source_branch="f31-update",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+                    author="packit",
+                )
+            ],
+            False,
+        ),
+        (
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+            "f31",
+            "f31-update",
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+                    author="packit-stg",
+                    source_branch="f31-update",
+                )
+            ],
+            False,
+        ),
+        (
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+            "f31",
+            "f32-update",
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    source_branch="f31-update",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+                    author="packit",
+                )
+            ],
+            False,
+        ),
+    ],
+)
+def test_existing_pr(title, description, branch, source_branch, prs, exists):
+    user_mock = flexmock().should_receive("get_username").and_return("packit").mock()
+    local_project = LocalProjectBuilder().build(
+        git_project=flexmock(service="something", get_pr_list=lambda: prs),
+        git_service=flexmock(user=user_mock),
+    )
+    distgit = DistGit(
+        config=flexmock(Config()),
+        package_config=flexmock(
+            PackageConfig(packages={"package": CommonPackageConfig()})
+        ),
+        local_project=local_project,
+    )
+    pr = distgit.existing_pr(title, description, branch, source_branch)
+    if exists:
+        assert pr is not None
+    else:
+        assert pr is None
