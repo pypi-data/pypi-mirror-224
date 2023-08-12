836d6f418971b23d85bb7b697e8e6580ff7ffa4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:21 +0000

    Add package pogo-api/0.0.3/pogo_api-0.0.3.tar.gz

---
+++ packages/pogo-api/pogo_api-0.0.3.tar.gz/pogo_api-0.0.3/src/pogo_api/route.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Callable
+
+from fastapi import APIRouter
+from fastapi import FastAPI
+
+from pogo_api.http import Method
+
+
+@dataclass
+class Route:
+    path: str
+    method: Method
+    endpoint: Callable
+    tag: str
+
+    def add_to_router(self, router: APIRouter | FastAPI) -> None:
+        router.add_api_route(
+            path=self.path,
+            methods=[self.method.value],
+            endpoint=self.endpoint,
+            tags=[self.tag],
+        )
