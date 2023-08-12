88df217e74fdaf2ad0e85f5dbb4a625d2a784c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:10:35 +0000

    Add package spinesTS/0.1.0/spinesTS-0.1.0.tar.gz

---
+++ packages/spinesTS/spinesTS-0.1.0.tar.gz/spinesTS-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import Extension, dist, find_packages, setup
+
+
+setup(
+    name='spinesTS',
+    version="0.1.0",
+    description='spinesTS, a powerful timeseries toolsets.',
+    keywords='computer vision',
+    packages=find_packages(),
+    long_description='./README.md',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    url='https://github.com/BirchKwok/spinesTS',
+    author='Birch Kwok',
+    author_email='birchkwok@gmail.com',
+    install_requires=[
+        'scikit-learn>=1.0.2',
+        'torch>=1.4',
+        'scipy>=1.7.0',
+        'numpy>=1.17.0',
+        'pandas>=1.0.0',
+        'tabulate>=0.8',
+        'matplotlib>=3.5.1'
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
---
+++ packages/spinesTS/spinesTS-0.1.0.tar.gz/spinesTS-0.1.0/spinesTS/data/__init__.py
@@ -0,0 +1,11 @@
+from ._data_generator import DataGenerator, RandomEventGenerator
+from ._data_reader import (
+    LoadElectricDataSets,
+    LoadMessagesSentHourDataSets,
+    LoadMessagesSentDataSets,
+    LoadWebSales,
+    LoadSupermarketIncoming,
+    BuiltInSeriesData
+)
+from ._data_base import DataTS
+
