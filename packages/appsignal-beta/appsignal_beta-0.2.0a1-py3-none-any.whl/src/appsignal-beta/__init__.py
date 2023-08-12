75e8cd14e30cdeaec24f0ef4c898a05a1490ef6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:11:14 +0000

    Add package appsignal-beta/0.2.0a1/appsignal_beta-0.2.0a1-py3-none-any.whl

---
+++ packages/appsignal-beta/appsignal_beta-0.2.0a1-py3-none-any.whl/src/appsignal-beta/__init__.py
@@ -0,0 +1,7 @@
+print(
+    "The AppSignal for Python integration is now published as the "
+    "`appsignal` package on PyPI. Update your project's dependencies "
+    "(e.g. requirements.txt) replacing the `appsignal-beta` package "
+    "with the `appsignal` package.",
+    file=sys.stderr,
+)
