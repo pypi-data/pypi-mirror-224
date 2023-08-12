44fa65cbbdd95d97f3e9ca011bdedd22f1362867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:48 +0000

    Add package humanloop/0.5.1/humanloop-0.5.1.tar.gz

---
+++ packages/humanloop/humanloop-0.5.1.tar.gz/humanloop-0.5.1/humanloop/apis/paths/evaluation_functions_id.py
@@ -0,0 +1,9 @@
+from humanloop.paths.evaluation_functions_id.delete import ApiFordelete
+from humanloop.paths.evaluation_functions_id.patch import ApiForpatch
+
+
+class EvaluationFunctionsId(
+    ApiFordelete,
+    ApiForpatch,
+):
+    pass
