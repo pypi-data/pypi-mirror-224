aa5bcc4b411999681bf7849d401c3a1b18f48dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:12:52 +0000

    Add package RepoDynamics/0.0.0.dev10/RepoDynamics-0.0.0.dev10-py3-none-any.whl

---
+++ packages/RepoDynamics/RepoDynamics-0.0.0.dev10-py3-none-any.whl/repodynamics/actions/__main__.py
@@ -0,0 +1,31 @@
+import argparse
+import importlib
+import sys
+
+from repodynamics.actions import io
+from repodynamics.ansi import SGR
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", type=str, help="Name of the action to run.")
+    action = parser.parse_args().action
+    action_name = action.replace('-', '_')
+    try:
+        action_module = importlib.import_module(f"repodynamics.actions.{action_name}")
+        action = getattr(action_module, action_name)
+        inputs = io.input(action=action)
+        outputs, summary = action(**inputs)
+        if outputs:
+            io.output(**outputs)
+        if summary:
+            io.summary(content=summary)
+    except Exception as e:
+        print(SGR.format("An unexpected error occurred:", "error"))
+        raise e
+        # sys.exit(1)
+    return
+
+
+if __name__ == "__main__":
+    main()
