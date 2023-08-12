fda841ff39988580e39e777827b29435842e03b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:08:46 +0000

    Add package thematos/0.8.0/thematos-0.8.0.tar.gz

---
+++ packages/thematos/thematos-0.8.0.tar.gz/thematos-0.8.0/src/thematos/runners/config.py
@@ -0,0 +1,41 @@
+import itertools
+from typing import Any, Dict, List
+
+from hyfi import HyFI
+from hyfi.composer import BaseModel
+
+logger = HyFI.getLogger(__name__)
+
+
+class LdaRunConfig(BaseModel):
+    _config_group_ = "/runner/config"
+    _config_name_ = "lda"
+
+    k: List[int] = [10, 20]
+    alpha: List[float] = [0.1]
+    eta: List[float] = [0.01]
+
+    @property
+    def total_runs(self) -> int:
+        return len(self.k) * len(self.alpha) * len(self.eta)
+
+    def iter_configs(self) -> Dict[str, Any]:
+        for k, alpha, eta in itertools.product(self.k, self.alpha, self.eta):
+            yield dict(k=k, alpha=alpha, eta=eta)
+
+
+class TopicRunnerResult(BaseModel):
+    runner_task_name: str = "topic"
+    runner_batch_name: str = "runner"
+    runner_batch_num: int = 0
+    runner_batch_id: str = ""
+    overrides: List[Dict] = []
+    model_summary: List[Dict] = []
+
+    def add_model_summary(
+        self,
+        overrides: Dict,
+        summary: Dict,
+    ):
+        self.overrides.append(overrides)
+        self.model_summary.append(summary)
