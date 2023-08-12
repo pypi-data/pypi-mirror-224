eb219bf7160572df38f2b51178300cabf5bf766b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:09:14 +0000

    Add package xia-gpt-prompts/0.0.5/xia_gpt_prompts-0.0.5-cp311-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-gpt-prompts/xia_gpt_prompts-0.0.5-cp311-none-macosx_11_0_x86_64.whl/xia_gpt_prompts/__init__.py
@@ -0,0 +1,11 @@
+from xia_gpt_prompts.task import Task, Produce
+from xia_gpt_prompts.mission import Mission
+from xia_gpt_prompts.goal import KnowledgeNode, KnowledgeMap, Goal
+
+__all__ = [
+    "Task", "Produce",
+    "Mission",
+    "KnowledgeNode", "KnowledgeMap", "Goal"
+]
+
+__version__ = "0.0.5"
