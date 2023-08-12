0eedea052f9ed531763744eacac77dbf0e4387d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:08:29 +0000

    Add package aidbox-python-sdk/0.1.6/aidbox-python-sdk-0.1.6.tar.gz

---
+++ packages/aidbox-python-sdk/aidbox-python-sdk-0.1.6.tar.gz/aidbox-python-sdk-0.1.6/aidbox_python_sdk/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "aidbox-python-sdk"
+__version__ = "0.1.6"
+__author__ = "beda.software"
+__license__ = "None"
+__copyright__ = "Copyright 2023 beda.software"
+
+# Version synonym
+VERSION = __version__
