11bdbe92b5b3823afaeb551ebb265e073f833c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:07:40 +0000

    Add package connectwise-client/1.0.0/connectwise_client-1.0.0-py3-none-any.whl

---
+++ packages/connectwise-client/connectwise_client-1.0.0-py3-none-any.whl/connectwise_client/main.py
@@ -0,0 +1,13 @@
+"""Module where the application starts."""
+
+
+def hello(my_name: str) -> str:
+    """Proves the library functions can be called properly.
+
+    parameters:
+        my_name: name to be processed.
+
+    returns:
+        hello name.
+    """
+    return f"hello {my_name}"
