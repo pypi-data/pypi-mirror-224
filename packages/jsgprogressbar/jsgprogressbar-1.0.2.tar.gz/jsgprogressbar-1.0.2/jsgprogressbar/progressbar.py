6a61704f6384f2f2c5bce72f7286ba3c161de1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:08:22 +0000

    Add package jsgprogressbar/1.0.2/jsgprogressbar-1.0.2.tar.gz

---
+++ packages/jsgprogressbar/jsgprogressbar-1.0.2.tar.gz/jsgprogressbar-1.0.2/jsgprogressbar/progressbar.py
@@ -0,0 +1,27 @@
+from sys import stdout
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def progressbar(it, prefix="", size=60, out=stdout): # Python3.3+
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        print("{}{}{}{}[{}{}] {}{}/{}".format(bcolors.BOLD,prefix,bcolors.ENDC,bcolors.WARNING, "█"*x, "."*(size-x),bcolors.ENDC, j, count), 
+                end='\r', file=out, flush=True)
+        
+        
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    print("\n", flush=True, file=out)
