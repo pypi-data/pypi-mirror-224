c4df5dd24358a558b38cb81ecbc35cd39d4e1035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:08:22 +0000

    Add package types-aiobotocore-entityresolution/2.6.0/types-aiobotocore-entityresolution-2.6.0.tar.gz

---
+++ packages/types-aiobotocore-entityresolution/types-aiobotocore-entityresolution-2.6.0.tar.gz/types-aiobotocore-entityresolution-2.6.0/types_aiobotocore_entityresolution/__init__.py
@@ -0,0 +1,43 @@
+"""
+Main interface for entityresolution service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_entityresolution import (
+        Client,
+        EntityResolutionClient,
+        ListMatchingJobsPaginator,
+        ListMatchingWorkflowsPaginator,
+        ListSchemaMappingsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("entityresolution") as client:
+        client: EntityResolutionClient
+        ...
+
+
+    list_matching_jobs_paginator: ListMatchingJobsPaginator = client.get_paginator("list_matching_jobs")
+    list_matching_workflows_paginator: ListMatchingWorkflowsPaginator = client.get_paginator("list_matching_workflows")
+    list_schema_mappings_paginator: ListSchemaMappingsPaginator = client.get_paginator("list_schema_mappings")
+    ```
+"""
+from .client import EntityResolutionClient
+from .paginator import (
+    ListMatchingJobsPaginator,
+    ListMatchingWorkflowsPaginator,
+    ListSchemaMappingsPaginator,
+)
+
+Client = EntityResolutionClient
+
+
+__all__ = (
+    "Client",
+    "EntityResolutionClient",
+    "ListMatchingJobsPaginator",
+    "ListMatchingWorkflowsPaginator",
+    "ListSchemaMappingsPaginator",
+)
