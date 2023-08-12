2c46e9c73cc90f859cf7beb968b1eb8739e088f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:08:16 +0000

    Add package pyfldm/0.0.4/pyfldm-0.0.4-py3-none-any.whl

---
+++ packages/pyfldm/pyfldm-0.0.4-py3-none-any.whl/pyfldm/AppMonitor.py
@@ -0,0 +1,262 @@
+############################################################################
+# 
+#  File: AppMonitor.py
+#  Copyright(c) 2023, Phillip Hall. All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#  USA
+#
+############################################################################
+
+import os, sys
+import psutil
+import subprocess
+import logging
+from time import time, sleep
+from .Client import Client
+
+logger = logging.getLogger(__name__)
+
+MAX_STARTUP_DELAY_SECS = 10
+MAX_SHUTDOWN_DELAY_SECS = 10
+
+class AppMonitor:
+
+    ''' ApplicationMonitor manages the running of Fldigi. 
+    Launches, monitors, and terminates the Fldigi process, using python subprocess.Popen
+
+    @param hostname(str): the IP address of the xmlrpc server for Fldigi to establish
+    @param port(int): the port number of the xmlrpc server connection
+    @param exe_path(str): [OPTIONAL, ONLY USE IF NEEDED] the path to the executable fldigi app, to be used when Fldigi installed in an other than default location. 
+    Example for windows: exe_path = "C:\\\\\\Users\\\\\\Me\\\\\\Desktop\\\\\\fldigi-folder\\\\\\fldigi.exe ***must use double slash for python to understand the windows path as a string***, 
+    Example for linux: exe_path = "/home/myself/Desktop/fldigi-folder/fldigi-4.1.26
+
+    >>> from pyfldm.AppMonitor import AppMonitor
+    >>> app = AppMonitor()
+    >>> app.start()
+    >>> # wait a few seconds for Fldigi to start up
+    >>> app.is_running() # checks that Fldigi is a currently running process
+    True
+    >>> app.is_functional() # verifies that the xmlrpc interface is responsive
+    True
+    >>> result = app.stop() # asks fldigi to gracefully shut down, returns a 0 if successfully stopped, 1 if not
+    >>> if not result:
+    ...     app.kill()  # forcibly kills the process
+    >>> app.is_running()
+    False
+    '''
+
+    def __init__(self, hostname:str='127.0.0.1', port:int=7362, exe_path:str=None) -> None:
+        self.platform = sys.platform
+        self.process = None
+        self.hostname = hostname
+        self.port = int(port)
+        self._client = Client(hostname, port)
+        self.exe_path = exe_path
+
+        # log platform
+        if self.platform == 'darwin':
+            logger.debug("Detected MacOS")
+        elif self.platform == 'win32':
+            logger.debug("Detected Windows")
+        else:
+            logger.debug("Detected Linux/Other OS")
+
+    def _get_process_id(self) -> int:
+        if self.platform == 'win32':
+            # filter processes to find fldigi, if it exists
+            fldigi_process = [proc for proc in psutil.process_iter(['pid', 'name']) if 'fldigi' in proc.name()]
+            if fldigi_process:
+                return fldigi_process[0].pid
+            # No fldigi process found
+            return 0
+        else:
+            process1 = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+            try: 
+                process2 = subprocess.check_output(['pgrep', '-f', 'fldigi'], stdin=process1.stdout)
+            except subprocess.CalledProcessError:
+                return 0
+            else:
+                return int(process2.decode().strip())
+
+    def is_running(self) -> bool:
+        return True if self._get_process_id() else False
+    
+    def is_functional(self) -> bool:
+        try:
+            if self._client.fldigi.name() == 'fldigi':
+                return True
+            else:
+                logger.warning("Connection made but Fldigi name() not responding with 'fldigi'")
+                return False
+        except ConnectionRefusedError:
+            return False
+        
+    def _wait_for_startup(self, timeout_secs = MAX_STARTUP_DELAY_SECS, sleep_secs = .5) -> bool:
+        start = time()
+        while (start + timeout_secs) > time():
+            if self.is_functional():
+                return True
+            sleep(sleep_secs)
+        return False
+    
+    def _wait_for_shutdown(self, timeout_secs = MAX_SHUTDOWN_DELAY_SECS, sleep_secs = .5) -> bool:
+        start = time()
+        while (start + timeout_secs) > time():
+            if not self.is_functional():
+                return True
+            sleep(sleep_secs)
+        return False
+
+    def start(self) -> int:
+        # check if application already running
+        if self.is_running():
+            logger.warning("Fldigi is already running. Shut down all instances of Fldigi before using AppMonitor.start()")
+            return 0
+        logger.info("Starting Fldigi")
+
+        addl_args = ['--xmlrpc-server-address', 
+                     self.hostname, 
+                     '--xmlrpc-server-port', 
+                     str(self.port)]
+        startup_args = ['fldigi']
+
+        if self.platform == 'darwin':
+            # start with MacOS
+            # find the application name with bash commands
+            process1 = subprocess.Popen(['ls', '/Applications'], stdout=subprocess.PIPE)
+            process2 = subprocess.check_output(['grep', 'fldigi'], stdin=process1.stdout)
+            app_name = process2.decode().strip()
+            # start the application
+            startup_args = ['open', '-a', f'{app_name}', '--args']
+
+        elif self.platform == 'win32':
+            # start with windows
+            # use the path given
+            if self.exe_path:
+                exe_file_name = os.path.basename(self.exe_path)
+                if ((not os.path.isfile(self.exe_path)) 
+                    or (not self.exe_path.endswith('.exe')) 
+                    or ('fldigi' not in exe_file_name.lower())):
+                    raise FileNotFoundError('The given fldigi .exe file not found')
+                startup_args = [self.exe_path]
+
+            # otherwise need to look for the path
+            else:
+                startup_args = [self._find_fldigi_exe()]
+
+        else:
+            # start with linux
+            pass
+        
+        # start the process with the gathered command line arguments
+        self.process = subprocess.Popen(startup_args + addl_args)
+
+        if self._wait_for_startup():
+            logger.debug("Fldigi fully functional")
+            return 0
+        logger.critical("Fldigi process started but reached max timeout with no connection to xmlrpc. Unconfirmed if Fldigi is functionally running")
+        return 1 
+    
+    def stop(self, save_options=False, save_log=False, save_macros=False, force_if_unsuccessful=False) -> int:
+        # first verify its actually running
+        if not self.is_running():
+            logger.info("No Fldigi instances running, nothing to shut down")
+            return 0
+        logger.debug("Starting graceful shutdown of Fldigi")
+        self._client.fldigi.terminate(save_options, save_log, save_macros)
+
+        # wait and verify that Fldigi has shut down
+        if self._wait_for_shutdown():
+            logger.info("Fldigi successfully shut down")
+            return 0
+        logger.critical("Fldigi graceful shutdown unsuccessful")
+        if force_if_unsuccessful:
+            return self.kill()
+        return 1
+    
+    def kill(self) -> int:
+        # first verify its actually running
+        if not self.is_running():
+            logger.info("No Fldigi instances running, nothing to shut down")
+            return 0
+        process_id = self._get_process_id()
+        process = psutil.Process(process_id)
+        logger.debug("Starting forced shutdown of Fldigi")
+        # start with a more graceful system SIGTERM
+        process.terminate()
+        # if self.platform == 'win32':
+        #     # kill with windows
+        #     psutil.Process(process_id)
+        # else:
+        #     # kill with macos/linux/other
+        #     subprocess.Popen(['kill', str(process_id)])
+
+        if self._wait_for_shutdown():
+            logger.info("Fldigi successfully force killed via SIGTERM")
+            return 0
+        logger.warn("Fldigi SIGTERM unsuccessful")
+
+        # now try the less graceful SIGKILL
+        process.kill()
+        # if self.platform == 'win32':
+        #     # kill with windows
+        #     pass
+        # else:
+        #     # kill with macos/linux/other
+        #     subprocess.Popen(['kill', '-9', str(process_id)])
+
+        if self._wait_for_shutdown():
+            logger.info("Fldigi successfully force killed via SIGKILL")
+            return 0
+        logger.critical("Fldigi SIGKILL unsuccessful, cannot shut down Fldigi")
+        return 1
+
+    def _find_fldigi_exe(self):
+        # first look in Program Files (x86)
+        prog_files = os.environ["ProgramFiles(x86)"]
+        fldigi_dir = [dir for dir in os.listdir(prog_files) if 'fldigi' in dir.lower()]
+        if not fldigi_dir:
+            # Not there, look in Program Files
+            prog_files = os.environ["ProgramFiles"]
+            fldigi_dir = [dir for dir in os.listdir(prog_files) if 'fldigi' in dir.lower()]
+        # No Fldigi folders found in any Program Files directories
+        if not fldigi_dir:
+            logger.critical("Cannot find FLDigi in either Program Files or Program Files (x86). Check that FLDigi is installed or pass in the path to the fldigi .exe")
+            raise ModuleNotFoundError("Cannot find FLDigi. Ensure it's installed or pass in path to .exe")
+
+        if len(fldigi_dir) > 1:
+            logger.warning("Found multiple versions or installs of Fldigi. Defaulting to the latest version, as best as can be determined. If another version or install is preferred, pass in the .exe path to AppMonitor")
+            # get the latest version number, sort so the latest is first
+            fldigi_dir.sort(reverse=True)
+            # ensure there is an exe in the folder we want, if not iterate through th fldigi folders to find one
+            for dir in fldigi_dir:
+                current_dir = os.path.join(prog_files, dir)
+                files_in_dir = os.listdir(current_dir)
+                for file in files_in_dir:
+                    if  (('fldigi' in file) and ('.exe' in file)):
+                        logger.info(f"The latest usable version found is {current_dir}, attempting to start with this installation")
+                        return os.path.join(current_dir, file)
+                logger.warn(f"No fldigi exe found in {current_dir}. This may be a corrupted installation. Attempting the next installed version, if any")
+            
+            return
+        path_to_exe = os.path.join(prog_files, fldigi_dir[0])
+        fldigi_exe = [f for f in os.listdir(path_to_exe) if (('fldigi' in f) and ('.exe' in f))]
+        if fldigi_exe:
+            return os.path.join(path_to_exe, fldigi_exe[0])
+        else:
+            # TODO
+            logger.critical(f"No fldigi exe found in {path_to_exe}. Check your installation of Fldigi, it may be incomplete or corrupted.")
+            raise ModuleNotFoundError('No fldigi exe found')
---
+++ packages/pyfldm/pyfldm-0.0.4-py3-none-any.whl/pyfldm/submodules/Main.py
@@ -0,0 +1,322 @@
+############################################################################
+# 
+#  File: Main.py
+#  Copyright(c) 2023, Phillip Hall. All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#  USA
+#
+############################################################################
+
+import logging
+from xmlrpc.client import ServerProxy
+from .BaseCall import BaseCall
+
+logger = logging.getLogger(__name__)
+
+class Main(BaseCall):
+    '''Houses the commands in the main group in the XML-RPC spec for fldigi.
+    Reference: http://www.w1hkj.com/FldigiHelp/xmlrpc_control_page.html
+
+    This class is not intended to be created or used directly, but rather utilized under the pyfldm client object. Reference Client.py which has an attribute self.log that interfaces these methods.
+
+    @param client(xmlrpc.client.ServerProxy): a ServerProxy client object used to make http requests via the fldigi xmlrpc api
+    
+    Example use:
+    >>> from pyfldm.Client import Client
+    >>> client = Client()
+    >>> client.main.get_frequency()
+    14070.000
+    '''
+    def __init__(self, client: ServerProxy) -> None:
+        self.client = client
+    
+    def __str__(self) -> str:
+        return __name__.lower().split(".")[-1]
+
+    def abort(self) -> None:
+        '''Aborts a transmit or tune'''
+        self.client.main.abort()
+    
+    def get_afc(self) -> bool:
+        '''Gets the AFC state
+
+        @return (bool): the AFC state
+        '''
+        return self.client.main.get_afc()
+    
+    def get_char_rates(self) -> str:
+        '''Gets the table of char rates
+
+        @return (str): the char rates
+        '''
+        return self.client.main.get_char_rates()
+    
+    def get_char_timing(self, char_bytes: bytes) -> str:
+        '''Gets transmit duration for the specified character
+
+        @param char_bytes(bytes): the bitmask to send for the character (samples:sample rate)
+        @return (bytes): the value of the character
+        '''
+        return self.client.main.get_char_timing(char_bytes)
+    
+    def get_frequency(self) -> float:
+        '''Gets the RF carrier frequency
+
+        @return (float): the RF carrier frequency
+        '''
+        return self.client.main.get_frequency()
+    
+    def get_local(self) -> bool:
+        '''Gets the Transmit Lock state
+
+        @return (bool): the Transmit Lock state
+        '''
+        return self.client.main.get_local()
+    
+    def get_max_macro_id(self) -> int:
+        '''Gets the maximum macro ID number
+
+        @return (int): the maximum macro ID number
+        '''
+        return self.client.main.get_max_macro_id()
+    
+    def get_rsid(self) -> bool:
+        '''Gets the RSID state
+
+        @return (bool): the RSID state
+        '''
+        return self.client.main.get_rsid()
+    
+    def get_txid(self) -> bool:
+        '''Gets the TxRSID state
+
+        @return (bool): the TxRSID state
+        '''
+        return self.client.main.get_txid()
+    
+    def get_squelch(self) -> bool:
+        '''Gets the squelch state
+
+        @return (bool): the squelch state
+        '''
+        return self.client.main.get_squelch()
+    
+    def get_squelch_level(self) -> float:
+        '''Gets the squelch level
+
+        @return (float): the squelch level
+        '''
+        return self.client.main.get_squelch_level()
+    
+    def get_status1(self) -> str:
+        '''Gets the contents of the first status field (typically s/n)
+
+        @return (str): the contents of the first status field
+        '''
+        return self.client.main.get_status1()
+    
+    def get_status2(self) -> str:
+        '''Gets the contents of the second status field
+
+        @return (str): the contents of the second status field
+        '''
+        return self.client.main.get_status2()
+    
+    def get_trx_state(self) -> str:
+        '''Gets the T/R state
+
+        @return (str): the T/R state
+        '''
+        return self.client.main.get_trx_state()
+    
+    def get_trx_status(self) -> str:
+        '''Gets the transmit/tune/receive status
+
+        @return (str): the transmit/tune/receive status
+        '''
+        return self.client.main.get_trx_status()
+    
+    def get_trx_timing(self, bitmask: bytes) -> str:
+        '''Gets the transmit duration for test string (samples:sample rate:secs)
+
+        @param bitmask(bytes): the bytes to specify the test string (samples:sample rate:secs)
+        @return (str): the transmit/tune/receive status
+        '''
+        return self.client.main.get_trx_timing(bitmask)
+
+    def get_wf_sideband(self) -> str:
+        '''Gets the current waterfall sideband
+
+        @return (str): the current waterfall sideband
+        '''
+        return self.client.main.get_wf_sideband()
+    
+    def inc_frequency(self, amount: float) -> float:
+        '''Increments the RF carrier frequency
+
+        @param amount(float): the amount to increment the frequency
+        @return (float): the new frequency
+        '''
+        return self.client.main.inc_frequency(amount)
+    
+    def inc_squelch_level(self, amount: float) -> float:
+        '''Increments the squelch level
+
+        @param amount(float): the amount to increment the squelch
+        @return (float): the new squelch level
+        '''
+        return self.client.main.inc_squelch_level(amount)
+    
+    def run_macro(self, macro_num: int) -> None:
+        '''Runs a macro
+
+        @param macro_num(float): the macro number to run
+        @return (float): the new frequency
+        '''
+        self.client.main.run_macro(macro_num)
+    
+    def rx(self) -> None:
+        '''Puts Fldigi into recieve mode'''
+        self.client.main.rx()
+    
+    def rx_only(self) -> None:
+        '''Puts Fldigi into recieve mode, disables Tx'''
+        self.client.main.rx_only()
+
+    def rx_tx(self) -> None:
+        '''Sets normal Rx/Tx switching'''
+        self.client.main.rx_tx()
+
+    def set_afc(self, new_state: bool) -> bool:
+        '''Sets the AFC state
+
+        @param new_state(bool): the new AFC state
+        @return (bool): the old state
+        '''
+        return self.client.main.set_afc(new_state)
+
+    def set_frequency(self, new_frequency: float) -> float:
+        '''Sets the RF carrier frequency
+
+        @param new_state(float): the new frequency
+        @return (bool): the old frequency
+        '''
+        return self.client.main.set_frequency(new_frequency)
+
+    def set_lock(self, new_state: bool) -> bool:
+        '''Sets the Transmit Lock state
+
+        @param new_state(bool): the new Transmit lock state
+        @return (bool): the old state
+        '''
+        return self.client.main.set_lock(new_state)
+    
+    def set_reverse(self, new_state: bool) -> bool:
+        '''Sets the reverse sideband state
+
+        @param new_state(bool): the new reverse sideband
+        @return (bool): the old state
+        '''
+        return self.client.main.set_reverse(new_state)
+    
+    def set_rsid(self, new_state: bool) -> bool:
+        '''Sets the RSID state
+
+        @param new_state(bool): the new RSID state
+        @return (bool): the old state
+        '''
+        return self.client.main.set_rsid(new_state)
+    
+    def set_txid(self, new_state: bool) -> bool:
+        '''Sets the TxRSID state
+
+        @param new_state(bool): the new TxRSID state
+        @return (bool): the old state
+        '''
+        return self.client.main.set_txid(new_state)
+    
+    def set_squelch(self, new_state: bool) -> bool:
+        '''Sets the Squelch state
+
+        @param new_state(bool): the new squelch state
+        @return (bool): the old state
+        '''
+        return self.client.main.set_squelch(new_state)
+    
+    def set_squelch_level(self, new_level: float) -> float:
+        '''Sets the Squelch level
+
+        @param new_level(float): the new squelch level
+        @return (float): the old level
+        '''
+        return self.client.main.set_squelch_level(new_level)
+    
+    def set_wf_sideband(self, new_sideband: str) -> None:
+        '''Sets the waterfall sideband to USB or LSB
+
+        @param new_sideband(float): the new squelch level
+        '''
+        self.client.main.set_wf_sideband(new_sideband)
+
+    def toggle_afc(self) -> bool:
+        '''Toggles the AFC state
+
+        @return (bool): the new state
+        '''
+        return self.client.main.toggle_afc()
+    
+    def toggle_lock(self) -> bool:
+        '''Toggles the Transmit Lock state
+
+        @return (bool): the new state
+        '''
+        return self.client.main.toggle_lock()
+    
+    def toggle_reverse(self) -> bool:
+        '''Toggles the Reverse Sideband state
+
+        @return (bool): the new state
+        '''
+        return self.client.main.toggle_reverse()
+    
+    def toggle_rsid(self) -> bool:
+        '''Toggles the RSID state
+
+        @return (bool): the new state
+        '''
+        return self.client.main.toggle_rsid()
+    
+    def toggle_txid(self) -> bool:
+        '''Toggles the TxRSID state
+
+        @return (bool): the new state
+        '''
+        return self.client.main.toggle_txid()
+    
+    def toggle_squelch(self) -> bool:
+        '''Toggles the squelch state
+
+        @return (bool): the new state
+        '''
+        return self.client.main.toggle_squelch()
+    
+    def tune(self) -> None:
+        '''Tunes'''
+        self.client.main.tune()
+
+    def tx(self) -> None:
+        '''Puts the Fldigi into transmit mode'''
+        self.client.main.tx()
