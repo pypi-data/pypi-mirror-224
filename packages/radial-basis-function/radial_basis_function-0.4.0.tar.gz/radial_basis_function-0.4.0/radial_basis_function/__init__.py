57afc5e7117728fd877d6fcb0aa6da4fe15e47ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:07:44 +0000

    Add package radial-basis-function/0.4.0/radial_basis_function-0.4.0.tar.gz

---
+++ packages/radial-basis-function/radial_basis_function-0.4.0.tar.gz/radial_basis_function-0.4.0/radial_basis_function/__init__.py
@@ -0,0 +1,13 @@
+"""
+Radial Basis Function
+Original Author: Victor Venites
+"""
+from .pseudo_inversa import PseudoInversa
+from .rbf import RadialBasisFunction
+
+__version__ = '0.4.0'
+
+__all__ = [
+    'PseudoInversa',
+    'RadialBasisFunction',
+]
