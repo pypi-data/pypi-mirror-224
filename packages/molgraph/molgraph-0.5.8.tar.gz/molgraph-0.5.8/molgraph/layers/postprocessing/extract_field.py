8fede3f59fb33a9842dcb708a1fb5614e9a206ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:08:13 +0000

    Add package molgraph/0.5.8/molgraph-0.5.8.tar.gz

---
+++ packages/molgraph/molgraph-0.5.8.tar.gz/molgraph-0.5.8/molgraph/layers/postprocessing/extract_field.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from tensorflow import keras
+
+from molgraph.internal import register_keras_serializable 
+
+from molgraph.tensors.graph_tensor import GraphTensor
+
+
+@register_keras_serializable(package='molgraph')
+class ExtractField(keras.layers.Layer):
+    '''Extract specific field of ``GraphTensor``.
+
+    **Example:**
+
+    >>> graph_tensor = molgraph.GraphTensor(
+    ...     data={
+    ...         'edge_src': [1, 0, 3, 4, 2, 4, 3, 2],
+    ...         'edge_dst': [0, 1, 2, 2, 3, 3, 4, 4],
+    ...         'node_feature': [
+    ...             [1., 0.],
+    ...             [2., 0.],
+    ...             [3., 0.],
+    ...             [4., 0.],
+    ...             [0., 5.]
+    ...         ],
+    ...         'graph_indicator': [0, 0, 1, 1, 1],
+    ...     }
+    ... )
+    >>> model = tf.keras.Sequential([
+    ...     tf.keras.layers.Input(type_spec=graph_tensor.unspecific_spec),
+    ...     molgraph.layers.ExtractField('node_feature')
+    ... ])
+    >>> model(graph_tensor)
+    <tf.Tensor: shape=(5, 2), dtype=float32, numpy=
+    array([[1., 0.],
+           [2., 0.],
+           [3., 0.],
+           [4., 0.],
+           [0., 5.]], dtype=float32)>
+
+    Args:
+        field (str):
+            Field to extract from ``GraphTensor``.
+    '''
+    def __init__(self, field, **kwargs):
+        super().__init__(**kwargs)
+        self.field = field
+
+    def call(self, tensor: GraphTensor) -> tf.Tensor:
+        '''Defines the computation from inputs to outputs.
+
+        This method should not be called directly, but indirectly
+        via ``__call__()``. Upon first call, the layer is automatically
+        built via ``build()``.
+
+        Args:
+            tensor (GraphTensor):
+                Input to the layer.
+
+        Returns:
+            A ``tf.Tensor`` or `tf.RaggedTensor` based on the node_feature
+            field of the inputted ``GraphTensor``.
+        '''
+        return tensor[self.field]
+
+    def get_config(self):
+        base_config = super().get_config()
+        base_config.update({'field': self.field})
+        return base_config
---
+++ packages/molgraph/molgraph-0.5.8.tar.gz/molgraph-0.5.8/molgraph/layers/readout/segment_pool.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+
+from molgraph.internal import register_keras_serializable 
+
+from molgraph.tensors.graph_tensor import GraphTensor
+
+
+@register_keras_serializable(package='molgraph')
+class SegmentPoolingReadout(layers.Layer):
+
+    '''Segmentation pooling for graph readout.
+
+    Alias: ``Readout``
+
+    **Example:**
+
+    >>> graph_tensor = molgraph.GraphTensor(
+    ...     data={
+    ...         'edge_src': [[1, 0], [1, 2, 0, 2, 1, 0]],
+    ...         'edge_dst': [[0, 1], [0, 0, 1, 1, 2, 2]],
+    ...         'node_feature': [
+    ...             [[1.0, 0.0], [1.0, 0.0]],
+    ...             [[1.0, 0.0], [1.0, 0.0], [0.0, 1.0]]
+    ...         ],
+    ...     }
+    ... )
+    >>> model = tf.keras.Sequential([
+    ...     tf.keras.layers.Input(type_spec=graph_tensor.unspecific_spec),
+    ...     # molgraph.layers.GCNConv(4),
+    ...     molgraph.layers.Readout('mean') # alias for SegmentPoolingReadout
+    ... ])
+    >>> model(graph_tensor)
+    <tf.Tensor: shape=(2, 2), dtype=float32, numpy=
+    array([[1.        , 0.        ],
+           [0.6666667 , 0.33333334]], dtype=float32)>
+
+    Args:
+        mode (str):
+            What type of pooling should be performed. Either of 'mean',
+            'max' or 'sum'. Specifically, performs a ``tf.math.segment_mean``,
+            ``tf.math.segment_max`` or ``tf.math.segment_sum`` (respectively)
+            on the ``node_feature`` field of the inputted graph tensor.
+            Defaults to 'mean'.
+    '''
+
+    def __init__(self, mode: str = 'mean', **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.mode = mode
+        if self.mode == 'mean' or self.mode == 'average' or self.mode == 'avg':
+            self.pooling_fn = tf.math.segment_mean
+        elif self.mode == 'sum':
+            self.pooling_fn = tf.math.segment_sum
+        elif self.mode == 'max':
+            self.pooling_fn = tf.math.segment_max
+        else:
+            raise ValueError('Value passed to mode is invalid, ' +
+                             'needs to be one of the following: ' +
+                             '"mean"/"average"/"avg", "sum" or "max"')
+
+    def call(self, tensor: GraphTensor) -> tf.Tensor:
+        '''Defines the computation from inputs to outputs.
+
+        This method should not be called directly, but indirectly
+        via ``__call__()``. Upon first call, the layer is automatically
+        built via ``build()``.
+
+        Args:
+            tensor (GraphTensor):
+                Input to the layer.
+
+        Returns:
+            A ``tf.Tensor`` or `tf.RaggedTensor` based on the ``node_feature``
+            field of the inputted ``GraphTensor``.
+        '''
+        if isinstance(tensor.node_feature, tf.RaggedTensor):
+            tensor = tensor.merge()
+        node_feature = self.pooling_fn(
+            tensor.node_feature, tensor.graph_indicator)
+        return node_feature
+
+    def compute_output_shape(self, input_shape):
+        if input_shape[0] is None and input_shape[1] is not None:
+            # input_shape corresponds to a tf.Tensor
+            return input_shape
+        # input_shape corresponds to a tf.RaggedTensor
+        return input_shape[1:]
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'mode': self.mode})
+        return config
