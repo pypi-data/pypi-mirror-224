c7b689dfc12e09614ea2864e0e434d1032e3320e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:11:20 +0000

    Add package turboyaml/0.0.5/turboyaml-0.0.5.tar.gz

---
+++ packages/turboyaml/turboyaml-0.0.5.tar.gz/turboyaml-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="turboyaml",
+    version="0.0.5",
+    author="Fred Setra",
+    author_email="fred.setra@gmail.com",
+    description="An AI-powered CLI tool for converting DBT SQL files to YAML using OpenAI.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fredbrowne/turboyaml",
+    packages=find_packages(),
+    install_requires=[
+        "openai==0.27.8",
+    ],
+    entry_points={
+        "console_scripts": [
+            "turboyaml=turboyaml.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
