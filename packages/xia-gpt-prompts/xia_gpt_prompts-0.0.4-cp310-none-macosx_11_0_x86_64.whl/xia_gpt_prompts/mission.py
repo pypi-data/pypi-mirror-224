51951ad80d747cc6d8b347fe38883f41adbdba75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:07:39 +0000

    Add package xia-gpt-prompts/0.0.4/xia_gpt_prompts-0.0.4-cp310-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-gpt-prompts/xia_gpt_prompts-0.0.4-cp310-none-macosx_11_0_x86_64.whl/xia_gpt_prompts/mission.py
@@ -0,0 +1,35 @@
+from xia_gpt_prompts.task import Task
+
+
+class Mission:
+    prompt_context = "# Context\n{}\n"
+    prompt_format = "# Format example\n\n---\n{}---\n\n"
+    prompt_instruction = "#----\n{}\n"
+
+    def __init__(self,
+                 instruction: str,
+                 context: str):
+        self.instruction = instruction
+        self.context = context
+        self.tasks = []
+
+    def add_task(self, task: Task):
+        self.tasks.append(task)
+
+    def get_formats(self):
+        formats = [task.get_format_text() for task in self.tasks]
+        formats = "\n".join([part for part in formats if part])
+        return formats
+
+    def get_guides(self):
+        guides = [task.get_guide_text() for task in self.tasks]
+        guides = "\n".join([part for part in guides if part])
+        return guides
+
+    def get_prompt(self):
+        prompt = self.prompt_context.format(self.context)
+        prompt += self.prompt_format.format(self.get_formats())
+        prompt += self.prompt_instruction.format(self.instruction)
+        prompt += self.get_guides()
+
+        return prompt
