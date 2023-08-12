2c4e14a7bf8a37ba7445934ef2616aa033be6cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:37 +0000

    Add package dxsp/4.2.23/dxsp-4.2.23.tar.gz

---
+++ packages/dxsp/dxsp-4.2.23.tar.gz/dxsp-4.2.23/dxsp/utils/utils.py
@@ -0,0 +1,34 @@
+"""
+ DEX SWAP
+🛠️ W3 UTILS
+"""
+
+import requests
+from loguru import logger
+
+
+async def get(url, params=None, headers=None):
+    """
+    Asynchronously gets a url payload
+    and returns the response
+
+    Args:
+        url (str): The url to get
+        params (dict, optional): The params to send. Defaults to None.
+        headers (dict, optional): The headers to send. Defaults to None.
+
+    Returns:
+        dict: The response
+
+    Raises:
+        Exception: Error
+
+    """
+    try:
+        response = requests.get(url, params=params, headers=headers, timeout=10)
+        logger.debug(response)
+        if response.status_code == 200:
+            return response.json()
+
+    except Exception as error:
+        logger.error("get: {}", error)
