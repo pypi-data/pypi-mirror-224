53f20e377c05c335cf7ce016b9553351b42d204f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:08:52 +0000

    Add package prueba-player/0.0.1/prueba_player-0.0.1-py3-none-any.whl

---
+++ packages/prueba-player/prueba_player-0.0.1-py3-none-any.whl/paquete/__init__.py
@@ -0,0 +1,3 @@
+"""
+    Documentación para el módulo paquete
+"""
---
+++ packages/prueba-player/prueba_player-0.0.1-py3-none-any.whl/paquete/player.py
@@ -0,0 +1,21 @@
+"""
+    Nódulo para reproducir música.
+"""
+
+class player:
+    """
+        Esta clase crea un reproductos de música.
+    """
+
+    def play(self):
+        """
+            Reproduce la canción recibida en el constructor.
+
+            Parameters:
+            song (str): este es un string con el path de la cación
+
+            Returns:
+            bool: retorna true en caso de que funcione con exito.
+        """
+        print("Reproducciendo canción")
+    
