07db5c7d3f6be1ad10fb85f80c5f734c45e9bf01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:12:05 +0000

    Add package AggiNScript/0.1/AggiNScript-0.1.tar.gz

---
+++ packages/AggiNScript/AggiNScript-0.1.tar.gz/AggiNScript-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AggiNScript',
+    version='0.1',
+    description='My Custom Programming Language',
+    author='Aman',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        
+    ],
+)
