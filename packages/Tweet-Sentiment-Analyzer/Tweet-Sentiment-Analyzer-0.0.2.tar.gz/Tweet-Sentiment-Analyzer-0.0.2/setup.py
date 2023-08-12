5505e468ab40c9c000f4bc9146d9fc5f03d904bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:10:24 +0000

    Add package Tweet-Sentiment-Analyzer/0.0.2/Tweet-Sentiment-Analyzer-0.0.2.tar.gz

---
+++ packages/Tweet-Sentiment-Analyzer/Tweet-Sentiment-Analyzer-0.0.2.tar.gz/Tweet-Sentiment-Analyzer-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Tweet-Sentiment-Analyzer",
+    version="0.0.2",
+    description="This python script delves into the depths of Twitter to bring you real-time insights into public opinion.",
+    author="Aleksandr",
+    author_email="novacyan545@gmail.com",
+    url="https://github.com/Aleksandr-Chelyshkin/Tweet-Sentiment-Analyzer.git",
+    packages=["Tweet-Sentiment-Analyzer"],
+    )
