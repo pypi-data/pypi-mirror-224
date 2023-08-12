36b51033a4179534c3c7d2f0932bedf3aef41186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:10:33 +0000

    Add package NJUlogin/3.0.6/NJUlogin-3.0.6.tar.gz

---
+++ packages/NJUlogin/NJUlogin-3.0.6.tar.gz/NJUlogin-3.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import NJUlogin
+
+with open("README.md", "r", encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name = NJUlogin.__title__,
+    version = NJUlogin.__version__,
+    author = NJUlogin.__author__,
+    author_email = NJUlogin.__author_email__,
+    description = NJUlogin.__description__,
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = NJUlogin.__url__,
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['requests', 'opencv-python-headless',
+        'numpy', 'lxml', 'pycryptodome', 'user_agents', 'captchaOCR'],
+    python_requires = '>=3.7'
+)
