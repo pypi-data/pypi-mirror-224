2501c726baf971d762c7d96723ea995d80d96c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:10:39 +0000

    Add package siliconcompiler/0.13.2/siliconcompiler-0.13.2-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.13.2-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/siliconcompiler/tools/verilator/lint.py
@@ -0,0 +1,24 @@
+from siliconcompiler.tools.verilator.verilator import setup as setup_tool
+from siliconcompiler.tools.verilator.verilator import runtime_options as runtime_options_tool
+
+
+def setup(chip):
+    '''
+    Lints Verilog source. Results of linting can be programmatically queried
+    using errors/warnings metrics.
+    '''
+
+    # Generic tool setup.
+    setup_tool(chip)
+
+    tool = 'verilator'
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+
+    chip.add('tool', tool, 'task', task, 'option', ['--lint-only'],
+             step=step, index=index)
+
+
+def runtime_options(chip):
+    return runtime_options_tool(chip)
