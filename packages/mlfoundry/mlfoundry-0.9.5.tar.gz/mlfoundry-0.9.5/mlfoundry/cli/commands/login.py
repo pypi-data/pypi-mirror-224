4a22b93a69a86f35e04897cfde41fb40159ac02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:10:50 +0000

    Add package mlfoundry/0.9.5/mlfoundry-0.9.5.tar.gz

---
+++ packages/mlfoundry/mlfoundry-0.9.5.tar.gz/mlfoundry-0.9.5/mlfoundry/cli/commands/login.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+import click
+
+from mlfoundry import env_vars
+from mlfoundry.login import login as login_
+
+
+@click.command(short_help="Store API key in the local file system")
+@click.option(
+    "--tracking_uri",
+    "--host",
+    "tracking_uri",
+    type=str,
+    required=True,
+    envvar=env_vars._TRACKING_HOST_GLOBAL,
+    help="Tracking server host",
+)
+@click.option(
+    "--relogin",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Overwrite existing API key for the given `tracking_uri`",
+)
+@click.option(
+    "--api-key",
+    type=str,
+    default=None,
+    prompt=True,
+    hide_input=True,
+    prompt_required=False,
+)
+def login(tracking_uri: Optional[str], relogin: bool, api_key: Optional[str]):
+    login_(tracking_uri=tracking_uri, relogin=relogin, api_key=api_key)
