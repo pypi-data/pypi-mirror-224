b343a925774eb1bf580dde509afbf2ac96c67b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:05 +0000

    Add package kgdata/4.0.6/kgdata-4.0.6-cp39-none-win_amd64.whl

---
+++ packages/kgdata/kgdata-4.0.6-cp39-none-win_amd64.whl/kgdata/wikidata/datasets/classes.py
@@ -0,0 +1,162 @@
+from __future__ import annotations
+
+from collections import deque
+
+import orjson
+import serde.jl
+from tqdm import tqdm
+
+from kgdata.dataset import Dataset
+from kgdata.spark import does_result_dir_exist, get_spark_context, saveAsSingleTextFile
+from kgdata.splitter import split_a_list
+from kgdata.wikidata.config import WikidataDirCfg
+from kgdata.wikidata.datasets.entities import entities
+from kgdata.wikidata.models import WDClass
+from kgdata.wikidata.models.wdentity import WDEntity
+
+
+def classes(lang: str = "en") -> Dataset[WDClass]:
+    cfg = WikidataDirCfg.get_instance()
+
+    if not does_result_dir_exist(cfg.classes / "ids"):
+        (
+            entities(lang)
+            .get_rdd()
+            .flatMap(get_class_ids)
+            .distinct()
+            .saveAsTextFile(
+                str(cfg.classes / "ids"),
+                compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+            )
+        )
+
+    if not does_result_dir_exist(cfg.classes / "classes"):
+        sc = get_spark_context()
+        class_ids = sc.broadcast(
+            set(sc.textFile(str(cfg.classes / "ids/*.gz")).collect())
+        )
+        (
+            entities(lang)
+            .get_rdd()
+            .filter(lambda ent: ent.id in class_ids.value)
+            .map(lambda x: orjson.dumps(extract_class(x).to_dict()))
+            .saveAsTextFile(
+                str(cfg.classes / "classes"),
+                compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+            )
+        )
+
+    if not does_result_dir_exist(cfg.classes / "ancestors"):
+        sc = get_spark_context()
+        saveAsSingleTextFile(
+            sc.textFile(str(cfg.classes / "classes/*.gz"))
+            .map(orjson.loads)
+            .map(WDClass.from_dict)
+            .map(lambda x: (x.id, x.parents))
+            .map(orjson.dumps),
+            str(cfg.classes / "ancestors/id2parents.ndjson.gz"),
+            compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+        )
+
+        id2parents = {
+            k: v
+            for k, v in serde.jl.deser(cfg.classes / "ancestors/id2parents.ndjson.gz")
+        }
+
+        id2ancestors = build_ancestors(id2parents)
+        split_a_list(
+            [orjson.dumps(x) for x in sorted(id2ancestors.items())],
+            (cfg.classes / "ancestors/id2ancestors/part.ndjson.gz"),
+        )
+        (cfg.classes / "ancestors" / "_SUCCESS").touch()
+
+    if not does_result_dir_exist(cfg.classes / "full_classes"):
+        sc = get_spark_context()
+        id2ancestors = sc.textFile(
+            str(cfg.classes / "ancestors/id2ancestors/*.gz")
+        ).map(orjson.loads)
+
+        def merge_ancestors(o):
+            id, (cls, ancestors) = o
+            cls.ancestors = set(ancestors)
+            return cls
+
+        (
+            sc.textFile(str(cfg.classes / "classes/*.gz"))
+            .map(orjson.loads)
+            .map(WDClass.from_dict)
+            .map(lambda x: (x.id, x))
+            .join(id2ancestors)
+            .map(merge_ancestors)
+            .map(WDClass.to_dict)
+            .map(orjson.dumps)
+            .saveAsTextFile(
+                str(cfg.classes / "full_classes"),
+                compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+            )
+        )
+
+    return Dataset(
+        cfg.classes / "full_classes/*.gz",
+        deserialize=lambda x: WDClass.from_dict(orjson.loads(x)),
+    )
+
+
+def extract_class(ent: WDEntity) -> WDClass:
+    cls = WDClass.from_entity(ent)
+    # we do have cases where the class is a subclass of itself, which is wrong.
+    cls.parents = [p for p in cls.parents if p != cls.id]
+    return cls
+
+
+def build_ancestors(id2parents: dict) -> dict:
+    id2ancestors = {}
+    for id in tqdm(id2parents, desc="build ancestors"):
+        id2ancestors[id] = get_ancestors(id, id2parents)
+    return id2ancestors
+
+
+def get_ancestors(id: str, id2parents: dict) -> list[str]:
+    # preserved the order
+    ancestors = {}
+    queue = deque(id2parents[id])
+    while len(queue) > 0:
+        ptr = queue.popleft()
+        if ptr in ancestors:
+            continue
+
+        ancestors[ptr] = len(ancestors)
+        queue.extend(id2parents[ptr])
+
+    return list(ancestors.keys())
+
+
+def get_class_ids(ent: WDEntity) -> list[str]:
+    # we can have a case where a property is mistakenly marked as a class such as P1072, which has been fixed.
+    # but we need to check it here.
+    if ent.id[0] == "P":
+        return []
+
+    lst = set()
+    if "P279" in ent.props:
+        lst.add(ent.id)
+
+    # P279: subclassof -- according to wikidata, this is the only property to detect class
+    # P31: instanceof -- sometimes entity that is not a class is value of this property (probably human-error)
+    for pid in ["P279", "P31"]:
+        for stmt in ent.props.get(pid, []):
+            if stmt.value.is_entity_id(stmt.value):
+                lst.add(stmt.value.as_entity_id())
+
+    # class of class:
+    # - Wikidata metaclass Q19361238
+    # - metaclass Q19478619
+    # - class Q5127848
+    if any(
+        stmt.value.as_entity_id() in {"Q19361238", "Q19478619", "Q5127848"}
+        for stmt in ent.props.get("P31", [])
+        if stmt.value.is_entity_id(stmt.value)
+    ):
+        lst.add(ent.id)
+
+    return list(lst)
---
+++ packages/kgdata/kgdata-4.0.6-cp39-none-win_amd64.whl/kgdata/wikidata/datasets/properties.py
@@ -0,0 +1,136 @@
+from typing import List
+
+import orjson
+import serde.jl
+from kgdata.dataset import Dataset
+from kgdata.spark import does_result_dir_exist, get_spark_context, saveAsSingleTextFile
+from kgdata.splitter import split_a_list
+from kgdata.wikidata.config import WikidataDirCfg
+from kgdata.wikidata.datasets.classes import build_ancestors
+from kgdata.wikidata.datasets.entities import entities
+from kgdata.wikidata.datasets.entity_ids import entity_ids
+from kgdata.wikidata.datasets.entity_redirections import entity_redirections
+from kgdata.wikidata.models import WDProperty
+from kgdata.wikidata.models.wdentity import WDEntity
+
+
+def properties(lang="en") -> Dataset[WDProperty]:
+    cfg = WikidataDirCfg.get_instance()
+
+    if not does_result_dir_exist(cfg.properties / "ids"):
+        (
+            entities(lang)
+            .get_rdd()
+            .flatMap(get_property_ids)
+            .distinct()
+            .coalesce(128, shuffle=True)
+            .saveAsTextFile(
+                str(cfg.properties / "ids"),
+                compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+            )
+        )
+
+    if not does_result_dir_exist(cfg.properties / "properties"):
+        sc = get_spark_context()
+        prop_ids = sc.broadcast(
+            set(sc.textFile(str(cfg.properties / "ids/*.gz")).collect())
+        )
+        (
+            entities(lang)
+            .get_rdd()
+            .filter(lambda ent: ent.id in prop_ids.value)
+            .map(lambda x: WDProperty.from_entity(x).to_dict())
+            .map(orjson.dumps)
+            .coalesce(128, shuffle=True)
+            .saveAsTextFile(
+                str(cfg.properties / "properties"),
+                compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+            )
+        )
+
+    if not (cfg.properties / "unknown_properties.txt").exists():
+        saveAsSingleTextFile(
+            get_spark_context()
+            .textFile(str(cfg.properties / "ids/*.gz"))
+            .subtract(entity_ids().get_rdd())
+            .subtract(entity_redirections().get_rdd().map(lambda x: x[0])),
+            cfg.properties / "unknown_properties.txt",
+        )
+
+    if not does_result_dir_exist(cfg.properties / "ancestors"):
+        sc = get_spark_context()
+
+        saveAsSingleTextFile(
+            sc.textFile(str(cfg.properties / "properties/*.gz"))
+            .map(orjson.loads)
+            .map(WDProperty.from_dict)
+            .map(lambda x: (x.id, x.parents))
+            .map(orjson.dumps),
+            str(cfg.properties / "ancestors/id2parents.ndjson.gz"),
+            compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+        )
+
+        id2parents = {
+            k: v
+            for k, v in serde.jl.deser(
+                cfg.properties / "ancestors/id2parents.ndjson.gz"
+            )
+        }
+
+        id2ancestors = build_ancestors(id2parents)
+        split_a_list(
+            [orjson.dumps(x) for x in sorted(id2ancestors.items())],
+            (cfg.properties / "ancestors/id2ancestors/part.ndjson.gz"),
+        )
+        (cfg.properties / "ancestors" / "_SUCCESS").touch()
+
+    if not does_result_dir_exist(cfg.properties / "full_properties"):
+        sc = get_spark_context()
+        id2ancestors = sc.textFile(
+            str(cfg.properties / "ancestors/id2ancestors/*.gz")
+        ).map(orjson.loads)
+
+        def merge_ancestors(o):
+            id, (prop, ancestors) = o
+            prop.ancestors = set(ancestors)
+            return prop
+
+        (
+            sc.textFile(str(cfg.properties / "properties/*.gz"))
+            .map(orjson.loads)
+            .map(WDProperty.from_dict)
+            .map(lambda x: (x.id, x))
+            .join(id2ancestors)
+            .map(merge_ancestors)
+            .map(WDProperty.to_dict)
+            .map(orjson.dumps)
+            .coalesce(128, shuffle=True)
+            .saveAsTextFile(
+                str(cfg.properties / "full_properties"),
+                compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+            )
+        )
+
+    return Dataset(
+        cfg.properties / "full_properties/*.gz",
+        deserialize=lambda x: WDProperty.from_dict(orjson.loads(x)),
+    )
+
+
+def get_property_ids(ent: WDEntity) -> List[str]:
+    prop_ids = set()
+    if ent.type == "property":
+        prop_ids.add(ent.id)
+
+    # P1647: subpropertyof
+    for stmt in ent.props.get("P1647", []):
+        if stmt.value.is_entity_id(stmt.value):
+            prop_ids.add(stmt.value.as_entity_id())
+
+    # statement property and qualifiers
+    prop_ids.update(ent.props.keys())
+    for stmts in ent.props.values():
+        for stmt in stmts:
+            prop_ids.update(stmt.qualifiers.keys())
+
+    return list(prop_ids)
