712de29db0db263cbf7da8c541ceb27153b7084e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:08:31 +0000

    Add package mmif-docloc-baapb/230811/mmif_docloc_baapb-230811.tar.gz

---
+++ packages/mmif-docloc-baapb/mmif_docloc_baapb-230811.tar.gz/mmif_docloc_baapb-230811/mmif_docloc_baapb/__init__.py
@@ -0,0 +1,25 @@
+import requests
+import os
+
+RESOVLER_ADDRESS_ENVVAR = 'BAAPB_RESOLVER_ADDRESS'
+if RESOVLER_ADDRESS_ENVVAR in os.environ:
+    RESOLVER_ADDRESS = os.environ[RESOVLER_ADDRESS_ENVVAR]
+else:
+    RESOLVER_ADDRESS = 'localhost:5000'
+
+
+def resolve(docloc):
+    scheme = 'baapb'
+    if docloc.startswith(f'{scheme}://'):
+        guid, document_type = docloc[len(scheme)+3:].rsplit('.', 1)
+        url = 'http://' + RESOLVER_ADDRESS + '/searchapi'
+        r = requests.get(url, params={'guid': guid, 'file':document_type})
+        if 199 < r.status_code < 299:
+            text = r.text
+            return text.split(", ")[0]
+        else:
+            raise ValueError(f'cannot resolve document location: "{docloc}", '
+                             f'is the resolver running at "{RESOLVER_ADDRESS}"?')
+    else:
+        raise ValueError(f'cannot handle document location scheme: "{docloc}"')
+
