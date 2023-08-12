7f0f2ecd5102d81041e03cd1d3a50be273d20eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:08:23 +0000

    Add package xethhung12-tg-msg/0.0.1/xethhung12_tg_msg-0.0.1-py3-none-any.whl

---
+++ packages/xethhung12-tg-msg/xethhung12_tg_msg-0.0.1-py3-none-any.whl/main.py
@@ -0,0 +1,8 @@
+import json
+import requests
+
+
+if __name__ == '__main__':
+    idv = "1396909267"
+    token = "5911418911:AAEyR-rmjKFG9TbbzS2cvo_tsHAmUinOflo"
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
