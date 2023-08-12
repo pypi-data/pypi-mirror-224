cd1005b0fb8cca85784fb8d8efd81f4ea7eeed99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 03:10:12 +0000

    Add package ssmptpaf/0.1/ssmptpaf-0.1.tar.gz

---
+++ packages/ssmptpaf/ssmptpaf-0.1.tar.gz/ssmptpaf-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssmptpaf',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ]
+)
---
+++ packages/ssmptpaf/ssmptpaf-0.1.tar.gz/ssmptpaf-0.1/ssmptpaf/__init__.py
@@ -0,0 +1,1 @@
+from .ssmptpaf import main
