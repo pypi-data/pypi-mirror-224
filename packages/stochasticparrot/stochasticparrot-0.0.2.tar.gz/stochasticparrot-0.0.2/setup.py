afe818b3a61fb39553cf9883fe30d9122f71cc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:40 +0000

    Add package stochasticparrot/0.0.2/stochasticparrot-0.0.2.tar.gz

---
+++ packages/stochasticparrot/stochasticparrot-0.0.2.tar.gz/stochasticparrot-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stochasticparrot',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'termcolor',
+    ],
+    author='Wes Ladd',
+    author_email='wesladd@traingrc.com',
+    description='A package for handling errors with custom logging that lets OpenAI API make suggestions.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/custom_error_handler',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
