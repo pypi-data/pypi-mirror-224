0b7678f2c14fd8015e2806ea1a379b95c3fba85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 05:07:37 +0000

    Add package winsdk/1.0.0b10/winsdk-1.0.0b10.tar.gz

---
+++ packages/winsdk/winsdk-1.0.0b10.tar.gz/winsdk-1.0.0b10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_namespace_packages
+
+from skbuild import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="winsdk",
+    version = "1.0.0b10",
+    description="Python bindings for the Windows SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author='Microsoft Corporation',
+    url="https://github.com/pywinrt/python-winsdk",
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Win32 (MS Windows)',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: Microsoft :: Windows :: Windows 10',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: 3.12',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: System :: Operating System',
+        ],
+    python_requires=">=3.8",
+    packages=find_namespace_packages(where=("pywinrt")),
+    package_dir={"": "pywinrt"}, 
+    # recursive glob (**) doesn't seem to work here
+    package_data={"winsdk": ["py.typed", "*.pyi", "*/*.pyi", "*/*/*.pyi", "*/*/*/*.pyi", "*/*/*/*/*.pyi"]},
+)
