0271cb927b753919d960145dd0a22d5b7b3e78b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 14:07:56 +0000

    Add package tetras-toolbox/0.0.15/tetras-toolbox-0.0.15.tar.gz

---
+++ packages/tetras-toolbox/tetras-toolbox-0.0.15.tar.gz/tetras-toolbox-0.0.15/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tetras_toolbox",
+    version="0.0.15",
+    packages=find_packages(),
+    install_requires=["more-itertools>=8.10.0"]
+)
