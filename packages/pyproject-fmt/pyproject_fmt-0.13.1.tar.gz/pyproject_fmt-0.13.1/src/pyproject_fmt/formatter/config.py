71dccdf5a3febb90e8f7129d8e0069d2976dfbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-12 04:11:29 +0000

    Add package pyproject-fmt/0.13.1/pyproject_fmt-0.13.1.tar.gz

---
+++ packages/pyproject-fmt/pyproject_fmt-0.13.1.tar.gz/pyproject_fmt-0.13.1/src/pyproject_fmt/formatter/config.py
@@ -0,0 +1,32 @@
+"""Defines configuration for the formatter."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import sys
+    from pathlib import Path
+
+    if sys.version_info >= (3, 8):  # pragma: no cover (py38+)
+        from typing import Final
+    else:  # pragma: no cover (<py38)
+        from typing_extensions import Final
+
+
+DEFAULT_INDENT: Final[int] = 2  #: default indentation level
+
+
+@dataclass(frozen=True)
+class Config:
+    """Configuration flags for the formatting."""
+
+    pyproject_toml: Path
+    toml: str  #: the text to format
+    indent: int = DEFAULT_INDENT  #: indentation to apply
+
+
+__all__ = [
+    "Config",
+    "DEFAULT_INDENT",
+]
