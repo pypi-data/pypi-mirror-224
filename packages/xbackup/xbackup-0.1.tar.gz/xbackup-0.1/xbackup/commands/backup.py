9f40684ff4553124d033c6202017e137b52efac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 14:07:46 +0000

    Add package xbackup/0.1/xbackup-0.1.tar.gz

---
+++ packages/xbackup/xbackup-0.1.tar.gz/xbackup-0.1/xbackup/commands/backup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+from typing import Optional
+from typing import Sequence
+
+from xarg import add_command
+from xarg import argp
+from xarg import commands
+from xarg import run_command
+
+from ..utils import URL_PROG
+from ..utils import __prog__
+from ..utils import __version__
+
+
+@add_command(__prog__)
+def add_cmd(_arg: argp):
+    pass
+
+
+@run_command(add_cmd)
+def run_cmd(cmds: commands) -> int:
+    return 0
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    cmds = commands()
+    cmds.version = f"{__prog__} {__version__}"
+    return cmds.run(root=add_cmd,
+                    argv=argv,
+                    prog=__prog__,
+                    # description="Backup. Restore.",
+                    epilog=f"For more, please visit {URL_PROG}")
---
+++ packages/xbackup/xbackup-0.1.tar.gz/xbackup-0.1/xbackup/utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+
+__prog__ = "xbackup"
+__version__ = "0.1"
+
+URL_PROG = "https://github.com/bondbox/xbackup"
