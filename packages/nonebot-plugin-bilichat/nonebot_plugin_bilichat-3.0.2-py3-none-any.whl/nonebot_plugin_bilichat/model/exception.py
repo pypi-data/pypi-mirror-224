76bd54fd245ec0197403e4b02ee93734d3440e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:21 +0000

    Add package nonebot-plugin-bilichat/3.0.2/nonebot_plugin_bilichat-3.0.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-bilichat/nonebot_plugin_bilichat-3.0.2-py3-none-any.whl/nonebot_plugin_bilichat/model/exception.py
@@ -0,0 +1,34 @@
+class AbortError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+# ===== BING EXCEPTION =====
+
+
+class BaseBingChatException(Exception):
+    pass
+
+
+class BingChatUnknownException(BaseBingChatException):
+    pass
+
+
+class BingchatNetworkException(BaseBingChatException):
+    pass
+
+
+class BingChatResponseException(BingchatNetworkException):
+    pass
+
+
+class BingChatInvalidSessionException(BingChatResponseException):
+    pass
+
+
+class BingChatAccountReachLimitException(BingChatResponseException):
+    pass
+
+
+class BingCaptchaException(BingChatResponseException):
+    pass
