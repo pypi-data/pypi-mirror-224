f4d90eafe0f7525a4de7557da1398442fa4b8dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:10:08 +0000

    Add package pbite/0.0.1/pbite-0.0.1.tar.gz

---
+++ packages/pbite/pbite-0.0.1.tar.gz/pbite-0.0.1/src/pb/__init__.py
@@ -0,0 +1,12 @@
+from pb.parser import Content
+
+__version__ = "0.0.1"
+
+
+def fmt_bite(content: Content) -> str:
+    """Format a content bite."""
+    return info(content)  # TODO
+
+
+def info(content: Content) -> str:
+    return content.info()
