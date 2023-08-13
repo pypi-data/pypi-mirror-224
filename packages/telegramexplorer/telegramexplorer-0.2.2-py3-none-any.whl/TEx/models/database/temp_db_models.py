b20e24671716d636773e8f896a0425f9d0d03ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:10:11 +0000

    Add package telegramexplorer/0.2.2/telegramexplorer-0.2.2-py3-none-any.whl

---
+++ packages/telegramexplorer/telegramexplorer-0.2.2-py3-none-any.whl/TEx/models/database/temp_db_models.py
@@ -0,0 +1,32 @@
+"""Temporary Data Model."""
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class TempDataBaseDeclarativeBase(DeclarativeBase):  # type: ignore
+    """Global Temporary Declarative Base."""
+
+
+class TempDataOrmEntity(TempDataBaseDeclarativeBase):
+    """Temporary Data ORM Model."""
+
+    __bind_key__ = 'temp'
+    __tablename__ = 'temporary_data'
+
+    path: Mapped[str] = mapped_column(String(255), primary_key=True)
+    module: Mapped[str] = mapped_column(String(255))
+    data: Mapped[str] = mapped_column(String)
+    created_at: Mapped[Integer] = mapped_column(Integer)
+    valid_at: Mapped[Integer] = mapped_column(Integer)
+
+
+class StateFileOrmEntity(TempDataBaseDeclarativeBase):
+    """Temporary Data ORM Model."""
+
+    __bind_key__ = 'temp'
+    __tablename__ = 'state_file'
+
+    path: Mapped[str] = mapped_column(String(255), primary_key=True)
+    data: Mapped[str] = mapped_column(String)
+    created_at: Mapped[Integer] = mapped_column(Integer)
