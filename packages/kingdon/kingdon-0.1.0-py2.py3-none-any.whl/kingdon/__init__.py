568d1805f34b805b8b796669be1fd942b84bc867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 04:11:39 +0000

    Add package kingdon/0.1.0/kingdon-0.1.0-py2.py3-none-any.whl

---
+++ packages/kingdon/kingdon-0.1.0-py2.py3-none-any.whl/kingdon/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Kingdon."""
+
+__author__ = """Martin Roelfs"""
+__email__ = 'martinroelfs@yahoo.com'
+__version__ = '0.1.0'
+
+from sympy import symbols
+
+from kingdon.algebra import Algebra
+from kingdon.multivector import MultiVector
