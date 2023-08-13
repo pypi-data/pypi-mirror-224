df970aa5616edb04a355a80a5d9602154016167a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:08:50 +0000

    Add package BFR/0.1/BFR-0.1.tar.gz

---
+++ packages/BFR/BFR-0.1.tar.gz/BFR-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BFR",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Ovi Quintanal",
+    author_email="oj2kOfficial@gmail.com",
+    description="A bulk file renaming utility.",
+    license="MIT",
+    
+)
