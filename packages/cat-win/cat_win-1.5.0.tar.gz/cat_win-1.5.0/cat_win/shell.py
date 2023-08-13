c969f70600c02e8e26a9453298acb594a3952ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:33 +0000

    Add package cat-win/1.5.0/cat_win-1.5.0.tar.gz

---
+++ packages/cat-win/cat_win-1.5.0.tar.gz/cat_win-1.5.0/cat_win/shell.py
@@ -0,0 +1,17 @@
+from sys import exit as sysexit, stderr
+
+
+try:
+    from cat_win import cat
+except KeyboardInterrupt:
+    sysexit(1)
+except Exception as e:
+    print('an error occured while loading the module:', file=stderr)
+    print(e, file=stderr)
+    sysexit(1)
+
+def entry_point():
+    cat.shell_main()
+
+if __name__ == '__main__':
+    entry_point()
