7c28ea1015126846ec1cf4d4efe03b9cd66a9714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:20 +0000

    Add package json-duplicate-keys/2023.8.12/json_duplicate_keys-2023.8.12.tar.gz

---
+++ packages/json-duplicate-keys/json_duplicate_keys-2023.8.12.tar.gz/json_duplicate_keys-2023.8.12/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import datetime
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="json_duplicate_keys",
+	version=datetime.datetime.now().strftime("%Y.%m.%d"),
+	author="Truoc Phan",
+	license="MIT",
+	author_email="truocphan112017@gmail.com",
+	description="Flatten/ Unflatten and Load(s)/ Dump(s) JSON File/ Object with Duplicate Keys",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	install_requires=open("requirements.txt").read().split(),
+	url="https://github.com/truocphan/json_duplicate_keys",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: Implementation :: Jython"
+	],
+	keywords=["json", "duplicate keys", "json duplicate keys", "flatten", "unflatten"],
+	packages=["json_duplicate_keys"],
+)
