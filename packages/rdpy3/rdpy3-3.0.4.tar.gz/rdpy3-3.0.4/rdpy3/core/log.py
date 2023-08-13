f5ee25be75e83e489378c55a5ea6131f96153205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:08:46 +0000

    Add package rdpy3/3.0.4/rdpy3-3.0.4.tar.gz

---
+++ packages/rdpy3/rdpy3-3.0.4.tar.gz/rdpy3-3.0.4/rdpy3/core/log.py
@@ -0,0 +1,85 @@
+#
+# Copyright (c) 2014-2015 Sylvain Peyrefitte
+#
+# This file is part of rdpy3.
+#
+# rdpy3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Log engine in RDPY
+Actually very basic log engine
+"""
+from __future__ import print_function
+
+from builtins import object
+class Level(object):
+    """
+    @summary: Level log
+    """
+    DEBUG = 0
+    INFO = 1
+    WARNING = 2
+    ERROR = 3
+    NONE = 4
+    
+_LOG_LEVEL = Level.DEBUG
+_LOG_FILE = False 
+
+def log(message):
+    """
+    @summary: Main log function
+    @param message: string to print
+    """
+    if _LOG_FILE:
+        f = open(_LOG_FILE, "a+")
+        f.write("%s\n"%message)
+        f.close()
+    print("[*] %s"%message)
+
+def error(message):
+    """
+    @summary: Log error message
+    @param message: string to print as error log
+    """
+    if _LOG_LEVEL > Level.ERROR:
+        return
+    log("ERROR:\t%s"%message)
+    
+def warning(message):
+    """
+    @summary: Log warning message
+    @param message: string to print as warning log
+    """
+    if _LOG_LEVEL > Level.WARNING:
+        return
+    log("WARNING:\t%s"%message)
+
+def info(message):
+    """
+    @summary: Log info message
+    @param message: string to print as info log
+    """
+    if _LOG_LEVEL > Level.INFO:
+        return
+    log("INFO:\t%s"%message)
+    
+def debug(message):
+    """
+    @summary: Log debug message
+    @param message: string to print as debug log
+    """
+    if _LOG_LEVEL > Level.DEBUG:
+        return
+    log("DEBUG:\t%s"%message)
