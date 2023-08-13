e41e7dd88d3d08ff39a5f6d046b25fba588415e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:10:12 +0000

    Add package pycuga/0.0.8/pycuga-0.0.8-py3-none-any.whl

---
+++ packages/pycuga/pycuga-0.0.8-py3-none-any.whl/pycuga/algos/tools.py
@@ -0,0 +1,13 @@
+import os
+
+
+def read_files_as_strings(directory_path):
+    file_string = " #include <cuda_runtime.h> \n "
+    print(directory_path)
+    for dirpath, _, filenames in os.walk("./cuda/"):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            with open(file_path, 'r') as file:
+                file_string+= file.read()
+            file_string+="\n"
+    return file_string
