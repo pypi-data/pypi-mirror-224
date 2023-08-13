065b2003fee07d048a2f21a900144db8bbec7259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:20 +0000

    Add package seiketsu-api/1.0.9/seiketsu-api-1.0.9.tar.gz

---
+++ packages/seiketsu-api/seiketsu-api-1.0.9.tar.gz/seiketsu-api-1.0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="seiketsu-api",
+    version="1.0.9",
+    description='A Python library to interact with a chat API',
+    author='LoLip_p',
+    author_email='mr.timon51@gmail.com',
+    packages=find_packages(
+        'google-cloud-firestore',
+    ),
+)
