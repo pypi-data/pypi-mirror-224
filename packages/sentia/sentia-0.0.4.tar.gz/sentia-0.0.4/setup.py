69cc3b513456a7678de4066191ea79ea57447836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 04:11:03 +0000

    Add package sentia/0.0.4/sentia-0.0.4.tar.gz

---
+++ packages/sentia/sentia-0.0.4.tar.gz/sentia-0.0.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+print(setuptools.find_packages())
+with open("D:\\SENTIA\\readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sentia", 
+    version="0.0.4",
+    author="Locutusque",
+    author_email="locutusque.airshipcraft@gmail.com",
+    description="A text generation model combining multiple neural network architectures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Locutusque/SENTIA.py",
+    packages=setuptools.find_packages(),
+    package_data={
+    'SENTIA': ['*'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "torch>=1.6.0",
+        "transformers>=3.0.0",
+        "datasets>=1.7.0"
+    ] 
+)
