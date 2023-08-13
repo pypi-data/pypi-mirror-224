30a25a3d21bba9aabdd684b8babc2ad518cb3a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 04:10:51 +0000

    Add package ratcliff-breast-cancer-predictor/0.1.0/ratcliff_breast_cancer_predictor-0.1.0-py3-none-any.whl

---
+++ packages/ratcliff-breast-cancer-predictor/ratcliff_breast_cancer_predictor-0.1.0-py3-none-any.whl/src/predictor.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def predict(model, values: list) -> str:
+    prediction = model.predict(values)
+    if prediction == 2:
+        return "benign"
+    else:
+        return "malignant"
+
+
+def train(modal_path: str, data_path: str | None) -> float:
+    if data_path is None:
+        data_path = "src/resources/breast-cancer-wisconsin.data"
+
+    df = pd.read_csv(data_path, header=None)
+    df = df[(df != "?").all(axis=1)]
+    cancer_data = df[[1, 2, 3, 4, 5, 6, 7, 8, 9]]
+    cancer_target = df[10]
+    X_train, X_test, y_train, y_test = train_test_split(
+        cancer_data, cancer_target, random_state=3
+    )
+
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train, y_train)
+    pickle.dump(knn, open(modal_path, "wb"))
+    return knn.score(X_test, y_test)
