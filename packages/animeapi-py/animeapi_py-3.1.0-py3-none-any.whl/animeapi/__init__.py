17798e96121739d6861ae2ecf631ad10fcc809e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:35 +0000

    Add package animeapi-py/3.1.0/animeapi_py-3.1.0-py3-none-any.whl

---
+++ packages/animeapi-py/animeapi_py-3.1.0-py3-none-any.whl/animeapi/__init__.py
@@ -0,0 +1,22 @@
+from animeapi.animeapi import AnimeAPI
+from animeapi.asyncaniapi import AsyncAnimeAPI
+from animeapi.excepts import MissingRequirement, UnsupportedVersion
+from animeapi.models import (AnimeRelation, ApiStatus, CountStruct, Heartbeat,
+                           Platform, TraktMediaType, UpdatedStruct, Version)
+
+__version__ = "3.1.0"
+
+__all__ = [
+    "AnimeAPI",
+    "AnimeRelation",
+    "ApiStatus",
+    "AsyncAnimeAPI",
+    "CountStruct",
+    "Heartbeat",
+    "MissingRequirement",
+    "Platform",
+    "TraktMediaType",
+    "UnsupportedVersion",
+    "UpdatedStruct",
+    "Version",
+]
