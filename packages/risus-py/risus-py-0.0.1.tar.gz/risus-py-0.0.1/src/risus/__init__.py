ed05cc33a053c4d8b165725965e4e5c2e9a7db99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 14:07:53 +0000

    Add package risus-py/0.0.1/risus-py-0.0.1.tar.gz

---
+++ packages/risus-py/risus-py-0.0.1.tar.gz/risus-py-0.0.1/src/risus/__init__.py
@@ -0,0 +1,13 @@
+"""
+.. include:: ../../README.md
+"""
+
+from risus.risus import (combat, team_combat, single_action_conflict,
+                         target_number, damage_team_mates_only,
+                         damage_volunteer, make_target_number_table,
+                         make_victory_table, make_team_victory_table)
+
+__all__ = ["combat", "team_combat", "single_action_conflict",
+           "target_number", "damage_team_mates_only",
+           "damage_volunteer", "make_target_number_table",
+           "make_victory_table", "make_team_victory_table"]
