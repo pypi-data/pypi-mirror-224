ad47f02f6195de37f6adbd80efddf26d2f865775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 14:08:01 +0000

    Add package kde-material-you-colors/1.5.1.post5/kde-material-you-colors-1.5.1.post5.tar.gz

---
+++ packages/kde-material-you-colors/kde-material-you-colors-1.5.1.post5.tar.gz/kde-material-you-colors-1.5.1.post5/src/kde_material_you_colors/data/kde-material-you-colors-set-path.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# This file was created by kde-material-you-colors
+# to make widget detect pip installed package
+# https://github.com/luisbocanegra/kde-material-you-colors/
+
+export PATH="${HOME}/.local/bin:${PATH}"
