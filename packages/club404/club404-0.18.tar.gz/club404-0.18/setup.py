dc95887924bf8ef89499a083b6d5e9d0f30aba5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:03 +0000

    Add package club404/0.18/club404-0.18.tar.gz

---
+++ packages/club404/club404-0.18.tar.gz/club404-0.18/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+setup(
+  name = 'club404',
+  packages = ['club404'],
+  version = '0.18',
+  license='MIT',
+  description = 'Simple and generic python web server and utils',
+  author = 'Club404',
+  author_email = 'info@club404.io',
+  url = 'https://github.com/Club404/server.py',
+  download_url = 'https://github.com/Club404/server.py/archive/refs/tags/0.18.tar.gz',
+  keywords = ['simple', 'http', 'server'],
+  install_requires=[],
+  classifiers=[  # Optional
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
