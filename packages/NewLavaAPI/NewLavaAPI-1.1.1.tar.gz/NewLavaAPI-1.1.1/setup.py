f0d73bb241634a63c9d30a042115b961afc34d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 14:07:54 +0000

    Add package NewLavaAPI/1.1.1/NewLavaAPI-1.1.1.tar.gz

---
+++ packages/NewLavaAPI/NewLavaAPI-1.1.1.tar.gz/NewLavaAPI-1.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='NewLavaAPI',
+      version='1.1.1',
+      description='New Lib for LavaAPI. DOCS: https://github.com/Banjirome/NewLavaAPI',
+      packages=['NewLavaAPI'],
+      author_email='banjiro_me@cyclonesoftware.space',
+      zip_safe=False)
+
