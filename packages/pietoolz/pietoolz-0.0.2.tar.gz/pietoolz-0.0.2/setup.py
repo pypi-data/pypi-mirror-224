eb70492bd6cbda73119e45383541f9efb78ebe51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 14:08:03 +0000

    Add package pietoolz/0.0.2/pietoolz-0.0.2.tar.gz

---
+++ packages/pietoolz/pietoolz-0.0.2.tar.gz/pietoolz-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pietoolz',
+    version='0.0.2',  # Update the version ONLY when you publish a new release.
+    packages=find_packages(),
+    install_requires=[],
+    author='coolhuip',
+    author_email='cool.huip@example.com',
+    description='PieToolz is a package houseing useful classes, methods, and functions designed to streamline the development process.'
+)
