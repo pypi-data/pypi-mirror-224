023dc643688f5455bc5d284af27335d7b2948f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:17 +0000

    Add package siebe/0.1/siebe-0.1.tar.gz

---
+++ packages/siebe/siebe-0.1.tar.gz/siebe-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="siebe",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[  ],
+    author="Siebe Albers",
+    description="Custom functions package",
+    long_description="Detailed description of your package",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+)
