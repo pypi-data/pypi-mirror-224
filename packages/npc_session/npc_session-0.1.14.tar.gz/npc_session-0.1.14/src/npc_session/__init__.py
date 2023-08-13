6ef71b77104c7145e98aaea28bae505fefa0458e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 04:11:24 +0000

    Add package npc_session/0.1.14/npc_session-0.1.14.tar.gz

---
+++ packages/npc_session/npc_session-0.1.14.tar.gz/npc_session-0.1.14/src/npc_session/__init__.py
@@ -0,0 +1,9 @@
+from npc_session.parsing import *
+from npc_session.records import (
+    DateRecord,
+    DatetimeRecord,
+    ProjectRecord,
+    SessionRecord,
+    SubjectRecord,
+    TimeRecord,
+)
