9a8b33cc881d7d74b79de6682ac5534b5e1d8ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:10:55 +0000

    Add package decorator-parser/1.0.5/decorator_parser-1.0.5.tar.gz

---
+++ packages/decorator-parser/decorator_parser-1.0.5.tar.gz/decorator_parser-1.0.5/src/decorator_parser/utils.py
@@ -0,0 +1,10 @@
+# Author: Michał Kostyk for Smartschool Inc.
+# Date: 2023
+# Version: 1.0.3
+# Description: Utility functions and constants used in other scripts.
+
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+BOLD = '\033[1m'
