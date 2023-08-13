ef1cdf727b6ff3d3c7f77996c891e766fe7cb940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 04:11:13 +0000

    Add package larvaworld/0.0.5/larvaworld-0.0.5-py3-none-any.whl

---
+++ packages/larvaworld/larvaworld-0.0.5-py3-none-any.whl/larvaworld/examples/data_import/Jonanic/import_Jovanic.py
@@ -0,0 +1,27 @@
+from larvaworld.lib import reg, aux, plot
+from larvaworld.lib.process.building import import_datasets
+
+
+kws0 = {
+    'labID': 'Jovanic',
+    'enrich' : True,
+    'merged' : False,
+    'match_ids' : True,
+}
+
+kws1 = {
+    'parent_dir': 'AttP240',
+    'source_ids': ['Fed', 'Starved'],
+    'ids': ['Fed','Starved'],
+    'refIDs': ['AttP240.Fed', 'AttP240.Starved'],
+    'colors':['black', 'red'],
+    **kws0
+}
+
+ds = import_datasets(**kws1)
+# ggs=['endpoint', 'dsp', 'general']
+# gd = reg.graphs.eval_graphgroups(graphgroups=ggs, datasets=ds, save_to=f'{reg.DATA_DIR}/JovanicGroup/plots_submission/3/{kws1["parent_dir"]}', subfolder=None)
+    # except :
+    #     pass
+    # for d in ds :
+    #     assert isinstance(d, LarvaDataset)
