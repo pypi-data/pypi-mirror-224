ae5ee0786609b4b9c10e02afefccc84cece38215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 14:08:02 +0000

    Add package pycelium/0.1.34/pycelium-0.1.34-py2.py3-none-any.whl

---
+++ packages/pycelium/pycelium-0.1.34-py2.py3-none-any.whl/pycelium/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for pycelium.
+
+- [ ] remote inventary
+  - [ ] based on mac/emai
+- [x] pip installer
+
+
+"""
+
+__author__ = """Asterio Gonzalez"""
+__email__ = 'asterio.gonzalez@gmail.com'
+__version__ = '0.1.34'
