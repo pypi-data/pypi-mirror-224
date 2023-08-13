ab1f00a4fe225f939cb961a4de237982f78263cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:20 +0000

    Add package airoboros/2.0.23/airoboros-2.0.23.tar.gz

---
+++ packages/airoboros/airoboros-2.0.23.tar.gz/airoboros-2.0.23/airoboros/instructors/trivia.py
@@ -0,0 +1,35 @@
+import re
+from loguru import logger
+from airoboros.instructors.inline_qa import generate as generate_inline
+
+
+async def generate(instructor, **kwargs):
+    """Generator for trivia training data."""
+    async for item in generate_inline(instructor, "trivia", **kwargs):
+        # Double check some wordgame style questions.
+        match = re.search(
+            r"(start|end)(?:s|ing) with ['\"]([^'\"]+)['\"]", item["instruction"]
+        )
+        if match:
+            if (
+                match.group(1) == "start"
+                and not item["response"].lower().startswith(match.group(2).lower())
+            ) or (
+                match.group(1) == "end"
+                and not item["response"].lower().endswith(match.group(2).lower())
+            ):
+                logger.warning(
+                    f"Validation failure: {item['instruction']} -- {item['response']}"
+                )
+                continue
+        match = re.search(
+            r"(?:with|contain(?:s|ing)|hav(?:e|ing)) ['\"]([^'\"]+)['\"]",
+            item["instruction"],
+        )
+        if match and match.group(1) not in item["response"]:
+            logger.warning(
+                f"Validation failure: {item['instruction']} -- {item['response']}"
+            )
+            continue
+        item["instruction"] = "trivia: " + item["instruction"]
+        yield item
