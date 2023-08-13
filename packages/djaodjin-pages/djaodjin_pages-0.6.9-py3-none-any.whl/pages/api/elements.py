4cdc72d0be5715100f811e6d63e75625c566ac15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 14:08:08 +0000

    Add package djaodjin-pages/0.6.9/djaodjin_pages-0.6.9-py3-none-any.whl

---
+++ packages/djaodjin-pages/djaodjin_pages-0.6.9-py3-none-any.whl/pages/api/elements.py
@@ -0,0 +1,649 @@
+# Copyright (c) 2023, Djaodjin Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import logging
+
+from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.db.models import Max, Q
+from django.http import Http404
+from rest_framework import generics, response as api_response
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.filters import OrderingFilter, SearchFilter
+
+from ..compat import reverse
+from ..helpers import ContentCut, get_extra
+from ..mixins import AccountMixin, PageElementMixin, TrailMixin
+from ..models import (PageElement, RelationShip, build_content_tree,
+    flatten_content_tree)
+from ..serializers import (NodeElementSerializer, PageElementSerializer,
+    PageElementTagSerializer)
+from ..utils import validate_title
+
+LOGGER = logging.getLogger(__name__)
+
+
+class PageElementAPIView(TrailMixin, generics.ListAPIView):
+    """
+    Lists a tree of page elements under a path
+
+    **Tags: content
+
+    **Example
+
+    .. code-block:: http
+
+        GET /api/content/boxes-and-enclosures HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+          "count": 8,
+          "next": null,
+          "previous": null,
+          "results": [
+          {
+            "slug": "metal",
+            "path": null,
+            "title": "Metal structures & equipment",
+            "indent": 0
+          },
+          {
+            "slug": "boxes-and-enclosures",
+            "path": "/metal/boxes-and-enclosures",
+            "title": "Boxes & enclosures",
+            "indent": 1,
+            "tags": [
+              "industry",
+              "pagebreak",
+              "public",
+              "scorecard"
+            ]
+          }
+          ]
+        }
+    """
+    serializer_class = PageElementSerializer
+    queryset = PageElement.objects.all()
+
+    @property
+    def visibility(self):
+        return None
+
+    @property
+    def owners(self):
+        return None
+
+    def attach(self, elements):
+        #pylint:disable=no-self-use
+        return elements
+
+    def get_cut(self):
+        cut_param = self.request.query_params.get('cut')
+        return ContentCut(cut_param) if cut_param else None
+
+    def get_results(self):
+        if self.element:
+            content_tree = build_content_tree(
+                roots=[self.element], prefix=self.full_path,
+                cut=self.get_cut(),
+                visibility=self.visibility,
+                accounts=self.owners)
+            items = flatten_content_tree(
+                content_tree, sort_by_key=False, depth=-1)
+            items.pop(0)
+        else:
+            cut = self.get_cut()
+            if not cut:
+                cut = ContentCut()
+            content_tree = build_content_tree(
+                roots=None, prefix=self.full_path,
+                cut=cut,
+                visibility=self.visibility,
+                accounts=self.owners)
+            # We do not re-sort the roots such that member-only content
+            # appears at the top.
+            items = flatten_content_tree(content_tree, sort_by_key=False)
+
+        results = []
+        for item in items:
+            searchable = get_extra(item, 'searchable', False)
+            if searchable:
+                results += [item]
+
+        return results
+
+    def list(self, request, *args, **kwargs):
+        #pylint:disable=unused-argument
+        results = self.get_results()
+        self.attach(results)
+
+        # We have multiple roots so we create an unifying top-level root.
+        element = self.element if self.element else PageElement()
+        element.path = self.full_path
+        element.results = results
+        element.count = len(results)
+        serializer = self.get_serializer(element)
+        return api_response.Response(serializer.data)
+
+
+class PageElementIndexAPIView(PageElementAPIView):
+    """
+    Lists the tree of page elements under the root
+
+    **Tags: content
+
+    **Example
+
+    .. code-block:: http
+
+        GET /api/content/ HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+          "count": 8,
+          "next": null,
+          "previous": null,
+          "results": [
+          {
+            "slug": "metal",
+            "path": null,
+            "title": "Metal structures & equipment",
+            "indent": 0
+          },
+          {
+            "slug": "boxes-and-enclosures",
+            "path": "/metal/boxes-and-enclosures",
+            "title": "Boxes & enclosures",
+            "indent": 1,
+            "tags": [
+              "industry",
+              "pagebreak",
+              "public",
+              "scorecard"
+            ]
+          }
+          ]
+        }
+    """
+    pass
+
+
+class PageElementSearchAPIView(PageElementAPIView):
+    """
+    Search through page elements
+
+    **Tags: content
+
+    **Example
+
+    .. code-block:: http
+
+        GET /api/content/search HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+          "count": 1,
+          "next": null,
+          "previous": null,
+          "results": [{
+            "slug": "hello",
+            "path": "/hello",
+            "title": "Hello"
+          }]
+        }
+    """
+
+    def get_results(self):
+        results = []
+        for item in super(PageElementSearchAPIView, self).get_results():
+            extra = item.get('extra', {})
+            if extra:
+                searchable = extra.get('searchable', False)
+                if searchable:
+                    results += [item]
+        return results
+
+    def get_queryset(self):
+        # XXX This code is actually not triggered because of inheritance
+        # from `PageElementAPIView`.
+        try:
+            queryset = PageElement.objects.filter(account=self.account)
+            search_string = self.request.query_params.get('q', None)
+            if search_string is not None:
+                validate_title(search_string)
+                queryset = queryset.filter(
+                    Q(extra__icontains=search_string)
+                    | Q(title__icontains=search_string))
+                return queryset
+        except ValidationError:
+            pass
+        return []
+
+
+class PageElementDetailAPIView(TrailMixin, generics.RetrieveAPIView):
+    """
+    Retrieves details on a page element
+
+    **Tags: content
+
+    **Example
+
+    .. code-block:: http
+
+        GET /api/content/detail/adjust-air-fuel-ratio HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+            "slug": "adjust-air-fuel-ratio",
+            "picture": null,
+            "title": "Adjust air/fuel ratio",
+            "text": "<h2>Background</h2><p>Some manufacturing processes may\
+ involve heating operations.</p>",
+            "extra": null
+        }
+    """
+    serializer_class = PageElementSerializer
+
+    def get_object(self):
+        return self.element
+
+
+class PageElementEditableListAPIView(AccountMixin, TrailMixin,
+                                     generics.ListCreateAPIView):
+    """
+    List editable page elements
+
+    This API endpoint lists page elements that are owned and thus editable
+    by an account.
+
+    **Tags**: editors
+
+    **Examples
+
+    .. code-block:: http
+
+        GET /api/content/editables/energy-utility/ HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+            "count": 5,
+            "next": null,
+            "previous": null,
+            "results": [
+                {
+                    "path": null,
+                    "title": "Construction",
+                    "tags": ["public"],
+                    "indent": 0
+                },
+                {
+                    "path": null,
+                    "title": "Governance & management",
+                    "picture": "https://assets.tspproject.org/management.png",
+                    "indent": 1
+                },
+                {
+                    "path": "/construction/governance/the-assessment\
+-process-is-rigorous",
+                    "title": "The assessment process is rigorous",
+                    "indent": 2,
+                    "environmental_value": 1,
+                    "business_value": 1,
+                    "profitability": 1,
+                    "implementation_ease": 1,
+                    "avg_value": 1
+                },
+                {
+                    "path": null,
+                    "title": "Production",
+                    "picture": "https://assets.tspproject.org/production.png",
+                    "indent": 1
+                },
+                {
+                    "path": "/construction/production/adjust-air-fuel\
+-ratio",
+                    "title": "Adjust Air fuel ratio",
+                    "indent": 2,
+                    "environmental_value": 2,
+                    "business_value": 2,
+                    "profitability": 2,
+                    "implementation_ease": 2,
+                    "avg_value": 2
+                }
+            ]
+        }
+    """
+    serializer_class = NodeElementSerializer
+
+    search_fields = (
+        'title',
+        'extra'
+    )
+    ordering_fields = (
+        ('title', 'title'),
+    )
+    ordering = ('title',)
+
+    filter_backends = (SearchFilter, OrderingFilter,)
+
+    def get_queryset(self):
+        """
+        Returns a list of heading and best practices
+        """
+        if self.account_url_kwarg in self.kwargs:
+            queryset = PageElement.objects.filter(account=self.account)
+        else:
+            queryset = PageElement.objects.all()
+        if self.path:
+            queryset = queryset.filter(question__path__startswith=self.path)
+        try:
+            search_string = self.request.query_params.get('q', None)
+            if search_string is not None:
+                validate_title(search_string)
+                queryset = queryset.filter(
+                    Q(extra__icontains=search_string)
+                    | Q(title__icontains=search_string))
+                return queryset
+        except ValidationError:
+            pass
+        return queryset
+
+    def post(self, request, *args, **kwargs):
+        """
+        Creates a page element
+
+        **Tags: editors
+
+        **Example
+
+        .. code-block:: http
+
+            POST /api/content/editables HTTP/1.1
+
+        .. code-block:: json
+
+            {
+                "title": "Boxes enclosures"
+            }
+
+        responds
+
+        .. code-block:: json
+
+            {
+                "slug": "boxes-enclosures",
+                "title": "Boxes enclosures"
+            }
+
+        """
+        #pylint:disable=useless-super-delegation
+        return super(PageElementEditableListAPIView, self).create(
+            request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(account=self.account)
+
+
+class PageElementEditableDetail(AccountMixin, TrailMixin, CreateModelMixin,
+                                generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieves an editable page element
+
+    **Tags: editors
+
+    **Example
+
+    .. code-block:: http
+
+        GET /api/content/editables/boxes-enclosures/ HTTP/1.1
+
+    responds
+
+    .. code-block:: json
+
+        {
+            "slug": "boxes-enclosures",
+            "path": "/boxes-enclosures",
+            "text": "Hello"
+        }
+    """
+    serializer_class = PageElementSerializer
+
+    def get_object(self):
+        self.element.results = self.element.get_relationships()
+        return self.element
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Deletes a page element
+
+        **Tags: editors
+
+        **Example
+
+        .. code-block:: http
+
+            DELETE /api/content/editables/boxes-enclosures/ HTTP/1.1
+        """
+        #pylint:disable=useless-super-delegation
+        return super(PageElementEditableDetail, self).delete(
+            request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Creates a page element
+
+        **Tags: editors
+
+        **Example
+
+        .. code-block:: http
+
+            POST /api/content/editables/boxes-enclosures/ HTTP/1.1
+
+        .. code-block:: json
+
+            {
+                "title": "Boxes enclosures"
+            }
+
+        responds
+
+        .. code-block:: json
+
+            {
+                "slug": "boxes-enclosures",
+                "text": "Hello"
+            }
+
+        """
+        #pylint:disable=useless-super-delegation
+        return super(PageElementEditableDetail, self).create(
+            request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        """
+        Updates a page element
+
+        **Tags: editors
+
+        **Example
+
+        .. code-block:: http
+
+            PUT /api/content/editables/boxes-enclosures/ HTTP/1.1
+
+        .. code-block:: json
+
+            {
+                "title": "Boxes and enclosures"
+            }
+
+        responds
+
+        .. code-block:: json
+
+            {
+                "slug": "boxes-enclosures",
+                "text": "Hello"
+            }
+        """
+        #pylint:disable=useless-super-delegation
+        return super(PageElementEditableDetail, self).put(
+            request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            element = serializer.save(account=self.account)
+
+            # Attach the element in the content DAG
+            parent = self.element
+            rank = RelationShip.objects.filter(
+                orig_element=parent).aggregate(Max('rank')).get(
+                'rank__max', None)
+            rank = 0 if rank is None else rank + 1
+            RelationShip.objects.create(
+                orig_element=parent, dest_element=element, rank=rank)
+
+    def get_success_headers(self, data):
+        path = data.get('path').strip(self.URL_PATH_SEP) + self.URL_PATH_SEP
+        return {'Location': reverse('pages_editables_element',
+            args=(self.element.account, path))}
+
+    def update(self, request, *args, **kwargs):
+        with transaction.atomic():
+            try:
+                _ = self.element
+                return super(PageElementEditableDetail, self).update(
+                    request, *args, **kwargs)
+            except Http404:
+                pass
+            return super(PageElementEditableDetail, self).create(
+                request, *args, **kwargs)
+
+
+class PageElementAddTags(AccountMixin, PageElementMixin,
+                         generics.UpdateAPIView):
+    """
+    Adds tags to an editable node
+
+    Add tags to a ``PageElement`` if they are not already present.
+
+    **Example
+
+    .. code-block:: http
+
+        PUT /api/content/editables/boxes-and-enclosures/add-tags/ HTTP/1.1
+
+    .. code-block:: json
+
+        {
+          "tag": "sometag"
+        }
+
+    responds
+
+    .. code-block:: json
+
+        {
+        }
+    """
+    serializer_class = PageElementTagSerializer
+
+    def get_object(self):
+        return self.element
+
+    def perform_update(self, serializer):
+        curr_tags = serializer.instance.extra
+        if curr_tags:
+            curr_tags = curr_tags.split(',')
+        else:
+            curr_tags = []
+        add_tags = serializer.validated_data['tag'].split(',')
+        for tag in add_tags:
+            if not tag in curr_tags:
+                curr_tags.append(tag)
+        serializer.instance.extra = ','.join(curr_tags)
+        serializer.instance.save()
+
+
+class PageElementRemoveTags(AccountMixin, PageElementMixin,
+                            generics.UpdateAPIView):
+    """
+    Remove tags from an editable node
+
+    Remove tags from a ``PageElement``.
+
+    **Examples
+
+    .. code-block:: http
+
+        PUT /api/content/editables/boxes-and-enclosures/remove-tags/ HTTP/1.1
+
+    .. code-block:: json
+
+        {
+          "tag": "sometag"
+        }
+
+    responds
+
+    **Examples
+
+    .. code-block:: json
+
+        {
+        }
+    """
+    serializer_class = PageElementTagSerializer
+
+    def get_object(self):
+        return self.element
+
+    def perform_update(self, serializer):
+        curr_tags = serializer.instance.extra
+        if curr_tags:
+            curr_tags = curr_tags.split(',')
+        else:
+            curr_tags = []
+        remove_tags = serializer.validated_data['tag'].split(',')
+        for tag in remove_tags:
+            if tag in curr_tags:
+                curr_tags.remove(tag)
+        serializer.instance.extra = ','.join(curr_tags)
+        serializer.instance.save()
---
+++ packages/djaodjin-pages/djaodjin_pages-0.6.9-py3-none-any.whl/pages/serializers.py
@@ -0,0 +1,263 @@
+# Copyright (c) 2023, Djaodjin Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from __future__ import unicode_literals
+
+import json
+
+import bleach
+from rest_framework import serializers
+
+from . import settings
+from .compat import gettext_lazy as _, is_authenticated
+from .models import Comment, Follow, PageElement, Vote
+
+
+#pylint: disable=no-init,abstract-method
+
+
+class HTMLField(serializers.CharField):
+
+    def __init__(self, **kwargs):
+        self.html_tags = kwargs.pop('html_tags', [])
+        self.html_attributes = kwargs.pop('html_attributes', {})
+        self.html_strip = kwargs.pop('html_strip', False)
+        super(HTMLField, self).__init__(**kwargs)
+
+    def to_internal_value(self, data):
+        return super(HTMLField, self).to_internal_value(
+            bleach.clean(data,
+                tags=self.html_tags,
+                attributes=self.html_attributes,
+                strip=self.html_strip))
+
+
+class NoModelSerializer(serializers.Serializer):
+
+    def create(self, validated_data):
+        raise RuntimeError('`create()` should not be called.')
+
+    def update(self, instance, validated_data):
+        raise RuntimeError('`update()` should not be called.')
+
+
+class AssetSerializer(NoModelSerializer):
+
+    location = serializers.CharField(
+        help_text=_("URL where the asset content is stored."))
+    updated_at = serializers.DateTimeField(required=False,
+        help_text=_("Last date/time the asset content was updated."))
+
+
+class EdgeCreateSerializer(serializers.Serializer):
+    """
+    Create a new edge between two ``PageElement``.
+
+    The path specified in the URL will be aliased, mirrored or moved
+    under *source*.
+    When *rank* is specified, the resulting index of the aliased/mirrored/moved
+    element in its parent list will be *rank*.
+
+    The state in the UI is particularly complex. We use the *external_key*
+    to log incorrect calls from the Javascript code.
+    """
+    source = serializers.CharField()
+    rank = serializers.IntegerField(required=False)
+    external_key = serializers.CharField(required=False)
+
+
+class RelationShipSerializer(serializers.Serializer):
+    #pylint: disable=abstract-method
+    orig_elements = serializers.ListField(
+        child=serializers.SlugField(), required=False
+        )
+    dest_elements = serializers.ListField(
+        child=serializers.SlugField(), required=False
+        )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializes a Comment.
+    """
+    text = HTMLField(required=False,
+        html_tags=settings.ALLOWED_TAGS,
+        html_attributes=settings.ALLOWED_ATTRIBUTES,
+        help_text=_("Long description of the page element"))
+    user = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    class Meta:
+        model = Comment
+        fields = ('text', 'created_at', 'user')
+        read_only_fields = ('created_at', 'user')
+
+
+class NodeElementSerializer(serializers.ModelSerializer):
+    """
+    Serializes a PageElement as a node in a content tree
+    """
+    path = serializers.SerializerMethodField(read_only=True, allow_null=True,
+        help_text=_("path from the root of content tree"))
+    indent = serializers.SerializerMethodField(required=False, allow_null=True,
+        help_text=_("level in the content tree"))
+    account = serializers.SlugRelatedField(read_only=True, required=False,
+        slug_field=settings.ACCOUNT_LOOKUP_FIELD,
+        help_text=_("Account that can edit the page element"))
+    picture = serializers.CharField(required=False, allow_null=True,
+        help_text=_("Picture icon that can be displayed alongside the title"))
+    extra = serializers.SerializerMethodField(required=False, allow_null=True,
+        help_text=_("Extra meta data (can be stringify JSON)"))
+
+    class Meta:
+        model = PageElement
+        fields = ('slug', 'path', 'indent', 'account',
+                  'title', 'picture', 'extra')
+        read_only_fields = ('slug', 'path', 'indent', 'account')
+
+    @staticmethod
+    def get_extra(obj):
+        try:
+            extra = obj.extra
+        except AttributeError:
+            extra = obj.get('extra', {})
+        if not isinstance(extra, dict):
+            try:
+                return json.loads(extra)
+            except (TypeError, ValueError):
+                pass
+        return extra
+
+    @staticmethod
+    def get_indent(obj):
+        try:
+            return obj.get('indent', 0)
+        except AttributeError:
+            pass
+        try:
+            return obj.indent
+        except AttributeError:
+            pass
+        return 0
+
+    @staticmethod
+    def get_path(obj):
+        try:
+            return obj.get('path', None)
+        except AttributeError:
+            pass
+        try:
+            return obj.path
+        except AttributeError:
+            pass
+        return "/%s" % obj.slug
+
+
+class PageElementSerializer(serializers.ModelSerializer):
+    """
+    Serializes a PageElement.
+    """
+
+    path = serializers.SerializerMethodField()
+    slug = serializers.SlugField(required=False,
+        help_text=_("Unique identifier that can be used in URL paths"))
+    account = serializers.SlugRelatedField(read_only=True, required=False,
+        slug_field=settings.ACCOUNT_LOOKUP_FIELD,
+        help_text=_("Account that can edit the page element"))
+    picture = serializers.CharField(required=False, allow_null=True,
+        help_text=_("Picture icon that can be displayed alongside the title"))
+    text = HTMLField(required=False,
+        html_tags=settings.ALLOWED_TAGS,
+        html_attributes=settings.ALLOWED_ATTRIBUTES,
+        help_text=_("Long description of the page element"))
+    extra = serializers.SerializerMethodField(required=False, allow_null=True,
+        help_text=_("Extra meta data (can be stringify JSON)"))
+    nb_upvotes = serializers.IntegerField(required=False)
+    nb_followers = serializers.IntegerField(required=False)
+    upvote = serializers.SerializerMethodField(required=False, allow_null=True)
+    follow = serializers.SerializerMethodField(required=False, allow_null=True)
+    count = serializers.IntegerField(required=False)
+    results = serializers.ListField(required=False,
+        child=NodeElementSerializer())
+
+    class Meta:
+        model = PageElement
+        fields = ('path', 'slug', 'picture', 'title', 'text', 'reading_time',
+            'lang', 'account', 'extra',
+            'nb_upvotes', 'nb_followers', 'upvote', 'follow',
+            'count', 'results')
+        read_only_fields = ('path', 'slug', 'account',
+            'nb_upvotes', 'nb_followers', 'upvote', 'follow',
+            'count', 'results')
+
+    @staticmethod
+    def get_extra(obj):
+        try:
+            extra = obj.extra
+        except AttributeError:
+            extra = obj.get('extra', {})
+        if not isinstance(extra, dict):
+            try:
+                return json.loads(extra)
+            except (TypeError, ValueError):
+                pass
+        return extra
+
+    def get_upvote(self, data):
+        request = self.context.get('request')
+        if request and is_authenticated(request):
+            if isinstance(data, PageElement):
+                vote = Vote.objects.filter(
+                    user=request.user, element=data).first()
+                return vote and vote.vote == Vote.UP_VOTE
+        return None
+
+    def get_follow(self, data):
+        request = self.context.get('request')
+        if request and is_authenticated(request):
+            if isinstance(data, PageElement):
+                return Follow.objects.filter(
+                    user=request.user, element=data).exists()
+        return None
+
+    def get_path(self, obj):
+        prefix = self.context.get('prefix', "")
+        if not prefix:
+            parents = obj.get_parent_paths()
+            if parents:
+                prefix = "/".join(
+                    [parent.slug for parent in parents[0][:-1]])
+                if prefix:
+                    prefix = "/" + prefix
+                prefix = prefix + "/"
+        try:
+            slug = obj.slug
+        except AttributeError:
+            slug = obj.get('slug', "")
+        return prefix + slug
+
+
+class PageElementTagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PageElement
+        fields = ('tag',)
