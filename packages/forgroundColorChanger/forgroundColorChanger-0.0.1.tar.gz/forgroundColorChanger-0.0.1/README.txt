af3b9e75318c487317a9632c115454e0e453c5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:09:05 +0000

    Add package forgroundColorChanger/0.0.1/forgroundColorChanger-0.0.1.tar.gz

---
+++ packages/forgroundColorChanger/forgroundColorChanger-0.0.1.tar.gz/forgroundColorChanger-0.0.1/README.txt
@@ -0,0 +1,1 @@
+this is a forground color changer that has these colors : Blue, Green, Aqua, Red, Purple, Yellow, White, Gray, LightBlue, LightGreen, LightRed, LightPurple, LightYellow and BrightWhite.
---
+++ packages/forgroundColorChanger/forgroundColorChanger-0.0.1.tar.gz/forgroundColorChanger-0.0.1/forgroundColorChanger/__init__.py
@@ -0,0 +1,46 @@
+import subprocess
+class forgroundcolor:
+    def Blue():
+        subprocess.run("color 1", shell=True)
+    
+    def Green():
+        subprocess.run("color 2", shell=True)
+
+    def Aqua():
+        subprocess.run("color 3", shell=True)
+
+    def Red():
+        subprocess.run("color 4", shell=True)
+
+    def Purple():
+        subprocess.run("color 5", shell=True)
+
+    def Yellow():
+        subprocess.run("color 6", shell=True)
+
+    def White():
+        subprocess.run("color 7", shell=True)
+
+    def Gray():
+        subprocess.run("color 8", shell=True)
+
+    def LightBlue():
+        subprocess.run("color 9", shell=True)
+
+    def LightGreen():
+        subprocess.run("color a", shell=True)
+
+    def LightAqua():
+        subprocess.run("color b", shell=True)
+
+    def LightRed():
+        subprocess.run("color c", shell=True)
+
+    def LightPurple():
+        subprocess.run("color d", shell=True)
+
+    def LightYellow():
+        subprocess.run("color e", shell=True)
+
+    def BrightWhite():
+        subprocess.run("color f", shell=True)
