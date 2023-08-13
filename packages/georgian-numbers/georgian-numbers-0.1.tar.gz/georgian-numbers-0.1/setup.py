3cece17cfcf2ce0badececfa5d44b15ea65ef35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-13 03:10:05 +0000

    Add package georgian-numbers/0.1/georgian-numbers-0.1.tar.gz

---
+++ packages/georgian-numbers/georgian-numbers-0.1.tar.gz/georgian-numbers-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='georgian-numbers',
+    version='0.1',
+    description='It converts numbers in Georgian Language',
+    author='CCXLV',
+    packages=['georgian_numbers'],
+)
