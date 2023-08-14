f1527f1a0297c244d36a01d4315e25993ff4cb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:11:27 +0000

    Add package pymobiledevice3/2.3.1/pymobiledevice3-2.3.1.tar.gz

---
+++ packages/pymobiledevice3/pymobiledevice3-2.3.1.tar.gz/pymobiledevice3-2.3.1/pymobiledevice3/remote/utils.py
@@ -0,0 +1,39 @@
+import contextlib
+import platform
+from typing import Generator
+
+import psutil
+
+REMOTED_PATH = '/usr/libexec/remoted'
+
+
+def _get_remoted_process() -> psutil.Process:
+    for process in psutil.process_iter():
+        if process.pid == 0:
+            # skip kernel task
+            continue
+        try:
+            if process.exe() == REMOTED_PATH:
+                return process
+        except psutil.ZombieProcess:
+            continue
+
+
+@contextlib.contextmanager
+def stop_remoted() -> Generator[None, None, None]:
+    if platform.system() != 'Darwin':
+        # only Darwin systems require it
+        yield
+        return
+
+    remoted = _get_remoted_process()
+    if remoted.status() == 'stopped':
+        # process already stopped, we don't need to do anything
+        yield
+        return
+
+    remoted.suspend()
+    try:
+        yield
+    finally:
+        remoted.resume()
