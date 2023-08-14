445e5b1304672890238827f1d0e97337761be1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:52 +0000

    Add package botrun-pdf-to-text/1.0.0/botrun-pdf-to-text-1.0.0.tar.gz

---
+++ packages/botrun-pdf-to-text/botrun-pdf-to-text-1.0.0.tar.gz/botrun-pdf-to-text-1.0.0/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name="botrun-pdf-to-text",
+    version="1.0.0",
+    packages=find_packages(),
+    py_modules=['botrun_pdf_to_text'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'botrun_pdf_to_text = botrun_pdf_to_text:main',
+        ],
+    },
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',)
