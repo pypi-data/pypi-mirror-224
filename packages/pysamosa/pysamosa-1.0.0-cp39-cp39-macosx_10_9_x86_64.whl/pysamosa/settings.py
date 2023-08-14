cc1c540d039623834a742192842b0a8abcee4a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:25 +0000

    Add package pysamosa/1.0.0/pysamosa-1.0.0-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/pysamosa/pysamosa-1.0.0-cp39-cp39-macosx_10_9_x86_64.whl/pysamosa/settings.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+TEST_DATA_DIR = Path(__file__).parent / ".testdata"
+
+S3_DATA_DIR = TEST_DATA_DIR / "s3"
+S6_DATA_DIR = TEST_DATA_DIR / "s6"
+FFSAR_DATA_DIR = TEST_DATA_DIR / "s6" / "ffsar"
+CS_DATA_DIR = TEST_DATA_DIR / "cs"
