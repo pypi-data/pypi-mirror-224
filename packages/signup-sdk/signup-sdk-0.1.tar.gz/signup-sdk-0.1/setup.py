c84301bc6a28b9623505b9fc25157c1901a6098d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:38 +0000

    Add package signup-sdk/0.1/signup-sdk-0.1.tar.gz

---
+++ packages/signup-sdk/signup-sdk-0.1.tar.gz/signup-sdk-0.1/setup.py
@@ -0,0 +1,11 @@
+# signup_sdk/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='signup-sdk',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
