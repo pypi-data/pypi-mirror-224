107c36cb1ff3f10514fc02162a9787eef427edf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:11:14 +0000

    Add package django-kakebo/0.0.6/django_kakebo-0.0.6.tar.gz

---
+++ packages/django-kakebo/django_kakebo-0.0.6.tar.gz/django_kakebo-0.0.6/setup.py
@@ -0,0 +1,44 @@
+import glob
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_kakebo",
+    version="0.0.6",
+    author="shadMod",
+    author_email="support@shadmod.it",
+    description="A simple site made with django and exploiting the kakebo method",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shadMod/django-kakebo/",
+    download_url="https://github.com/shadMod/django-kakebo/archive/refs/tags/0.0.6.tar.gz",
+    project_urls={
+        "Bug Tracker": "https://github.com/shadMod/django-kakebo/issues/",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    include_package_data=True,
+    packages=["django_kakebo", "django_kakebo.src.django_kakebo"],
+    data_files=[
+        (
+            "static",
+            [fn for fn in glob.iglob("django_kakebo/src/django_kakebo/static/**/*", recursive=True) if "." in fn],
+        ),
+        (
+            "themes",
+            [fn for fn in glob.iglob("django_kakebo/src/themes/**/*", recursive=True) if "." in fn],
+        ),
+    ],
+    python_requires=">=3.7",
+)
