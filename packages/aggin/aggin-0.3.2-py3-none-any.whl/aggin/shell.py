2dacd2f32a92bbbe4b9ec4b02951e514ed9e3fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:10:37 +0000

    Add package aggin/0.3.2/aggin-0.3.2-py3-none-any.whl

---
+++ packages/aggin/aggin-0.3.2-py3-none-any.whl/aggin/shell.py
@@ -0,0 +1,19 @@
+from . import aggin
+
+def run():
+    while True:
+        text = input('aggin > ')
+        if text.strip() == "":
+            continue
+        result, error = aggin.run('<stdin>', text)
+
+        if error:
+            print(error.as_string())
+        elif result:
+            if len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
+
+if __name__ == "__main__":
+    run()
