86cc32fd10ed9291f0ced1260da620d572eb176c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:22 +0000

    Add package clean-confluent-kafka/0.0.9/clean_confluent_kafka-0.0.9-py3-none-any.whl

---
+++ packages/clean-confluent-kafka/clean_confluent_kafka-0.0.9-py3-none-any.whl/clean_confluent_kafka/utils/__init__.py
@@ -0,0 +1,35 @@
+def reverse_flatten_dict(d, sep="."):
+    result = dict()
+    for key, value in d.items():
+        parts = key.split(sep)
+        d = result
+        for part in parts[:-1]:
+            if part not in d:
+                d[part] = {}
+            d = d[part]
+        d[parts[-1]] = value
+    return result
+
+
+def unflatten_dict(d, sep="."):
+    result = dict()
+    for key, value in d.items():
+        parts = key.split(sep)
+        part = parts[0]
+        if part not in result:
+            result[part] = dict()
+        if len(parts) > 1:
+            result[part][sep.join(parts[1:])] = value
+    return result
+
+
+def flatten_dict(hierarchical_dict, parent_key="", sep="."):
+    items = []
+    for key, value in hierarchical_dict.items():
+        new_key = (f"{parent_key}{sep}{key}" if parent_key else key) \
+            if key != "self" else parent_key
+        if isinstance(value, dict):
+            items.extend(flatten_dict(value, new_key, sep).items())
+        else:
+            items.append((new_key, value))
+    return dict(items)
