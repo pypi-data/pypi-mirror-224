e8c6151c3ef51620f2508a88fc59f1c3800fdddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:18 +0000

    Add package gemato/20.5/gemato-20.5.tar.gz

---
+++ packages/gemato/gemato-20.5.tar.gz/gemato-20.5/tests/test_util.py
@@ -0,0 +1,56 @@
+# gemato: Utility function tests
+# (c) 2017-2022 Michał Górny
+# Licensed under the terms of 2-clause BSD license
+
+import pytest
+
+from gemato.util import (
+    path_starts_with,
+    path_inside_dir,
+    )
+
+
+@pytest.mark.parametrize(
+    'p1,p2,expected',
+    [("", "", True),
+     ("foo", "", True),
+     ("foo/", "", True),
+     ("foo/bar", "", True),
+     ("bar", "", True),
+     ("bar/", "", True),
+     ("bar/bar", "", True),
+     ("foo", "foo", True),
+     ("foo/", "foo", True),
+     ("foo/bar", "foo", True),
+     ("bar", "foo", False),
+     ("fooo", "foo", False),
+     ("foo.", "foo", False),
+     ("foo", "foo/", True),
+     ("foo/", "foo/", True),
+     ("foo/bar", "foo/bar/", True),
+     ])
+def test_path_starts_with(p1, p2, expected):
+    assert path_starts_with(p1, p2) is expected
+
+
+@pytest.mark.parametrize(
+    'p1,p2,expected',
+    [("", "", False),
+     ("foo", "", True),
+     ("foo/", "", True),
+     ("foo/bar", "", True),
+     ("bar", "", True),
+     ("bar/", "", True),
+     ("bar/bar", "", True),
+     ("foo", "foo", False),
+     ("foo/", "foo", False),
+     ("foo/bar", "foo", True),
+     ("bar", "foo", False),
+     ("fooo", "foo", False),
+     ("foo.", "foo", False),
+     ("foo", "foo/", False),
+     ("foo/", "foo/", False),
+     ("foo/bar", "foo/bar/", False),
+     ])
+def test_path_inside_dir(p1, p2, expected):
+    assert path_inside_dir(p1, p2) is expected
