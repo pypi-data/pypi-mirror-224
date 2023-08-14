5ae25b65750c1c1bf461f6826e1624122897bb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:55 +0000

    Add package nonebot-plugin-alconna/0.14.0/nonebot_plugin_alconna-0.14.0.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.14.0.tar.gz/nonebot_plugin_alconna-0.14.0/src/nonebot_plugin_alconna/argv.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from nonebot.adapters import Message
+from arclet.alconna._internal._argv import Argv
+from arclet.alconna.argv import argv_config, set_default_argv_type
+
+
+class MessageArgv(Argv[Message]):
+    @staticmethod
+    def generate_token(data: list) -> int:
+        return hash("".join(i.__class__.__name__ + i.__repr__() for i in data))
+
+
+set_default_argv_type(MessageArgv)
+argv_config(
+    MessageArgv,
+    filter_out=[],
+    checker=lambda x: isinstance(x, Message),
+    to_text=lambda x: x if x.__class__ is str else str(x) if x.is_text() else None,
+)
