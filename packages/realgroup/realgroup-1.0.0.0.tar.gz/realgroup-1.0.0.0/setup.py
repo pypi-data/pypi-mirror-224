c82c557adbc8ee3262aeb6fd3602de4b46fa5caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:49 +0000

    Add package realgroup/1.0.0.0/realgroup-1.0.0.0.tar.gz

---
+++ packages/realgroup/realgroup-1.0.0.0.tar.gz/realgroup-1.0.0.0/setup.py
@@ -0,0 +1,12 @@
+# encoding: utf-8 
+from setuptools import setup, find_packages
+
+setup(
+    name='realgroup',
+    version='1.0.0.0',  # 替换为您的版本号
+    description='${Description}',
+    author='dyc',
+    author_email='your@email.com',
+    packages=['importProject4','importProject5'],
+    install_requires=['requests']
+)
