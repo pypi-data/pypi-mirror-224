8a39e2ecb30972e28c9e3da5eab23584de5ba669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:10:08 +0000

    Add package cad-viewer-widget/2.0.0/cad_viewer_widget-2.0.0.tar.gz

---
+++ packages/cad-viewer-widget/cad_viewer_widget-2.0.0.tar.gz/cad_viewer_widget-2.0.0/js/lib/labplugin.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-undef
+var plugin = require("./index");
+
+import { IJupyterWidgetRegistry } from "@jupyter-widgets/base";
+
+import App from "./app.js";
+
+const cadViewerWidget = {
+  id: "cad-viewer-widget:plugin",
+  requires: [IJupyterWidgetRegistry],
+  activate: function (app, widgets) {
+    widgets.registerWidget({
+      name: "cad-viewer-widget",
+      version: plugin.version,
+      exports: plugin
+    });
+    console.log(
+      `cad-viewer-widget version ${plugin.version} is registered`
+    );
+
+    App.setShell(app.shell);
+  },
+  autoStart: true
+};
+
+export default cadViewerWidget;
