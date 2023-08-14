bdea445fafd8021f9bb5803d67f879029f20f21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:10:34 +0000

    Add package gflownets/0.0.1/gflownets-0.0.1.tar.gz

---
+++ packages/gflownets/gflownets-0.0.1.tar.gz/gflownets-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gflownets",
+    version="0.0.1",
+    license="MIT",
+    author="",
+    author_email="",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="",
+    keywords="gflownets",
+    install_requires=[],
+)
