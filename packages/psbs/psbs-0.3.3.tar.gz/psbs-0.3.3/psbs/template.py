d0a81ac8fe0c8e8b292d2173a10ab3480327bc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:11:17 +0000

    Add package psbs/0.3.3/psbs-0.3.3.tar.gz

---
+++ packages/psbs/psbs-0.3.3.tar.gz/psbs-0.3.3/psbs/template.py
@@ -0,0 +1,82 @@
+from os import path
+import traceback
+
+import jinja2
+from .extension import Extension
+
+
+class Template:
+    def __init__(self, filename, config):
+        self.file = path.basename(filename)
+        self.jinja_env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(path.dirname(filename)),
+            autoescape=False,
+            block_start_string="(%",
+            block_end_string="%)",
+            variable_start_string="((",
+            variable_end_string="))",
+            comment_start_string="(#",
+            comment_end_string="#)",
+            extensions=["jinja2.ext.do"],
+            trim_blocks=True,
+            lstrip_blocks=True,
+        )
+        self.postprocessing_steps = []
+        user_extensions = config.get("user_extensions", [])
+        extensions = Extension.get_extensions(user_extensions)
+        for extension in extensions:
+            if extension.__name__ not in config:
+                config[extension.__name__] = {}
+            ext_object = extension(config[extension.__name__])
+            for func_name, function in ext_object.methods.items():
+                self.jinja_env.globals[func_name] = function
+            for func_name, function in ext_object.filters.items():
+                self.jinja_env.filters[func_name] = function
+            for function in ext_object.post:
+                self.postprocessing_steps.append(function)
+
+    def render(self):
+        try:
+            template = self.jinja_env.get_template(self.file)
+            output = template.render()
+        except jinja2.exceptions.TemplateNotFound as err:
+            print(f"Error: Unable to find template '{err}'")
+            raise SystemExit(1) from err
+        except (jinja2.exceptions.TemplateError, TypeError) as err:
+            print(f"Error: Unable to render template\n  {err}")
+            traceback_list = traceback.format_exc().split("\n")
+            for index, line in enumerate(traceback_list):
+                if line.startswith('  File "src'):
+                    print(line)
+                    print(traceback_list[index + 1])
+                    print(traceback_list[index + 2])
+            raise SystemExit(1) from err
+        output = self.postprocess(output)
+        return output
+
+    def postprocess(self, input_str):
+        output = input_str
+        for post_function in self.postprocessing_steps:
+            output = post_function(output)
+        return output
+
+    @staticmethod
+    def make_template(src_tree):
+        output = ""
+        for section in src_tree:
+            if section != "prelude":
+                if section != "prelude":
+                    output += "=" * (len(section) + 1)
+                    output += f"\n{section.upper()}\n"
+                    output += "=" * (len(section) + 1)
+                    output += "\n\n"
+            if not src_tree[section]:
+                src_tree[section] = [""]
+            for index in range(len(src_tree[section])):
+                index += 1
+                if len(src_tree[section]) == 1:
+                    index = ""
+                src_filename = f"{section}{index}.pss"
+                output += f'(% include "{src_filename}" %)\n'
+            output += "\n"
+        return output.strip()
---
+++ packages/psbs/psbs-0.3.3.tar.gz/psbs-0.3.3/psbs/utils.py
@@ -0,0 +1,75 @@
+import webbrowser
+from os import mkdir
+
+import yaml
+
+
+def read_file(filename):
+    try:
+        with open(filename, "r", encoding="UTF-8") as file:
+            return file.read()
+    except IOError as err:
+        print(f"Error: Unable to read input file\n  {err}")
+        raise SystemExit(1) from err
+    except UnicodeDecodeError as err:
+        print(
+            "Error: Unable to read input file, "
+            "are you sure this is a text file?"
+        )
+        raise SystemExit(1) from err
+
+
+def write_file(filename, data):
+    try:
+        with open(filename, "w", encoding="UTF-8") as file:
+            file.write(data)
+    except IOError as err:
+        print(f"Error: Unable to write file {filename}\n  {err}")
+        raise SystemExit(1) from err
+
+
+def read_yaml(filename):
+    try:
+        with open(filename, "rb") as file:
+            output = yaml.safe_load(file)
+        if isinstance(output, str):
+            raise yaml.YAMLError("YAML syntax malformed")
+    except IOError as err:
+        print(f"Error: Config file not found\n  {err}")
+        raise SystemExit(1) from err
+    except yaml.YAMLError as err:
+        print(f"Error: Problem parsing config file\n  {err}")
+        raise SystemExit(1) from err
+    return output
+
+
+def write_yaml(filename, data):
+    try:
+        with open(filename, "w", encoding="UTF-8") as file:
+            yaml.safe_dump(data, file, sort_keys=False)
+    except IOError as err:
+        print(f"Error: Unable to write file {filename}\n  {err}")
+        raise SystemExit(1) from err
+
+
+def make_dir(filename):
+    try:
+        mkdir(filename)
+    except (OSError, PermissionError) as err:
+        print(f"Error: Unable to create directory {filename}\n  {err}")
+        raise SystemExit(1) from err
+
+
+def run_in_browser(url):
+    try:
+        webbrowser.open(url)
+    except webbrowser.Error as err:
+        print("Error: Unable to launch browser")
+        raise SystemExit(1) from err
+
+
+def url_join(*url_strings):
+    output_list = []
+    for part in url_strings:
+        output_list.extend(part.rstrip("/").split("/"))
+    return "/".join(output_list)
