73b7508e53a1387854b2965a2332e48de13894a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:20 +0000

    Add package red-postgres/0.1.7/red-postgres-0.1.7.tar.gz

---
+++ packages/red-postgres/red-postgres-0.1.7.tar.gz/red-postgres-0.1.7/red_postgres/__init__.py
@@ -0,0 +1,11 @@
+from .engine import (
+    create_database,
+    create_tables,
+    diagnose,
+    register_cog,
+    run_migrations,
+)
+
+__author__ = "Vertyco"
+__all__ = ["create_database", "create_tables", "run_migrations", "diagnose", "register_cog"]
+__version__ = "0.1.7"
