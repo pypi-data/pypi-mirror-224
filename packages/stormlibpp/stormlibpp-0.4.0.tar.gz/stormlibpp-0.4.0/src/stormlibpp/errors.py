4f0b9313f0652a9c005bc0a3a2197d7ac73d80a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 04:11:40 +0000

    Add package stormlibpp/0.4.0/stormlibpp-0.4.0.tar.gz

---
+++ packages/stormlibpp/stormlibpp-0.4.0.tar.gz/stormlibpp-0.4.0/src/stormlibpp/errors.py
@@ -0,0 +1,30 @@
+"""Errors for StormLib++."""
+
+
+class StormSyntaxError(Exception):
+    """There was a syntax error in Storm code."""
+
+
+class StormPkgError(Exception):
+    """An error in a StormPkg."""
+
+
+class StormPkgNoProtoError(StormPkgError):
+    """The proto Yaml file for a StormPkg does not exist."""
+
+
+class StormPkgResolveError(StormPkgError):
+    """The package proto's directory for a StormPkg could not be resolved."""
+
+
+class StormPkgBadDefError(StormPkgError):
+    """The package proto's directory for a StormPkg could not be resolved."""
+
+class StormPkgSyntaxError(StormPkgError, StormSyntaxError):
+    """The Storm code defined in a StormPkg has a syntax error."""
+
+class HttpCortexError(Exception):
+    """An error occurred in HttpCortex."""
+
+class HttpCortexLoginError(HttpCortexError):
+    """Unable to login to Synapse with HttpCortex."""
