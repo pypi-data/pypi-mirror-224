13d0315f9d9b7ad8c53e43ec2078cecd3efc1ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:10:34 +0000

    Add package coo-img-sorter/0.0.1/coo-img-sorter-0.0.1.tar.gz

---
+++ packages/coo-img-sorter/coo-img-sorter-0.0.1.tar.gz/coo-img-sorter-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="utf8") as f:
+    requires = f.read()
+
+setuptools.setup(
+    name='coo-img-sorter',
+    version='0.0.1',
+    license="MIT",
+    author="xcenweb",
+    author_email="xcenweb@qq.com",
+    url="https://github.com/xcenweb/coo-img-sorter",
+
+    description="多种图片分类工具脚手架",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=[
+        "coo_imgSorter",
+        "coo_imgSorter.util",
+    ],
+
+    python_requires=">=3.8",
+    install_requires=requires.splitlines(),
+)
