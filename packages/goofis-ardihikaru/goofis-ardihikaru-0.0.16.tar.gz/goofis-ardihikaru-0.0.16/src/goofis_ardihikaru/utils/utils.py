fa500d1f479d5aba699388a995e0cd51d2a20393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:54 +0000

    Add package goofis-ardihikaru/0.0.16/goofis-ardihikaru-0.0.16.tar.gz

---
+++ packages/goofis-ardihikaru/goofis-ardihikaru-0.0.16.tar.gz/goofis-ardihikaru-0.0.16/src/goofis_ardihikaru/utils/utils.py
@@ -0,0 +1,20 @@
+from goofis_ardihikaru.enums.language import Language
+
+
+async def reformat_name(name: str, about: str, lang: str) -> str:
+    # do nothing if this logic below got triggered
+    if len(about) < 0 or len(name) < 0:
+        return name
+    if lang != Language.INDONESIA.value:
+        return name
+
+    # starts converting value
+    if "merupakan" in about:
+        about_arr = about.split("merupakan")
+        name = about_arr[0]
+
+    if "adalah" in about:
+        about_arr = about.split("adalah")
+        name = about_arr[0]
+
+    return name
