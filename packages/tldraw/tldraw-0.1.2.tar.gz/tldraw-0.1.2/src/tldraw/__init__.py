4e7d8277fa5bcdb1cc8de370ae766f79c522f756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:10:39 +0000

    Add package tldraw/0.1.2/tldraw-0.1.2.tar.gz

---
+++ packages/tldraw/tldraw-0.1.2.tar.gz/tldraw-0.1.2/src/tldraw/__init__.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+
+
+try:
+    __version__ = importlib.metadata.version("tldraw")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+
+
+from .tldraw import TldrawWidget
+from .matplotlib_annotate import TldrawMatplotlib
