78d62d1bda6304f083d45cd3466e9baa79ec5896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:19 +0000

    Add package movis/0.0.1/movis-0.0.1.tar.gz

---
+++ packages/movis/movis-0.0.1.tar.gz/movis-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='movis',
+    version='0.0.1',
+    description='A video editing library',
+    author='Masaki Saito',
+    author_email='msaito@preferred.jp',
+    packages=find_packages(),
+)
+
