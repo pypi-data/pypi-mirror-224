e2a17206262330cee50a811d92882f5cca1b8c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:28 +0000

    Add package kabupy/0.0.5rc5.post1/kabupy-0.0.5rc5.post1-py3-none-any.whl

---
+++ packages/kabupy/kabupy-0.0.5rc5.post1-py3-none-any.whl/kabupy/util/__init__.py
@@ -0,0 +1,28 @@
+"""util functions"""
+from __future__ import annotations
+
+import re
+
+from money import Money
+
+__all__ = ["str2money", "str2float"]
+
+jpy_unit = str.maketrans({"百": "0" * 2, "千": "0" * 3, "万": "0" * 4, "億": "0" * 8})
+
+
+def str2money(price: str) -> Money | None:
+    """Convert str to JPY Money object"""
+    if not re.search(r"\d", price):
+        return None
+    amount = price.translate(jpy_unit)
+    amount = re.sub(r"[^\d.]", "", amount)
+    if amount == "":
+        return None
+    return Money(amount, "JPY")
+
+
+def str2float(value: str) -> float | None:
+    amount = re.sub(r"[^\d.]", "", value)
+    if amount == "":
+        return None
+    return float(amount)
