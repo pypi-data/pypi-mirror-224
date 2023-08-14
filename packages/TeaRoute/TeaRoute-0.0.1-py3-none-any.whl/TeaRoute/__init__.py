dd17b85eff3a28f7a920cd73a7c274c23400d686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 04:12:37 +0000

    Add package TeaRoute/0.0.1/TeaRoute-0.0.1-py3-none-any.whl

---
+++ packages/TeaRoute/TeaRoute-0.0.1-py3-none-any.whl/TeaRoute/__init__.py
@@ -0,0 +1,8 @@
+from .main import Tear
+from .router.inner import Router
+from .router.methods import chat, cos, embed, route, training
+from chat import chat
+from cos import cos
+from embed import embed
+from route import route
+from training import trainWithFile
