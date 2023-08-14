160892869ea6efbf8afed9e4aa97010bc2d3b689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:11:10 +0000

    Add package evduty-free/0.0.2a0/evduty-free-0.0.2a0.tar.gz

---
+++ packages/evduty-free/evduty-free-0.0.2a0.tar.gz/evduty-free-0.0.2a0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name), encoding='utf-8').read()
+
+setup(
+    name="evduty-free",
+    version="v0.0.2-alpha",
+    description="Unofficial module for interacting with EVDuty EV charger api",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    keywords="",
+    author="Jean-Marc G",
+    author_email="none@none.none",
+    url="https://github.com/planetefrench/evduty-free",
+    download_url="https://github.com/planetefrench/evduty-free/archive/refs/tags/v0.0.1-alpha2.tar.gz",
+    license="Apache 2",
+    packages=["evdutyfree"],
+    install_requires=["requests>=2.22.0", "simplejson>=3.16.0"],
+    python_requires=">=3.7",
+    classifiers=["Programming Language :: Python :: 3",],
+)
