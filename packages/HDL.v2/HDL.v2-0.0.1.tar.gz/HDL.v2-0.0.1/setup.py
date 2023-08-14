0150e836e51a70facef4f3b04bfb9996f321d6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:50 +0000

    Add package HDL.v2/0.0.1/HDL.v2-0.0.1.tar.gz

---
+++ packages/HDL.v2/HDL.v2-0.0.1.tar.gz/HDL.v2-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="HDL.v2",
+    version="1.0.0",
+    author="Pouyatlp",
+    author_email="useracctp@gmail.com",
+    description="a package that contains handtracking modules such as:handDetection,FingerCount,EmojiDetection",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
