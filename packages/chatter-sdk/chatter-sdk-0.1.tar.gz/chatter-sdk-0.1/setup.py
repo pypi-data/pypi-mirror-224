0cdecd4429fd3ff853166b90117d694abf082499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:10:17 +0000

    Add package chatter-sdk/0.1/chatter-sdk-0.1.tar.gz

---
+++ packages/chatter-sdk/chatter-sdk-0.1.tar.gz/chatter-sdk-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chatter-sdk',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies here
+    ],
+    author='Anish Agrawal',
+    author_email='anish@trychatter.ai',
+    description='SDK to interact with Chatter!',
+)
