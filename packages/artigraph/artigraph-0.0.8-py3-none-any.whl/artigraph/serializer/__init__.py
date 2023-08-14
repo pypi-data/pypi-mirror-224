92535520a3cde14797d5f0ed61e6e17b1c53a021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:11:21 +0000

    Add package artigraph/0.0.8/artigraph-0.0.8-py3-none-any.whl

---
+++ packages/artigraph/artigraph-0.0.8-py3-none-any.whl/artigraph/serializer/__init__.py
@@ -0,0 +1,9 @@
+from artigraph.serializer.core import (
+    Serializer,
+    get_serializer_by_name,
+)
+
+__all__ = [
+    "get_serializer_by_name",
+    "Serializer",
+]
---
+++ packages/artigraph/artigraph-0.0.8-py3-none-any.whl/artigraph/storage/__init__.py
@@ -0,0 +1,9 @@
+from artigraph.storage.core import (
+    Storage,
+    get_storage_by_name,
+)
+
+__all__ = [
+    "get_storage_by_name",
+    "Storage",
+]
