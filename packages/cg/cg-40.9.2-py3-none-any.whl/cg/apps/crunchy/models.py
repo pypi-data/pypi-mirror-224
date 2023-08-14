065860961d6561d90f3b6e0d6d21c0acd277b252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:10:45 +0000

    Add package cg/40.9.2/cg-40.9.2-py3-none-any.whl

---
+++ packages/cg/cg-40.9.2-py3-none-any.whl/cg/apps/crunchy/models.py
@@ -0,0 +1,17 @@
+from datetime import date
+from typing import List, Optional
+
+from typing_extensions import Annotated, Literal
+from pydantic import BaseModel, Field
+
+
+class CrunchyFile(BaseModel):
+    path: str
+    file: Literal["first_read", "second_read", "spring"]
+    checksum: Optional[str]
+    algorithm: Literal["sha1", "md5", "sha256"] = None
+    updated: date = None
+
+
+class CrunchyMetadata(BaseModel):
+    files: Annotated[List[CrunchyFile], Field(max_length=3, min_length=3)]
---
+++ packages/cg/cg-40.9.2-py3-none-any.whl/cg/apps/tb/models.py
@@ -0,0 +1,48 @@
+import datetime as dt
+from pathlib import Path
+from typing import Optional
+
+from dateutil.parser import parse as parse_datestr
+from pydantic import BaseModel, ConfigDict, field_validator
+
+
+class TrailblazerAnalysis(BaseModel):
+    id: int
+    family: str
+    case_id = Optional[str]
+
+    version: Optional[str]
+    logged_at: Optional[str]
+    started_at: Optional[str]
+    completed_at: Optional[str]
+    status: Optional[str]
+    priority: Optional[str]
+    out_dir: Optional[str]
+    config_path: Optional[str]
+    comment: Optional[str]
+    is_deleted: Optional[bool]
+    is_visible: Optional[bool]
+    type: Optional[str]
+    user_id: Optional[int]
+    progress: Optional[float] = 0.0
+    data_analysis: Optional[str]
+    ticket: Optional[str]
+    uploaded_at: Optional[str]
+
+    @field_validator("case_id", mode="before")
+    def inherit_family_value(cls, value: str, values: dict) -> str:
+        return values.get("family")
+
+    @field_validator("logged_at", "started_at", "completed_at")
+    @classmethod
+    def parse_str_to_datetime(cls, value: str) -> Optional[dt.datetime]:
+        if value:
+            return parse_datestr(value)
+
+    @field_validator("out_dir", "config_path")
+    @classmethod
+    def parse_str_to_path(cls, value: str) -> Optional[Path]:
+        if value:
+            return Path(value)
+
+    model_config = ConfigDict(extra="allow", validate_default=True, arbitrary_types_allowed=True)
