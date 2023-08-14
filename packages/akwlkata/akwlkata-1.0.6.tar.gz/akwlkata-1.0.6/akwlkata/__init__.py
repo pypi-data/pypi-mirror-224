41758646b3a288e44b570e75db4ee03e4b5b2779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:14 +0000

    Add package akwlkata/1.0.6/akwlkata-1.0.6.tar.gz

---
+++ packages/akwlkata/akwlkata-1.0.6.tar.gz/akwlkata-1.0.6/akwlkata/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+"""
+    @header __init__.py.py
+    @abstract   
+    
+    @MyBlog: http://www.kuture.com.cn
+    @author  Created by Kuture on 2023/7/25
+    @version 1.0.0 2023/7/25 Creation()
+    
+    @Copyright © 2023年 Mr.Li All rights reserved
+"""
