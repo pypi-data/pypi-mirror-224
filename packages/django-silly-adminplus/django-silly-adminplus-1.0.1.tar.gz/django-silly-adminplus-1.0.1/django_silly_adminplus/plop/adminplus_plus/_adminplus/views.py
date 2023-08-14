5e9805cc562c83ffb28036ffe6c11615082dc86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 04:12:10 +0000

    Add package django-silly-adminplus/1.0.1/django-silly-adminplus-1.0.1.tar.gz

---
+++ packages/django-silly-adminplus/django-silly-adminplus-1.0.1.tar.gz/django-silly-adminplus-1.0.1/django_silly_adminplus/plop/adminplus_plus/_adminplus/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+
+from django_silly_adminplus.config import SILLY_ADMINPLUS
+from .models import Configuration
+
+User = get_user_model()
+
+
+def create_user(request):
+    if not request.user.is_staff and request.user.is_active:
+        return redirect('admin:index')
+
+    if request.method == 'POST':
+        print("=== request.post: ", request.POST)
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if len(username) < 1:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                message="Userame must be at least 1 character long",
+                extra_tags="danger")
+
+            return render(request, '_adminplus/create_user.html')
+        try:
+            user = User.objects.create(username=username, email=email)
+            user.set_password(password)
+            user.save()
+        except Exception as e:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                message=str(e),
+                extra_tags="danger")
+
+            return render(request, '_adminplus/create_user.html')
+
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            message="User created successfully",
+            extra_tags="success")
+
+    return render(request, '_adminplus/create_user.html')
+
+
+def adminplus(request):
+    if not request.user.is_staff or not request.user.is_active:
+        return redirect('admin:index')
+
+    if request.method == 'POST':
+        print("=== request.POST - open_subscriptions: ", request.POST.get('open_subscriptions'))
+        configuration = Configuration.objects.first()
+        if request.POST.get('open_subscriptions') == "on":  # checkbox returns "on" or None
+            configuration.open_subscriptions = True
+        else:
+            configuration.open_subscriptions = False
+        configuration.save()
+
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            message="Configuration updated successfully",
+            extra_tags="success")
+
+    configuration = Configuration.objects.first()
+    context = {
+        'configuration': configuration,
+    }
+
+    return render(request, SILLY_ADMINPLUS['TEMPLATE'], context)
