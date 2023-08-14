1d672e04d25e16bbbf373e0283f15ebfb3a183b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:50 +0000

    Add package trddt/0.3.1/trddt-0.3.1.tar.gz

---
+++ packages/trddt/trddt-0.3.1.tar.gz/trddt-0.3.1/README.md
@@ -0,0 +1,16 @@
+# TradeDatetime
+한국거래소 기준 Datetime Functions 패키지
+
+
+
+
+
+<!-- ============================================================ -->
+## Dependancy
+
+    holidays==0.21
+    pandas
+    ipylib
+    xlrd
+    openpyxl
+
---
+++ packages/trddt/trddt-0.3.1.tar.gz/trddt-0.3.1/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+SOURCE_DIRECTORY = 'src'
+
+setuptools.setup(
+    name="trddt",
+    version="0.3.1",
+    author="innovata",
+    author_email="iinnovata@gmail.com",
+    description='한국거래소 기준 Datetime Functions 패키지',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/innovata/TradeDatetime",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": SOURCE_DIRECTORY},
+    packages=setuptools.find_packages(SOURCE_DIRECTORY),
+    python_requires=">=3.8",
+    install_requires=['ipylib', 'holidays', 'pandas','xlrd', 'openpyxl'],
+)
