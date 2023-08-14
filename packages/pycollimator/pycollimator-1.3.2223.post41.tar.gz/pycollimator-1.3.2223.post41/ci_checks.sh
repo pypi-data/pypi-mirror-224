12a94ff3ef4b604645733864268633167ee37e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:10:24 +0000

    Add package pycollimator/1.3.2223.post41/pycollimator-1.3.2223.post41.tar.gz

---
+++ packages/pycollimator/pycollimator-1.3.2223.post41.tar.gz/pycollimator-1.3.2223.post41/ci_checks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+cd `dirname $0`
+source "env.sh"
+
+do_echo "Running CI checks for collimator package in ENV=${ENV}..."
+
+source venv/bin/activate
+do_exec black --check .
+do_exec pylint -E pycollimator
+do_exec flake8 pycollimator
+
+do_echo "Running pytest..."
+# FIXME: disabled compile_pip_requirements_test because there is some unknown discrepancy between
+# local and CI.
+do_exec bazel test -- //src/lib/pycollimator:all -//src/lib/pycollimator:compile_pip_requirements_test
+
+do_echo "All good! You can now merge this PR."
