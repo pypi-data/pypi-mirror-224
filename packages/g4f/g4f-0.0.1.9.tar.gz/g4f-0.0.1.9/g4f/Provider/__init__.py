d4e09f1e763fe23d986780c70a1e3cb6df2ea924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 04:12:36 +0000

    Add package g4f/0.0.1.9/g4f-0.0.1.9.tar.gz

---
+++ packages/g4f/g4f-0.0.1.9.tar.gz/g4f-0.0.1.9/g4f/Provider/__init__.py
@@ -0,0 +1,30 @@
+from . import Provider
+from .Providers import (
+    Ails,
+    You,
+    Bing,
+    Yqcloud,
+    Theb,
+    Aichat,
+    Bard,
+    Vercel,
+    Forefront,
+    Lockchat,
+    Liaobots,
+    H2o,
+    ChatgptLogin,
+    DeepAi,
+    GetGpt,
+    AItianhu,
+    EasyChat,
+    Acytoo,
+    DfeHub,
+    AiService,
+    BingHuan,
+    Wewordle,
+    ChatgptAi,
+    opchatgpts,
+    Raycast,
+)
+
+Palm = Bard
