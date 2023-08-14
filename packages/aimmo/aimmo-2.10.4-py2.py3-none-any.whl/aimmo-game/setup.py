11561cf365f14cbf75728d1cf5354ffe3fe1ee86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 04:12:38 +0000

    Add package aimmo/2.10.4/aimmo-2.10.4-py2.py3-none-any.whl

---
+++ packages/aimmo/aimmo-2.10.4-py2.py3-none-any.whl/aimmo-game/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import find_packages, setup
+
+withcoverage = os.environ.get("WITH_COVERAGE")
+
+setup(
+    name="aimmo-game",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "aiohttp==3.8.5",
+        "aiohttp-cors",
+        "aiohttp-wsgi",
+        "eventlet==0.31.0",
+        "python-socketio==5.0.3",
+        "docker==4.4.4",
+        "google-api-python-client==1.12.8",
+        "google-cloud-logging==2.2.0",
+        "grpcio==1.53.0",
+    ],
+    tests_require=[
+        "pytest~=6.2",
+        "pytest-mock",
+        "pytest-asyncio==0.14.0",  # downgraded because of this issue: https://github.com/pytest-dev/pytest-asyncio/issues/209
+        "httmock",
+        "asynctest",
+        "hypothesis",
+        "mock",
+        "pytest-aiohttp==0.3.0",
+        "aioresponses",
+    ],
+    setup_requires=["pytest-runner"],
+    test_suite="tests",
+    zip_safe=False,
+)
