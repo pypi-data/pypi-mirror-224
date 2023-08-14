6d3b4ee23f154841a5c42ffefc1dbf17219ec180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:10:35 +0000

    Add package btt/0.0.2/btt-0.0.2.tar.gz

---
+++ packages/btt/btt-0.0.2.tar.gz/btt-0.0.2/btt/main.py
@@ -0,0 +1,21 @@
+import numpy
+import torch
+
+
+def get_device():
+    return "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def get_batch_quota():
+    return numpy.inf
+
+
+def main():
+    print("hello world")
+    get_device()
+    get_batch_quota()
+
+
+if __name__ == "__main__":
+    print("v2")
+    main()
