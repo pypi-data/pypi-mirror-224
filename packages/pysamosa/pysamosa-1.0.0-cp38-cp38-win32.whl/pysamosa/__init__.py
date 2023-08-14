12e263e43827b572577ded3bff573a0507e4fe43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:09:24 +0000

    Add package pysamosa/1.0.0/pysamosa-1.0.0-cp38-cp38-win32.whl

---
+++ packages/pysamosa/pysamosa-1.0.0-cp38-cp38-win32.whl/pysamosa/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for PySAMOSA."""
+from pysamosa.version import __version__
+
+__author__ = """Florian Schlembach"""
+__email__ = "florian.schlembach@tum.de"
+__version__ = __version__
