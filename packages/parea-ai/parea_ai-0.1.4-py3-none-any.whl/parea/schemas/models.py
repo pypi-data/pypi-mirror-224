2ea00b15cf54a80f6e34913a5505f9ed9db07dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:11:12 +0000

    Add package parea-ai/0.1.4/parea_ai-0.1.4-py3-none-any.whl

---
+++ packages/parea-ai/parea_ai-0.1.4-py3-none-any.whl/parea/schemas/models.py
@@ -0,0 +1,110 @@
+from typing import Any, Optional, Union
+
+from enum import Enum
+
+from attrs import define, field, validators
+
+
+class Role(str, Enum):
+    user = "user"
+    assistant = "assistant"
+    system = "system"
+    example_user = "example_user"
+    example_assistant = "example_assistant"
+
+
+@define
+class Message:
+    content: str
+    role: Role = Role.user
+
+
+@define
+class ModelParams:
+    model: str
+    temp: float = 0.5
+    top_p: float = 1.0
+    frequency_penalty: float = 0.0
+    presence_penalty: float = 0.0
+    max_length: Optional[int] = None
+
+
+@define
+class LLMInputs:
+    model: Optional[str] = None
+    provider: Optional[str] = None
+    model_params: Optional[ModelParams] = None
+    messages: Optional[list[Message]] = None
+    functions: Optional[list[Any]] = None
+    function_call: Optional[Union[str, dict[str, str]]] = None
+
+
+@define
+class Completion:
+    trace_id: Optional[str] = None
+    trace_name: Optional[str] = None
+    llm_inputs: Optional[dict[str, Any]] = None
+    llm_configuration: LLMInputs = LLMInputs()
+    end_user_identifier: Optional[str] = None
+    deployment_id: Optional[str] = None
+    name: Optional[str] = None
+    metadata: Optional[dict] = None
+    tags: Optional[list[str]] = None
+    target: Optional[str] = None
+    cache: bool = True
+    log_omit_inputs: bool = False
+    log_omit_outputs: bool = False
+    log_omit: bool = False
+
+
+@define
+class CompletionResponse:
+    inference_id: str
+    content: str
+    latency: float
+    input_tokens: int
+    output_tokens: int
+    total_tokens: int
+    cost: float
+    model: str
+    provider: str
+    cache_hit: bool
+    status: str
+    start_timestamp: str
+    end_timestamp: str
+    trace_id: Optional[str] = None
+    error: Optional[str] = None
+
+
+@define
+class UseDeployedPrompt:
+    deployment_id: str
+    llm_inputs: Optional[dict[str, Any]] = None
+
+
+@define
+class Prompt:
+    raw_messages: list[dict[str, Any]]
+    messages: list[dict[str, Any]]
+    inputs: Optional[dict[str, Any]] = None
+
+
+@define
+class UseDeployedPromptResponse:
+    deployment_id: str
+    name: Optional[str] = None
+    functions: Optional[dict[str, Any]] = None
+    function_call: Optional[str] = None
+    prompt: Optional[Prompt] = None
+    model: Optional[str] = None
+    provider: Optional[str] = None
+    model_params: Optional[dict[str, Any]] = None
+
+
+@define
+class FeedbackRequest:
+    score: float = field(validator=[validators.ge(0), validators.le(1)])
+    trace_id: Optional[str] = None
+    inference_id: Optional[str] = None
+    name: Optional[str] = None
+    target: Optional[str] = None
