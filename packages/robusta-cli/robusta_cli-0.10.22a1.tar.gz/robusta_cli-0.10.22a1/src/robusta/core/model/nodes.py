d768a086faba752be44cfee2de1d1eabb5aa8ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:10:02 +0000

    Add package robusta-cli/0.10.22a1/robusta_cli-0.10.22a1.tar.gz

---
+++ packages/robusta-cli/robusta_cli-0.10.22a1.tar.gz/robusta_cli-0.10.22a1/src/robusta/core/model/nodes.py
@@ -0,0 +1,39 @@
+from typing import Dict
+
+from pydantic.main import BaseModel
+
+
+class NodeInfo(BaseModel):
+    name: str
+    node_creation_time: str
+    internal_ip: str
+    external_ip: str
+    taints: str
+    conditions: str
+    memory_capacity: int  # MB
+    memory_allocatable: int  # MB
+    memory_allocated: int  # MB
+    cpu_capacity: float
+    cpu_allocatable: float
+    cpu_allocated: float
+    pods_count: int
+    pods: str
+    node_info: Dict
+    deleted: bool = False
+    resource_version: int = 0
+
+    def __compare_node_info(self, other_node_info):
+        return self.node_info == other_node_info
+
+    def __eq__(self, other):
+        if not isinstance(other, NodeInfo):
+            return NotImplemented
+
+        ignored_fields = [
+            "deleted",
+            "node_creation_time",
+            "resource_version",
+        ]  # ignore node_creation_time because of dates format
+        filtered_self = {k: v for k, v in self.dict().items() if k not in ignored_fields}
+        filtered_other = {k: v for k, v in other.dict().items() if k not in ignored_fields}
+        return filtered_self == filtered_other
