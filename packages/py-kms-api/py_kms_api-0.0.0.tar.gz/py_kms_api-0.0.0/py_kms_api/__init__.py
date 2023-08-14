5e30bf4ec6d97917113d16a4fa98cdd8e270f80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:30 +0000

    Add package py-kms-api/0.0.0/py_kms_api-0.0.0.tar.gz

---
+++ packages/py-kms-api/py_kms_api-0.0.0.tar.gz/py_kms_api-0.0.0/py_kms_api/__init__.py
@@ -0,0 +1,14 @@
+"""
+# PyKMS API
+
+KMS = Key Management Service
+"""
+
+print("⚠️  Archived ⚠️")
+print("This code is no longer maintained!")
+
+from py_kms_api.definitions import start_server, stop_server
+
+__all__ = [
+    'start_server', 'stop_server'
+]
