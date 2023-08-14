6da492c04ecd1082e19f8bb8b5179c9336b03fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:32 +0000

    Add package gpt-prompt/0.0.13/gpt_prompt-0.0.13.tar.gz

---
+++ packages/gpt-prompt/gpt_prompt-0.0.13.tar.gz/gpt_prompt-0.0.13/gpt_prompt/character/relationship_coach.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/10 15:40
+# @Author  : limaoyi
+# @File    : relationship_coach.py
+# @Software: PyCharm
+# @GitHub  : https://github.com/limaoyi1/GPT-prompt
+# 关系教练 情感类
+
---
+++ packages/gpt-prompt/gpt_prompt-0.0.13.tar.gz/gpt_prompt-0.0.13/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gpt_prompt",
+    version="0.0.13",
+    author="limaoyi",
+    author_email="2420845058@qq.com",
+    description="python - pip package for a collection of high-quality GPT questions and prompts/ 包含GPT优质提问合集的pip包,支持多种语言",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/limaoyi1/GPT-prompt",
+    packages=setuptools.find_packages(),
+    # 不需要
+    # install_requires=['Pillow>=5.1.0', 'numpy==1.14.4'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'douyin_image=douyin_image:main'
+    #     ],
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
