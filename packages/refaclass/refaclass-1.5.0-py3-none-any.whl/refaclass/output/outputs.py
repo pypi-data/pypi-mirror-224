75708924fddc39fd79b1ec105169f1d35fe992e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:27 +0000

    Add package refaclass/1.5.0/refaclass-1.5.0-py3-none-any.whl

---
+++ packages/refaclass/refaclass-1.5.0-py3-none-any.whl/refaclass/output/outputs.py
@@ -0,0 +1,43 @@
+import abc
+from typing import Dict
+
+import pandas as pd
+
+from .color import ColorText
+
+
+class AbstractOutput(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def output(self, data):
+        pass
+
+
+class TerminalOutput(AbstractOutput):
+    def output(self, results: Dict[str, bool]):
+        ng_count = 0
+        for class_name, result in results.items():
+            if result["result"] == "NG":
+                ng_count += 1
+                print(class_name + " : " + ColorText(result["result"]).coloring("red"))
+                # indent for easy to read
+                print("  details: ")
+                for label, sentences in result["violation_details"].items():
+                    print("    group: " + str(label))
+                    for sentence in sentences:
+                        print("    - " + sentence[len(class_name) + 1 :])
+            else:
+                print(
+                    class_name + " : " + ColorText(result["result"]).coloring("green")
+                )
+
+        print("-----------------------------")
+        print("Total: " + str(len(results)))
+        print("OK: " + str(len(results) - ng_count))
+        print("NG: " + str(ng_count))
+
+
+class CsvOutput(AbstractOutput):
+    # TODO: detail csv output
+    def output(self, results: Dict[str, bool]):
+        data = pd.DataFrame(results.items(), columns=["class_name", "result"])
+        data.to_csv("result.csv", index=False)
