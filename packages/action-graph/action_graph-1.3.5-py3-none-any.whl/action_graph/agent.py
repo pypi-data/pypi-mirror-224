cc785b60dd3141c691570a6607d82476f4ad8002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 04:12:14 +0000

    Add package action-graph/1.3.5/action_graph-1.3.5-py3-none-any.whl

---
+++ packages/action-graph/action_graph-1.3.5-py3-none-any.whl/action_graph/agent.py
@@ -0,0 +1,273 @@
+#! /usr/bin/env python3
+
+import logging
+from time import sleep, time
+from typing import Iterable, List
+
+from action_graph.action import (Action, ActionStatus, State,
+                                 ActionTimedOutException, ActionAbortedException, 
+                                 ActionFailedException, ActionRevokedException)
+from action_graph.planner import Planner, PlanningFailedException
+
+
+class Agent:
+    """Autonomous agent to monitor system state, keep track of feasible actions, generate plans and achive desired goals"""
+
+    __planner: Planner = Planner([])
+    __abort: bool = False
+    __revoked: bool = False
+
+    def __init__(self, agent_name=None) -> None:
+        if not agent_name:
+            agent_name = self.__class__.__name__
+        self.name = agent_name
+        #
+        self.state: State = {}
+        self.__actions: List[Action] = []
+
+    def load_actions(self, actions: List[Action]):
+        """
+        Load actions list; refresh in case of any changes to the actions data.
+
+        :param actions:List[Action]: List of actions.
+        """
+
+        self.__actions = actions
+        self.__planner.update_actions(actions)
+
+    def update_state(self, state: State):
+        """
+        Updates system state with the incoming state.        
+
+        :param state:State: New state
+        """
+
+        self.state.update(state)
+
+    def abort(self):
+        """
+        Abort execution.
+        """
+
+        self.__abort = True
+
+    def revoke(self):
+        """
+        Revoke goals.
+        """
+
+        self.__revoked = True
+
+    def reset(self):
+        """
+        Resets the abort/revoke status back to False in the event abort was triggered.
+        """
+
+        self.__abort = False
+        self.__revoked = False
+
+    def is_goal_met(self, goal: State) -> bool:
+        """
+        Compares the desired goal state against the system state to check if it is already met.
+
+        :param goal:State: Desired goal state
+        :return:bool: True if goal state and current system state is same; False otherwise
+        """
+
+        if not goal:
+            return True
+
+        for k, v in goal.items():
+            if k not in self.state or self.state[k] != v:
+                return False
+
+        return True
+
+    def get_plan(self, goal: State, start_state: State = None, actions: List[Action] = None) -> List[Action]:
+        """
+        Generate an action plan for the specified goal state.
+        If no start_state is provided, the current state of the system is used. 
+
+        :param goal:State: Specify the goal state.
+        :param start_state:State=None: Specify a start state that is not the current state.
+        :param actions:List[Action]=None: List of actions that the planner can use; 
+                                          If this is not specified, the planner will use the 
+                                          previously loaded actions to plan.
+        :return:List[Action]: The plan - dictionary of actions and their expected outcomes. 
+        """
+
+        if not start_state:
+            start_state = self.state
+
+        if actions:
+            self.__planner.update_actions(actions)
+
+        try:
+            return self.__planner.generate_plan(goal, start_state)
+            #
+        except PlanningFailedException as pfx:
+            logging.error(f"PLANNING FAILED! {pfx}")
+            return []
+
+    def execute_plan(self, plan: List[Action]):
+        """
+        Execute a previously generated plan.
+
+        :param plan:List[Action]: Dictionary of actions and their expected outcomes (State)
+        """
+
+        for action in plan:
+            try:
+                self.execute_action(action)
+
+            except Exception as _ex:
+                logging.error(f"{_ex}")
+                raise
+
+        logging.info(f"EXECUTION SUCCEDED!")
+
+    def plan_and_execute(self, goal: State, verbose: bool = False) -> Iterable:
+        """
+        Creates a plan to satisfy the goal state; executes it action-by-action; re-evaluates the plan at each step;
+
+        :param goal:State: Desired goal state
+        :param verbose:bool: if True, prints formatted plan to console at each step
+        """
+
+        blacklisted_actions: List[str] = []
+
+        # state might have changed since the last step was executed
+        while not self.is_goal_met(goal):
+
+            try:
+                # (re)generate the plan
+                plan: List[Action] = self.__planner.generate_plan(goal, self.state, blacklisted_actions)
+                # print formatted plan to console
+                if verbose:
+                    self.print_plan_to_console(plan)
+
+                yield plan  # yields plan before execution
+
+                # execute one plan step at a time
+                first_action = plan[0]
+                self.execute_action(first_action)
+                #
+                # if the latest executed action has the same effect as any of the blacklisted actions,
+                # then it is prudent(?) to remove such a blacklisted action
+                ba: List[Action] = [a for a in self.__actions if str(a) in blacklisted_actions]
+                for action in ba:
+                    if set(first_action.effects.keys()) <= set(action.effects.keys()):
+                        blacklisted_actions.remove(str(action))
+
+            except ActionFailedException as ex_fail:
+                logging.error(f"{ex_fail} / ATTEMPTING ALTERNATIVE PLAN")
+                if str(first_action) not in blacklisted_actions:
+                    blacklisted_actions.append(str(first_action))
+                continue
+
+            except ActionRevokedException as _ex_revoked:
+                logging.info(f"{_ex_revoked} / STOPPING.")
+                self.__revoked = False  # reset revoked status
+                break
+
+            except Exception as _ex:
+                logging.error(f"{_ex}")
+                raise
+
+        logging.info(f"EXECUTION SUCCEDED!")
+
+    def print_plan_to_console(self, plan: List[Action]):
+        if plan:
+            plan_str = '\nPLAN:\n'
+            for ix, action in enumerate(plan):
+                plan_str += str(ix+1).zfill(2) + ' ' + str(action) + (25-len(str(action)))*'.' + str(action.effects) + '\n'
+            print(plan_str)
+
+    def execute_action(self, action: Action):
+        # Check for abort status
+        if self.__abort:
+            # logging.error(f'ACTION: {action} : EXECUTION ABORTED BEFORE START !!')
+            action.on_aborted(action.effects)
+            raise ActionAbortedException(f'ACTION: {action} FAILED. ABORTED STATE IS ACTIVE!!')
+
+        # Check for revoked status
+        if self.__revoked:
+            # logging.error(f'ACTION: {action} : EXECUTION REVOKED BEFORE START !!')
+            action.on_revoked(action.effects)
+            raise ActionRevokedException(f'ACTION/GOALS REVOKED WHILE AT ACTION: {action}')
+
+        # Check runtime precondition
+        if not action.check_runtime_precondition(action.effects):
+            raise ActionFailedException(f'ACTION: {action} RUNTIME PRECONDITION CHECK FAILED!!.')
+
+        # Execute the plan step
+        action._execute(action.effects)
+        # action.execute is an async process inside _execute,
+
+        if action.allow_async:
+            # if this is an async action; just apply the effects and return
+            action.apply_effects(action.effects, self.state)
+            return
+
+        # monitor the status; wait until execution is complete
+        time0 = time()
+        while action.is_running():
+            if self.__abort:
+                # if an abort was signalled
+                # logging.critical(f'ACTION: {action} : EXECUTION ABORTED!!')
+                action.status = ActionStatus.ABORTED
+                break
+            if self.__revoked:
+                # if an revoke was signalled
+                # logging.critical(f'ACTION: {action} : GOALS REVOKED. STOPPING!!')
+                action.status = ActionStatus.REVOKED
+                break
+            if time()-time0 > action.timeout:
+                # Action timeout exceeded
+                raise ActionTimedOutException(f'ACTION: {action} : TIMED OUT!!')
+            if not action.status == ActionStatus.RUNNING:
+                # thread is alive but the status has changed
+                break  # so move on
+            sleep(0.05)  # throttle loop
+            # logging.debug(f'Action: {str(action)} is running...')
+
+        # Execution completed but with RUNNING Status
+        if action.status == ActionStatus.RUNNING:
+            # the user forgot to set the status; or something bad happened;
+            # let's treat this as FAILURE!
+            action.status = ActionStatus.FAILURE
+            logging.warning(f'ACTION: {action} STATUS UNKNOWN; ASSUMING FAILURE!!')
+
+        # Execution completed with NEUTRAL Status
+        if action.status == ActionStatus.NEUTRAL:
+            # this is when action completes with no change to the state
+            # logging.debug(f'ACTION: {action} Action completed with neutral state.')
+            action.on_neutral(action.effects)  # ignore effects
+
+        # Execution completed with SUCCESS Status
+        if action.status == ActionStatus.SUCCESS:
+            # Action executed without errors;
+            action.apply_effects(action.effects, self.state)
+            # logging.debug(f'ACTION: {action} Action succeded.')
+            action.on_success(action.effects)
+
+        # Execution failed
+        if action.status == ActionStatus.FAILURE:
+            action.on_failure(action.effects)
+            # Stop execution as action failed
+            raise ActionFailedException(f'ACTION: {action} FAILED!')
+
+        # Execution aborted
+        if action.status == ActionStatus.ABORTED:
+            action.on_aborted(action.effects)
+            # Stop execution as action aborted
+            raise ActionAbortedException(f'ACTION: {action} ABORTED!')
+
+        # Goals revoked
+        if action.status == ActionStatus.REVOKED:
+            action.on_revoked(action.effects)
+            # Stop execution as goals revoked
+            raise ActionRevokedException(f'ACTION: {action} REVOKED!')
+
+        # Any clean up needed after execution e.g. updating system states
+        action.on_exit(action.effects)
---
+++ packages/action-graph/action_graph-1.3.5-py3-none-any.whl/action_graph/planner.py
@@ -0,0 +1,100 @@
+#! /usr/bin/env python3
+
+import sys
+from collections import defaultdict
+from typing import Any, Dict, List, Tuple
+
+from action_graph.action import Action, State, ImpossibleAction
+
+
+class PlanningFailedException(Exception):
+    pass
+
+
+class Planner():
+    """Search and determine a plan (sequence of actions) that satifies a desired goal state"""
+
+    def __init__(self, actions: List[Action]) -> None:
+        self.update_actions(actions)
+
+    def update_actions(self, actions: List[Action]):
+        """
+        The refereshes/reloads the list of Actions available to the Planner.
+
+        :param actions:List[Action]: List of actions (instances of Action class)
+        """
+
+        self._action_lookup: defaultdict = self.__create_action_lookup(actions)
+
+    def generate_plan(self, target_state: State, start_state: State, avoid_actions: List[Action] = None) -> List[Action]:
+        """
+        Find and return an optimal sequence of actions (the plan) that will 
+        lead from the start state to the target state.
+
+        :param target_state:State: Desired goal (target) state
+        :param start_state:State: Current/start state of the system
+        :return:List[Action]: List of actions updated with their expected outcomes (effects)
+        """
+
+        if len(target_state.items()) > 1:
+            raise PlanningFailedException(f'target_state [{target_state}] should be a single state')
+        tk, tv = list(target_state.items())[0]
+        # in case target state value is a reference to another state variable
+        tv = self.__parse_references(tv, start_state, '@')
+        # check if the target state is already satisfied
+        if (tk, tv) in list(start_state.items()):
+            return []   # goal already met, move on
+        #
+        # find action(s) that satisfy the state current effect-item
+        probable_actions: List[Action] = self._action_lookup[(tk, tv)]
+        if not probable_actions:  # if no actions are found, try with templated actions
+            probable_actions = self._action_lookup[(tk, Ellipsis)]
+        if avoid_actions:  # actions we do not want to consider for planning
+            probable_actions = [a for a in probable_actions if str(a) not in avoid_actions]
+        if not probable_actions:
+            return [ImpossibleAction(effects={tk: tv})]
+
+        chosen_path: List[Action] = []
+        for p_action in probable_actions:  # explore each available action...
+            action = p_action.__copy__()
+            if action.effects[tk] is Ellipsis:
+                action.effects[tk] = tv  # apply variable effects
+            #
+            action_path: List[Action] = []
+            for pk, pv in action.preconditions.items():  # for each pre-condition ...
+                try:  # choose the shortest feasible path
+                    pv = self.__parse_references(pv, action.effects, '$')
+                    action_path.extend(self.generate_plan({pk: pv}, start_state, avoid_actions))  # merge the actions
+                except RecursionError:  # watch out for cyclic references
+                    raise PlanningFailedException(f'Found cyclic references! {pk}:{pv}')
+            # include the current action;  remove duplicates; keep the order intact
+            action_path = self.__make_unique(action_path + [action])
+            #
+            if not chosen_path:  # if no other path is available...
+                chosen_path = action_path  # use the current path
+                continue
+            # if alternative paths exist, use the one with the lowest cost
+            if sum(a.cost for a in action_path) < sum(a.cost for a in chosen_path):
+                chosen_path = action_path
+
+        # check if path is feasible; path cost should be < infinite cost
+        impossible_actions = [a for a in chosen_path if a.cost >= sys.float_info.max]
+        for ia in impossible_actions:
+            raise PlanningFailedException(f'No action available to satisfy: {ia.effects}')
+        return chosen_path
+
+    def __create_action_lookup(self, actions: List[Action]) -> Dict[Tuple[Any, Any], List[Action]]:
+        action_lookup: Dict[Tuple[Any, Any], List[Action]] = defaultdict(list)
+        for action in actions:
+            for k, v in action.effects.items():
+                action_lookup[(k, v)].append(action)
+        return action_lookup
+
+    def __parse_references(self, ref: Any, state: State, prefix: str) -> Any:
+        while ref and isinstance(ref, str) and ref[0] == prefix and ref[1:] in state:
+            ref = state[ref[1:]]
+        return ref
+
+    def __make_unique(self, path):
+        unique = set()
+        return [x for x in path if x not in unique and not unique.add(x)]
