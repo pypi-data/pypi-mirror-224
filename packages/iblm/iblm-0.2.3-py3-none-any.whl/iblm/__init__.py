d373bfdd1d5d45b3a920f41347771db350e5840c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 03:11:13 +0000

    Add package iblm/0.2.3/iblm-0.2.3-py3-none-any.whl

---
+++ packages/iblm/iblm-0.2.3-py3-none-any.whl/iblm/__init__.py
@@ -0,0 +1,8 @@
+#from ._version import version
+
+from .iblmodel.iblm_classifier import IBLMClassifier
+from .iblmodel.iblm_regressor import IBLMRegressor
+
+from .iclmodel.icl_classifier import ICLClassifier
+
+from .ibboosting.ibboosting_classifier import IBBoostingClassifier
