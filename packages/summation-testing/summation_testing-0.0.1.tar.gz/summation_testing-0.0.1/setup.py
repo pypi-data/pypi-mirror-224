5c8a4ecedbb843858b3dfa7842f9df3eff60f8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-14 14:09:20 +0000

    Add package summation-testing/0.0.1/summation_testing-0.0.1.tar.gz

---
+++ packages/summation-testing/summation_testing-0.0.1.tar.gz/summation_testing-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basis sum function'
+LONG_DESCRIPTION = 'a basic sum function'
+
+setup(
+    name="summation_testing",
+    version=VERSION,
+    author="Mohd Sabir",
+    author_email="sabir9202@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    requires=[]
+)
