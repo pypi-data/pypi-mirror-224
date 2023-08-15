825b6313fbfbbb7d789b97a2a1498e6cd3e10428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:23 +0000

    Add package gpt4docs/0.1.1/gpt4docs-0.1.1-py3-none-any.whl

---
+++ packages/gpt4docs/gpt4docs-0.1.1-py3-none-any.whl/gpt4docs/modules/managers/ProjectManager.py
@@ -0,0 +1,19 @@
+from gpt4docs.modules.directory import Project
+from typing import Dict
+from pathlib import Path
+
+
+class ProjectManager:
+    def __init__(self, project_path: str):
+        self.project = Project(Path(project_path))
+
+    def update_docstrings(self, all_docstrings: Dict[str, Dict]):
+        for file_path, definitions in all_docstrings.items():
+            for def_ in definitions:
+                self.project.files[str(file_path.name)].set_definition(def_)
+
+    def save(self):
+        self.project.save()
+
+    def get_files(self):
+        return self.project.files.values()
