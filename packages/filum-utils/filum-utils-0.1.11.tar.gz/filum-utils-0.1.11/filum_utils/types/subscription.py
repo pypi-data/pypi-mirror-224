0e34706afec8d2d33d5e135bc32b088b05f1c8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:02 +0000

    Add package filum-utils/0.1.11/filum-utils-0.1.11.tar.gz

---
+++ packages/filum-utils/filum-utils-0.1.11.tar.gz/filum-utils-0.1.11/filum_utils/types/subscription.py
@@ -0,0 +1,11 @@
+from typing import TypedDict, Optional, Dict, Any
+
+
+class SubscriptionData(TypedDict, total=False):
+    input_data: Optional[Dict[str, Any]]
+    last_current_index: Optional[int]
+
+
+class Subscription(TypedDict, total=False):
+    id: str
+    data: Optional[SubscriptionData]
