5571728a4726956a712ad6a040ef26b8904ce78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:40 +0000

    Add package lupin3/0.0.20/lupin3-0.0.20.tar.gz

---
+++ packages/lupin3/lupin3-0.0.20.tar.gz/lupin3-0.0.20/lupin3/__init__.py
@@ -0,0 +1,10 @@
+from .func import str2date, date2str, mkdir, dateminus, dateplus, showeveryday, splitlist, cartesian, \
+    find_pd, chineseloto, list_inter, list_union, list_dif
+from .plt import Stackedbar, multiplebar, pieplt
+from .ml import  MachineLearningClassify
+
+
+# from .autoinstall import install_package
+
+
+
