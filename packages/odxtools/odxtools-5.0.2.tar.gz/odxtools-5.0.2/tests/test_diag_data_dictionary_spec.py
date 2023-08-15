b94feff1df3072be46fa2bfb5002f0f251496a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:53 +0000

    Add package odxtools/5.0.2/odxtools-5.0.2.tar.gz

---
+++ packages/odxtools/odxtools-5.0.2.tar.gz/odxtools-5.0.2/tests/test_diag_data_dictionary_spec.py
@@ -0,0 +1,295 @@
+# SPDX-License-Identifier: MIT
+import unittest
+
+from examples import somersaultecu
+from odxtools.compumethods.identicalcompumethod import IdenticalCompuMethod
+from odxtools.dataobjectproperty import DataObjectProperty
+from odxtools.diagdatadictionaryspec import DiagDataDictionarySpec
+from odxtools.diagnostictroublecode import DiagnosticTroubleCode
+from odxtools.dtcdop import DtcDop
+from odxtools.environmentdata import EnvironmentData
+from odxtools.environmentdatadescription import EnvironmentDataDescription
+from odxtools.multiplexer import Multiplexer
+from odxtools.multiplexercase import MultiplexerCase
+from odxtools.multiplexerdefaultcase import MultiplexerDefaultCase
+from odxtools.multiplexerswitchkey import MultiplexerSwitchKey
+from odxtools.nameditemlist import NamedItemList
+from odxtools.odxlink import OdxDocFragment, OdxLinkDatabase, OdxLinkId, OdxLinkRef
+from odxtools.odxtypes import DataType
+from odxtools.parameters.physicalconstantparameter import PhysicalConstantParameter
+from odxtools.parameters.tablekeyparameter import TableKeyParameter
+from odxtools.parameters.valueparameter import ValueParameter
+from odxtools.physicaltype import PhysicalType
+from odxtools.standardlengthtype import StandardLengthType
+from odxtools.table import Table
+from odxtools.tablerow import TableRow
+from odxtools.utils import short_name_as_id
+
+# the document fragment which is used throughout the test
+doc_frags = [OdxDocFragment("UnitTest", "unit_test_doc")]
+
+
+class TestDiagDataDictionarySpec(unittest.TestCase):
+
+    def test_initialization(self):
+        uint_type = StandardLengthType(
+            base_data_type=DataType.A_UINT32,
+            base_type_encoding=None,
+            bit_length=8,
+            bit_mask=None,
+            is_highlow_byte_order_raw=None,
+            is_condensed_raw=None,
+        )
+        ident_compu_method = IdenticalCompuMethod(
+            internal_type=DataType.A_UINT32, physical_type=DataType.A_UINT32)
+
+        dtc_dop = DtcDop(
+            odx_id=OdxLinkId("DOP.dtc_dop", doc_frags),
+            short_name="dtc_dop",
+            long_name=None,
+            description=None,
+            is_visible_raw=None,
+            diag_coded_type=uint_type,
+            physical_type=PhysicalType(DataType.A_UINT32, display_radix=None, precision=None),
+            linked_dtc_dop_refs=[],
+            compu_method=ident_compu_method,
+            unit_ref=None,
+            dtcs_raw=[
+                DiagnosticTroubleCode(
+                    odx_id=OdxLinkId("DOP.dtc_dop.DTC.X10", doc_frags),
+                    short_name="X10",
+                    trouble_code=0x10,
+                    text="Something exploded.",
+                    display_trouble_code="X10",
+                    level=None,
+                    is_temporary_raw=None,
+                    sdgs=[],
+                )
+            ],
+            sdgs=[],
+        )
+
+        dop_1 = DataObjectProperty(
+            odx_id=OdxLinkId("DOP.the_dop", doc_frags),
+            short_name="the_dop",
+            long_name=None,
+            description=None,
+            is_visible_raw=None,
+            diag_coded_type=uint_type,
+            physical_type=PhysicalType(DataType.A_UINT32, display_radix=None, precision=None),
+            compu_method=ident_compu_method,
+            unit_ref=None,
+            sdgs=[],
+        )
+
+        dop_2 = DataObjectProperty(
+            odx_id=OdxLinkId("DOP.another_dop", doc_frags),
+            short_name="another_dop",
+            long_name=None,
+            description=None,
+            is_visible_raw=None,
+            diag_coded_type=uint_type,
+            physical_type=PhysicalType(DataType.A_UINT32, display_radix=None, precision=None),
+            compu_method=ident_compu_method,
+            unit_ref=None,
+            sdgs=[],
+        )
+
+        flip_quality_id = OdxLinkId("somersault.table.flip_quality", doc_frags)
+        flip_quality_ref = OdxLinkRef.from_id(flip_quality_id)
+        table = Table(
+            odx_id=flip_quality_id,
+            short_name="flip_quality",
+            long_name="Flip Quality",
+            description=None,
+            key_label=None,
+            struct_label=None,
+            admin_data=None,
+            key_dop_ref=None,
+            semantic=None,
+            table_rows_raw=[
+                TableRow(
+                    odx_id=OdxLinkId("somersault.table.flip_quality.average", doc_frags),
+                    table_ref=flip_quality_ref,
+                    short_name="average",
+                    long_name="Average",
+                    description=None,
+                    semantic=None,
+                    dop_ref=None,
+                    dop_snref=None,
+                    key_raw="3",
+                    structure_ref=None,
+                    structure_snref=None,
+                    sdgs=[],
+                ),
+                TableRow(
+                    odx_id=OdxLinkId("somersault.table.flip_quality.good", doc_frags),
+                    table_ref=flip_quality_ref,
+                    short_name="good",
+                    long_name="Good",
+                    description=None,
+                    semantic=None,
+                    dop_ref=None,
+                    dop_snref=None,
+                    key_raw="5",
+                    structure_ref=None,
+                    structure_snref=None,
+                    sdgs=[],
+                ),
+                TableRow(
+                    odx_id=OdxLinkId("somersault.table.flip_quality.best", doc_frags),
+                    table_ref=flip_quality_ref,
+                    short_name="best",
+                    long_name="Best",
+                    description=None,
+                    semantic=None,
+                    dop_ref=None,
+                    dop_snref=None,
+                    key_raw="10",
+                    structure_ref=None,
+                    structure_snref=None,
+                    sdgs=[],
+                ),
+            ],
+            sdgs=[],
+        )
+
+        env_data = EnvironmentData(
+            odx_id=OdxLinkId("somersault.env_data.flip_env_data", doc_frags),
+            short_name="flip_env_data",
+            long_name="Flip Env Data",
+            description=None,
+            sdgs=[],
+            is_visible_raw=None,
+            parameters=[
+                ValueParameter(
+                    short_name="flip_speed",
+                    long_name="Flip Speed",
+                    description=None,
+                    semantic="DATA",
+                    byte_position=0,
+                    dop_ref=OdxLinkRef("somersault.DOP.float", doc_frags),
+                    dop_snref=None,
+                    physical_default_value_raw=None,
+                    bit_position=None,
+                    sdgs=[],
+                ),
+                PhysicalConstantParameter(
+                    short_name="flip_direction",
+                    long_name="Flip Direction",
+                    description=None,
+                    semantic="DATA",
+                    byte_position=1,
+                    physical_constant_value_raw="1",
+                    dop_ref=OdxLinkRef("somersault.DOP.boolean", doc_frags),
+                    dop_snref=None,
+                    bit_position=None,
+                    sdgs=[],
+                ),
+                TableKeyParameter(
+                    short_name="flip_quality",
+                    long_name=None,
+                    description=None,
+                    sdgs=[],
+                    semantic="DATA",
+                    byte_position=2,
+                    bit_position=None,
+                    odx_id=OdxLinkId("somersault.env_data.flip_quality", doc_frags),
+                    table_ref=None,
+                    table_row_ref=None,
+                    table_snref="flip_quality",  # cf. somersaultecu
+                    table_row_snref="good",
+                ),
+            ],
+            byte_size=None,
+            dtc_values=[],
+        )
+
+        env_data_desc = EnvironmentDataDescription(
+            odx_id=OdxLinkId("somersault.env_data_desc.flip_env_data_desc", doc_frags),
+            short_name="flip_env_data_desc",
+            long_name="Flip Env Data Desc",
+            description=None,
+            sdgs=[],
+            is_visible_raw=None,
+            param_snref="flip_speed",
+            param_snpathref=None,
+            env_datas=[],
+            env_data_refs=[OdxLinkRef.from_id(env_data.odx_id)],
+        )
+
+        mux = Multiplexer(
+            odx_id=OdxLinkId("somersault.multiplexer.flip_preference", doc_frags),
+            short_name="flip_preference",
+            long_name="Flip Preference",
+            description=None,
+            sdgs=[],
+            is_visible_raw=None,
+            byte_position=0,
+            switch_key=MultiplexerSwitchKey(
+                byte_position=0,
+                bit_position=0,
+                dop_ref=OdxLinkRef("somersault.DOP.boolean", doc_frags),
+            ),
+            default_case=MultiplexerDefaultCase(
+                short_name="default_case",
+                long_name="Default Case",
+                structure_ref="structure_ref",
+            ),
+            cases=[
+                MultiplexerCase(
+                    short_name="forward_flip",
+                    long_name="Forward Flip",
+                    lower_limit="1",
+                    upper_limit="3",
+                    structure_ref="structure_ref",
+                ),
+                MultiplexerCase(
+                    short_name="backward_flip",
+                    long_name="Backward Flip",
+                    lower_limit="1",
+                    upper_limit="3",
+                    structure_ref="structure_ref",
+                ),
+            ],
+        )
+
+        ddds = DiagDataDictionarySpec(
+            dtc_dops=[dtc_dop],
+            data_object_props=NamedItemList(short_name_as_id, [dop_1, dop_2]),
+            tables=NamedItemList(short_name_as_id, [table]),
+            env_data_descs=NamedItemList(short_name_as_id, [env_data_desc]),
+            env_datas=NamedItemList(short_name_as_id, [env_data]),
+            muxs=NamedItemList(short_name_as_id, [mux]),
+            structures=NamedItemList(short_name_as_id),
+            end_of_pdu_fields=NamedItemList(short_name_as_id),
+            unit_spec=None,
+            sdgs=[],
+        )
+
+        # test the short name resolution of TableKeyParameter.
+        odxlinks = OdxLinkDatabase()
+        ecu = somersaultecu.database.ecus.somersault_lazy
+        ecu.diag_layer_raw.diag_data_dictionary_spec = ddds
+        odxlinks.update(ecu._build_odxlinks())
+        table._resolve_odxlinks(odxlinks)
+
+        self.assertEqual(ddds.dtc_dops[0], dtc_dop)
+        self.assertEqual(ddds.data_object_props[0], dop_1)
+        self.assertEqual(ddds.data_object_props.another_dop, dop_2)
+        self.assertEqual(ddds.tables[0], table)
+        self.assertEqual(ddds.env_data_descs[0], env_data_desc)
+        self.assertEqual(ddds.env_datas[0], env_data)
+        self.assertEqual(ddds.muxs[0], mux)
+
+        self.assertEqual(len(ddds.structures), 0)
+        self.assertEqual(len(ddds.end_of_pdu_fields), 0)
+
+        self.assertEqual({x.short_name
+                          for x in ddds.all_data_object_properties},
+                         {x.short_name
+                          for x in (dtc_dop, dop_1, dop_2, table)})
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/odxtools/odxtools-5.0.2.tar.gz/odxtools-5.0.2/tests/test_odxtools.py
@@ -0,0 +1,191 @@
+# SPDX-License-Identifier: MIT
+import unittest
+
+import odxtools
+from odxtools.exceptions import OdxError
+from odxtools.load_pdx_file import load_pdx_file
+from odxtools.nameditemlist import NamedItemList
+from odxtools.odxlink import OdxLinkRef
+
+odxdb = load_pdx_file("./examples/somersault.pdx")
+
+# use the diag layer container's document fragments as the default for
+# resolving references
+container_doc_frags = odxdb.diag_layer_containers.somersault.odx_id.doc_fragments
+
+
+class TestStrictMode(unittest.TestCase):
+
+    def test_strict_mode(self):
+        # by default, we must be in strict mode
+        self.assertTrue(odxtools.exceptions.strict_mode)
+
+        # in strict mode, odxraise() must raise OdxError ...
+        with self.assertRaises(OdxError):
+            odxtools.exceptions.odxraise()
+        # ... odxassert() must raise OdxError for failed
+        # assertations ...
+        odxtools.exceptions.odxassert(True)  # nothing happens
+        with self.assertRaises(OdxError):
+            odxtools.exceptions.odxassert(False)
+        # ... and odxrequire() raises OdxError if passed `None` and
+        # passes through everything else.
+        with self.assertRaises(OdxError):
+            odxtools.exceptions.odxrequire(None)
+        self.assertEqual(odxtools.exceptions.odxrequire(123), 123)
+
+        # change to non-strict mode
+        odxtools.exceptions.strict_mode = False
+
+        # in non-strict mode none of the above functions raises anything
+        odxtools.exceptions.odxraise()
+        odxtools.exceptions.odxassert(True)  # nothing happens
+        odxtools.exceptions.odxassert(False)
+        odxtools.exceptions.odxrequire(None)
+        self.assertEqual(odxtools.exceptions.odxrequire(123), 123)
+
+        # go back to strict mode
+        odxtools.exceptions.strict_mode = True
+
+
+class TestDataObjectProperty(unittest.TestCase):
+
+    def test_bit_length(self):
+        self.dop = odxdb.odxlinks.resolve(
+            OdxLinkRef("somersault.DOP.num_flips", container_doc_frags))
+        self.assertEqual(self.dop.bit_length, 8)
+
+    def test_convert_physical_to_internal(self):
+        self.dop = odxdb.odxlinks.resolve(OdxLinkRef("somersault.DOP.boolean", container_doc_frags))
+        self.assertEqual(self.dop.convert_physical_to_internal("false"), 0)
+        self.assertEqual(self.dop.convert_physical_to_internal("true"), 1)
+
+
+class TestComposeUDS(unittest.TestCase):
+
+    def test_encode_with_coded_const(self):
+        request = odxdb.odxlinks.resolve(
+            OdxLinkRef("somersault.RQ.tester_present", container_doc_frags))
+        self.assertEqual(bytes(request.encode()), 0x3E00.to_bytes(2, "big"))
+
+    def test_encode_with_texttable(self):
+        request = odxdb.odxlinks.resolve(
+            OdxLinkRef("somersault.RQ.set_operation_params", container_doc_frags))
+        self.assertEqual(
+            bytes(request.encode(**{"use_fire_ring": "true"})), 0xBD01.to_bytes(2, "big"))
+        self.assertEqual(bytes(request.encode(use_fire_ring="false")), 0xBD00.to_bytes(2, "big"))
+
+    def test_encode_response_with_matching_request_param_and_structure(self):
+        request = odxdb.odxlinks.resolve(
+            OdxLinkRef("somersault.RQ.do_forward_flips", container_doc_frags))
+        response = odxdb.odxlinks.resolve(
+            OdxLinkRef("somersault.PR.happy_forward", container_doc_frags))
+
+        coded_request = request.encode(forward_soberness_check=0x12, num_flips=12)
+        coded_response = response.encode(yeha_level=3, coded_request=coded_request)
+        self.assertEqual(bytes(coded_response), 0xFA0003.to_bytes(3, "big"))
+
+
+class TestNamedItemList(unittest.TestCase):
+
+    def test_NamedItemList(self):
+        foo = NamedItemList(lambda x: x[0], [("hello", 0), ("world", 1)])
+        self.assertEqual(foo.hello, ("hello", 0))
+        self.assertEqual(foo[0], ("hello", 0))
+        self.assertEqual(foo[1], ("world", 1))
+        self.assertEqual(foo[:1], [("hello", 0)])
+        self.assertEqual(foo[-1:], [("world", 1)])
+        with self.assertRaises(KeyError):
+            foo[2]
+        self.assertEqual(foo["hello"], ("hello", 0))
+        self.assertEqual(foo["world"], ("world", 1))
+        self.assertEqual(foo.hello, ("hello", 0))
+        self.assertEqual(foo.world, ("world", 1))
+
+        foo.append(("hello", 2))
+        self.assertEqual(foo[2], ("hello", 2))
+        self.assertEqual(foo["hello"], ("hello", 0))
+        self.assertEqual(foo["hello_2"], ("hello", 2))
+        self.assertEqual(foo.hello, ("hello", 0))
+        self.assertEqual(foo.hello_2, ("hello", 2))
+
+        # try to append an item that cannot be mapped to a name
+        with self.assertRaises(AttributeError):
+            foo.append((0, 3))
+
+        # add a keyword identifier
+        foo.append(("as", 3))
+        self.assertEqual(foo[3], ("as", 3))
+        self.assertEqual(foo["as_"], ("as", 3))
+        self.assertEqual(foo.as_, ("as", 3))
+
+        # add an object which's name conflicts with a method of the class
+        foo.append(("sort", 4))
+        self.assertEqual(foo[4], ("sort", 4))
+        self.assertEqual(foo["sort_2"], ("sort", 4))
+        self.assertEqual(foo.sort_2, ("sort", 4))
+
+        # test the get() function
+        self.assertEqual(foo.get(0), ("hello", 0))
+        self.assertEqual(foo.get(1234), None)
+        self.assertEqual(foo.get(1234, 5678), 5678)
+
+        self.assertEqual(foo.get("hello"), ("hello", 0))
+        self.assertEqual(foo.get("dunno"), None)
+        self.assertEqual(foo.get("dunno", "woho!"), "woho!")
+
+        # ensure that we can iterate over NamedItemList objects
+        for i, x in enumerate(foo):
+            self.assertTrue(x in foo)
+        self.assertEqual(i, len(foo) - 1)
+
+        # make sure that the keys(), values() and items() exist
+        self.assertEqual(set(foo.keys()), {"hello", "world", "hello_2", "as_", "sort_2"})
+        self.assertEqual(len(foo.items()), len(foo))
+        self.assertEqual(len(foo.values()), len(foo))
+
+
+class TestNavigation(unittest.TestCase):
+
+    def test_find_ecu_by_name(self):
+        with self.assertRaises(KeyError):
+            ecu = odxdb.ecus["somersault_crazy"]
+        with self.assertRaises(KeyError):
+            ecu = odxdb.ecus[len(odxdb.ecus) + 10]
+
+        ecu = odxdb.ecus.get("somersault_crazy")
+        self.assertEqual(ecu, None)
+
+        ecu = odxdb.ecus.get(len(odxdb.ecus) + 10)
+        self.assertEqual(ecu, None)
+
+        # make sure that NamedItemLists support slicing
+        ecus = odxdb.ecus[-2:]
+        self.assertEqual(len(ecus), 2)
+        self.assertEqual(ecus[0].odx_id.local_id, "somersault_lazy")
+        self.assertEqual(ecus[1].odx_id.local_id, "somersault_assiduous")
+
+        ecu = odxdb.ecus["somersault_lazy"]
+        self.assertEqual(ecu.odx_id.local_id, "somersault_lazy")
+
+        ecu = odxdb.ecus[0]
+        self.assertEqual(ecu.odx_id.local_id, "somersault_lazy")
+
+    def test_find_service_by_name(self):
+        ecu = odxdb.ecus["somersault_lazy"]
+
+        service_names = [s.short_name for s in ecu.services]
+        self.assertIn("session_start", service_names)
+        self.assertIn("session_stop", service_names)
+        self.assertIn("tester_present", service_names)
+        self.assertIn("do_forward_flips", service_names)
+        self.assertNotIn("do_backward_flips", service_names)
+        self.assertIn("report_status", service_names)
+
+        service = ecu.services.session_start
+        self.assertEqual(service.odx_id.local_id, "somersault.service.session_start")
+        self.assertEqual(service.semantic, "SESSION")
+
+
+if __name__ == "__main__":
+    unittest.main()
