3f9dfce8f43bdd3abd01349d229935879091e45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:33 +0000

    Add package odoo14-addon-ssi-elearning/14.0.1.0.2.dev1/odoo14_addon_ssi_elearning-14.0.1.0.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-elearning/odoo14_addon_ssi_elearning-14.0.1.0.2.dev1-py3-none-any.whl/odoo/addons/ssi_elearning/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "E-Learning Extension",
+    "version": "14.0.1.0.1",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "website_slides",
+    ],
+    "data": [
+        "views/website_slides_templates_course.xml",
+    ],
+    "demo": [],
+}
