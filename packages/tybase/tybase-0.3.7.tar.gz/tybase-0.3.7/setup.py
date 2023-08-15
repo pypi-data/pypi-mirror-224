4210cdd55e261316d4e31a8db70aabf96d48f354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:02 +0000

    Add package tybase/0.3.7/tybase-0.3.7.tar.gz

---
+++ packages/tybase/tybase-0.3.7.tar.gz/tybase-0.3.7/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tybase',
+    version='0.3.7',
+    include_package_data=True,
+    description='split_video_to_jpg 新增的这个方法,可以将视频拆分成图片帧,并且可以指定图片的质量,默认是15',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',  # 版本描述
+    author='Tuya',
+    author_email='353335447@qq.com',
+    url='https://github.com/yourusername/your_package',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        "opencv-python",
+        "loguru",
+        "openpyxl",
+        'requests',
+        "python-dotenv",
+        "retrying",
+        "pymysql",
+        "mysql-connector-python",
+        "sqlalchemy",
+        "pandas",
+        "langchain",
+        "openai",
+        'python-dotenv',
+        "oss2"
+        ""
+
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
