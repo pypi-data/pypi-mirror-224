95101c5476066640d6a5458bb075eeb431b538b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:20 +0000

    Add package naruno-tests/0.60.5/naruno_tests-0.60.5.tar.gz

---
+++ packages/naruno-tests/naruno_tests-0.60.5.tar.gz/naruno_tests-0.60.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from setuptools import setup
+
+setup(
+    name="naruno_tests",
+    version="0.60.5",
+    description="""This is a tool for tests on Naruno""",
+    url="https://docs.naruno.org/",
+    author="Naruno Developers",
+    author_email="onur.atakan.ulusoy@naruno.org",
+    license="MPL-2.0",
+    install_requires="""
+requests==2.28.2
+pytest==7.4.0
+pytest_profiling==1.7.0
+speed_calculator==0.4.1
+naruno_api==0.60.5
+""",
+    python_requires=">=3.8",
+    zip_safe=False,
+)
