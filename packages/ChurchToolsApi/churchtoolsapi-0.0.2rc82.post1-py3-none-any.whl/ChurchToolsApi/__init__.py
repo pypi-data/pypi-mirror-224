c6545904f7c92ae97cce05d53d4df981dc63fda2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:35 +0000

    Add package ChurchToolsApi/0.0.2rc82.post1/churchtoolsapi-0.0.2rc82.post1-py3-none-any.whl

---
+++ packages/ChurchToolsApi/churchtoolsapi-0.0.2rc82.post1-py3-none-any.whl/ChurchToolsApi/__init__.py
@@ -0,0 +1,8 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Felix Kotschenreuther. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Init file for the church_tools_api package."""
+from __future__ import annotations
+
+__version__ = "0.0.2""-rc82-post1"
