cecd989fdf49e42c68f8e07f76cdb7285f9a9ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:46 +0000

    Add package wiliot-tools/4.1.0/wiliot_tools-4.1.0-py3-none-any.whl

---
+++ packages/wiliot-tools/wiliot_tools-4.1.0-py3-none-any.whl/wiliot_tools/local_gateway_gui/live_portal/feature_flags.py
@@ -0,0 +1,7 @@
+LIVE_PLOTS_ENABLE = False
+
+LIVE_PLOTS_ATTRIBUTES = ['lo_max_freq', 'charge_time', 'total_dco', 'temperature',
+                         'gpio4_sense']  # 'gpio4_sense' charge_time
+LIVE_PLOTS_ANIMATE = False
+LIVE_PLOTS_DEQUE_LENGTH = 1000
+LIVE_PLOTS_HISTORY = 1800 # history shown in graph [seconds]
