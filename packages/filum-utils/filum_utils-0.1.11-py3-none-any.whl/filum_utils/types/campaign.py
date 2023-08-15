9901d02b4a994b622b0331a6b9db3a81344c4b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:02 +0000

    Add package filum-utils/0.1.11/filum_utils-0.1.11-py3-none-any.whl

---
+++ packages/filum-utils/filum_utils-0.1.11-py3-none-any.whl/filum_utils/types/campaign.py
@@ -0,0 +1,14 @@
+from typing import TypedDict, Optional, Dict, Any
+
+from filum_utils.types.account import Account
+from filum_utils.types.survey import Survey
+
+
+class Campaign(TypedDict, total=False):
+    id: str
+    name: str
+    run_type: str
+    segment_id: Optional[str]
+    account: Account
+    survey: Survey
+    data: Optional[Dict[str, Any]]
