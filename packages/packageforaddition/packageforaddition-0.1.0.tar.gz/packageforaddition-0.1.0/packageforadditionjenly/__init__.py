4e58e396c4d8dd33a8a47bf948c43db613356ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:31 +0000

    Add package packageforaddition/0.1.0/packageforaddition-0.1.0.tar.gz

---
+++ packages/packageforaddition/packageforaddition-0.1.0.tar.gz/packageforaddition-0.1.0/packageforadditionjenly/__init__.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    print("Adding ")
+    return a + b
+
+import pandas as pd 
+
+def getheadBrain():
+    return pd.read_csv("./datas/headbrain.csv")
+
---
+++ packages/packageforaddition/packageforaddition-0.1.0.tar.gz/packageforaddition-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='packageforaddition',
+    version='0.1.0',
+    description='My first Python package',
+    author='Ashik',
+    author_email='jenlyashik@gmail.com',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[],
+)
