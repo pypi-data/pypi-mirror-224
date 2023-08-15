679bc181e6cb06e3cd901cf0ca862075e54bcb99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:20 +0000

    Add package TeaRoute/0.0.7/TeaRoute-0.0.7.tar.gz

---
+++ packages/TeaRoute/TeaRoute-0.0.7.tar.gz/TeaRoute-0.0.7/TeaRoute/router/inner.py
@@ -0,0 +1,54 @@
+import openai, os
+
+from .manual.functions import makeFunctions
+from .methods.chat import chat
+from .methods.embed import embed
+from .methods.training import trainFile
+from .methods.route import route
+
+class Router:
+    def __init__(self):
+        self.buckets = {}
+        pass
+
+    def trainWithFile(self, file, batchSize):
+        instructions = makeFunctions(self.buckets)
+        code = trainFile(file, instructions, chat, embed, batchSize)
+        return code[0], code[1]
+    
+    def addBuckets(self, buckets):
+        self.buckets.update(buckets)
+        return 200
+    
+    def removeBuckets(self, buckets):
+        if buckets in self.buckets:
+            return 404
+        else:
+            self.buckets.remove(buckets)
+            return 200
+    
+    def route(self,   asasdquestion):
+        return route(question, embed)
+
+    def manualAnswer(self, question):
+        return chat(question, makeFunctions(self.buckets), False), 200
+
+    def learnAndRoute(self, question):
+        instructions = makeFunctions(self.buckets)
+        code = chat(question, instructions, True), 200
+        return code
+
+    def wipe(self):
+        file_path = r"router\db\trainingData.json"
+
+        try:
+            # Try to open the file in write mode, creating it if it doesn't exist
+            with open(file_path, 'w') as file:
+                pass
+            return 200
+        except FileNotFoundError:
+            # File doesn't exist, so create it and return 200
+            with open(file_path, 'w') as file:
+                pass
+            return 200
+
