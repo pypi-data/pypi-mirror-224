aab90bab03103a696ee87fac10fad1414a21100e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:43 +0000

    Add package show-gpu-usage/0.1/show_gpu_usage-0.1.tar.gz

---
+++ packages/show-gpu-usage/show_gpu_usage-0.1.tar.gz/show_gpu_usage-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='show_gpu_usage',
+    version='0.1',
+    description='A library to show GPU usage in a terminal',
+    url='https://github.com/your-username/your-library-name',
+    author='StudyingLover',
+    author_email='studyinglover1@gmail.com',
+    license='MIT',
+    packages=['show_gpu_usage'],
+    install_requires=[
+        'asciichartpy==1.5.25'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
