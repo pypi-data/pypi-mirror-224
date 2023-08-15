a37b21e3cf21941f70969d95de036936285551af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:20 +0000

    Add package naruno-gui/0.60.5/naruno_gui-0.60.5.tar.gz

---
+++ packages/naruno-gui/naruno_gui-0.60.5.tar.gz/naruno_gui-0.60.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from setuptools import setup
+
+setup(
+    name="naruno_gui",
+    version="0.60.5",
+    description="""This is GUI mode installer for Naruno""",
+    url="https://docs.naruno.org/",
+    author="Naruno Developers",
+    author_email="onur.atakan.ulusoy@naruno.org",
+    license="MPL-2.0",
+    install_requires="""
+Kivy==2.1.0
+kivymd==0.104.2
+qrcode==7.3.1
+kivymd_extensions.sweetalert==0.1.5
+plyer==2.1.0
+""",
+    python_requires=">=3.8, <=3.10",
+    zip_safe=False,
+)
