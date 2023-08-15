1606522ae78e17dc356e302a29db4bde1c31323a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:10:09 +0000

    Add package mastapy/12.0.4.post2/mastapy-12.0.4.post2-py3-none-any.whl

---
+++ packages/mastapy/mastapy-12.0.4.post2-py3-none-any.whl/mastapy/system_model/analyses_and_results/harmonic_analyses_single_excitation/compound/_6072.py
@@ -0,0 +1,63 @@
+﻿"""_6072.py
+
+AbstractAssemblyCompoundHarmonicAnalysisOfSingleExcitation
+"""
+
+
+from typing import List
+
+from mastapy.system_model.analyses_and_results.harmonic_analyses_single_excitation import _5942
+from mastapy._internal import constructor, conversion
+from mastapy.system_model.analyses_and_results.harmonic_analyses_single_excitation.compound import _6151
+from mastapy._internal.python_net import python_net_import
+
+_ABSTRACT_ASSEMBLY_COMPOUND_HARMONIC_ANALYSIS_OF_SINGLE_EXCITATION = python_net_import('SMT.MastaAPI.SystemModel.AnalysesAndResults.HarmonicAnalysesSingleExcitation.Compound', 'AbstractAssemblyCompoundHarmonicAnalysisOfSingleExcitation')
+
+
+__docformat__ = 'restructuredtext en'
+__all__ = ('AbstractAssemblyCompoundHarmonicAnalysisOfSingleExcitation',)
+
+
+class AbstractAssemblyCompoundHarmonicAnalysisOfSingleExcitation(_6151.PartCompoundHarmonicAnalysisOfSingleExcitation):
+    """AbstractAssemblyCompoundHarmonicAnalysisOfSingleExcitation
+
+    This is a mastapy class.
+    """
+
+    TYPE = _ABSTRACT_ASSEMBLY_COMPOUND_HARMONIC_ANALYSIS_OF_SINGLE_EXCITATION
+
+    def __init__(self, instance_to_wrap: 'AbstractAssemblyCompoundHarmonicAnalysisOfSingleExcitation.TYPE'):
+        super().__init__(instance_to_wrap)
+        self._freeze()
+
+    @property
+    def assembly_analysis_cases(self) -> 'List[_5942.AbstractAssemblyHarmonicAnalysisOfSingleExcitation]':
+        """List[AbstractAssemblyHarmonicAnalysisOfSingleExcitation]: 'AssemblyAnalysisCases' is the original name of this property.
+
+        Note:
+            This property is readonly.
+        """
+
+        temp = self.wrapped.AssemblyAnalysisCases
+
+        if temp is None:
+            return None
+
+        value = conversion.pn_to_mp_objects_in_list(temp)
+        return value
+
+    @property
+    def assembly_analysis_cases_ready(self) -> 'List[_5942.AbstractAssemblyHarmonicAnalysisOfSingleExcitation]':
+        """List[AbstractAssemblyHarmonicAnalysisOfSingleExcitation]: 'AssemblyAnalysisCasesReady' is the original name of this property.
+
+        Note:
+            This property is readonly.
+        """
+
+        temp = self.wrapped.AssemblyAnalysisCasesReady
+
+        if temp is None:
+            return None
+
+        value = conversion.pn_to_mp_objects_in_list(temp)
+        return value
